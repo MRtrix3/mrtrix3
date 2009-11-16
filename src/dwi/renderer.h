811187abbc3241d3e8084ea1d5573b2ef06189c3
J-Donald Tournier <jdtournier@gmail.com>
2009-11-16 00:10:25 +0000

    minor tweak to prevent unnecessary recomputations when using vertex shading

--- src/dwi/renderer.h
+++ src/dwi/renderer.h
@@ -45,7 +45,7 @@ namespace MR {
         bool ready () const { return (shader_program); }
         void init ();
         void set_values (const std::vector<float>& values) { SH = values; recalculate = true; }
-        void set_hide_neg_lobes (bool hide) { if (hide_neg_lobes != hide) recalculate = true; hide_neg_lobes = hide; }
+        void set_hide_neg_lobes (bool hide) { if (hide_neg_lobes != hide && !use_shading()) recalculate = true; hide_neg_lobes = hide; }
         void set_lmax (int lmax) { if (lmax != lmax_computed) recompute = true; lmax_computed = lmax; }
         void set_LOD (int lod) { if (lod != lod_computed) recompute = true; lod_computed = lod; }
         void draw (bool use_normals, const float* colour = NULL);
