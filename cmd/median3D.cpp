36bcaa3db5d7ea72ed631779c41247d23df42dd3
J-Donald Tournier <jdtournier@gmail.com>
2010-12-30 01:41:05 +0000

    new DataSet::Kernel infrastructure for neighbourhood-based multi-threaded image processing. This is now used for median3D, and the new mrsmooth command to apply smoothing to images.

--- cmd/median3D.cpp
+++ cmd/median3D.cpp
@@ -21,315 +21,121 @@
 */
 
 #include "app.h"
-#include "ptr.h"
-#include "progressbar.h"
-#include "thread/exec.h"
-#include "thread/queue.h"
-#include "dataset/misc.h"
-#include "dataset/loop.h"
+#include "dataset/kernel.h"
 #include "image/voxel.h"
 
-using namespace std;
-using namespace MR;
+using namespace std; 
+using namespace MR; 
 
 SET_VERSION_DEFAULT;
 SET_AUTHOR (NULL);
 SET_COPYRIGHT (NULL);
 
 DESCRIPTION = {
-  "smooth images using a 3x3x3 median filter.",
+ "smooth images using median filtering.",
   NULL
 };
 
 ARGUMENTS = {
   Argument ("input", "input image to be median-filtered.").type_image_in (),
   Argument ("output", "the output image.").type_image_out (),
-  Argument()
+  Argument ()
 };
 
-OPTIONS = { Option () };
+OPTIONS = { 
+
+  Option ("extent", "specify extent of median filtering neighbourhood in voxels. "
+      "This can be specified either as a single value to be used for all 3 axes, "
+      "or as a comma-separated list of 3 values, one for each axis (default: 3x3x3).")
+    + Argument ("size").type_sequence_int(),
+
+  Option () 
+};
 
 
-namespace KernelOp
+
+
+
+template <typename T> class MedianFunctor 
 {
-
-  template <typename T>
-  class Item
-  {
-    public:
-      Item (size_t ndim) : pos (ndim) { }
-      RefPtr<T,true> data[3][3];
-      std::vector<ssize_t> pos;
-  };
-
-
-  template <typename T>
-  class Kernel
-  {
-    public:
-      Kernel (const Item<T>& item, size_t x_axis, size_t y_axis, size_t z_axis) : I (item), x (x_axis), y (y_axis), z (z_axis) { }
-      ssize_t from[3], to[3], offset;
-
-      T operator() (ssize_t p[3]) const {
-        return (I.data[p[y]+1][p[z]+1].get() [p[x]+offset]);
+  public:
+    MedianFunctor (const std::vector<int>& extent)
+    {
+      if (extent.size() == 1) 
+        dim[0] = dim[1] = dim[2] = extent[0];
+      else {
+        dim[0] = extent[0];
+        dim[1] = extent[1];
+        dim[2] = extent[2];
       }
-      size_t count () const {
-        return ( (to[0]-from[0]) * (to[1]-from[1]) * (to[2]-from[2]));
-      }
-    private:
-      const Item<T>& I;
-      const size_t x, y, z;
-  };
+      allocate();
+    }
 
+    MedianFunctor (const MedianFunctor& F) 
+    {
+      dim[0] = F.dim[0];
+      dim[1] = F.dim[1];
+      dim[2] = F.dim[2];
+      allocate();
+    }
 
+    ssize_t extent (size_t axis) const { return (dim[axis]); }
 
-  template <typename T>
-  class Allocator
-  {
-    public:
-      Allocator (size_t ndim) : N (ndim) { }
-      Item<T>* alloc () {
-        Item<T>* item = new Item<T> (N);
-        return (item);
-      }
-      void reset (Item<T>* item) { }
-      void dealloc (Item<T>* item) {
-        delete item;
-      }
-    private:
-      size_t N;
-  };
+    template <class Set> 
+      void prepare (Set& set, size_t x_axis, size_t y_axis, size_t z_axis) { }
 
+    T operator() (const DataSet::Kernel::Data<T>& kernel) const
+    {
+      size_t nc = 0;
+      T cm = -INFINITY;
+      const size_t n = kernel.count();
+      const size_t m = n/2 + 1;
 
-  template <class Input>
-  class Loader
-  {
-    public:
-      typedef typename Input::value_type value_type;
-      typedef Thread::Queue<Item<value_type>,Allocator<value_type> > Queue;
-
-      Loader (Queue& queue, Input& input, const std::vector<size_t>& axes) :
-        writer (queue), src (input), x (axes[0]), y (axes[1]), z (axes[2]) { }
-
-      void execute () {
-        typename Queue::Writer::Item item (writer);
-        std::vector<size_t> axes (src.ndim()-1);
-        axes[0] = y;
-        axes[1] = z;
-        for (size_t n = 3; n < src.ndim(); ++n)
-          axes[n-1] = n;
-
-        DataSet::LoopInOrder loop (axes, "median filtering...");
-
-        for (loop.start (src); loop.ok(); loop.next (src)) {
-          // get data:
-          if (src[y] == 0) get_plane (0);
-          if (src[y] < src.dim (y)-1) get_plane (1);
-
-          // set item data:
-          for (size_t j = 0; j < 3; ++j)
-            for (size_t i = 0; i < 3; ++i)
-              set_row (item->data[j][i], data[j][i]);
-
-          // set item position:
-          for (size_t n = 0; n < src.ndim(); ++n)
-            item->pos[n] = src[n];
-
-          // dispatch:
-          if (!item.write()) throw Exception ("error writing to work queue");
-
-          // shuffle along:
-          set_row (data[0][0], data[1][0]);
-          set_row (data[0][1], data[1][1]);
-          set_row (data[0][2], data[1][2]);
-          set_row (data[1][0], data[2][0]);
-          set_row (data[1][1], data[2][1]);
-          set_row (data[1][2], data[2][2]);
-          release_row (data[2][0]);
-          release_row (data[2][1]);
-          release_row (data[2][2]);
-        }
-      }
-
-    private:
-      typename Queue::Writer writer;
-      Input&  src;
-      RefPtr<value_type,true> data[3][3];
-      const size_t x, y, z;
-
-      std::vector<RefPtr<value_type,true> > row_buffer;
-
-      void new_row (RefPtr<value_type,true>& row) {
-        if (row_buffer.size()) {
-          row = row_buffer.back();
-          row_buffer.pop_back();
-        }
-        else row = new value_type [src.dim (x)];
-      }
-
-      void set_row (RefPtr<value_type,true>& row, RefPtr<value_type,true>& original) {
-        if (row && row.unique())
-          row_buffer.push_back (row);
-        row = original;
-      }
-
-      void release_row (RefPtr<value_type,true>& row) {
-        if (row && row.unique())
-          row_buffer.push_back (row);
-        row = NULL;
-      }
-
-      void get_plane (ssize_t plane) {
-        src[y] += plane;
-        assert (src[y] < src.dim (y));
-        value_type* row;
-
-        if (src[z] > 0) {
-          --src[z];
-          new_row (data[plane+1][0]);
-          row = data[plane+1][0].get();
-          for (src[x] = 0; src[x] < src.dim (x); ++src[x])
-            row[src[x]] = src.value();
-          ++src[z];
-        }
-        else release_row (data[plane+1][0]);
-
-        new_row (data[plane+1][1]);
-        row = data[plane+1][1].get();
-        for (src[x] = 0; src[x] < src.dim (x); ++src[x])
-          row[src[x]] = src.value();
-
-        if (src[z] < src.dim (z)-1) {
-          ++src[z];
-          new_row (data[plane+1][2]);
-          row = data[plane+1][2].get();
-          for (src[x] = 0; src[x] < src.dim (x); ++src[x])
-            row[src[x]] = src.value();
-          --src[z];
-        }
-        else release_row (data[plane+1][2]);
-
-        src[y] -= plane;
-      }
-  };
-
-
-
-
-
-  template <class Input, class Output, class Functor>
-  class Processor
-  {
-    public:
-      typedef typename Input::value_type value_type;
-      typedef typename Loader<Input>::Queue Queue;
-
-      Processor (Queue& queue, Output& output, Functor functor, const std::vector<size_t>& axes) :
-        reader (queue), dest (output), x (axes[0]), y (axes[1]), z (axes[2]), func (functor) { }
-
-      void execute () {
-        typename Queue::Reader::Item item (reader);
-
-        while (item.read()) {
-          for (size_t i = 0; i < dest.ndim(); ++i)
-            dest[i] = item->pos[i];
-
-          Kernel<value_type> kernel (*item, x, y, z);
-          kernel.from[y] = dest[y] > 0 ? -1 : 0;
-          kernel.to[y] = dest[y] < dest.dim (y)-1 ? 2 : 1;
-          kernel.from[z] = dest[z] > 0 ? -1 : 0;
-          kernel.to[z] = dest[z] < dest.dim (z)-1 ? 2 : 1;
-
-          for (dest[x] = 0; dest[x] < dest.dim (x); ++dest[x]) {
-            kernel.offset = dest[x];
-            kernel.from[x] = dest[x] > 0 ? -1 : 0;
-            kernel.to[x] = dest[x] < dest.dim (x)-1 ? 2 : 1;
-
-            dest.value() = func (kernel);
+      for (ssize_t k = kernel.from(2); k < kernel.to(2); ++k) {
+        for (ssize_t j = kernel.from(1); j < kernel.to(1); ++j) {
+          for (ssize_t i = kernel.from(0); i < kernel.to(0); ++i) {
+            const T val = kernel (i,j,k);
+            if (nc < m) {
+              v[nc] = val;
+              if (v[nc] > cm) cm = val;
+              ++nc;
+            }
+            else if (val < cm) {
+              size_t x;
+              for (x = 0; v[x] != cm; ++x);
+              v[x] = val;
+              cm = -INFINITY;
+              for (x = 0; x < m; x++)
+                if (v[x] > cm) cm = v[x];
+            }
           }
         }
       }
 
-    private:
-      typename Queue::Reader reader;
-      Output dest;
-      const size_t x, y, z;
-      Functor func;
-  };
-
-
-
-
-
-
-  template <class Input, class Output, class Functor>
-  inline void run (Output& output, Input& input, Functor functor)
-  {
-    typename Loader<Input>::Queue queue ("work queue", 100, Allocator<typename Input::value_type> (input.ndim()));
-    std::vector<size_t> axes = DataSet::Stride::order (input, 0, 3);
-    Loader<Input> loader (queue, input, axes);
-    Processor<Input,Output,Functor> processor (queue, output, functor, axes);
-
-    Thread::Exec loader_thread (loader, "loader");
-    Thread::Array<Processor<Input,Output,Functor> > processor_list (processor);
-    Thread::Exec processor_threads (processor_list, "processor");
-  }
-
-}
-
-
-
-
-
-
-
-
-
-
-template <typename T> inline T MedianFunc (const KernelOp::Kernel<T>& kernel)
-{
-  T v[14];
-  size_t nc = 0;
-  T cm = -INFINITY;
-  const size_t n = kernel.count();
-  const size_t m = n/2 + 1;
-
-  ssize_t x[3];
-  for (x[2] = kernel.from[2]; x[2] < kernel.to[2]; ++x[2]) {
-    for (x[1] = kernel.from[1]; x[1] < kernel.to[1]; ++x[1]) {
-      for (x[0] = kernel.from[0]; x[0] < kernel.to[0]; ++x[0]) {
-        const T val = kernel (x);
-        if (nc < m) {
-          v[nc] = val;
-          if (v[nc] > cm) cm = val;
-          ++nc;
+      if ((n+1) & 1) {
+        T t = cm = -INFINITY;
+        for (size_t i = 0; i < m; ++i) {
+          if (v[i] > cm) {
+            t = cm;
+            cm = v[i];
+          }
+          else if (v[i] > t) t = v[i];
         }
-        else if (val < cm) {
-          size_t i;
-          for (i = 0; v[i] != cm; ++i);
-          v[i] = val;
-          cm = -INFINITY;
-          for (i = 0; i < m; i++)
-            if (v[i] > cm) cm = v[i];
-        }
+        cm = (cm+t)/2.0;
       }
-    }
-  }
 
-  if ( (n+1) & 1) {
-    T t = cm = -INFINITY;
-    for (size_t i = 0; i < m; ++i) {
-      if (v[i] > cm) {
-        t = cm;
-        cm = v[i];
-      }
-      else if (v[i] > t) t = v[i];
+      return (cm);
     }
-    cm = (cm+t) /2.0;
-  }
 
-  return (cm);
-}
+  private:
+    ssize_t dim[3];
+    Ptr<T,true> v;
+
+    void allocate () 
+    {
+      v = new T [(dim[0]*dim[1]*dim[2]+1)/2];
+    }
+};
 
 
 
@@ -337,6 +143,20 @@ template <typename T> inline T MedianFunc (const KernelOp::Kernel<T>& kernel)
 
 
 EXECUTE {
+  std::vector<int> extent (1);
+  extent[0] = 3;
+  
+  Options opt = get_options ("extent");
+
+  if (opt.size()) {
+    extent = parse_ints (opt[0][0]);
+    for (size_t i = 0; i < extent.size(); ++i)
+      if (! (extent[i] & int(1)))
+        throw Exception ("expected odd number for extent");
+    if (extent.size() != 1 && extent.size() != 3)
+      throw Exception ("unexpected number of elements specified in extent");
+  }
+
   Image::Header source (argument[0]);
   Image::Header destination (source);
   destination.create (argument[1]);
@@ -344,6 +164,6 @@ EXECUTE {
   Image::Voxel<float> src (source);
   Image::Voxel<float> dest (destination);
 
-  KernelOp::run (dest, src, MedianFunc<float>);
+  DataSet::Kernel::run (dest, src, MedianFunctor<float>(extent), "median filtering...");
 }
 
---
+++ lib/dataset/kernel.h
@@ -0,0 +1,283 @@
+/*
+    Copyright 2010 Brain Research Institute, Melbourne, Australia
+
+    Written by J-Donald Tournier, 26/09/10.
+
+    This file is part of MRtrix.
+
+    MRtrix is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MRtrix is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MRtrix.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef __dataset_kernel_h__
+#define __dataset_kernel_h__
+
+#include "ptr.h"
+#include "progressbar.h"
+#include "thread/exec.h"
+#include "thread/queue.h"
+#include "dataset/misc.h"
+#include "dataset/loop.h"
+
+
+namespace MR {
+  namespace DataSet {
+    namespace Kernel {
+
+      template <typename T> 
+        class Item 
+        {
+          public:
+            Item (size_t ndim, size_t nslices) : slice (nslices), pos (ndim) { }
+            std::vector<RefPtr<T,true> > slice;
+            std::vector<ssize_t> pos;
+        };
+
+
+      template <typename T>
+        class Allocator 
+        {
+          public: 
+            Allocator (size_t ndim, size_t nslices) : N (ndim), ns (nslices) { } 
+            Item<T>* alloc () { Item<T>* item = new Item<T> (N, ns); return (item); }
+            void reset (Item<T>* item) { }
+            void dealloc (Item<T>* item) { delete item; }
+          private:
+            size_t N, ns;
+        };
+
+
+      //! \cond skip
+      namespace {
+        template <typename T> 
+          class DataPrivate
+          {
+            public:
+              DataPrivate (const size_t axes_ordering[3], ssize_t j_skip) :
+                axes (axes_ordering), jskip (j_skip) { }
+
+              const size_t* axes;
+              const ssize_t jskip;
+              ssize_t from[3], to[3], offset[3];
+              const Item<T>* item;
+
+              T operator() (ssize_t i, ssize_t j, ssize_t k) const 
+              { 
+                const T* slice = item->slice[k+offset[2]].get();
+                ssize_t index = jskip * (j+offset[1]) + i+offset[0];
+                return (slice[index]);
+              }
+          };
+      }
+      //! \endcond
+
+
+      template <typename T> 
+        class Data
+        {
+          public:
+            explicit Data (const DataPrivate<T>& roi) : R (roi) { }
+
+            size_t count () const { return ((to(0)-from(0)) * (to(1)-from(1)) * (to(2)-from(2))); }
+            ssize_t from (size_t i) const { return (R.from[i]); }
+            ssize_t to   (size_t i) const { return (R.to  [i]); }
+            T operator() (ssize_t i, ssize_t j, ssize_t k) const { return (R (i,j,k)); }
+
+          private:
+            const DataPrivate<T>& R;
+        };
+
+
+
+      template <class Input,class Functor> 
+        class Loader 
+        {
+          public:
+            typedef typename Input::value_type value_type;
+            typedef Thread::Queue<Item<value_type>,Allocator<value_type> > Queue;
+
+            Loader (
+                Queue& queue, 
+                Input& input, 
+                const Functor& func, 
+                const std::vector<size_t>& axes, 
+                const std::string& progress_message) : 
+              writer (queue), 
+              src (input),
+              x (axes[0]), y (axes[1]), z(axes[2]),
+              nslices (func.extent(z)),
+              data (nslices),
+              prog_message (progress_message) { } 
+
+            void execute () 
+            {
+              std::vector<size_t> axes (src.ndim()-1);
+              axes[0] = y;
+              axes[1] = z;
+              for (size_t n = 3; n < src.ndim(); ++n)
+                axes[n-1] = n;
+
+              std::vector<size_t> slice_axes (2);
+              slice_axes[0] = x;
+              slice_axes[1] = y;
+
+              ssize_t slice = 0;
+              typename Queue::Writer::Item item (writer);
+              DataSet::LoopInOrder loop (axes, prog_message);
+
+              const ssize_t slice_offset = (nslices+1)/2;
+
+              for (loop.start (src); loop.ok(); loop.next (src)) {
+                // get data:
+                while (slice < src[z] + slice_offset)
+                  get_slice (slice, slice_axes);
+
+                // set item:
+                for (ssize_t i = 0; i < nslices; ++i) 
+                  item->slice[i] = data[i];
+
+                // set item position:
+                for (size_t n = 0; n < src.ndim(); ++n) 
+                  item->pos[n] = src[n];
+
+                // dispatch:
+                if (!item.write()) throw Exception ("error writing to work queue");
+              }
+            }
+
+          private:
+            typename Queue::Writer writer;
+            Input&  src;
+            const size_t x, y, z;
+            const ssize_t nslices;
+            std::vector<RefPtr<value_type,true> > data;
+
+            const std::string prog_message;
+
+            void get_slice (ssize_t& slice, const std::vector<size_t>& axes)
+            {
+              for (ssize_t i = 0; i < nslices-1; ++i) 
+                data[i] = data[i+1];
+              value_type* p = slice < src.dim(z) ? new value_type [src.dim(x)*src.dim(y)] : NULL;
+              data[nslices-1] = p;
+              if (p) {
+                const ssize_t pos[] = { src[0], src[1], src[2] };
+                DataSet::LoopInOrder loop (axes);
+                src[z] = slice;
+                for (loop.start (src); loop.ok(); loop.next (src)) {
+                  *p = src.value();
+                  ++p;
+                }
+                src[0] = pos[0];
+                src[1] = pos[1];
+                src[2] = pos[2];
+              }
+              ++slice;
+            }
+        };
+
+
+
+
+
+      template <class Input, class Output, class Functor> 
+        class Processor 
+        {
+          public:
+            typedef typename Input::value_type value_type;
+            typedef typename Loader<Input,Functor>::Queue Queue;
+
+            Processor (Queue& queue, Output& output, Functor functor, const size_t axes_ordering[3]) : 
+              reader (queue), dest (output), axes (axes_ordering), func (functor) { }
+
+            void execute () 
+            {
+              typename Queue::Reader::Item item (reader);
+              DataPrivate<value_type> kernel (axes, dest.dim(axes[0]));
+              const ssize_t extent[] = { 
+                (func.extent(axes[0])-1)/2, 
+                (func.extent(axes[1])-1)/2, 
+                (func.extent(axes[2])-1)/2 
+              };
+              kernel.offset[2] = extent[2];
+
+              while (item.read()) {
+
+                for (size_t i = 0; i < dest.ndim(); ++i)
+                  dest[i] = item->pos[i];
+
+                kernel.offset[1] = item->pos[axes[1]];
+                kernel.item = &(*item);
+
+                kernel.from[1] = get_from (dest[axes[1]], extent[1]); 
+                kernel.to[1] = get_to (dest[axes[1]], extent[1], dest.dim(axes[1]));
+                kernel.from[2] = get_from (dest[axes[2]], extent[2]); 
+                kernel.to[2] = get_to (dest[axes[2]], extent[2], dest.dim(axes[2]));
+
+                for (dest[axes[0]] = 0; dest[axes[0]] < dest.dim(axes[0]); ++dest[axes[0]]) {
+                  kernel.offset[0] = dest[axes[0]];
+                  kernel.from[0] = get_from (dest[axes[0]], extent[0]); 
+                  kernel.to[0] = get_to (dest[axes[0]], extent[0], dest.dim(axes[0]));
+
+                  dest.value() = func (Data<value_type> (kernel));
+                }
+              }
+            }
+
+          private:
+            typename Queue::Reader reader;
+            Output dest;
+            const size_t* axes;
+            Functor func;
+
+            ssize_t get_from (ssize_t pos, ssize_t offset) const 
+            { 
+              return (std::max (offset-pos, ssize_t(0))-offset);
+            }
+
+            ssize_t get_to (ssize_t pos, ssize_t offset, ssize_t max) const 
+            { 
+              return (offset+1 - std::max (offset - max+1 + pos, ssize_t(0)));
+            }
+        };
+
+
+
+
+
+
+      template <class Input, class Output, class Functor> 
+        inline void run (Output& output, Input& input, Functor functor, const std::string& progress_message)
+        {
+          std::vector<size_t> ax = DataSet::Stride::order (input, 0, 3);
+          const size_t axes[] = { ax[0], ax[1], ax[2] };
+          functor.prepare (input, axes[0], axes[1], axes[2]);
+
+          typename Loader<Input,Functor>::Queue queue 
+            ("work queue", 100, Allocator<typename Input::value_type> (input.ndim(), functor.extent (axes[2])));
+
+          Loader<Input,Functor> loader (queue, input, functor, ax, progress_message);
+          Processor<Input,Output,Functor> processor (queue, output, functor, axes);
+
+          Thread::Array<Processor<Input,Output,Functor> > processor_list (processor);
+          Thread::Exec processor_threads (processor_list, "processor");
+          loader.execute();
+        }
+
+    }
+  }
+}
+
+
+#endif
