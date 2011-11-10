88338d8cbc5cc73a32da1e1ef198661dc0af3623
J-Donald Tournier <jdtournier@gmail.com>
2011-11-10 06:22:47 +0000

    simplified read_dicom and added -tag option to read specific DICOM tags

--- cmd/read_dicom.cpp
+++ cmd/read_dicom.cpp
@@ -22,7 +22,9 @@
 
 
 #include "app.h"
+#include "debug.h"
 #include "file/path.h"
+#include "file/dicom/element.h"
 #include "file/dicom/quick_scan.h"
 
 MRTRIX_APPLICATION
@@ -36,52 +38,68 @@ void usage ()
   + "output DICOM fields in human-readable format.";
 
   ARGUMENTS
-  + Argument ("file", "the DICOM file to be scanned.")
-  .allow_multiple().type_file ();
+  + Argument ("file", "the DICOM file to be scanned.").type_file ();
 
   OPTIONS
   + Option ("all", "print all DICOM fields.")
-  + Option ("csa", "print all Siemens CSA fields");
+
+  + Option ("csa", "print all Siemens CSA fields")
+
+  + Option ("tag", "print field specified by the group & element tags supplied. "
+      "Tags should be supplied as Hexadecimal (i.e. as they appear in the -all listing).")
+  .allow_multiple()
+  + Argument ("group")
+  + Argument ("element");
 }
 
 
+class Tag {
+  public:
+    uint16_t group, element;
+    std::string value;
+};
+
+inline uint16_t read_hex (const std::string& m)
+{
+  uint16_t value;
+  std::istringstream hex (m);
+  hex >> std::hex >> value;
+  return value;
+}
+
 void run ()
 {
-  File::Dicom::QuickScan reader;
+  Options opt = get_options("tag");
+  if (opt.size()) {
+    std::istringstream hex; 
 
-  bool print_DICOM_fields = false;
-  bool print_CSA_fields = false;
-
-  if (get_options ("all").size())
-    print_DICOM_fields = true;
-
-  if (get_options ("csa").size())
-    print_CSA_fields = true;
-
-  for (size_t n = 0; n < argument.size();  n++) {
-
-    if (Path::is_dir (argument[n])) {
-      Path::Dir* dir;
-      try {
-        dir = new Path::Dir (argument[n]);
-      }
-      catch (...) {
-        throw Exception ("error opening folder \"" + argument[n]
-                         + "\": " + strerror (errno));
-      }
-
-      std::string entry;
-      while ( (entry = dir->read_name()).size()) {
-        if (reader.read (Path::join (argument[n], entry), print_DICOM_fields, print_CSA_fields))
-          error ("error reading file \"" + reader.filename + "\"");
-        else std::cout << reader << "\n";
-      }
+    Tag tags[opt.size()];
+    for (size_t n = 0; n < opt.size(); ++n) {
+      tags[n].group = read_hex (opt[n][0]);
+      tags[n].element = read_hex (opt[n][1]);
     }
-    else if (reader.read (argument[n], print_DICOM_fields, print_CSA_fields))
-      error ("error reading file \"" + reader.filename + "\"");
 
-    else if (!print_DICOM_fields) std::cout << reader << "\n";
+    File::Dicom::Element item;
+    item.set (argument[0]);
+    while (item.read()) {
+      for (size_t n = 0; n < opt.size(); ++n) 
+        if (item.is (tags[n].group, tags[n].element)) 
+          tags[n].value = item.get_string()[0];
+    }
+
+    for (size_t n = 0; n < opt.size(); ++n) 
+      std::cout << tags[n].value << "\n";
+
+    return;
   }
 
+
+  File::Dicom::QuickScan reader;
+
+  if (reader.read (argument[0], get_options ("all").size(), get_options ("csa").size()))
+    throw Exception ("error reading file \"" + reader.filename + "\"");
+
+  if (!get_options ("all").size() && !get_options ("csa").size()) 
+    std::cout << reader;
 }
 
