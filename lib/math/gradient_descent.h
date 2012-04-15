d0d9f0a4e19569fd3347b37925e930abd9fd9bd3
J-Donald Tournier <jdtournier@gmail.com>
2012-04-15 15:37:36 +0000

    removed value_type from template argument list for gradient descent class - cost functions are now expected to provide this information as a typedef <type> value_type declaration.

--- lib/math/gradient_descent.h
+++ lib/math/gradient_descent.h
@@ -32,128 +32,120 @@ namespace MR
   namespace Math
   {
 
-		//! \addtogroup Optimisation
-		// @{
+    //! \addtogroup Optimisation
+    // @{
 
-		//! Computes the minimum of a function using a gradient decent approach.
-    template <class F, typename T = float, bool verbose = false> class GradientDescent
-    {
-      public:
-        GradientDescent (F& function, T step_size_upfactor = 1.5, T step_size_downfactor = 0.1) :
-          func (function),
-          step_up (step_size_upfactor),
-          step_down (step_size_downfactor),
-          x (func.size()),
-          x2 (func.size()),
-          g (func.size()),
-          g2 (func.size()) { }
+    //! Computes the minimum of a function using a gradient descent approach.
+    template <class Function, bool verbose = false> 
+      class GradientDescent
+      {
+        public:
+          typedef typename Function::value_type value_type;
 
-
-        T                value () const throw ()     {
-          return (f);
-        }
-        const Vector<T>& state () const throw ()     {
-          return (x);
-        }
-        const Vector<T>& gradient () const throw ()  {
-          return (g);
-        }
-
-        T   gradient_norm () const throw ()        {
-          return (normg);
-        }
-        int function_evaluations () const throw () {
-          return (nfeval);
-        }
+          GradientDescent (Function& function, value_type step_size_upfactor = 1.5, value_type step_size_downfactor = 0.1) :
+            func (function),
+            step_up (step_size_upfactor),
+            step_down (step_size_downfactor),
+            x (func.size()),
+            x2 (func.size()),
+            g (func.size()),
+            g2 (func.size()) { }
 
 
-        void run (const int max_iterations = 1000, const T grad_tolerance = 1e-4) {
-          init();
 
-          T gradient_tolerance = grad_tolerance * gradient_norm();
+          value_type value () const throw () { return f; }
+          const Vector<value_type>& state () const throw () { return x; }
+          const Vector<value_type>& gradient () const throw ()  { return g; }
+          value_type gradient_norm () const throw () { return normg; }
+          int function_evaluations () const throw () { return nfeval; }
 
-          for (int niter = 0; niter < max_iterations; niter++) {
-            bool retval = iterate();
-            T grad_norm = gradient_norm();
-            error ("iteration " + str (niter) + ": f = " + str (f) + ", |g| = " + str (grad_norm));
-            for (size_t n = 0; n < x.size(); ++n)
-              std::cout << x[n] << " ";
-            std::cout << "\n";
+          void run (const int max_iterations = 1000, const value_type grad_tolerance = 1e-4) {
+            init();
 
-            if (!retval)
-              return;
+            value_type gradient_tolerance = grad_tolerance * gradient_norm();
 
-            if (grad_norm < gradient_tolerance)
-              return;
+            for (int niter = 0; niter < max_iterations; niter++) {
+              bool retval = iterate();
+              value_type grad_norm = gradient_norm();
+              error ("iteration " + str (niter) + ": f = " + str (f) + ", |g| = " + str (grad_norm));
+              for (size_t n = 0; n < x.size(); ++n)
+                std::cout << x[n] << " ";
+              std::cout << "\n";
+
+              if (!retval)
+                return;
+
+              if (grad_norm < gradient_tolerance)
+                return;
+            }
+            // throw Exception ("failed to converge");
           }
-//          throw Exception ("failed to converge");
-        }
 
 
-        void init () {
-          dt = func.init (x);
-          nfeval = 0;
-          f = evaluate_func (x, g);
-          normg = norm (g);
-          assert (finite (f));
-          assert (finite (normg));
-        }
-
-
-        bool iterate () {
-          assert (normg != 0.0);
-          T step = dt / normg;
-          T f2;
-
-          while (true) {
-            bool no_change = true;
-            for (size_t n = 0; n < func.size(); n++) {
-              x2[n] = x[n] - step * g[n];
-              if (x2[n] != x[n])
-                no_change = false;
-            }
-            if (no_change)
-              return false;
-
-            f2 = evaluate_func (x2, g2);
-
-            if (f2 < f) {
-              dt *= step_up;
-              f = f2;
-              x.swap (x2);
-              g.swap (g2);
-              normg = norm (g);
-              return true;
-            }
-            // quadratic minimum
-
-            T denom = 2.0 * (f2 - f + normg*normg*step);
-            if (denom) {
-              T step_mult = step * normg * normg / denom;
-              assert (step_mult > 0.0 && step_mult < 1.0);
-              step *= step_mult;
-            }
-            else step *= 2.0;
-
-            dt *= step_down;
+          void init () {
+            dt = func.init (x);
+            nfeval = 0;
+            f = evaluate_func (x, g);
+            normg = norm (g);
+            assert (finite (f));
+            assert (finite (normg));
           }
-        }
 
-      protected:
-        F& func;
-        const T step_up, step_down;
-        Vector<T> x, x2, g, g2;
-        T f, dt, normg;
-        int nfeval;
 
-        T evaluate_func (const Vector<T>& newx, Vector<T>& newg) {
-          nfeval++;
-          T cost = func (newx, newg);
-          if (verbose)
-            error ("gradient descent evaluation " + str(nfeval) + ", cost function " +str (cost));
-          return cost;
-        }
-    };
+          bool iterate () {
+            assert (normg != 0.0);
+            value_type step = dt / normg;
+            value_type f2;
+
+            while (true) {
+              bool no_change = true;
+              for (size_t n = 0; n < func.size(); n++) {
+                x2[n] = x[n] - step * g[n];
+                if (x2[n] != x[n])
+                  no_change = false;
+              }
+              if (no_change)
+                return false;
+
+              f2 = evaluate_func (x2, g2);
+
+              if (f2 < f) {
+                dt *= step_up;
+                f = f2;
+                x.swap (x2);
+                g.swap (g2);
+                normg = norm (g);
+                return true;
+              }
+              // quadratic minimum
+
+              value_type denom = 2.0 * (f2 - f + normg*normg*step);
+              if (denom) {
+                value_type step_mult = step * normg * normg / denom;
+                assert (step_mult > 0.0 && step_mult < 1.0);
+                step *= step_mult;
+              }
+              else step *= 2.0;
+
+              dt *= step_down;
+            }
+          }
+
+        protected:
+          Function& func;
+          const value_type step_up, step_down;
+          Vector<value_type> x, x2, g, g2;
+          value_type f, dt, normg;
+          int nfeval;
+
+          value_type evaluate_func (const Vector<value_type>& newx, Vector<value_type>& newg) {
+            nfeval++;
+            value_type cost = func (newx, newg);
+            if (verbose)
+              error ("gradient descent evaluation " + str(nfeval) + ", cost function " +str (cost));
+            return cost;
+          }
+      };
     //! @}
   }
 }
