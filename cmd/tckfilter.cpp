dbedcfe6e29670cf93016cc541788956f3076dea
Robert Smith <r.smith@brain.org.au>
2013-01-25 06:52:49 +0000

    Added command tckfilter; filter streamlines based on inclusion / exclusion ROIs, and minimum / maximum length criteria.

---
+++ cmd/tckfilter.cpp
@@ -0,0 +1,236 @@
+/*
+    Copyright 2008 Brain Research Institute, Melbourne, Australia
+
+    Written by Robert E. Smith, 25/01/13.
+
+
+    This file is part of MRtrix.
+
+    MRtrix is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MRtrix is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MRtrix.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <fstream>
+#include <cstdio>
+
+#include "app.h"
+#include "timer.h"
+#include "dwi/tractography/file.h"
+#include "dwi/tractography/properties.h"
+#include "dwi/tractography/roi.h"
+#include "dwi/tractography/mapping/common.h"
+#include "dwi/tractography/mapping/loader.h"
+#include "thread/queue.h"
+
+
+MRTRIX_APPLICATION
+
+using namespace MR;
+using namespace MR::DWI;
+using namespace App;
+
+void usage ()
+{
+  DESCRIPTION
+  + "filter streamlines according to criteria such as inclusion / exclusion ROIs and length";
+
+  ARGUMENTS
+  + Argument ("in_tracks",  "the input track files").type_file().allow_multiple()
+  + Argument ("out_tracks", "the output track file").type_file();
+
+  OPTIONS
+  + Option ("include",
+            "specify an inclusion region of interest, as either a binary mask image, "
+            "or as a sphere using 4 comma-separared values (x,y,z,radius). Streamlines "
+            "must traverse ALL inclusion regions to be accepted.")
+          .allow_multiple()
+          + Argument ("spec")
+
+  + Option ("exclude",
+            "specify an exclusion region of interest, as either a binary mask image, "
+            "or as a sphere using 4 comma-separared values (x,y,z,radius). Streamlines "
+            "that enter ANY exclude region will be discarded.")
+          .allow_multiple()
+          + Argument ("spec")
+
+  + Option ("maxlength", "set the maximum length of any track in mm.")
+          + Argument ("value").type_float (0.0, 0.0, INFINITY)
+
+  + Option ("minlength", "set the minimum length of any track in mm.")
+          + Argument ("value").type_float (0.0, 0.0, INFINITY);
+
+}
+
+
+
+class Filter
+{
+
+    typedef std::vector< Point<float> > Track;
+
+  public:
+    Filter (Tractography::Properties& p) :
+      properties (p)
+    {
+      const float step_size = to<float>(properties["step_size"]);
+      const float step_scaling = properties.find ("samples_per_step") == properties.end() ? 1.0 : (1.0 / (to<float>(properties["samples_per_step"]) - 1));
+      if (properties.find ("min_dist") != properties.end())
+        min_num_points = Math::round (to<float>(properties["min_dist"]) / (step_size * step_scaling)) + 1;
+      else
+        min_num_points = 0;
+      if (properties.find ("max_dist") != properties.end())
+        max_num_points = Math::round (to<float>(properties["max_dist"]) / (step_size * step_scaling));
+      else
+        max_num_points = INT_MAX;
+    }
+
+
+    bool operator() (const Track& in, Track& out)
+    {
+      out.clear();
+      if ((in.size() < min_num_points) || (in.size() > max_num_points))
+        return true;
+      track_included.assign (properties.include.size(), false);
+      for (Track::const_iterator i = in.begin(); i != in.end(); ++i) {
+        if (!test_point (*i))
+          return true;
+      }
+      if (traversed_all_include_regions())
+        out = in;
+      return true;
+    }
+
+
+  private:
+
+    // Settings
+    Tractography::Properties& properties;
+    size_t min_num_points, max_num_points;
+
+
+    // Members to be re-used
+    std::vector<bool> track_included;
+
+
+    // Helper functions
+    bool test_point (const Point<float>& p)
+    {
+      if (properties.exclude.contains (p))
+        return false;
+      properties.include.contains (p, track_included);
+      return true;
+    }
+
+    bool traversed_all_include_regions () const
+    {
+      for (size_t n = 0; n < track_included.size(); ++n)
+        if (!track_included[n])
+          return false;
+      return true;
+    }
+
+};
+
+
+
+
+#define UPDATE_INTERVAL 0.0333333 // 30 Hz - most monitors are 60Hz
+
+class Writer : public Tractography::Writer<float>
+{
+
+  typedef Tractography::Writer<float> WriterBase;
+  typedef std::vector< Point<float> > Track;
+
+  public:
+    Writer (const std::string& path, Tractography::Properties& properties) :
+      WriterBase (path, properties),
+      in_count (properties.find ("count") == properties.end() ? 0 : to<size_t>(properties["count"])),
+      timer (),
+      next_time (timer.elapsed()) { }
+
+    ~Writer ()
+    {
+      if (App::log_level > 0)
+        fprintf (stderr, "\r%8zu read, %8zu filtered    [100%%]\n", WriterBase::total_count, WriterBase::count);
+    }
+
+    bool operator() (const Track& in)
+    {
+      if (App::log_level > 0 && timer.elapsed() >= next_time) {
+        next_time += UPDATE_INTERVAL;
+        fprintf (stderr, "\r%8zu read, %8zu filtered    [%3d%%]",
+            WriterBase::total_count, WriterBase::count,
+            in_count ? (int(100.0 * WriterBase::total_count / float(in_count))) : 0);
+      }
+      WriterBase::append (in);
+      return true;
+    }
+
+
+  private:
+    const size_t in_count;
+    Timer timer;
+    double next_time;
+
+};
+
+
+
+
+void run ()
+{
+
+  Tractography::Reader<float> reader;
+  Tractography::Properties properties;
+  reader.open (argument[0], properties);
+
+  Options opt = get_options ("include");
+  for (size_t i = 0; i < opt.size(); ++i)
+    properties.include.add (Tractography::ROI (opt[i][0]));
+
+  opt = get_options ("exclude");
+  for (size_t i = 0; i < opt.size(); ++i)
+    properties.exclude.add (Tractography::ROI (opt[i][0]));
+
+  bool using_length_filtering = false;
+
+  opt = get_options ("maxlength");
+  if (opt.size()) {
+    properties["max_dist"] = std::string (opt[0][0]);
+    using_length_filtering = true;
+  }
+
+  opt = get_options ("minlength");
+  if (opt.size()) {
+    properties["min_dist"] = std::string (opt[0][0]);
+    using_length_filtering = true;
+  }
+
+  if (using_length_filtering) {
+    if (properties.find ("step_size") == properties.end())
+      throw Exception ("Cannot filter streamlines by length as tractography step size is not specified in file");
+    const float step_size = to<float> (properties["step_size"]);
+    if (!step_size || !finite (step_size))
+      throw Exception ("Cannot filter streamlines by length as tractography step size is malformed");
+  }
+
+  Filter filter (properties);
+  Writer writer (argument[1], properties);
+
+  typedef std::vector< Point<float> > Track;
+  Thread::run_batched_queue_threaded_pipe (reader, Track(), 100, filter, Track(), 100, writer);
+
+}
+
