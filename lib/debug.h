3b7122a3e5943df880dd703a37ecec37c7dcda05
J-Donald Tournier <jdtournier@gmail.com>
2013-04-12 07:42:37 +0000

    cleanup of deguf macros (TEST is now replaced by TRACE), and fix bug in Interp::Linear when handling complex numbers

--- lib/debug.h
+++ lib/debug.h
@@ -27,16 +27,30 @@
 #include "mrtrix.h"
 #include "app.h"
 
-/** Prints the file and line number. Useful for debugging purposes. */
-#define TEST std::cerr << MR::App::NAME << ": line " << __LINE__ \
-                       << " in " << __func__ << "() from file " << __FILE__ << "\n"
+/** \defgroup debug Debugging 
+ * \brief functions and macros provided to ease debugging. */
+
+/** \addtogroup debug
+ * @{ */
+
+/** \brief Prints the current function, file and line number. */
+#define TRACE \
+  std::cerr << MR::App::NAME << ": " << __PRETTY_FUNCTION__ << " (" << __FILE__ << ": " << __LINE__ << ")\n"
 
 
-/** Prints a variable name and its value, followed by the file and line number. Useful for debugging purposes. */
-#define VAR(variable) std::cerr << MR::App::NAME << ": " << #variable << " = " << (variable) \
-                                << " (in " << __func__ << "() from " << __FILE__  << ": " << __LINE__ << ")\n"
+/** \brief Prints a variable name and its value, followed by the function, file and line number. */
+#define VAR(variable) \
+  std::cerr << MR::App::NAME << ": " << #variable << " = " << (variable) \
+  << " (" << __PRETTY_FUNCTION__ << "; " << __FILE__  << ": " << __LINE__ << ")\n"
 
-#define PAUSE { std::cerr << "paused at line " << __LINE__ << " in " << __func__ << "() from "__FILE__" ..."; std::string __n__; std::getline (std::cin, __n__); }
+/** \brief Stops execution and prints current function, file and line number. 
+  Remuses on user input (i.e. Return key). */
+#define PAUSE { \
+  std::cerr << MR::App::NAME << ": paused, " << __PRETTY_FUNCTION__ << " (" << __FILE__ << ": " << __LINE__ << ")...\n"; \
+  std::string __n__; std::getline (std::cin, __n__); \
+}
+
+/** @} */
 
 #endif
 
