6704b4276f3b9e2dfdd7b13c801e1574cc5a634f
Dave Raffelt <draffelt@gmail.com>
2013-05-08 07:31:04 +0000

    Added smoothing kernel option for tsfsmooth

--- cmd/tsfsmooth.cpp
+++ cmd/tsfsmooth.cpp
@@ -40,6 +40,11 @@ void usage ()
   ARGUMENTS
   + Argument ("input",  "the input track scalar file.").type_file ()
   + Argument ("output", "the output track scalar file");
+
+  OPTIONS
+  + Option ("stdev", "apply Gaussian smoothing with the specified standard deviation. "
+            "The standard deviation is defined in units of track points (Default: 4)")
+  + Argument ("sigma").type_float();
 }
 
 typedef float value_type;
@@ -51,33 +56,37 @@ void run ()
   DWI::Tractography::ScalarReader<value_type> reader (argument[0], properties);
   DWI::Tractography::ScalarWriter<value_type> writer (argument[1], properties);
 
+  Options opt = get_options("stdev");
+  float stdev = 4.0;
+  if (opt.size())
+    stdev = opt[0][0];
+
+  std::vector<float> kernel (2 * ceil(2.5 * stdev) + 1, 0);
+  float norm_factor = 0.0;
+  float radius = (kernel.size() - 1.0) / 2.0;
+  for (size_t c = 0; c < kernel.size(); ++c) {
+    kernel[c] = exp(-(c - radius) * (c - radius)  / (2 * stdev * stdev));
+    norm_factor += kernel[c];
+  }
+  for (size_t c = 0; c < kernel.size(); c++)
+    kernel[c] /= norm_factor;
+
   std::vector<value_type> tck_scalar;
-  std::vector<value_type> values;
-  while (reader.next(tck_scalar)) {
-    std::vector<value_type> tck_scalar_mean (tck_scalar.size());
+  while (reader.next (tck_scalar)) {
+    std::vector<value_type> tck_scalars_smoothed (tck_scalar.size());
 
     for (size_t i = 0; i < tck_scalar.size(); ++i) {
-      values.clear();
-      if (i > 2)
-        values.push_back(tck_scalar[i-3]);
-      if (i > 1)
-        values.push_back(tck_scalar[i-2]);
-      if (i > 0)
-        values.push_back(tck_scalar[i-1]);
-      if (i < tck_scalar.size() - 1 )
-        values.push_back(tck_scalar[i+1]);
-      if (i < tck_scalar.size() - 2 )
-        values.push_back(tck_scalar[i+2]);
-      if (i < tck_scalar.size() - 3 )
-        values.push_back(tck_scalar[i+3]);
-      values.push_back(tck_scalar[i]);
-      float mean_val = 0.0;
-      for (int j = 0; j < 5; j++)
-        mean_val += values[j];
-      mean_val /= 5.0;
-      tck_scalar_mean[i] = mean_val;
+      float norm_factor = 0.0;
+      float value = 0.0;
+      for (int k = -(int)radius; k <= (int)radius; ++k) {
+        if (i + k >= 0 && i + k < tck_scalar.size()) {
+          value += kernel[k + radius] * tck_scalar[i + k];
+          norm_factor += kernel[k + radius];
+        }
+      }
+      tck_scalars_smoothed[i] = value / norm_factor;
     }
-    writer.append (tck_scalar_mean);
+    writer.append (tck_scalars_smoothed);
   }
 }
 
