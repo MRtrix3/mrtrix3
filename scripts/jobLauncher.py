bff6ef9845da60c6ef2253f6137073c1133c2f19
Dave Raffelt <draffelt@gmail.com>
2013-05-20 05:13:31 +0000

    Added foreach batch processing script Added loop_dir_parallel Reverted single threaded loop scripts

--- scripts/jobLauncher.py
+++ scripts/jobLauncher.py
@@ -2,7 +2,6 @@ import subprocess
 import time
 import os
 import sys
-import multiprocessing
 
 def is_int(s):
   try:
@@ -13,70 +12,12 @@ def is_int(s):
 
 def launchJobs(jobs, logfile='None'):
   if logfile == 'None':
-    cmd = ['parallel','--env', 'PATH', '--sshlogin', '2/beast,2/beauty']
+    cmd = ['parallel','--env', 'PATH', '--sshlogin', '2/beast,2/beauty', '--wd', os.getcwd()]
   else:
-    cmd = ['parallel','--env', 'PATH', '--sshlogin', '2/beast,2/beauty', '--joblog', logfile]
+    cmd = ['parallel','--env', 'PATH', '--sshlogin', '2/beast,2/beauty', '--wd', os.getcwd(), '--joblog', logfile]
   proc = subprocess.Popen (cmd, stdin=subprocess.PIPE)
   alljobs = ''
+  print jobs
   for job in jobs:
-    alljobs = alljobs + str(job) + ';\n'
+    alljobs = alljobs + job + ';'
   proc.communicate (alljobs)
-
-def launchLocal(jobs, maxProcessors):
-  numJobs = len(jobs)
-  currentJob = 0
-  processes = {}
-  if maxProcessors == 0:
-    maxProcessors = multiprocessing.cpu_count()
-
-  total = len(jobs)
-  p = progress.ProgressMeter(total=total)
-  p.update(0)
-  while currentJob < numJobs:
-
-    if len(processes.keys()) < maxProcessors:
-      pid = os.fork()
-
-      # If we have the child process then launch this job
-      if pid == 0:
-        if (os.system(jobs[currentJob])):
-          print 'Command failed: ' +  jobs[currentJob][:-16]
-        sys.exit(1)
-      # We have the parent so store this id in the processes list
-      else:
-        for i in range(0, maxProcessors):
-          if i not in processes.keys():
-            processes[i] = pid
-            break
-        currentJob = currentJob + 1
-
-    else:
-      # can't launch anymore so we wait until a job finishes
-      procToDel = []
-      for i in processes.keys():
-        status = os.waitpid(processes[i],os.WNOHANG)
-        if status[0] != 0:
-          # the process has finished so add it to the list to be deleted
-          # this is needed because processes.keys() returns a reference
-          procToDel.append(i)
-      # delete the process(es)
-      for proc in procToDel:
-        del processes[proc]
-        p.update(1)
-
-      time.sleep(1)
-
-  # wait for all of them to finish
-  while len(processes.keys()) > 0:
-    procToDel = []
-    for i in processes.keys():
-      status = os.waitpid(processes[i],os.WNOHANG)
-      if status[0] != 0:
-        procToDel.append(i)
-
-    # delete the process
-    for proc in procToDel:
-      del processes[proc]
-      p.update(1)
-
-    time.sleep(1)
