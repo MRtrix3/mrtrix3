bfd3c1c588bd5acb5cbfe37fe69ccbe0deda9d64
Lestropie <r.smith@brain.org.au>
2013-11-16 18:19:35 +1100

    Added capability to mrmath to either compute a statistic along an axis of a single image, or across multiple images. Also updated scripts to use this functionality instead of running mrcat before mrmath.

--- cmd/mrmath.cpp
+++ cmd/mrmath.cpp
@@ -21,11 +21,13 @@
 */
 
 #include "app.h"
-#include "progressbar.h"
-#include "image/voxel.h"
 #include "image/buffer.h"
 #include "image/buffer_preload.h"
-#include "image/loop.h"
+#include "image/threaded_loop.h"
+#include "image/utils.h"
+#include "image/voxel.h"
+
+#include <vector>
 
 MRTRIX_APPLICATION
 
@@ -48,25 +50,30 @@ const char* operations[] = {
 void usage ()
 {
   DESCRIPTION
-  + "compute summary statistic (e.g. mean, min, max, ...) on image intensities along specified axis."
-
+  + "compute summary statistic (e.g. mean, min, max, ...) on image intensities either across images, or along a specified axis for a single image. "
   + "See also 'mrcalc' to compute per-voxel operations.";
 
   ARGUMENTS
-  + Argument ("input", "the input image.").type_image_in ()
+  + Argument ("input", "the input image.").type_image_in ().allow_multiple()
   + Argument ("operation", "the operation to apply, one of: " + join(operations, ", ") + ".").type_choice (operations)
-  + Argument ("axis", "the axis along which to compute summary statistic.").type_integer (0)
   + Argument ("output", "the output image.").type_image_out ();
+
+  OPTIONS
+  + Option ("axis", "perform operation along a specified axis of a single input image")
+    + Argument ("index").type_integer();
+
 }
 
 
 typedef float value_type;
 
-typedef Image::BufferPreload<value_type> InputBufferType;
-typedef InputBufferType::voxel_type InputVoxelType;
+typedef Image::BufferPreload<value_type> PreloadBufferType;
+typedef PreloadBufferType::voxel_type PreloadVoxelType;
 
-typedef Image::Buffer<value_type> OutputBufferType;
-typedef OutputBufferType::voxel_type OutputVoxelType;
+typedef Image::Buffer<value_type> BufferType;
+typedef BufferType::voxel_type VoxelType;
+
+
 
 class Mean {
   public:
@@ -191,23 +198,62 @@ class MagMax {
 
 
 template <class Operation>
-class Kernel {
+class AxisKernel {
   public:
-    Kernel (InputVoxelType& in, OutputVoxelType& out, size_t axis) :
-      in (in), out (out), axis (axis) { }
+    AxisKernel (PreloadBufferType& buffer_in, BufferType& buffer_out, size_t axis) :
+      in (buffer_in), out (buffer_out), axis (axis) { }
     void operator() (const Image::Iterator& pos) {
-      Image::voxel_assign (in, pos);
+      Image::voxel_assign (in,  pos);
       Image::voxel_assign (out, pos);
       Operation op;
-      for (in[axis] = 0; in[axis] < in.dim(axis); ++in[axis]) {
+      for (in[axis] = 0; in[axis] < in.dim(axis); ++in[axis])
         op (in.value());
-      }
       out.value() = op.result();
     }
   protected:
-    InputVoxelType in;
-    OutputVoxelType out;
+    PreloadVoxelType in;
+    VoxelType out;
     const size_t axis;
+};
+
+
+
+
+
+template <class Operation>
+class ImageKernel {
+  public:
+    ImageKernel (std::vector<BufferType*>& input_buffers, BufferType& output_buffer) :
+        out (output_buffer)
+    {
+      for (std::vector<BufferType*>::const_iterator i = input_buffers.begin(); i != input_buffers.end(); ++i)
+        inputs.push_back (new VoxelType (**i));
+    }
+    ImageKernel (const ImageKernel& that) :
+        out (that.out)
+    {
+      inputs.reserve (that.inputs.size());
+      for (std::vector<VoxelType*>::const_iterator i = that.inputs.begin(); i != that.inputs.end(); ++i)
+        inputs.push_back (new VoxelType (**i));
+    }
+    ~ImageKernel()
+    {
+      for (std::vector<VoxelType*>::iterator i = inputs.begin(); i != inputs.end(); ++i) {
+        delete *i; *i = NULL;
+      }
+    }
+    void operator() (const Image::Iterator& pos) {
+      Operation op;
+      for (std::vector<VoxelType*>::iterator i = inputs.begin(); i != inputs.end(); ++i) {
+        Image::voxel_assign (**i, pos);
+        op ((*i)->value());
+      }
+      Image::voxel_assign (out, pos);
+      out.value() = op.result();
+    }
+  protected:
+    std::vector<VoxelType*> inputs;
+    VoxelType out;
 };
 
 
@@ -216,35 +262,82 @@ class Kernel {
 
 void run ()
 {
-  int op = argument[1];
-  size_t axis = argument[2];
 
-  InputBufferType buffer_in (argument[0], Image::Stride::contiguous_along_axis (axis));
+  const size_t num_inputs = argument.size() - 2;
+  const int op = argument[num_inputs];
+  const std::string& output_path = argument.back();
 
-  Image::Header header_out (buffer_in);
-  header_out.datatype() = DataType::Float32;
-  header_out.dim(axis) = 1;
-  Image::squeeze_dim (header_out);
+  Options opt = get_options ("axis");
+  if (opt.size()) {
 
-  OutputBufferType buffer_out (argument[3], header_out);
+    if (num_inputs != 1)
+      throw Exception ("Option -axis only applies if a single input image is used");
 
-  InputVoxelType vox_in (buffer_in);
-  OutputVoxelType vox_out (buffer_out);
+    const size_t axis = opt[0][0];
 
+    PreloadBufferType buffer_in (argument[0], Image::Stride::contiguous_along_axis (axis));
 
-  Image::ThreadedLoop loop (std::string("computing ") + operations[op] + " along axis " + str(axis) + "...", vox_in);
+    Image::Header header_out (buffer_in);
+    header_out.datatype() = DataType::Float32;
+    header_out.dim(axis) = 1;
+    Image::squeeze_dim (header_out);
 
-  switch (op) {
-    case 0: loop.run_outer (Kernel<Mean> (vox_in, vox_out, axis)); return;
-    case 1: loop.run_outer (Kernel<Sum> (vox_in, vox_out, axis)); return;
-    case 2: loop.run_outer (Kernel<RMS> (vox_in, vox_out, axis)); return;
-    case 3: loop.run_outer (Kernel<Var> (vox_in, vox_out, axis)); return;
-    case 4: loop.run_outer (Kernel<Std> (vox_in, vox_out, axis)); return;
-    case 5: loop.run_outer (Kernel<Min> (vox_in, vox_out, axis)); return;
-    case 6: loop.run_outer (Kernel<Max> (vox_in, vox_out, axis)); return;
-    case 7: loop.run_outer (Kernel<AbsMax> (vox_in, vox_out, axis)); return;
-    case 8: loop.run_outer (Kernel<MagMax> (vox_in, vox_out, axis)); return;
-    default: assert (0);
+    BufferType buffer_out (output_path, header_out);
+
+    Image::ThreadedLoop loop (std::string("computing ") + operations[op] + " along axis " + str(axis) + "...", buffer_in);
+
+    switch (op) {
+      case 0: loop.run_outer (AxisKernel<Mean>   (buffer_in, buffer_out, axis)); return;
+      case 1: loop.run_outer (AxisKernel<Sum>    (buffer_in, buffer_out, axis)); return;
+      case 2: loop.run_outer (AxisKernel<RMS>    (buffer_in, buffer_out, axis)); return;
+      case 3: loop.run_outer (AxisKernel<Var>    (buffer_in, buffer_out, axis)); return;
+      case 4: loop.run_outer (AxisKernel<Std>    (buffer_in, buffer_out, axis)); return;
+      case 5: loop.run_outer (AxisKernel<Min>    (buffer_in, buffer_out, axis)); return;
+      case 6: loop.run_outer (AxisKernel<Max>    (buffer_in, buffer_out, axis)); return;
+      case 7: loop.run_outer (AxisKernel<AbsMax> (buffer_in, buffer_out, axis)); return;
+      case 8: loop.run_outer (AxisKernel<MagMax> (buffer_in, buffer_out, axis)); return;
+      default: assert (0);
+    }
+
+  } else {
+
+    if (num_inputs < 2)
+      throw Exception ("mrmath requires either multiple input images, or the -axis option to be provided");
+
+    std::vector<BufferType*> buffers;
+    for (size_t index = 0; index != num_inputs; ++index)
+      buffers.push_back (new BufferType (argument[index]));
+
+    Image::Header header_out (*buffers.front());
+    header_out.datatype() = DataType::Float32;
+    for (size_t index = 1; index != buffers.size(); ++index) {
+      if (!Image::dimensions_match (header_out, *buffers[index]))
+        throw Exception ("Input images must have the same dimensions!");
+    }
+
+    BufferType buffer_out (output_path, header_out);
+
+    Image::ThreadedLoop loop (std::string("computing ") + operations[op] + " across " + str(num_inputs) + " images...", buffer_out);
+
+    switch (op) {
+      case 0: loop.run (ImageKernel<Mean>   (buffers, buffer_out)); return;
+      case 1: loop.run (ImageKernel<Sum>    (buffers, buffer_out)); return;
+      case 2: loop.run (ImageKernel<RMS>    (buffers, buffer_out)); return;
+      case 3: loop.run (ImageKernel<Var>    (buffers, buffer_out)); return;
+      case 4: loop.run (ImageKernel<Std>    (buffers, buffer_out)); return;
+      case 5: loop.run (ImageKernel<Min>    (buffers, buffer_out)); return;
+      case 6: loop.run (ImageKernel<Max>    (buffers, buffer_out)); return;
+      case 7: loop.run (ImageKernel<AbsMax> (buffers, buffer_out)); return;
+      case 8: loop.run (ImageKernel<MagMax> (buffers, buffer_out)); return;
+      default: assert (0);
+    }
+
+    while (buffers.size()) {
+      delete buffers.back();
+      buffers.pop_back();
+    }
+
   }
+
 }
 
