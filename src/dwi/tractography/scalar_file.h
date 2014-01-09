MRtrix3/mrtrix3

commit f9fc2f2a618ef2ad1b15ee53be5266212928cec5

Added dynamic count field updates to TSF writer
The Tractography::ScalarWriter class now updates the 'count' and 'total_count' fields on every buffer commit, just as is done in the tractography writer.

src/dwi/tractography/scalar_file.h
@@ -223,6 +223,12 @@ namespace MR
             out.write (reinterpret_cast<char*> (&(buffer[0])), sizeof (value_type)*(buffer_size));
             if (!out.good())
               throw Exception ("error writing track scalars file \"" + name + "\": " + strerror (errno));
+
+            out.seekp (count_offset);
+            out << count << "\ntotal_count: " << total_count << "\nEND\n";
+            if (!out.good())
+              throw Exception ("error writing track scalars file \"" + name + "\": " + strerror (errno));
+
             buffer_size = 0;
           }
 

commit b9ce9db3f23d454078624ce765627064de99080a

Fix compilation error in src/dwi/tractography/scalar_file.h
Fixes compilation error that was introduced in commit f9fc2f2a6 due to ScalarWriter class not automatically inheriting members from templated base class

src/dwi/tractography/scalar_file.h
@@ -146,6 +146,7 @@ namespace MR
         public:
           typedef T value_type;
           using __WriterBase__<T>::count;
+          using __WriterBase__<T>::count_offset;
           using __WriterBase__<T>::total_count;
           using __WriterBase__<T>::name;
           using __WriterBase__<T>::dtype;