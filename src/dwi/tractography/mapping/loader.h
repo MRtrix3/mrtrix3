MRtrix3/mrtrix3

commit 620092f4528a0fe058974c831e0abb57362258e8

Tractography: lots of changes to file handling
- Writer classes now update counts at commit time.

- TrackData now renamed to Streamline. This includes weight, and is now
  the default type for streamline data.

- std::vector<Point> as data type for streamlines is now deprecated,
  with all apps updated to using Streamline class instead.

- removed next()/append() methods from Writer/Reader classes, since the
  operator() methods perform the same function and can be used directly
  in multithreading pipelines.

- new IntervalTimer class to ease fixed-interval checking (used in
  particular for displaying progress updates). Generalises Rob's
  approach, which was already used in a couple of places in the
  tractography code.

src/dwi/tractography/mapping/loader.h
@@ -44,20 +44,20 @@ class TrackLoader
     TrackLoader (Tractography::Reader<float>& file, const size_t to_load = 0, const std::string& msg = "mapping tracks to image...") :
       reader (file),
       tracks_to_load (to_load),
+      num_loaded (0),
       progress (msg.size() ? new ProgressBar (msg, tracks_to_load) : NULL)
     { }
 
     virtual ~TrackLoader() { }
-    virtual bool operator() (Tractography::TrackData<float>& out)
+    virtual bool operator() (Streamline<float>& out)
     {
-      if (!reader.next_data (out)) {
+      if (!reader (out)) {
         delete progress;
         progress = NULL;
         return false;
       }
-      if (tracks_to_load && out.index >= tracks_to_load) {
+      if (tracks_to_load && num_loaded >= tracks_to_load) {
         out.clear();
-        out.index = -1;
         delete progress;
         progress = NULL;
         return false;
@@ -69,8 +69,9 @@ class TrackLoader
 
   protected:
     Tractography::Reader<float>& reader;
-    size_t tracks_to_load;
-    ProgressBar* progress;
+    const size_t tracks_to_load;
+    size_t num_loaded;
+    Ptr<ProgressBar> progress;
 
 };
 

cmd/afd_connectivity.cpp
@@ -190,7 +190,7 @@ void run ()
   Image::Buffer<value_type> fod_buffer (argument[0]);
   DWI::FMLS::Segmenter fmls (dirs, Math::SH::LforN (fod_buffer.dim(3)));
   TrackProcessor tract_processor (fod_buffer, fixel_indexer, fixel_directions, fixel_AFD, fixel_TDI, fmls);
-  Thread::run_queue_custom_threading (loader, 1, DWI::Tractography::TrackData<float>(), mapper, 1, SetVoxelDir(), tract_processor, 1);
+  Thread::run_queue_custom_threading (loader, 1, DWI::Tractography::Streamline<float>(), mapper, 1, SetVoxelDir(), tract_processor, 1);
 
   double total_AFD = 0.0;
   Image::Loop loop (0, 3);
@@ -215,11 +215,11 @@ void run ()
   }
 
   Tractography::Reader<value_type> tck_file (argument[1], properties);
-  std::vector<Point<value_type> > tck;
+  Tractography::Streamline<value_type> tck;
   double total_track_length = 0.0;
   {
     ProgressBar progress ("normalising apparent fibre density by mean track length...", track_count);
-    while (tck_file.next (tck)) {
+    while (tck_file (tck)) {
       total_track_length += static_cast<value_type>(tck.size() - 1) * step_size;
       progress++;
     }

cmd/fixel2tracks.cpp
@@ -100,17 +100,17 @@ void run ()
   Image::LoopInOrder loop (input_fixel, "generating fixel-wise track segments");
   for (loop.start (input_fixel); loop.ok(); loop.next (input_fixel)) {
     for (size_t f = 0; f != input_fixel.value().size(); ++f) {
-      std::vector< Point<float> > tck;
+      DWI::Tractography::Streamline<float> tck;
       transform.voxel2scanner (input_fixel, voxel_pos);
       const float step = half_length * (scale_length_by_value ? input_fixel.value()[f].value : 1.0);
       tck.push_back (voxel_pos + (input_fixel.value()[f].dir *  step));
       tck.push_back (voxel_pos + (input_fixel.value()[f].dir * -step));
-      tck_writer.append (tck);
+      tck_writer (tck);
       if (tsf_writer) {
         std::vector<float> scalars;
         scalars.push_back (input_fixel.value()[f].value);
         scalars.push_back (input_fixel.value()[f].value);
-        tsf_writer->append (scalars);
+        (*tsf_writer) (scalars);
       }
     }
   }

cmd/tckconnectome.cpp
@@ -129,7 +129,7 @@ void run ()
   Mapping::TrackLoader loader (reader, properties["count"].empty() ? 0 : to<size_t>(properties["count"]), "Constructing connectome... ");
   Mapper mapper (*tck2nodes, *metric);
   Connectome connectome (max_node_index);
-  Thread::run_batched_queue_threaded_pipe (loader, Tractography::TrackData<float>(), 100, mapper, Mapped_track(), 100, connectome);
+  Thread::run_batched_queue_threaded_pipe (loader, Tractography::Streamline<float>(), 100, mapper, Mapped_track(), 100, connectome);
 
   if (metric->scale_edges_by_streamline_count())
     connectome.scale_by_streamline_count();

cmd/tckfilter.cpp
@@ -76,7 +76,7 @@ void usage ()
 }
 
 
-typedef Tractography::TrackData<float> TrackData;
+typedef Tractography::Streamline<float> Streamline;
 
 
 class Filter
@@ -97,13 +97,13 @@ class Filter
     }
 
 
-    bool operator() (const TrackData& in, TrackData& out)
+    bool operator() (const Streamline& in, Streamline& out)
     {
       out.clear();
       if ((in.size() < min_num_points) || (in.size() > max_num_points))
         return true;
       track_included.assign (properties.include.size(), false);
-      for (TrackData::const_iterator i = in.begin(); i != in.end(); ++i) {
+      for (Streamline::const_iterator i = in.begin(); i != in.end(); ++i) {
         if (!test_point (*i))
           return true;
       }
@@ -146,8 +146,6 @@ class Filter
 
 
 
-#define UPDATE_INTERVAL 0.0333333 // 30 Hz - most monitors are 60Hz
-
 class Writer : public Tractography::Writer<float>
 {
 
@@ -156,9 +154,7 @@ class Writer : public Tractography::Writer<float>
   public:
     Writer (const std::string& path, Tractography::Properties& properties) :
       WriterBase (path, properties),
-      in_count (properties.find ("count") == properties.end() ? 0 : to<size_t>(properties["count"])),
-      timer (),
-      next_time (timer.elapsed()) { }
+      in_count (properties.find ("count") == properties.end() ? 0 : to<size_t>(properties["count"])) { }
 
     ~Writer ()
     {
@@ -166,23 +162,21 @@ class Writer : public Tractography::Writer<float>
         fprintf (stderr, "\r%8zu read, %8zu filtered    [100%%]\n", WriterBase::total_count, WriterBase::count);
     }
 
-    bool operator() (const TrackData& in)
+    bool operator() (const Streamline& in)
     {
-      if (App::log_level > 0 && timer.elapsed() >= next_time) {
-        next_time += UPDATE_INTERVAL;
+      if (App::log_level > 0 && timer) {
         fprintf (stderr, "\r%8zu read, %8zu filtered    [%3d%%]",
             WriterBase::total_count, WriterBase::count,
             in_count ? (int(100.0 * WriterBase::total_count / float(in_count))) : 0);
       }
-      WriterBase::append (in);
+      WriterBase::operator() (in);
       return true;
     }
 
 
   private:
     const size_t in_count;
-    Timer timer;
-    double next_time;
+    IntervalTimer timer;
 
 };
 
@@ -229,7 +223,7 @@ void run ()
   Filter filter (properties, step_size);
   Writer writer (argument[1], properties);
 
-  Thread::run_batched_queue_threaded_pipe (reader, TrackData(), 100, filter, TrackData(), 100, writer);
+  Thread::run_batched_queue_threaded_pipe (reader, Streamline(), 100, filter, Streamline(), 100, writer);
 
 }
 

cmd/tckfixcount.cpp
@@ -77,11 +77,11 @@ void run ()
   }
 
   // Read the actual number of streamlines in the file
-  std::vector< Point<float> > tck;
+  Tractography::Streamline<float> tck;
   size_t count = 0;
   {
     ProgressBar progress ("evaluating actual streamline data count...");
-    while (reader.next (tck)) {
+    while (reader (tck)) {
       ++count;
       ++progress;
     }

cmd/tckimport.cpp
@@ -63,11 +63,11 @@ void run ()
       if (M.columns() != 3)
         throw Exception ("file \"" + argument[n] + "\" does not contain 3 columns - ignored");
 
-      std::vector<Point<float> > tck (M.rows());
+      DWI::Tractography::Streamline<float> tck (M.rows());
       for (size_t i = 0; i < M.rows(); i++) {
         tck[i].set (M (i,0), M (i,1), M (i,2));
       }
-      writer.append (tck);
+      writer (tck);
       writer.total_count++;
     }
     catch (Exception) { }

cmd/tckinfo.cpp
@@ -90,11 +90,11 @@ void run ()
 
 
     if (actual_count) {
-      std::vector<Point<float> > tck;
+      Tractography::Streamline<float> tck;
       size_t count = 0;
       {
         ProgressBar progress ("counting tracks in file... ");
-        while (file.next (tck)) {
+        while (file (tck)) {
           ++count;
           ++progress;
         }
@@ -104,9 +104,9 @@ void run ()
 
     if (opt.size()) {
       ProgressBar progress ("writing track data to ascii files");
-      std::vector<Point<float> > tck;
+      Tractography::Streamline<float> tck;
       size_t count = 0;
-      while (file.next (tck)) {
+      while (file (tck)) {
         std::string filename (opt[0][0]);
         filename += "-000000.txt";
         std::string num (str (count));

commit ec4ef0d318f6542e839e28addcbedaf692ea8b25

Fix double free() error in TrackLoader class
This bug was introduced in commit 620092f45 due to changing progress bar from a C pointer to a Ptr<> class.

src/dwi/tractography/mapping/loader.h
@@ -51,13 +51,11 @@ class TrackLoader
     virtual bool operator() (Streamline<float>& out)
     {
       if (!reader (out)) {
-        delete progress;
         progress = NULL;
         return false;
       }
       if (tracks_to_load && out.index >= tracks_to_load) {
         out.clear();
-        delete progress;
         progress = NULL;
         return false;
       }