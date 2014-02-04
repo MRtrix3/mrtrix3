b93c5e851472ae6809b950b8d96e53cf64cd6a82
J-Donald Tournier <jdtournier@gmail.com>
2014-02-04 21:04:30 +0000

    MRView: change slightly disturbing eyeball icon for a camera one

--- src/gui/disp_profile_icon.cpp
+++ src/gui/disp_profile_icon.cpp
@@ -1,8 +1,7 @@
 /****************************************************************************
 ** Resource object code
 **
-** Created: Tue Jan 21 13:03:29 2014
-**      by: The Resource Compiler for Qt version 4.8.1
+** Created by: The Resource Compiler for Qt version 4.8.5
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -10,7 +9,7 @@
 #include <QtCore/qglobal.h>
 
 static const unsigned char qt_resource_data[] = {
-  // /home/jdt13/mrtrix3/icons/mrtrix.png
+  // /home/donald/mrtrix3/icons/mrtrix.png
   0x0,0x0,0x19,0x8e,
   0x89,
   0x50,0x4e,0x47,0xd,0xa,0x1a,0xa,0x0,0x0,0x0,0xd,0x49,0x48,0x44,0x52,0x0,
