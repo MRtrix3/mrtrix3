ae6e070604b77f5586077dfe16ecffc802140344
J-Donald Tournier <jdtournier@gmail.com>
2014-02-17 16:53:55 +0000

    Threadedloop: fix bug in introduced in previous commit
    Problem was with use of Image::voxel_assign() rather
    ThreadedLoop::set_outer_axes(), which may have caused issues with
    volume-wise processing, etc. Added new Image::voxel_assign() functions
    that take a vector of axes, and use those instead.

--- lib/image/voxel.h
+++ lib/image/voxel.h
@@ -178,6 +178,12 @@ namespace MR
           out[n] = in[n];
       }
 
+    template <class InputVoxelType, class OutputVoxelType>
+      void voxel_assign (OutputVoxelType& out, const InputVoxelType& in, const std::vector<size_t>& axes) {
+        for (size_t n = 0; n < axes.size(); ++n)
+          out[axes[n]] = in[axes[n]];
+      }
+
     template <class InputVoxelType, class OutputVoxelType, class OutputVoxelType2>
       void voxel_assign2 (OutputVoxelType& out, OutputVoxelType2& out2, const InputVoxelType& in, size_t from_axis = 0, size_t to_axis = std::numeric_limits<size_t>::max()) {
         to_axis = std::min (to_axis, std::min (in.ndim(), std::min (out.ndim(), out2.ndim())));
@@ -185,11 +191,23 @@ namespace MR
           out[n] = out2[n] = in[n];
       }
 
+    template <class InputVoxelType, class OutputVoxelType, class OutputVoxelType2>
+      void voxel_assign2 (OutputVoxelType& out, OutputVoxelType2& out2, const InputVoxelType& in, const std::vector<size_t>& axes) {
+        for (size_t n = 0; n < axes.size(); ++n)
+          out[axes[n]] = out2[axes[n]] = in[axes[n]];
+      }
+
     template <class InputVoxelType, class OutputVoxelType, class OutputVoxelType2, class OutputVoxelType3>
-      void voxel_assign3 (OutputVoxelType& out, OutputVoxelType2& out2, OutputVoxelType2& out3, const InputVoxelType& in, size_t from_axis = 0, size_t to_axis = std::numeric_limits<size_t>::max()) {
+      void voxel_assign3 (OutputVoxelType& out, OutputVoxelType2& out2, OutputVoxelType3& out3, const InputVoxelType& in, size_t from_axis = 0, size_t to_axis = std::numeric_limits<size_t>::max()) {
         to_axis = std::min (to_axis, std::min (in.ndim(), std::min (out.ndim(), std::min (out2.ndim(), out3.ndim()))));
         for (size_t n = from_axis; n < to_axis; ++n)
           out[n] = out2[n] = out3[n] = in[n];
+      }
+
+    template <class InputVoxelType, class OutputVoxelType, class OutputVoxelType2 , class OutputVoxelType3>
+      void voxel_assign3 (OutputVoxelType& out, OutputVoxelType2& out2, OutputVoxelType3& out3, const InputVoxelType& in, const std::vector<size_t>& axes) {
+        for (size_t n = 0; n < axes.size(); ++n)
+          out[axes[n]] = out2[axes[n]] = out3[axes[n]] = in[axes[n]];
       }
 
     //! reset all coordinates to zero.
