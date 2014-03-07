e12d0451179bfd8fc6d2dcb1c640e8fba545a2db
J-Donald Tournier <jdtournier@gmail.com>
2014-03-07 10:36:37 +0000

    NRView: remove Lighting tool since unused
    ODF & Tractography tools both have their own lighting dialogs

--- src/gui/mrview/tool/list.h
+++ src/gui/mrview/tool/list.h
@@ -2,7 +2,6 @@
 
 // place #include files in here:
 #include "gui/mrview/tool/view.h"
-#include "gui/mrview/tool/lighting.h"
 //#include "gui/mrview/tool/roi_analysis.h"
 #include "gui/mrview/tool/overlay.h"
 #include "gui/mrview/tool/odf.h"
@@ -17,7 +16,6 @@ The second argument is the name of the mode as displayed in the menu.
 The third argument is the text to be shown in the menu tooltip. */
 
 TOOL(View, View options, Adjust view settings)
-TOOL(Lighting, Lighting options, Adjust lighting settings for those modes that support it)
 //TOOL(ROI, ROI analysis, Draw & analyse regions of interest)
 TOOL(Overlay, Overlay, Overlay other images over the current image)
 TOOL(Tractography, Tractography, Display tracks over the current image)
