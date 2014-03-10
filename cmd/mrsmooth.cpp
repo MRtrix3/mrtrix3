c30a977477056603ad248cf12683a0099396d8f7
David Raffelt <draffelt@gmail.com>
2014-03-11 10:05:15 +1100

    removed -anisotropic (directional smoothing) option from mrsmooth

--- cmd/mrsmooth.cpp
+++ cmd/mrsmooth.cpp
@@ -25,7 +25,6 @@
 #include "image/buffer_preload.h"
 #include "image/voxel.h"
 #include "image/filter/gaussian_smooth.h"
-#include "image/filter/anisotropic_smooth.h"
 #include "progressbar.h"
 
 
@@ -63,17 +62,6 @@ void usage ()
             "The default extent is 2 * ceil(2.5 * stdev / voxel_size) - 1.")
   + Argument ("voxels").type_sequence_int()
 
-  + Option ("anisotropic", "smooth each 3D volume of a 4D image using an anisotropic gaussian kernel "
-                           "oriented along a corresponding direction. Note that when this option is used "
-                           "the -stdev option takes two comma separated values, the first value defines the standard "
-                           "deviation along the major eigenvector of the kernel (default: 1.27mm (FWHM = 3mm)) and "
-                           "the second value defines the standard deviation along the other two eigenvectors "
-                           "(default: 0.42mm (FWHM = 1mm)). The directions can be specified within the header of the input image, "
-                           "or by using the -directions option.")
-
-  + Option ("directions", "the directions for anisotropic smoothing.")
-  + Argument ("file", "a list of directions [az el] generated using the gendir command.").type_file()
-
   + Image::Stride::StrideOption;
 }
 
@@ -84,22 +72,12 @@ void run () {
   Image::BufferPreload<float> input_data (header);
   Image::BufferPreload<float>::voxel_type input_vox (input_data);
 
-  bool do_anisotropic = false;
-  Options opt = get_options ("anisotropic");
-  if (opt.size())
-    do_anisotropic = true;
-
   std::vector<float> stdev;
-  if (do_anisotropic) {
-    stdev.resize(2, 3.0 / 2.3548);
-    stdev[1] = 1.0 / 2.3548;
-  } else {
-    stdev.resize(3);
-    for (size_t dim = 0; dim < 3; dim++)
-      stdev[dim] = input_data.vox (dim);
-  }
+  stdev.resize(3);
+  for (size_t dim = 0; dim < 3; dim++)
+    stdev[dim] = input_data.vox (dim);
 
-  opt = get_options ("stdev");
+  Options opt = get_options ("stdev");
   int stdev_supplied = opt.size();
   if (stdev_supplied) {
     stdev = parse_floats (opt[0][0]);
@@ -122,52 +100,17 @@ void run () {
       throw Exception ("too many axes supplied to -stride option");
   }
 
+  Image::Filter::GaussianSmooth<> smooth_filter (input_vox, stdev);
+  opt = get_options ("extent");
+  if (opt.size())
+    smooth_filter.set_extent(parse_ints (opt[0][0]));
 
-  if (do_anisotropic) {
-
-    Math::Matrix<float> directions;;
-    opt = get_options ("directions");
-    if (opt.size()) {
-      directions.load(opt[0][0]);
-    } else {
-      if (!header["directions"].size())
-        throw Exception ("no mask directions have been specified.");
-      std::vector<float> dir_vector;
-      std::vector<std::string > lines = split (header["directions"], "\n", true);
-      for (size_t l = 0; l < lines.size(); l++) {
-        std::vector<float> v (parse_floats(lines[l]));
-        dir_vector.insert (dir_vector.end(), v.begin(), v.end());
-      }
-      directions.resize(dir_vector.size() / 2, 2);
-      for (size_t i = 0; i < dir_vector.size(); i += 2) {
-        directions(i / 2, 0) = dir_vector[i];
-        directions(i / 2, 1) = dir_vector[i + 1];
-      }
-    }
-    Image::Filter::AnisotropicSmooth smooth_filter (input_vox, stdev, directions);
-    header.info() = smooth_filter.info();
-    if (strides.size()) {
-      for (size_t n = 0; n < strides.size(); ++n)
-        header.stride(n) = strides[n];
-    }
-    Image::Buffer<float> output_data (argument[1], header);
-    Image::Buffer<float>::voxel_type output_vox (output_data);
-    smooth_filter (input_vox, output_vox);
-
-  } else {
-
-    Image::Filter::GaussianSmooth<> smooth_filter (input_vox, stdev);
-    opt = get_options ("extent");
-    if (opt.size())
-      smooth_filter.set_extent(parse_ints (opt[0][0]));
-
-    header.info() = smooth_filter.info();
-    if (strides.size()) {
-      for (size_t n = 0; n < strides.size(); ++n)
-        header.stride(n) = strides[n];
-    }
-    Image::Buffer<float> output_data (argument[1], header);
-    Image::Buffer<float>::voxel_type output_vox (output_data);
-    smooth_filter (input_vox, output_vox);
+  header.info() = smooth_filter.info();
+  if (strides.size()) {
+    for (size_t n = 0; n < strides.size(); ++n)
+      header.stride(n) = strides[n];
   }
+  Image::Buffer<float> output_data (argument[1], header);
+  Image::Buffer<float>::voxel_type output_vox (output_data);
+  smooth_filter (input_vox, output_vox);
 }
