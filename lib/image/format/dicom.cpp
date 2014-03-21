12251fc24305f86381dc715f2a021eb5aabe2da0
J-Donald Tournier <jdtournier@gmail.com>
2014-03-21 12:17:07 +0000

    DICOM: allow reading of single files with suffix .dcm

--- lib/image/format/dicom.cpp
+++ lib/image/format/dicom.cpp
@@ -43,12 +43,15 @@ namespace MR
       RefPtr<Handler::Base> DICOM::read (Header& H) const
       {
         if (!Path::is_dir (H.name())) 
-          return RefPtr<Handler::Base>();
+          if (!Path::has_suffix (H.name(), ".dcm"))
+            return RefPtr<Handler::Base>();
 
         File::Dicom::Tree dicom;
 
         dicom.read (H.name());
         dicom.sort();
+
+        std::cout << dicom;
 
         std::vector< RefPtr<File::Dicom::Series> > series = File::Dicom::select_func (dicom);
         if (series.empty()) 
