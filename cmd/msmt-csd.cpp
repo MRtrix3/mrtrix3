96b3bb181ab21409120a529a2d2b20bd91669836
J-Donald Tournier <jdtournier@gmail.com>
2014-05-30 17:00:52 +0100

    new msmt-csd command can now perform standard linear least-square SD
    This uses the new DWI::Response and DWI::init_SH_to_DW_mapping()
    functionality.

--- cmd/msmt-csd.cpp
+++ cmd/msmt-csd.cpp
@@ -3,7 +3,8 @@
 #include "progressbar.h"
 #include "image/threaded_loop.h"
 #include "image/buffer.h"
-#include "image/buffer_preload.h"
+#include "image/buffer_scratch.h"
+#include "image/adapter/extract.h"
 #include "math/hermite.h"
 #include "image/voxel.h"
 #include "dwi/gradient.h"
@@ -13,132 +14,108 @@
   InputBufferType::voxel_type dwi_vox (dwi_buffer);
   OutputBufferType::voxel_type FOD_vox (FOD_buffer);
   Ptr<MaskBufferType::voxel_type> mask_vox;
-    mask_vox = new MaskBufferType::voxel_type (*mask_data);
+    mask_vox = new MaskBufferType::voxel_type (*mask_buffer);
 */
 
 
 namespace MR {
   namespace DWI {
+    namespace MTCSD {
 
-    using namespace App;
+      using namespace App;
 
-    const OptionGroup MT_CSDOptions = OptionGroup ("multi-tissue CSD options")
-      + Option ("lmax",
-                "set the maximum harmonic order for the output series. By default, the "
-                "program will use the highest possible lmax given the number of "
-                "diffusion-weighted images.")
-      + Argument ("order").type_integer (2, 8, 30)
+      const OptionGroup Options = OptionGroup ("multi-tissue CSD options")
+        + Option ("lmax",
+            "set the maximum harmonic order for the output series. By default, the "
+            "program will use the highest possible lmax given the number of "
+            "diffusion-weighted images.")
+        + Argument ("order").type_integer (2, 8, 30)
 
-      + Option ("mask",
-                "only perform computation within the specified binary brain mask image.")
-      + Argument ("image").type_image_in()
+        + Option ("mask",
+            "only perform computation within the specified binary brain mask image.")
+        + Argument ("image").type_image_in()
 
-      + Option ("directions",
-                "specify the directions over which to apply the non-negativity constraint "
-                "(by default, the built-in 300 direction set is used). These should be "
-                "supplied as a text file containing the [ az el ] pairs for the directions.")
-      + Argument ("file").type_file()
+        + Option ("directions",
+            "specify the directions over which to apply the non-negativity constraint "
+            "(by default, the built-in 300 direction set is used). These should be "
+            "supplied as a text file containing the [ az el ] pairs for the directions.")
+        + Argument ("file").type_file()
 
-      + Option ("norm_lambda",
-                "the regularisation parameter lambda that controls the strength of the "
-                "constraint on the norm of the solution (default = 1.0).")
-      + Argument ("value").type_float (0.0, 1.0, 1.0e12)
+        + Option ("norm_lambda",
+            "the regularisation parameter lambda that controls the strength of the "
+            "constraint on the norm of the solution (default = 1.0).")
+        + Argument ("value").type_float (0.0, 1.0, 1.0e12)
 
-      + Option ("niter",
-                "the maximum number of iterations to perform for each voxel (default = 50).")
-      + Argument ("number").type_integer (1, 50, 1000);
+        + Option ("niter",
+            "the maximum number of iterations to perform for each voxel (default = 50).")
+        + Argument ("number").type_integer (1, 50, 1000);
 
+
+
+      template <typename ValueType>
+        class Shared {
+          public:
+            Shared (const Image::Header& header, const std::vector< Response<ValueType> >& response) 
+            {
+              std::vector<size_t> bzeros;
+              Math::Matrix<ValueType> grad, directions;
+              M = DWI::get_SH_to_DWI_mapping (header, grad, directions, dwis, bzeros, response, ncoefs);
+              Minv = Math::pinv (M);
+            }
+
+            bool is_multi_shell () const { 
+              return dwis.empty();
+            }
+
+            size_t num_dwi () const {
+              return is_multi_shell() ? M.rows() : dwis.size();
+            }
+
+            std::vector<size_t> dwis;
+            std::vector<size_t> ncoefs;
+            Math::Matrix<ValueType> M, Minv;
+        };
+
+
+
+
+      template <typename ValueType>
+        class Processor {
+          public:
+            Processor (const Shared<ValueType>& shared) :
+              shared (shared),
+              dwi (shared.M.rows()),
+              fod (shared.M.columns()) { }
+
+            template <class VoxelTypeDWI, class VoxelTypeFOD>
+              void operator() (VoxelTypeDWI& dwi_vox, VoxelTypeFOD& fod_vox) {
+                for (dwi_vox[3] = 0; dwi_vox[3] < dwi_vox.dim(3); ++dwi_vox[3]) 
+                  dwi[dwi_vox[3]] = dwi_vox.value();
+
+                Math::mult (fod, shared.Minv, dwi);
+
+                for (fod_vox[3] = 0; fod_vox[3] < fod_vox.dim(3); ++fod_vox[3]) 
+                  fod_vox.value() = fod[fod_vox[3]];
+              }
+
+            template <class VoxelTypeDWI, class VoxelTypeFOD, class VoxelTypeMask>
+              void operator() (VoxelTypeDWI& dwi_vox, VoxelTypeFOD& fod_vox, VoxelTypeMask& mask_vox) {
+                if (mask_vox.value()) 
+                  operator() (dwi_vox, fod_vox);
+              }
+
+          protected:
+            const Shared<ValueType>& shared;
+            Math::Vector<ValueType> dwi, fod;
+        };
+
+
+
+
+    }
   }
 }
 
-
-    /*
-
-      template <typename ValueType>
-        class MSMT_CSD
-        {
-          public:
-            Processor (InputBufferType::voxel_type& DWI_vox,
-                OutputBufferType::voxel_type& FOD_vox,
-                Ptr<MaskBufferType::voxel_type>& mask_vox,
-        const DWI::CSDeconv<value_type>::Shared& shared) :
-      dwi (DWI_vox),
-      fod (FOD_vox),
-      mask (mask_vox),
-      sdeconv (shared),
-      data (shared.dwis.size()) {
-        if (mask)
-          Image::check_dimensions (*mask, dwi, 0, 3);
-      }
-
-
-
-    void operator () (const Image::Iterator& pos) {
-      if (!load_data (pos))
-        return;
-
-      sdeconv.set (data);
-
-      size_t n;
-      for (n = 0; n < sdeconv.shared.niter; n++)
-        if (sdeconv.iterate())
-          break;
-
-      if (n >= sdeconv.shared.niter)
-        INFO ("voxel [ " + str (pos[0]) + " " + str (pos[1]) + " " + str (pos[2]) +
-            " ] did not reach full convergence");
-
-      write_back (pos);
-    }
-
-
-    void run () {
-      Image::ThreadedLoop loop ("performing multi-shell multi-tissue constrained spherical deconvolution...", dwi_vox, 1, 0, 3);
-      loop.run (processor);
-    }
-
-  private:
-    InputBufferType::voxel_type dwi;
-    OutputBufferType::voxel_type fod;
-    Ptr<MaskBufferType::voxel_type> mask;
-    DWI::CSDeconv<value_type> sdeconv;
-    Math::Vector<value_type> data;
-
-
-
-    bool load_data (const Image::Iterator& pos) {
-      if (mask) {
-        Image::voxel_assign (*mask, pos);
-        if (!mask->value())
-          return false;
-      }
-
-      Image::voxel_assign (dwi, pos);
-
-      for (size_t n = 0; n < sdeconv.shared.dwis.size(); n++) {
-        dwi[3] = sdeconv.shared.dwis[n];
-        data[n] = dwi.value();
-        if (!std::isfinite (data[n]))
-          return false;
-        if (data[n] < 0.0)
-          data[n] = 0.0;
-      }
-
-      return true;
-    }
-
-
-
-    void write_back (const Image::Iterator& pos) {
-      Image::voxel_assign (fod, pos);
-      for (fod[3] = 0; fod[3] < fod.dim (3); ++fod[3])
-        fod.value() = sdeconv.FOD() [fod[3]];
-    }
-
-};
-
-
-*/
 
 
 
@@ -169,10 +146,10 @@ void usage ()
     + Argument ("dwi",
         "the input diffusion-weighted image.").type_image_in()
     + Argument ("SH",
-        "the output spherical harmonics coefficients image.").type_image_out();
+        "the output spherical harmonics coefficients image.").type_image_out().allow_multiple();
 
   OPTIONS
-    + DWI::MT_CSDOptions
+    + DWI::MTCSD::Options
     + DWI::GradOption
     + DWI::ShellOption
     + Image::Stride::StrideOption;
@@ -181,8 +158,9 @@ void usage ()
 
 
 typedef float value_type;
-typedef double cost_value_type;
-typedef Image::BufferPreload<value_type> InputBufferType;
+typedef double compute_type;
+typedef Image::Buffer<value_type> SourceBufferType;
+typedef Image::BufferScratch<value_type> DWIBufferType;
 typedef Image::Buffer<value_type> OutputBufferType;
 typedef Image::Buffer<bool> MaskBufferType;
 
@@ -193,36 +171,62 @@ typedef Image::Buffer<bool> MaskBufferType;
 
 void run ()
 {
-  Image::Header dwi_header (argument[0]);
+  const Image::Header dwi_header (argument[0]);
 
-  std::vector< DWI::Response<float> > response (1);
+  // load response(s):
+  std::vector< DWI::Response<compute_type> > response (1);
   response[0].init (DWI::default_WM_response());
 
-  Math::Matrix<float> M = DWI::get_SH_to_DWI_mapping (dwi_header, response);
-  M.save ("M.txt");
+  // initialise MTCSD:
+  DWI::MTCSD::Shared<compute_type> shared (dwi_header, response);
 
-  /*
-  InputBufferType dwi_buffer (argument[0], Image::Stride::contiguous_along_axis (3));
-
-  Ptr<MaskBufferType> mask_data;
+  // load mask if needed:
+  Ptr<MaskBufferType> mask_buffer;
+  Ptr<MaskBufferType::voxel_type> mask_vox;
   Options opt = get_options ("mask");
-  if (opt.size()) 
-    mask_data = new MaskBufferType (opt[0][0]);
+  if (opt.size()) {
+    mask_buffer = new MaskBufferType (opt[0][0]);
+    Image::check_dimensions (dwi_header, *mask_buffer, 0, 3);
+    mask_vox = new MaskBufferType::voxel_type (*mask_buffer);
+  }
 
-  DWI::MSMT_CSD<value_type>::Shared shared (dwi_buffer);
-  shared.parse_cmdline_options();
-  shared.init();
-
-
-  Image::Header header (dwi_buffer);
+  // set up output FOD buffer:
+  // this will need to be changed to use a BufferScratch
+  // and a write-back to separate files implemented at the end of processing
+  Image::Header header (dwi_header);
   header.set_ndim (4);
-  header.dim(3) = shared.nSH();
+  header.dim(3) = shared.M.columns();
   header.datatype() = DataType::from_command_line (DataType::Float32);
   Image::Stride::set_from_command_line (header, Image::Stride::contiguous_along_axis(3));
-  OutputBufferType FOD_buffer (argument[2], header);
+  OutputBufferType fod_buffer (argument[1], header);
+  OutputBufferType::voxel_type fod_vox (fod_buffer);
+
+  // set up DWI scratch buffer:
+  Image::Info info (header);
+  info.set_ndim (4);
+  info.dim(3) = shared.num_dwi();
+  Image::Stride::set (info, Image::Stride::contiguous_along_axis(3));
+  DWIBufferType dwi_buffer (info, "DWI");
+  DWIBufferType::voxel_type dwi_vox (dwi_buffer);
+
+  { // preload dwi data:
+    SourceBufferType dwi_source_buffer (dwi_header);
+    SourceBufferType::voxel_type dwi_source (dwi_source_buffer);
+    if (shared.is_multi_shell()) 
+      Image::threaded_copy_with_progress_message ("loading DWI data... ", dwi_source, dwi_vox);
+    else {
+      Image::Adapter::Extract1D<SourceBufferType::voxel_type> dwi_only (dwi_source, 3, container_cast< std::vector<int> > (shared.dwis));
+      Image::threaded_copy_with_progress_message ("loading DWI data... ", dwi_only, dwi_vox);
+    }
+  }
 
 
-  Processor processor (shared).run();
-  */
+  // launch the processing:
+  if (mask_vox) 
+    Image::ThreadedLoop (std::string("performing ") + (shared.is_multi_shell() ? "multi-shell " : "") + (shared.ncoefs.size() > 1 ? "multi-tissue " : "" ) + "CSD...", 
+        dwi_vox, 1, 0, 3).run (DWI::MTCSD::Processor<compute_type> (shared), dwi_vox, fod_vox, *mask_vox);
+  else
+    Image::ThreadedLoop (std::string("performing ") + (shared.is_multi_shell() ? "multi-shell " : "") + (shared.ncoefs.size() > 1 ? "multi-tissue " : "" ) + "CSD...", 
+        dwi_vox, 1, 0, 3).run (DWI::MTCSD::Processor<compute_type> (shared), dwi_vox, fod_vox);
 }
 
