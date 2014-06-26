ceb2549790fe505de23247474372cc86bf075b25
Dave Raffelt <draffelt@gmail.com>
2014-06-26 16:07:28 +1000

    removed C input option to tfce

--- src/stats/tfce.h
+++ src/stats/tfce.h
@@ -121,9 +121,9 @@ namespace MR
                         value_type dh, value_type E, value_type H) :
                           connectivity_map (connectivity_map), dh (dh), E (E), H (H) { }
 
-          // TODO remove tfce_C for tckpermute after cfe_roc experiments
+
           value_type operator() (const value_type max_stat, const std::vector<value_type>& stats,
-                                 std::vector<value_type>* get_tfce_stats, value_type tfce_C = 1.0)
+                                 std::vector<value_type>* get_tfce_stats)
           {
             tfce_stats.resize (stats.size());
             std::fill (tfce_stats.begin(), tfce_stats.end(), 0.0);
@@ -135,8 +135,8 @@ namespace MR
                 value_type extent = 0.0;
                 for (connected_fixel = connectivity_map[fixel].begin(); connected_fixel != connectivity_map[fixel].end(); ++connected_fixel)
                   if (stats[connected_fixel->first] > h)
-                    extent += Math::pow (connected_fixel->second.value, tfce_C);
-                tfce_stats[fixel] += pow (extent, E) * pow (h, H);
+                    extent += connected_fixel->second.value;
+                tfce_stats[fixel] += Math::pow (extent, E) * Math::pow (h, H);
               }
               if (tfce_stats[fixel] > max_tfce_stat)
                 max_tfce_stat = tfce_stats[fixel];
