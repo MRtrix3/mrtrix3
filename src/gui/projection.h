e9dd500ad6077ba536f1cecc98d1dd5ff516952b
J-Donald Tournier <jdtournier@gmail.com>
2014-07-07 20:53:15 +0100

    MRView: Disable HiDPI rendering if Qt version < 5.1

--- src/gui/projection.h
+++ src/gui/projection.h
@@ -35,8 +35,12 @@ namespace MR
         }
 
         void set_viewport (const QWidget& frame) const {
+#if QT_VERSION >= 0x050100
           int m = frame.windowHandle()->devicePixelRatio();
           gl::Viewport (m*viewport[0], m*viewport[1], m*viewport[2], m*viewport[3]);
+#else
+          gl::Viewport (viewport[0], viewport[1], viewport[2], viewport[3]);
+#endif
         }
 
 
