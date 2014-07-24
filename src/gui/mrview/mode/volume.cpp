c2d5b85e026c9be962962def4fc80283c161ccf9
J-Donald Tournier <jdtournier@gmail.com>
2014-07-24 17:10:52 +0300

    MRView: fix for volume render mode on Retina displays

--- src/gui/mrview/mode/volume.cpp
+++ src/gui/mrview/mode/volume.cpp
@@ -447,7 +447,12 @@ namespace MR
             depth_texture.bind();
 
           gl::ReadBuffer (gl::BACK);
+#if QT_VERSION >= 0x050100
+          int m = window.windowHandle()->devicePixelRatio();
+          gl::CopyTexImage2D (gl::TEXTURE_2D, 0, gl::DEPTH_COMPONENT, 0, 0, m*projection.width(), m*projection.height(), 0);
+#else
           gl::CopyTexImage2D (gl::TEXTURE_2D, 0, gl::DEPTH_COMPONENT, 0, 0, projection.width(), projection.height(), 0);
+#endif
 
           gl::Uniform1i (gl::GetUniformLocation (volume_shader, "depth_sampler"), 1);
 
