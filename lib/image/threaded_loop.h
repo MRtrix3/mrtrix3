MRtrix3/mrtrix3

commit ac9883f9c96374efb1de759be25f90c019f183db

Image::Loop: reinstate previous start() & next() methods
These had previously been modified to take tuples of voxels. This has
been reverted to taking individual voxels as variadic template
arguments, which replicates the previous syntax. This can be used
alongside the preferred LoopIter syntax.

lib/image/threaded_loop.h
@@ -340,7 +340,7 @@ namespace MR
         const Iterator& iterator () const { return dummy; }
 
         void start (Iterator& pos) {
-          loop.start (std::forward_as_tuple (pos));
+          loop.start (pos);
         }
 
         //! get next position in the outer loop
@@ -348,7 +348,7 @@ namespace MR
           Thread::Mutex::Lock lock (mutex);
           if (loop.ok()) {
             loop.set_position (dummy, pos);
-            loop.next (std::forward_as_tuple (dummy));
+            loop.next (dummy);
             return true;
           }
           else return false;
@@ -356,7 +356,7 @@ namespace MR
 
         //! invoke \a functor (const Iterator& pos) per voxel <em> in the outer axes only</em>
         template <class Functor> 
-          void run_outer (Functor&& functor, const std::string& thread_label = "unknown") 
+          void run_outer (Functor& functor, const std::string& thread_label = "unknown") 
           {
             if (Thread::number_of_threads() == 0) {
               for (auto i = loop (dummy); i; ++i)
@@ -369,11 +369,16 @@ namespace MR
             Thread::Exec threads (thread_list, thread_label);
           }
 
+        template <class Functor> 
+          void run_outer (Functor&& functor, const std::string& thread_label = "unknown") {
+            run_outer (functor);
+          }
+
 
 
 
         template <class Functor, class... VoxelType, typename std::enable_if<sizeof...(VoxelType) == 0, int>::type = 0> 
-          void run (Functor&& functor, VoxelType&&... vox)
+          void run (Functor& functor, VoxelType&... vox)
           {
             if (Thread::number_of_threads() == 0) {
               LoopInOrder inner_loop (axes);
@@ -390,7 +395,7 @@ namespace MR
 
 
         template <class Functor, class... VoxelType, typename std::enable_if<sizeof...(VoxelType) != 0, int>::type = 0> 
-          void run (Functor&& functor, VoxelType&&... vox)
+          void run (Functor& functor, VoxelType&... vox)
           {
             if (Thread::number_of_threads() == 0) {
               LoopInOrder inner_loop (axes);
@@ -404,6 +409,11 @@ namespace MR
             __RunFunctor<Functor, VoxelType...> 
               loop_thread (*this, functor, vox...);
             run_outer (loop_thread, "run thread");
+          }
+
+        template <class Functor, class... VoxelType> 
+          void run (Functor&& functor, VoxelType&&... vox) {
+            run (functor, vox...);
           }
 
 

cmd/sh2peaks.cpp
@@ -120,17 +120,17 @@ class DataLoader
       if (mask_data) {
         Image::check_dimensions (*mask_data, sh, 0, 3);
         mask = new Image::Buffer<bool>::voxel_type (*mask_data);
-        loop.start (std::forward_as_tuple (*mask, sh));
+        loop.start (*mask, sh);
       }
       else
-        loop.start (std::forward_as_tuple (sh));
+        loop.start (sh);
       }
 
     bool operator() (Item& item) {
       if (loop.ok()) {
         if (mask) {
           while (!mask->value()) {
-            loop.next (std::forward_as_tuple (*mask, sh));
+            loop.next (*mask, sh);
             if (!loop.ok())
               return false;
           }
@@ -147,9 +147,9 @@ class DataLoader
           item.data[sh[3]] = sh.value();
 
         if (mask)
-          loop.next (std::forward_as_tuple (*mask, sh));
+          loop.next (*mask, sh);
         else
-          loop.next (std::forward_as_tuple (sh));
+          loop.next (sh);
 
         return true;
       }

lib/image/loop.h
@@ -42,7 +42,7 @@ namespace MR
         public:
           LoopIter (const LoopType& loop_type, VoxelType&... voxels) : 
             loop (loop_type), vox (voxels...) { 
-              loop.start (std::forward<std::tuple<VoxelType&...>> (vox)); 
+              unpack ([this](VoxelType&... v) { loop.start (v...); }, vox); 
             }
           LoopIter (LoopIter&&) = default;
 
@@ -51,8 +51,8 @@ namespace MR
           LoopIter& operator=(const LoopIter&) = delete;
 
           operator bool() const { return loop.ok(); }
-          void operator++ () { loop.next (std::forward<std::tuple<VoxelType&...>> (vox)); }
-          void operator++ (int) { loop.next (std::forward<std::tuple<VoxelType&...>> (vox)); }
+          void operator++ () { unpack ([this](VoxelType&... v) { loop.next (v...); }, vox); }
+          void operator++ (int) { unpack ([this](VoxelType&... v) { loop.next (v...); }, vox); }
         private:
           LoopType loop;
           std::tuple<VoxelType&...> vox;
@@ -212,14 +212,15 @@ namespace MR
            * constructor will have their coordinates set to zero; the coordinates
            * of all other axes will be left untouched. */
           template <typename... VoxelType>
-            void start (std::tuple<VoxelType&...>&& vox) {
+            void start (VoxelType&... vox) {
               cont_ = true;
-              max_axis_ = std::min (std::get<0> (vox).ndim(), to_);
+              auto& ref = std::get<0> (std::tie (vox...));
+              max_axis_ = std::min (ref.ndim(), to_);
               for (size_t n = from_; n < max_axis_; ++n) 
-                apply (set_pos (n, 0), std::forward<std::tuple<VoxelType&...>> (vox));
+                apply (set_pos (n, 0), std::tie (vox...));
 
               if (progress_)
-                progress_.set_max (voxel_count (std::get<0> (vox), from_, to_));
+                progress_.set_max (voxel_count (ref, from_, to_));
             }
 
           //! Check whether the loop should continue iterating
@@ -232,15 +233,10 @@ namespace MR
           /*! Advance coordinates of all specified VoxelType objects to the next position
            * to be processed, and update the progress status if appropriate. */
           template <typename... VoxelType>
-            void next (std::tuple<VoxelType&...>& vox) {
-              next_axis (from_, vox);
+            void next (VoxelType&... vox) {
+              auto tvox = std::tie (vox...);
+              next_axis (from_, tvox);
               ++progress_;
-            }
-
-          //! \copydoc Loop::next()
-          template <typename... VoxelType>
-            void next (std::tuple<VoxelType&...>&& vox) {
-              next (vox);
             }
 
           //! set position along relevant axes of \a target to that of \a reference
@@ -249,7 +245,8 @@ namespace MR
            * unchanged. */
           template <typename RefVoxelType, typename... VoxelType>
             void set_position (const RefVoxelType& reference, VoxelType&... target) const {
-              set_position (reference, std::tuple<VoxelType&...> (target...));
+              auto t = std::tie (target...);
+              set_position (reference, t);
             }
 
           //! set position along relevant axes of \a target to that of \a reference
@@ -281,7 +278,8 @@ namespace MR
                   }
                   else {
                     next_axis (axis+1, vox);
-                    if (cont_) apply (set_pos (axis, 0), vox);
+                    if (cont_) 
+                      apply (set_pos (axis, 0), vox);
                   }
                 }
               }
@@ -469,19 +467,14 @@ namespace MR
            * constructor will have their coordinates set to zero; the coordinates
            * of all other axes will be left untouched. */
           template <typename... VoxelType>
-            void start (std::tuple<VoxelType&...>& vox) {
+            void start (VoxelType&... vox) {
               cont_ = true;
-              first_axis_dim = std::get<0>(vox).dim (first_axis) - 1;
+              auto& ref = std::get<0> (std::tie (vox...));
+              first_axis_dim = ref.dim (first_axis) - 1;
               for (size_t n = 0; n < axes_.size(); ++n) 
-                apply (set_pos (axes_[n], 0), vox);
+                apply (set_pos (axes_[n], 0), std::tie (vox...));
               if (progress_)
-                progress_.set_max (voxel_count (std::get<0> (vox), axes_));
-            }
-
-          //! \copydoc LoopInOrder::start()
-          template <typename... VoxelType>
-            void start (std::tuple<VoxelType&...>&& vox) {
-              start (vox);
+                progress_.set_max (voxel_count (ref, axes_));
             }
 
           //! Check whether the loop should continue iterating
@@ -494,20 +487,17 @@ namespace MR
           /*! Advance coordinates of all specified VoxelType objects to the next position
            * to be processed, and update the progress status if appropriate. */
           template <typename... VoxelType>
-            void next (std::tuple<VoxelType&...>& vox) {
-              if (std::get<0>(vox)[first_axis] < first_axis_dim)
-                apply (inc_pos (first_axis), vox);
+            void next (VoxelType&... vox) {
+              auto tvox = std::tie (vox...);
+              auto& ref = std::get<0> (tvox);
+              if (ref[first_axis] < first_axis_dim)
+                apply (inc_pos (first_axis), tvox);
               else {
-                next_axis (1, vox);
-                if (cont_) apply (set_pos (first_axis, 0), vox);
+                next_axis (1, tvox);
+                if (cont_) 
+                  apply (set_pos (first_axis, 0), tvox);
               }
               ++progress_;
-            }
-
-          //! \copydoc LoopInOrder::next()
-          template <typename... VoxelType>
-            void next (std::tuple<VoxelType&...>&& vox) {
-              next (vox);
             }
 
           //! set position along relevant axes of \a target to that of \a reference
@@ -516,7 +506,8 @@ namespace MR
            * unchanged. */
           template <typename RefVoxelType, typename... VoxelType>
             void set_position (const RefVoxelType& reference, VoxelType&... target) const {
-              set_position (reference, std::tuple<VoxelType&...> (target...));
+              auto t = std::tie (target...);
+              set_position (reference, t);
             }
 
           //! set position along relevant axes of \a target to that of \a reference
@@ -527,11 +518,6 @@ namespace MR
             void set_position (const RefVoxelType& reference, std::tuple<VoxelType&...>& target) const {
               for (size_t i = 0; i < axes_.size(); ++i) 
                 apply (set_pos (axes_[i], reference[axes_[i]]), target);
-            }
-
-          template <typename RefVoxelType, typename... VoxelType>
-            void set_position (const RefVoxelType& reference, std::tuple<VoxelType&...>&& target) const {
-              set_position (reference, target);
             }
 
 

src/dwi/fmls.h
@@ -201,7 +201,7 @@ class FODQueueWriter
       FODQueueWriter (FODVoxelOrBufferType& fod) :
         fod_vox (fod),
         loop ("Segmenting FODs...", 0, 3) {
-          loop.start (std::forward_as_tuple (fod_vox));
+          loop.start (fod_vox);
         }
 
     template <class FODVoxelOrBufferType, class MaskVoxelOrBufferType>
@@ -210,7 +210,7 @@ class FODQueueWriter
         loop ("Segmenting FODs...", 0, 3),
         mask_vox_ptr (new MaskVoxelType (mask))
     {
-      loop.start (std::forward_as_tuple (fod_vox));
+      loop.start (fod_vox);
     }
 
 
@@ -228,14 +228,14 @@ class FODQueueWriter
         do {
           Image::voxel_assign (*mask_vox_ptr, fod_vox, 0, 3);
           if (!mask_vox_ptr->value())
-            loop.next (std::forward_as_tuple (fod_vox));
+            loop.next (fod_vox);
         } while (loop.ok() && !mask_vox_ptr->value());
       }
       out.vox[0] = fod_vox[0]; out.vox[1] = fod_vox[1]; out.vox[2] = fod_vox[2];
       out.allocate (fod_vox.dim (3));
       for (fod_vox[3] = 0; fod_vox[3] != fod_vox.dim (3); ++fod_vox[3])
         out[fod_vox[3]] = fod_vox.value();
-      loop.next (std::forward_as_tuple (fod_vox));
+      loop.next (fod_vox);
       return true;
     }
 

commit 0d9494866b731a02aacc0fc5f7f700587b747c44

Fix error introduced in previous commit ac9883f

lib/image/threaded_loop.h
@@ -372,7 +372,7 @@ namespace MR
 
         //! invoke \a functor (const Iterator& pos) per voxel <em> in the outer axes only</em>
         template <class Functor> 
-          void run_outer (Functor& functor, const std::string& thread_label = "unknown") 
+          void run_outer (Functor&& functor, const std::string& thread_label = "unknown") 
           {
             if (Thread::number_of_threads() == 0) {
               for (auto i = loop (dummy); i; ++i)
@@ -385,16 +385,11 @@ namespace MR
             Thread::Exec threads (thread_list, thread_label);
           }
 
-        template <class Functor> 
-          void run_outer (Functor&& functor, const std::string& thread_label = "unknown") {
-            run_outer (functor);
-          }
-
 
 
 
         template <class Functor, class... VoxelType, typename std::enable_if<sizeof...(VoxelType) == 0, int>::type = 0> 
-          void run (Functor& functor, VoxelType&... vox)
+          void run (Functor&& functor, VoxelType&&... vox)
           {
             if (Thread::number_of_threads() == 0) {
               LoopInOrder inner_loop (axes);
@@ -411,7 +406,7 @@ namespace MR
 
 
         template <class Functor, class... VoxelType, typename std::enable_if<sizeof...(VoxelType) != 0, int>::type = 0> 
-          void run (Functor& functor, VoxelType&... vox)
+          void run (Functor&& functor, VoxelType&&... vox)
           {
             if (Thread::number_of_threads() == 0) {
               LoopInOrder inner_loop (axes);
@@ -425,11 +420,6 @@ namespace MR
             __RunFunctor<Functor, VoxelType...> 
               loop_thread (*this, functor, vox...);
             run_outer (loop_thread, "run thread");
-          }
-
-        template <class Functor, class... VoxelType> 
-          void run (Functor&& functor, VoxelType&&... vox) {
-            run (functor, vox...);
           }
 
 