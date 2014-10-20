1ec0817314bae2e7e297678261c6da3a569a34a7
J-Donald Tournier <jdtournier@gmail.com>
2014-10-20 11:48:38 +0100

    remove __PRETTY_FUNCTION__ from debug output
    If anything this only made the output less pretty...

--- lib/debug.h
+++ lib/debug.h
@@ -35,18 +35,18 @@
 
 /** \brief Prints the current function, file and line number. */
 #define TRACE \
-  std::cerr << MR::App::NAME << ": " << __PRETTY_FUNCTION__ << " (" << __FILE__ << ": " << __LINE__ << ")\n"
+  std::cerr << MR::App::NAME << ": at " << __FILE__ << ": " << __LINE__ << "\n";
 
 
 /** \brief Prints a variable name and its value, followed by the function, file and line number. */
 #define VAR(variable) \
-  std::cerr << MR::App::NAME << ": " << #variable << " = " << (variable) \
-  << " (" << __PRETTY_FUNCTION__ << "; " << __FILE__  << ": " << __LINE__ << ")\n"
+  std::cerr << MR::App::NAME << " [" << __FILE__  << ": " << __LINE__ << "]: " << #variable << " = " << (variable) << "\n";
+
 
 /** \brief Stops execution and prints current function, file and line number. 
   Remuses on user input (i.e. Return key). */
 #define PAUSE { \
-  std::cerr << MR::App::NAME << ": paused, " << __PRETTY_FUNCTION__ << " (" << __FILE__ << ": " << __LINE__ << ")...\n"; \
+  std::cerr << MR::App::NAME << " [" << __FILE__  << ": " << __LINE__ << "]: paused (press any key to resume)\n"; \
   std::string __n__; std::getline (std::cin, __n__); \
 }
 
