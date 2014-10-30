8083914913cebd3b898d00eea6c08eb34f21c095
J-Donald Tournier <jdtournier@gmail.com>
2014-10-30 10:36:13 +0000

    fix Image::copy() functions to compile clean on Ubuntu 14.04

--- lib/image/copy.h
+++ lib/image/copy.h
@@ -32,30 +32,31 @@ namespace MR
   {
 
     template <class InputVoxelType, class OutputVoxelType>
-    void copy (InputVoxelType& source, OutputVoxelType& destination, size_t from_axis = 0, size_t to_axis = std::numeric_limits<size_t>::max())
-    {
-      LoopInOrder loop (source, from_axis, to_axis);
-      for (auto i = loop (source, destination); i; ++i)
-        destination.value() = source.value();
-    }
+      void copy (InputVoxelType&& source, OutputVoxelType&& destination, size_t from_axis = 0, size_t to_axis = std::numeric_limits<size_t>::max())
+      {
+        LoopInOrder loop (source, from_axis, to_axis);
+        for (auto i = loop (source, destination); i; ++i)
+          destination.value() = source.value();
+      }
 
 
 
     template <class InputVoxelType, class OutputVoxelType>
-    void copy_with_progress (InputVoxelType& source, OutputVoxelType& destination, size_t from_axis = 0, size_t to_axis = std::numeric_limits<size_t>::max())
-    {
-      copy_with_progress_message ("copying from \"" + shorten (source.name()) + "\" to \"" + shorten (destination.name()) + "\"...",
-         source, destination, from_axis, to_axis);
-    }
+      void copy_with_progress (InputVoxelType&& source, OutputVoxelType&& destination, size_t from_axis = 0, size_t to_axis = std::numeric_limits<size_t>::max())
+      {
+        copy_with_progress_message ("copying from \"" + shorten (source.name()) + "\" to \"" + shorten (destination.name()) + "\"...",
+            source, destination, from_axis, to_axis);
+      }
 
 
     template <class InputVoxelType, class OutputVoxelType>
-    void copy_with_progress_message (const std::string& message, InputVoxelType& source, OutputVoxelType& destination, size_t from_axis = 0, size_t to_axis = std::numeric_limits<size_t>::max())
-    {
-      LoopInOrder loop (source, message, from_axis, to_axis);
-      for (auto i = loop (source, destination); i; ++i)
-        destination.value() = source.value();
-    }
+      void copy_with_progress_message (const std::string& message, InputVoxelType&& source, OutputVoxelType&& destination, size_t from_axis = 0, size_t to_axis = std::numeric_limits<size_t>::max())
+      {
+        LoopInOrder loop (source, message, from_axis, to_axis);
+        for (auto i = loop (source, destination); i; ++i)
+          destination.value() = source.value();
+      }
+
 
   }
 }
