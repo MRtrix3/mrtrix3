2dad787279951f288ecec758497224e22aac0ecf
Robert Smith <r.smith@brain.org.au>
2015-02-03 17:11:13 +1100

    Changes to script writing interface
    Neatened some of the parameter handling in the provided Python scripts; should make them slightly easier to read, and make new scripts easier to write.

--- scripts/lib/printMessage.py
+++ scripts/lib/printMessage.py
@@ -1,5 +1,6 @@
 def printMessage(message):
-  import os, sys
-  sys.stdout.write(os.path.basename(sys.argv[0]) + ': ' + message + '\n')
-  sys.stdout.flush()
+  import app, os, sys
+  if app.verbosity:
+    sys.stdout.write(os.path.basename(sys.argv[0]) + ': ' + message + '\n')
+    sys.stdout.flush()
   
