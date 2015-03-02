095c3e6019021ccb0d531820286964df18c7de4f
Dave Raffelt <draffelt@gmail.com>
2015-03-02 13:32:13 +1100

    Tidy up before making CFE public

--- scripts/compute_IQR.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import numpy
-from sys import argv
-
-aucs = numpy.loadtxt(argv[1])
-output_file = open(argv[3], 'w')
-output_file.write("%f\n" % numpy.percentile(aucs, float(argv[2]))
-output_file.close()
