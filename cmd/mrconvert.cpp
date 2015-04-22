319b0ef51fceef6210af6bf98d983852ce8b8673
J-Donald Tournier <jdtournier@gmail.com>
2015-04-22 11:06:59 +0100

    revert mrconvert to master version to ease later merges

--- cmd/mrconvert.cpp
+++ cmd/mrconvert.cpp
@@ -22,10 +22,13 @@
 
 #include "command.h"
 #include "progressbar.h"
-#include "image.h"
-#include "algo/threaded_copy.h"
-#include "adapter/extract.h"
-#include "adapter/permute_axes.h"
+#include "image/buffer.h"
+#include "image/voxel.h"
+#include "image/axis.h"
+#include "image/threaded_copy.h"
+#include "image/adapter/extract.h"
+#include "image/adapter/permute_axes.h"
+#include "image/stride.h"
 #include "dwi/gradient.h"
 
 
@@ -69,7 +72,7 @@ void usage ()
             "-1 at the corresponding position in the list.")
   + Argument ("axes").type_sequence_int()
 
-  + Stride::Options
+  + Image::Stride::StrideOption
 
   + DataType::options()
 
@@ -84,16 +87,16 @@ typedef cfloat complex_type;
 
 
 
-template <class HeaderType>
-inline std::vector<int> set_header (Header& header, const HeaderType& input)
+template <class InfoType>
+inline std::vector<int> set_header (Image::Header& header, const InfoType& input)
 {
-  header = input.header();
+  header.info() = input.info();
 
   header.intensity_offset() = 0.0;
   header.intensity_scale() = 1.0;
 
   if (get_options ("grad").size() || get_options ("fslgrad").size())
-    header.set_DW_scheme (DWI::get_DW_scheme<default_type> (header));
+    header.DW_scheme() = DWI::get_DW_scheme<float> (header);
 
   Options opt = get_options ("axes");
   std::vector<int> axes;
@@ -103,22 +106,22 @@ inline std::vector<int> set_header (Header& header, const HeaderType& input)
     for (size_t i = 0; i < axes.size(); ++i) {
       if (axes[i] >= static_cast<int> (input.ndim()))
         throw Exception ("axis supplied to option -axes is out of bounds");
-      header.size(i) = axes[i] < 0 ? 1 : input.size (axes[i]);
+      header.dim(i) = axes[i] < 0 ? 1 : input.dim (axes[i]);
     }
   }
 
   opt = get_options ("vox");
   if (opt.size()) {
-    std::vector<default_type> vox = opt[0][0];
+    std::vector<float> vox = opt[0][0];
     if (vox.size() > header.ndim())
       throw Exception ("too many axes supplied to -vox option");
     for (size_t n = 0; n < vox.size(); ++n) {
       if (std::isfinite (vox[n]))
-        header.voxsize(n) = vox[n];
+        header.vox(n) = vox[n];
     }
   }
 
-  Stride::set_from_command_line (header);
+  Image::Stride::set_from_command_line (header);
 
   return axes;
 }
@@ -126,21 +129,23 @@ inline std::vector<int> set_header (Header& header, const HeaderType& input)
 
 
 
-  template <class InputImageType>
-inline void copy_permute (InputImageType& in, Header& header, const std::string& output_filename)
+  template <class InputVoxelType>
+inline void copy_permute (InputVoxelType& in, Image::Header& header_out, const std::string& output_filename)
 {
-  DataType datatype = header.datatype();
-  std::vector<int> axes = set_header (header, in);
-  header.datatype() = datatype;
-  auto out = header.create (output_filename).get_image<complex_type>();
-  DWI::export_grad_commandline (out.header());
+  DataType datatype = header_out.datatype();
+  std::vector<int> axes = set_header (header_out, in);
+  header_out.datatype() = datatype;
+  Image::Buffer<complex_type> buffer_out (output_filename, header_out);
+  DWI::export_grad_commandline (buffer_out);
+
+  auto out = buffer_out.voxel();
 
   if (axes.size()) {
-    Adapter::PermuteAxes<InputImageType> perm (in, axes);
-    threaded_copy_with_progress (perm, out, 2);
+    Image::Adapter::PermuteAxes<InputVoxelType> perm (in, axes);
+    Image::threaded_copy_with_progress (perm, out, 2);
   }
   else 
-    threaded_copy_with_progress (in, out, 2);
+    Image::threaded_copy_with_progress (in, out, 2);
 }
 
 
@@ -154,51 +159,54 @@ inline void copy_permute (InputImageType& in, Header& header, const std::string&
 
 void run ()
 {
-  auto in = Header::open (argument[0]).get_image<complex_type>();
+  Image::Header header_in (argument[0]);
 
-  Header header (in.header());
-  header.datatype() = DataType::from_command_line (header.datatype());
+  Image::Buffer<complex_type> buffer_in (header_in);
+  auto in = buffer_in.voxel();
 
-  if (in.datatype().is_complex() && !header.datatype().is_complex())
+  Image::Header header_out (header_in);
+  header_out.datatype() = DataType::from_command_line (header_out.datatype());
+
+  if (header_in.datatype().is_complex() && !header_out.datatype().is_complex())
     WARN ("requested datatype is real but input datatype is complex - imaginary component will be ignored");
 
 
   Options opt = get_options ("coord");
   if (opt.size()) {
-    std::vector<std::vector<int> > pos (in.ndim());
+    std::vector<std::vector<int> > pos (buffer_in.ndim());
     for (size_t n = 0; n < opt.size(); n++) {
       int axis = opt[n][0];
       if (pos[axis].size())
         throw Exception ("\"coord\" option specified twice for axis " + str (axis));
-      pos[axis] = parse_ints (opt[n][1], in.size(axis)-1);
-      if (axis == 3 && in.header().keyval().find("dw_scheme") != in.header().keyval().end()) {
-        auto grad = in.header().parse_DW_scheme<default_type>();
-        if ((int)grad.rows() != in.size(3)) {
+      pos[axis] = parse_ints (opt[n][1], buffer_in.dim(axis)-1);
+      if (axis == 3 && header_in.DW_scheme().is_set()) {
+        Math::Matrix<float>& grad (header_in.DW_scheme());
+        if ((int)grad.rows() != header_in.dim(3)) {
           WARN ("Diffusion encoding of input file does not match number of image volumes; omitting gradient information from output image");
-          header.keyval().erase ("dw_scheme");
+          header_out.DW_scheme().clear();
         }
         else {
-          Math::Matrix<default_type> extract_grad (pos[3].size(), 4);
+          Math::Matrix<float> extract_grad (pos[3].size(), 4);
           for (size_t dir = 0; dir != pos[3].size(); ++dir)
             extract_grad.row(dir) = grad.row((pos[3])[dir]);
-          header.set_DW_scheme (extract_grad);
+          header_out.DW_scheme() = extract_grad;
         }
       }
     }
 
-    for (size_t n = 0; n < in.ndim(); ++n) {
+    for (size_t n = 0; n < buffer_in.ndim(); ++n) {
       if (pos[n].empty()) {
-        pos[n].resize (in.size (n));
+        pos[n].resize (buffer_in.dim (n));
         for (size_t i = 0; i < pos[n].size(); i++)
           pos[n][i] = i;
       }
     }
 
-    Adapter::Extract<decltype(in)> extract (in, pos);
-    copy_permute (extract, header, argument[1]);
+    Image::Adapter::Extract<decltype(in)> extract (in, pos);
+    copy_permute (extract, header_out, argument[1]);
   }
   else
-    copy_permute (in, header, argument[1]);
+    copy_permute (in, header_out, argument[1]);
 
 }
 
