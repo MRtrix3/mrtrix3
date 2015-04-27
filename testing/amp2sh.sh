4d834bc77e29a62e3556682fb04f6fe1b8ed0d10
J-Donald Tournier <jdtournier@gmail.com>
2015-04-27 19:40:09 +0100

    remove testing-related stuff, move to dedicated repo

--- testing/amp2sh.sh
+++
@@ -1,1 +0,0 @@
-amp2sh dwi.mif - | testing_diff_data - amp2sh/out.mif 1e-15
--- testing/dirgen.sh
+++
@@ -1,3 +0,0 @@
-MRTRIX_RNG_SEED=1 dirgen 20 tmp.txt -force && diff tmp.txt dirgen/dir20.txt
-MRTRIX_RNG_SEED=7 dirgen 22 -unipolar tmp.txt -force && diff tmp.txt dirgen/dir22_unipolar.txt
-MRTRIX_RNG_SEED=12 dirgen 25 -cartesian tmp.txt -force && diff tmp.txt dirgen/dir25_cartesian.txt
