MRtrix3/mrtrix3

commit 4d041e1a0cc92318c291c6df35fde70de7dfa32f

MRView: new static Window::main pointer to main window
This should simplify things like make the GL context current in deep
member classes where necessary. `Mode::Base` & `Tool::Base` have
`.window()` convenience methods to access the main window by reference
(equivalent to `*Window::main`).

src/gui/mrview/tool/tractography/tractogram.cpp
@@ -316,7 +316,7 @@ namespace MR
 
 
 
-        Tractogram::Tractogram (Window& window, Tractography& tool, const std::string& filename) :
+        Tractogram::Tractogram (Tractography& tool, const std::string& filename) :
             Displayable (filename),
             scalarfile_by_direction (false),
             show_colour_bar (true),

src/gui/mrview/displayable.cpp
@@ -35,7 +35,7 @@ namespace MR
     {
 
       Displayable::Displayable (const std::string& filename) :
-        QAction (NULL),
+        QAction (nullptr),
         lessthan (NAN),
         greaterthan (NAN),
         display_midpoint (NAN),
@@ -55,39 +55,21 @@ namespace MR
         }
 
 
-      Displayable::Displayable (Window& window, const std::string& filename) :
-        QAction (shorten (filename, 20, 0).c_str(), &window),
-        lessthan (NAN),
-        greaterthan (NAN),
-        display_midpoint (NAN),
-        display_range (NAN),
-        transparent_intensity (NAN),
-        opaque_intensity (NAN),
-        alpha (NAN),
-        colourmap (0), 
-        show (true),
-        show_colour_bar (true),
-        filename (filename),
-        value_min (NAN),
-        value_max (NAN),
-        flags_ (0x00000000) {
-          connect (this, SIGNAL(scalingChanged()), &window, SLOT(on_scaling_changed()));
-          colour[0] = colour[1] = 255;
-          colour[2] = 0;
-      }
-
 
       Displayable::~Displayable ()
       {
       }
 
 
-      bool Displayable::Shader::need_update (const Displayable& object) const { 
+
+      bool Displayable::Shader::need_update (const Displayable& object) const 
+      { 
         return flags != object.flags() || colourmap != object.colourmap;
       }
 
 
-      void Displayable::Shader::update (const Displayable& object) {
+      void Displayable::Shader::update (const Displayable& object) 
+      {
         flags = object.flags();
         colourmap = object.colourmap;
       }

src/gui/mrview/displayable.h
@@ -70,7 +70,6 @@ namespace MR
 
         public:
           Displayable (const std::string& filename);
-          Displayable (Window& window, const std::string& filename);
 
           virtual ~Displayable ();
 

src/gui/mrview/image.cpp
@@ -44,19 +44,6 @@ namespace MR
         position[0] = position[1] = position[2] = std::numeric_limits<ssize_t>::min();
       }
 
-      ImageBase::ImageBase (Window& window, const MR::Image::Info& image_info) :
-        Volume (window, image_info),
-        position (image_info.ndim())
-      {
-        position[0] = position[1] = position[2] = std::numeric_limits<ssize_t>::min();
-        setCheckable (true);
-        setToolTip (info().name().c_str());
-        setStatusTip (info().name().c_str());
-        window.image_group->addAction (this);
-        window.image_menu->addAction (this);
-        connect (this, SIGNAL(scalingChanged()), &window, SLOT(on_scaling_changed()));
-      }
-
 
 
 
@@ -131,14 +118,6 @@ namespace MR
 
       Image::Image (const MR::Image::Header& image_header) :
         ImageBase (image_header),
-        buffer (image_header),
-        interp (buffer)
-      {
-        set_colourmap (guess_colourmap ());
-      }
-
-      Image::Image (Window& window, const MR::Image::Header& image_header) :
-        ImageBase (window, image_header),
         buffer (image_header),
         interp (buffer)
       {

src/gui/mrview/image.h
@@ -49,7 +49,6 @@ namespace MR
       {
         public:
           ImageBase (const MR::Image::Info&);
-          ImageBase (Window&, const MR::Image::Info&);
 
           void render2D (Displayable::Shader& shader_program, const Projection& projection, const int plane, const int slice);
           void render3D (Displayable::Shader& shader_program, const Projection& projection, const float depth);
@@ -69,7 +68,6 @@ namespace MR
       {
         public:
           Image (const MR::Image::Header& image_header);
-          Image (Window& parent, const MR::Image::Header& image_header);
 
           MR::Image::Header& header () { return buffer; }
           const MR::Image::Header& header () const { return buffer; }

src/gui/mrview/mode/base.cpp
@@ -33,9 +33,8 @@ namespace MR
       namespace Mode
       {
 
-        Base::Base (Window& parent, int flags) :
-          window (parent),
-          projection (window.glarea, window.font),
+        Base::Base (int flags) :
+          projection (window().glarea, window().font),
           features (flags),
           update_overlays (false),
           visible (true) { }
@@ -52,7 +51,7 @@ namespace MR
         {
           GL_CHECK_ERROR;
 
-          projection.set_viewport (window, 0, 0, width(), height());
+          projection.set_viewport (window(), 0, 0, width(), height());
 
           GL_CHECK_ERROR;
           gl::Clear (gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
@@ -76,7 +75,7 @@ namespace MR
             GL_CHECK_ERROR;
 
             projection.setup_render_text();
-            if (window.show_voxel_info()) {
+            if (window().show_voxel_info()) {
               Point<> voxel (image()->interp.scanner2voxel (focus()));
               Image::VoxelType& imvox (image()->voxel());
               ssize_t vox [] = { ssize_t(std::round (voxel[0])), ssize_t(std::round (voxel[1])), ssize_t(std::round (voxel[2])) };
@@ -90,15 +89,15 @@ namespace MR
               projection.render_text (vox_str, LeftEdge | BottomEdge, 1);
               std::string value_str = "value: ";
               cfloat value = image()->interpolate() ?
-                image()->trilinear_value(window.focus()) :
-                image()->nearest_neighbour_value(window.focus());
+                image()->trilinear_value(window().focus()) :
+                image()->nearest_neighbour_value(window().focus());
               if(std::isnan(std::abs(value)))
                 value_str += "?";
               else value_str += str(value);
               projection.render_text (value_str, LeftEdge | BottomEdge, 2);
 
               // Draw additional labels from tools
-              QList<QAction*> tools = window.tools()->actions();
+              QList<QAction*> tools = window().tools()->actions();
               for (size_t i = 0, line_num = 3, N = tools.size(); i < N; ++i) {
                 Tool::Dock* dock = dynamic_cast<Tool::__Action__*>(tools[i])->dock;
                 if (dock)
@@ -107,7 +106,7 @@ namespace MR
             }
             GL_CHECK_ERROR;
 
-            if (window.show_comments()) {
+            if (window().show_comments()) {
               for (size_t i = 0; i < image()->header().comments().size(); ++i)
                 projection.render_text (image()->header().comments() [i], LeftEdge | TopEdge, i);
             }
@@ -115,15 +114,15 @@ namespace MR
             projection.done_render_text();
 
             GL_CHECK_ERROR;
-            if (window.show_colourbar()) {
+            if (window().show_colourbar()) {
 
-              auto &colourbar_renderer = window.colourbar_renderer;
+              auto &colourbar_renderer = window().colourbar_renderer;
 
-              colourbar_renderer.begin_render_colourbars (&projection, window.colourbar_position, 1);
+              colourbar_renderer.begin_render_colourbars (&projection, window().colourbar_position, 1);
               colourbar_renderer.render (*image(), image()->scale_inverted());
               colourbar_renderer.end_render_colourbars ();
 
-              QList<QAction*> tools = window.tools()->actions();
+              QList<QAction*> tools = window().tools()->actions();
               size_t num_tool_colourbars = 0;
               for (size_t i = 0, N = tools.size(); i < N; ++i) {
                 Tool::Dock* dock = dynamic_cast<Tool::__Action__*>(tools[i])->dock;
@@ -132,7 +131,7 @@ namespace MR
               }
 
 
-              colourbar_renderer.begin_render_colourbars (&projection, window.tools_colourbar_position, num_tool_colourbars);
+              colourbar_renderer.begin_render_colourbars (&projection, window().tools_colourbar_position, num_tool_colourbars);
 
               for (size_t i = 0, N = tools.size(); i < N; ++i) {
                 Tool::Dock* dock = dynamic_cast<Tool::__Action__*>(tools[i])->dock;
@@ -176,7 +175,7 @@ done_painting:
         {
           const Projection* proj = get_current_projection();
           if (!proj) return;
-          set_focus (proj->screen_to_model (window.mouse_position(), focus()));
+          set_focus (proj->screen_to_model (window().mouse_position(), focus()));
           updateGL();
         }
 
@@ -185,8 +184,8 @@ done_painting:
 
         void Base::contrast_event ()
         {
-          image()->adjust_windowing (window.mouse_displacement());
-          window.on_scaling_changed();
+          image()->adjust_windowing (window().mouse_displacement());
+          window().on_scaling_changed();
           updateGL();
         }
 
@@ -196,7 +195,7 @@ done_painting:
         {
           const Projection* proj = get_current_projection();
           if (!proj) return;
-          set_target (target() - proj->screen_to_model_direction (window.mouse_displacement(), target()));
+          set_target (target() - proj->screen_to_model_direction (window().mouse_displacement(), target()));
           updateGL();
         }
 
@@ -206,7 +205,7 @@ done_painting:
         {
           const Projection* proj = get_current_projection();
           if (!proj) return;
-          move_in_out_FOV (window.mouse_displacement().y(), *proj);
+          move_in_out_FOV (window().mouse_displacement().y(), *proj);
           move_target_to_focus_plane (*proj);
           updateGL();
         }
@@ -225,7 +224,7 @@ done_painting:
             return rot;
           }
 
-          QPoint dpos = window.mouse_displacement();
+          QPoint dpos = window().mouse_displacement();
           if (dpos.x() == 0 && dpos.y() == 0) {
             rot.invalidate();
             return rot;
@@ -256,12 +255,12 @@ done_painting:
           if (!proj) 
             return rot;
 
-          QPoint dpos = window.mouse_displacement();
+          QPoint dpos = window().mouse_displacement();
           if (dpos.x() == 0 && dpos.y() == 0) 
             return rot;
 
-          Point<> x1 (window.mouse_position().x() - proj->x_position() - proj->width()/2,
-              window.mouse_position().y() - proj->y_position() - proj->height()/2,
+          Point<> x1 (window().mouse_position().x() - proj->x_position() - proj->width()/2,
+              window().mouse_position().y() - proj->y_position() - proj->height()/2,
               0.0);
 
           if (x1.norm() < 16.0f) 
@@ -287,7 +286,7 @@ done_painting:
         void Base::tilt_event ()
         {
           if (snap_to_image()) 
-            window.set_snap_to_image (false);
+            window().set_snap_to_image (false);
 
           Math::Versor<float> rot = get_tilt_rotation();
           if (!rot) 
@@ -305,7 +304,7 @@ done_painting:
         void Base::rotate_event ()
         {
           if (snap_to_image()) 
-            window.set_snap_to_image (false);
+            window().set_snap_to_image (false);
 
           Math::Versor<float> rot = get_rotate_rotation();
           if (!rot) 

src/gui/mrview/mode/base.h
@@ -76,10 +76,10 @@ namespace MR
         class Base : public QObject
         {
           public:
-            Base (Window& parent, int flags = FocusContrast | MoveTarget);
+            Base (int flags = FocusContrast | MoveTarget);
             virtual ~Base ();
 
-            Window& window;
+            Window& window () const { return *Window::main; }
             Projection projection;
             const int features;
             QList<ImageBase*> overlays_for_3D;
@@ -104,22 +104,22 @@ namespace MR
 
             void paintGL ();
 
-            const Image* image () const { return window.image(); }
-            const Point<>& focus () const { return window.focus(); }
-            const Point<>& target () const { return window.target(); }
-            float FOV () const { return window.FOV(); }
-            int plane () const { return window.plane(); }
+            const Image* image () const { return window().image(); }
+            const Point<>& focus () const { return window().focus(); }
+            const Point<>& target () const { return window().target(); }
+            float FOV () const { return window().FOV(); }
+            int plane () const { return window().plane(); }
             Math::Versor<float> orientation () const { 
               if (snap_to_image()) 
                 return Math::Versor<float>(1.0f, 0.0f, 0.0f, 0.0f);
-              return window.orientation(); 
+              return window().orientation(); 
             }
 
             int width () const { return glarea()->width(); }
             int height () const { return glarea()->height(); }
-            bool snap_to_image () const { return window.snap_to_image(); }
+            bool snap_to_image () const { return window().snap_to_image(); }
 
-            Image* image () { return window.image(); }
+            Image* image () { return window().image(); }
 
             void move_target_to_focus_plane (const Projection& projection) {
               Point<> in_plane_target = projection.model_to_screen (target());
@@ -127,11 +127,11 @@ namespace MR
               set_target (projection.screen_to_model (in_plane_target));
             }
             void set_visible (bool v) { if(visible != v) { visible = v; updateGL(); } }
-            void set_focus (const Point<>& p) { window.set_focus (p); }
-            void set_target (const Point<>& p) { window.set_target (p); }
-            void set_FOV (float value) { window.set_FOV (value); }
-            void set_plane (int p) { window.set_plane (p); }
-            void set_orientation (const Math::Versor<float>& Q) { window.set_orientation (Q); }
+            void set_focus (const Point<>& p) { window().set_focus (p); }
+            void set_target (const Point<>& p) { window().set_target (p); }
+            void set_FOV (float value) { window().set_FOV (value); }
+            void set_plane (int p) { window().set_plane (p); }
+            void set_orientation (const Math::Versor<float>& Q) { window().set_orientation (Q); }
             void reset_orientation () { 
               Math::Versor<float> orient;
               if (image()) 
@@ -140,7 +140,7 @@ namespace MR
             }
 
             QGLWidget* glarea () const {
-              return reinterpret_cast <QGLWidget*> (window.glarea);
+              return reinterpret_cast <QGLWidget*> (window().glarea);
             }
 
             Point<> move_in_out_displacement (float distance, const Projection& projection) const {
@@ -161,7 +161,7 @@ namespace MR
             }
 
             void render_tools (const Projection& projection, bool is_3D = false, int axis = 0, int slice = 0) {
-              QList<QAction*> tools = window.tools()->actions();
+              QList<QAction*> tools = window().tools()->actions();
               for (int i = 0; i < tools.size(); ++i) {
                 Tool::Dock* dock = dynamic_cast<Tool::__Action__*>(tools[i])->dock;
                 if (dock)
@@ -178,19 +178,19 @@ namespace MR
             }
 
             void draw_crosshairs (const Projection& with_projection) const {
-              if (window.show_crosshairs())
+              if (window().show_crosshairs())
                 with_projection.render_crosshairs (focus());
             }
 
             void draw_orientation_labels (const Projection& with_projection) const {
-              if (window.show_orientation_labels())
+              if (window().show_orientation_labels())
                 with_projection.draw_orientation_labels();
             }
 
             int slice (int axis) const { return std::round (voxel_at (focus())[axis]); }
             int slice () const { return slice (plane()); }
 
-            void updateGL () { window.updateGL(); } 
+            void updateGL () { window().updateGL(); } 
 
           protected:
 
@@ -221,7 +221,7 @@ namespace MR
               setStatusTip (tr (description));
             }
 
-            virtual Base* create (Window& parent) const = 0;
+            virtual Base* create() const = 0;
         };
         //! \endcond
 
@@ -236,8 +236,8 @@ namespace MR
                     int index) :
               __Action__ (parent, name, description, index) { }
 
-            virtual Base* create (Window& parent) const {
-              return new T (parent);
+            virtual Base* create() const {
+              return new T;
             }
         };
 

src/gui/mrview/mode/lightbox.cpp
@@ -38,8 +38,7 @@ namespace MR
         float LightBox::slice_focus_inc_adjust_rate(0.2f);
         std::string LightBox::prev_image_name;
 
-        LightBox::LightBox(Window &parent) :
-          Slice(parent),
+        LightBox::LightBox () :
           layout_is_dirty(true),
           current_slice_index((n_rows*n_cols) / 2),
           slices_proj_focusdelta(n_rows*n_cols, proj_focusdelta(projection, 0.f))
@@ -137,7 +136,7 @@ namespace MR
           GLint w = projection.width(), h = projection.height();
           GLfloat dw = w / (float)n_cols, dh = h / (float)n_rows;
 
-          const Point<> orig_focus = window.focus();
+          const Point<> orig_focus = window().focus();
 
           if(layout_is_dirty)
           {
@@ -153,7 +152,7 @@ namespace MR
               Projection& slice_proj = slices_proj_focusdelta[slice_idx].first;
 
               // Place the first slice in the top-left corner
-              slice_proj.set_viewport(window, x + dw * col, y + h - (dh * (row+1)), dw, dh);
+              slice_proj.set_viewport(window(), x + dw * col, y + h - (dh * (row+1)), dw, dh);
 
               // We need to setup the modelview/proj matrices before we set the new focus
               // because move_in_out_displacement is reliant on MVP
@@ -177,7 +176,7 @@ namespace MR
 
           // Restore view state
           set_focus(orig_focus);
-          projection.set_viewport(window, x, y, w, h);
+          projection.set_viewport(window(), x, y, w, h);
 
           // Users may want to screen capture without grid lines
           if(show_grid_lines) {
@@ -268,7 +267,7 @@ namespace MR
           GLint w = projection.width(), h = projection.height();
           GLint dw = w / n_cols, dh = h / n_rows;
 
-          const auto& mouse_pos = window.mouse_position();
+          const auto& mouse_pos = window().mouse_position();
 
           size_t col = (mouse_pos.x() - x) / dw;
           size_t row = n_rows - (mouse_pos.y() - y) / dh - 1;

commit 26d27bd0e2b85eeff2690fdd2e43aff03cb99428

Fix segfault in MRview tractography tool
Bug introduced in 4d041e1a.

src/gui/mrview/tool/tractography/tractogram.cpp
@@ -323,7 +323,7 @@ namespace MR
             color_type (Direction),
             original_fov (NAN),
             scalar_filename (""),
-            window (window),
+            window (tool.window()),
             tractography_tool (tool),
             filename (filename),
             sample_stride (0)