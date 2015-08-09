MRtrix3/mrtrix3

commit 7f97a84da9db3301d80239d9c8e7fe427d118dd5

Math::Matrix / Vector: Enhance file import
- Matrix and vector files containing a range of delimiting characters can now be imported, not only those using whitespace.
Vector files can have the data stored in either a row or a column.

lib/math/matrix.h
@@ -31,6 +31,7 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_linalg.h>
 
+#include "mrtrix.h"
 #include "file/ofstream.h"
 #include "math/math.h"
 #include "math/vector.h"
@@ -618,7 +619,7 @@ namespace MR
         //! read the matrix data from \a stream and assign to the matrix \a M
         friend std::istream& operator>> (std::istream& stream, Matrix& M) {
           std::vector<std::unique_ptr<std::vector<ValueType>>> V;
-          std::string sbuf, entry;
+          std::string sbuf;
 
           while (getline (stream, sbuf)) {
             sbuf = strip (sbuf.substr (0, sbuf.find_first_of ('#')));
@@ -627,11 +628,9 @@ namespace MR
 
             V.push_back (std::unique_ptr<std::vector<ValueType>> (new std::vector<ValueType>));
 
-            std::istringstream line (sbuf);
-            while (line >> entry) 
-              V.back()->push_back (to<ValueType> (entry));
-            if (line.bad())
-              throw Exception (strerror (errno));
+            const std::vector<std::string> elements = MR::split (sbuf, " ,;\t");
+            for (std::vector<std::string>::const_iterator entry = elements.begin(); entry != elements.end(); ++entry)
+              V.back()->push_back (to<ValueType> (*entry));
 
             if (V.size() > 1)
               if (V.back()->size() != V[0]->size())

lib/math/vector.h
@@ -552,16 +552,46 @@ namespace MR
 
         //! read the vector data from \a stream and assign to the vector \a V
         friend std::istream& operator>> (std::istream& stream, Vector& V) {
-          std::vector<ValueType> vec;
-          std::string entry;
-          while (stream >> entry) 
-            vec.push_back (to<ValueType> (entry));
+
+          std::vector<ValueType> data;
+          size_t axis = 0; // Expect data in rows, 1 value per line (value 1 = data in columns, 1 line only)
+          std::string sbuf;
+
+          while (getline (stream, sbuf)) {
+            sbuf = strip (sbuf.substr (0, sbuf.find_first_of ('#')));
+            if (sbuf.empty())
+              continue;
+
+            std::vector<ValueType> line;
+
+            const std::vector<std::string> elements = MR::split (sbuf, " ,;\t");
+            for (std::vector<std::string>::const_iterator entry = elements.begin(); entry != elements.end(); ++entry)
+              line.push_back (to<ValueType> (*entry));
+
+            if (line.size()) {
+              if (axis) {
+                throw Exception ("file contains matrix, not vector");
+              } else {
+                if (line.size() > 1) {
+                  if (data.size())
+                    throw Exception ("file contains matrix, not vector");
+                  data = std::move (line);
+                  axis = 1;
+                } else {
+                  data.push_back (line[0]);
+                }
+              }
+            }
+          }
           if (stream.bad()) 
             throw Exception (strerror (errno));
 
-          V.allocate (vec.size());
-          for (size_t n = 0; n < V.size(); n++)
-            V[n] = vec[n];
+          if (!data.size())
+            throw Exception ("no data in file");
+
+          V.allocate (data.size());
+          for (size_t i = 0; i < V.size(); i++)
+            V[i] = data[i];
           return stream;
         }
 

commit ad2c774f761f02b0d1cc70524dab39f670481316

Fix issue reading matrix files, introduced by 7f97a84d
Every space was interpreted as a separator, causing the matrix loader to
try to parse additional empty entries.

lib/math/matrix.h
@@ -628,9 +628,9 @@ namespace MR
 
             V.push_back (std::unique_ptr<std::vector<ValueType>> (new std::vector<ValueType>));
 
-            const std::vector<std::string> elements = MR::split (sbuf, " ,;\t");
-            for (std::vector<std::string>::const_iterator entry = elements.begin(); entry != elements.end(); ++entry)
-              V.back()->push_back (to<ValueType> (*entry));
+            const auto elements = MR::split (sbuf, " ,;\t", true);
+            for (const auto& entry : elements)
+              V.back()->push_back (to<ValueType> (entry));
 
             if (V.size() > 1)
               if (V.back()->size() != V[0]->size())