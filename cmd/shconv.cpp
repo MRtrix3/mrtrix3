c6b26ecf811d27d59864169d2d9148bb3ced488e
Daan Christiaens <daan.christiaens@gmail.com>
2015-08-12 11:05:15 +0200

    shconv: code cleanup, tested and OK.

--- cmd/shconv.cpp
+++ cmd/shconv.cpp
@@ -59,18 +59,14 @@ class SConvFunctor {
   public:
   SConvFunctor (const size_t n, Image<bool>& mask, 
                 const Eigen::Matrix<value_type, Eigen::Dynamic, 1>& response) :
-                    image_mask (mask),// ? new Image::Buffer<bool>::voxel_type (*mask) : nullptr),
+                    image_mask (mask),
                     response (response), SH_in(n), SH_out(n) { } 
 
     void operator() (Image<value_type>& in, Image<value_type>& out) {
       if (image_mask.valid()) {
         assign_pos_of(in).to(image_mask);
-//        Image::voxel_assign (*mask_vox_ptr, pos);
-        if (!image_mask.value()) {
-//          for (output_vox[3] = 0; output_vox[3] < output_vox.dim(3); ++output_vox[3])
-//            output_vox.value() = 0.0;
+        if (!image_mask.value())
           return;
-        }
       }
       
       // copy input to vector.   TODO: view instead of copy?
@@ -85,20 +81,10 @@ class SConvFunctor {
         out.index(3) = i;
         out.value() = SH_out[i];
       }
-//      Image::voxel_assign (input_vox, pos);
-//      input_vox[3] = 0;
-//      Math::Vector<value_type> input (input_vox.address(), input_vox.dim(3));
-//      Math::Vector<value_type> output (output_vox.dim(3));
-//      Math::SH::sconv (output, response, input);
-//      Image::voxel_assign (output_vox, pos);
-//      for (output_vox[3] = 0; output_vox[3] < output_vox.dim(3); ++output_vox[3])
-//        output_vox.value() = output[output_vox[3]];
 
     }
 
   protected:
-//    Image<value_type> image_in;   // not needed
-//    Image<value_type> image_out;  // not needed
     Image<bool> image_mask;
     Eigen::Matrix<value_type, Eigen::Dynamic, 1> response;
     Eigen::Matrix<value_type, Eigen::Dynamic, 1> SH_in; // initialize size in constructor?
@@ -110,23 +96,11 @@ void run() {
 
   auto image_in = Image<value_type>::open(argument[0]).with_direct_io(Stride::contiguous_along_axis(3));
   Math::SH::check(image_in);
-//  Image::Header input_SH_header (argument[0]);
-//  Math::SH::check (input_SH_header);
-//  Image::BufferPreload<value_type> input_buf (input_SH_header, Image::Stride::contiguous_along_axis (3));
 
   auto responseSH = load_vector<value_type>(argument[1]);
   Eigen::Matrix<value_type, Eigen::Dynamic, 1> responseRH;
-//  Math::Vector<value_type> responseSH;
-//  responseSH.load (argument[1]);
-//  Math::Vector<value_type> responseRH;
   Math::SH::SH2RH (responseRH, responseSH);
 
-//  std::unique_ptr<Image::Buffer<bool> > mask_buf;
-//  Options opt = get_options ("mask");
-//  if (opt.size()) {
-//    mask_buf.reset (new Image::Buffer<bool> (opt[0][0]));
-//    Image::check_dimensions (*mask_buf, input_buf, 0, 3);
-//  }
   auto mask = Image<bool>();
   auto opt = get_options ("mask");
   if (opt.size()) {
@@ -135,16 +109,9 @@ void run() {
   }
 
   auto header = Header(image_in);
-//  header.set_ndim (4);
-//  header.size(3) = shared.nSH();
-//  header.datatype() = DataType::Float32;
   Stride::set_from_command_line (header);
   auto image_out = Image<value_type>::create (argument[2], header);
   
-//  Image::Header output_SH_header (input_SH_header);
-//  Image::Stride::set_from_command_line (output_SH_header, Image::Stride::contiguous_along_axis (3));
-//  Image::Buffer<value_type> output_SH_buf (argument[2], output_SH_header);
-
   SConvFunctor sconv (image_in.size(3), mask, responseRH);
   ThreadedLoop ("performing convolution...", image_in, 0, 3, 2).run(sconv, image_in, image_out);
 }
