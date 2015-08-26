18d7b869ccf9b76664b15d3c4caf4e1874eaa8a0
J-Donald Tournier <jdtournier@gmail.com>
2015-08-26 17:53:19 +0300

    Math::check_function_gradient(): clean up output by using transpose of vector outputs

--- lib/math/check_gradient.h
+++ lib/math/check_gradient.h
@@ -32,7 +32,7 @@ namespace MR {
     template <class Function>
       void check_function_gradient (
           Function& function, 
-          Eigen::Matrix<typename Function::value_type, Eigen::Dynamic, 1>& x,
+          Eigen::Matrix<typename Function::value_type, Eigen::Dynamic, 1> x,
           typename Function::value_type increment, 
           bool show_hessian = false,
           Eigen::Matrix<typename Function::value_type, Eigen::Dynamic, 1> conditioner = Eigen::Matrix<typename Function::value_type, Eigen::Dynamic, 1>())
@@ -45,13 +45,13 @@ namespace MR {
             " parameters of type " + DataType::from<value_type>().specifier());
         value_type step_size = function.init (g);
         CONSOLE ("cost function suggests initial step size = " + str(step_size));
-        CONSOLE ("cost function suggests initial position at [ " + str(g) + "]");
+        CONSOLE ("cost function suggests initial position at [ " + str(g.transpose()) + "]");
 
-        CONSOLE ("checking gradient at position [ " + str(x) + "]:");
+        CONSOLE ("checking gradient at position [ " + str(x.transpose()) + "]:");
         Eigen::Matrix<value_type, Eigen::Dynamic, 1> g0 (N);
         value_type f0 = function (x, g0);
         CONSOLE ("  cost function = " + str(f0));
-        CONSOLE ("  gradient from cost function         = [ " + str(g0) + "]");
+        CONSOLE ("  gradient from cost function         = [ " + str(g0.transpose()) + "]");
 
         Eigen::Matrix<value_type, Eigen::Dynamic, 1> g_fd (N);
         Eigen::Matrix<value_type, Eigen::Dynamic, Eigen::Dynamic> hessian;
@@ -88,8 +88,8 @@ namespace MR {
 
         }
 
-        CONSOLE ("gradient by central finite difference = [ " + str(g_fd) + "]");
-        CONSOLE ("normalised dot product = " + str(g_fd.dot(g0)) / g_fd.squaredNorm());
+        CONSOLE ("gradient by central finite difference = [ " + str(g_fd.transpose()) + "]");
+        CONSOLE ("normalised dot product = " + str(g_fd.dot(g0) / g_fd.squaredNorm()));
 
         if (show_hessian) {
           hessian /= 4.0*increment;
