ec2c0f4383c2102e7fdab6132a5eb53f4d4fae2a
J-Donald Tournier <jdtournier@gmail.com>
2015-08-28 23:29:53 +0100

    New Adapter::AllowEmpty class
    This is used to allow empty (default constructed) images to be used in
    looping structures. Useful for optional images such as masks, etc.

--- lib/adapter/allow_empty.h
+++ lib/adapter/allow_empty.h
@@ -38,10 +38,9 @@ namespace MR
 
         using Base<ImageType>::valid;
 
-        template <class VectorType>
-          AllowEmpty (const ImageType& original, value_type value_if_empty = value_type (0.0)) :
-            Base<ImageType> (original),
-            value_if_empty (value_if_empty) { }
+        AllowEmpty (const ImageType& original, value_type value_if_empty = value_type (0.0)) :
+          Base<ImageType> (original),
+          value_if_empty (value_if_empty) { }
 
         FORCE_INLINE void reset () {
           if (valid())
@@ -49,21 +48,37 @@ namespace MR
               set_pos (n, 0);
         }
 
-        FORCE_INLINE ssize_t size (size_t axis) const { return valid() ? size_ [axis] : 0; }
+        FORCE_INLINE const Header& header () const { 
+          if (valid()) 
+            return parent().header(); 
+          throw Exception ("FIXME: attempt to access header information from invalid image!"); }
+
+        FORCE_INLINE const std::string& name() const { return valid() ? parent().name() : std::string("<empty image>"); }
+        FORCE_INLINE const transform_type& transform() const { return valid() ? parent().transform() : transform_type(); }
+
+        FORCE_INLINE size_t  ndim () const { return valid() ? parent().ndim() : 0; }
+        FORCE_INLINE ssize_t size (size_t axis) const { return valid() ? parent().size(axis) : 0; }
+        FORCE_INLINE default_type spacing (size_t axis) const { return valid() ? parent().spacing (axis) : NaN; }
+        FORCE_INLINE ssize_t stride (size_t axis) const { return valid() ? parent().stride (axis) : 0; }
 
         FORCE_INLINE ssize_t index (size_t axis) const { return valid() ? parent().index (axis) : 0; }
         FORCE_INLINE auto index (size_t axis) -> decltype(Helper::index(*this, axis)) { return { *this, axis }; } 
         FORCE_INLINE void move_index (size_t axis, ssize_t increment) { if (valid()) parent().index(axis) += increment; }
 
-        FORCE_INLINE value_type value () const { return valid() ? parent_.value() : value_if_empty; } 
+        FORCE_INLINE value_type value () const { return valid() ? parent().value() : value_if_empty; } 
         FORCE_INLINE auto value () -> decltype(Helper::value(*this)) { return { *this }; }
-        FORCE_INLINE void set_value (value_type val) { if (valid()) parent_.value() = val; } 
+        FORCE_INLINE void set_value (value_type val) { if (valid()) parent().value() = val; } 
 
 
       protected:
         using Base<ImageType>::parent;
         const value_type value_if_empty;
     };
+
+    template <class ImageType>
+      inline AllowEmpty<ImageType> allow_empty (const ImageType& parent, typename ImageType::value_type value_if_empty = typename ImageType::value_type (0.0)) {
+        return AllowEmpty<ImageType> (parent, value_if_empty);
+      }
 
   }
 }
