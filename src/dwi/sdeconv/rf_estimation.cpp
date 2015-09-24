853dbfe32f96c393e1111b595fabd80b1ad63c00
Robert Smith <r.smith@brain.org.au>
2015-09-24 23:27:24 +1000

    Ported dwi2response to updated syntax

--- src/dwi/sdeconv/rf_estimation.cpp
+++ src/dwi/sdeconv/rf_estimation.cpp
@@ -35,11 +35,11 @@ bool FODSegResult::is_sf (const SFThresholds& thresholds) const
 {
   if (volume_ratio > thresholds.get_volume_ratio())
     return false;
-  if (integral < thresholds.get_min_integral())
+  if (integral     < thresholds.get_min_integral())
     return false;
-  if (integral > thresholds.get_max_integral())
+  if (integral     > thresholds.get_max_integral())
     return false;
-  if (dispersion > thresholds.get_max_dispersion())
+  if (dispersion   > thresholds.get_max_dispersion())
     return false;
   return true;
 }
@@ -47,7 +47,7 @@ bool FODSegResult::is_sf (const SFThresholds& thresholds) const
 
 
 
-void SFThresholds::update (const std::vector<FODSegResult>& data, const float dispersion_multiplier, const float integral_multiplier, const size_t iter)
+void SFThresholds::update (const std::vector<FODSegResult>& data, const default_type dispersion_multiplier, const default_type integral_multiplier, const size_t iter)
 {
 
 /*
@@ -86,9 +86,9 @@ void SFThresholds::update (const std::vector<FODSegResult>& data, const float di
   // - Integral to be +- 'x' standard deviations (either side; also reject voxels where FOD is abnormally large)
   // - Dispersion limit to be defined using some multiple of the difference between the minimum (sharpest peak) and the mean
 
-  double dispersion_sum = 0.0;
-  float dispersion_min = std::numeric_limits<float>::max();
-  double integral_sum = 0.0, integral_sq_sum = 0.0;
+  default_type dispersion_sum = 0.0;
+  default_type dispersion_min = std::numeric_limits<default_type>::max();
+  default_type integral_sum = 0.0, integral_sq_sum = 0.0;
   size_t count = 0;
   for (std::vector<FODSegResult>::const_iterator i = data.begin(); i != data.end(); ++i) {
     if (i->get_volume_ratio() < volume_ratio) {
@@ -99,9 +99,9 @@ void SFThresholds::update (const std::vector<FODSegResult>& data, const float di
       ++count;
     }
   }
-  const float dispersion_mean  = dispersion_sum / double(count);
-  const float integral_mean  = integral_sum / double(count);
-  const float integral_stdev = std::sqrt ((integral_sq_sum / double(count)) - Math::pow2 (integral_mean));
+  const default_type dispersion_mean  = dispersion_sum / default_type(count);
+  const default_type integral_mean  = integral_sum / default_type(count);
+  const default_type integral_stdev = std::sqrt ((integral_sq_sum / default_type(count)) - Math::pow2 (integral_mean));
 
   min_integral = integral_mean - (integral_multiplier * integral_stdev);
   max_integral = integral_mean + (integral_multiplier * integral_stdev);
@@ -115,17 +115,18 @@ void SFThresholds::update (const std::vector<FODSegResult>& data, const float di
 
 
 
-bool FODCalcAndSeg::operator() (const Image::Iterator& pos)
+bool FODCalcAndSeg::operator() (const Iterator& pos)
 {
-  if (!Image::Nav::get_value_at_pos (mask, pos))
+  assign_pos_of (pos).to (mask);
+  if (!mask.value())
     return true;
 
-  Image::Nav::set_pos (in, pos, 0, 3);
+  assign_pos_of (pos, 0, 3).to (in);
 
   // Load the raw DWI data
-  Math::Vector<float> dwi_data (csd.shared.dwis.size());
+  Eigen::Matrix<default_type, Eigen::Dynamic, 1> dwi_data (csd.shared.dwis.size());
   for (size_t n = 0; n < csd.shared.dwis.size(); n++) {
-    in[3] = csd.shared.dwis[n];
+    in.index(3) = csd.shared.dwis[n];
     dwi_data[n] = in.value();
     if (!std::isfinite (dwi_data[n]))
       return true;
@@ -148,7 +149,7 @@ bool FODCalcAndSeg::operator() (const Image::Iterator& pos)
 
   // Perform FOD segmentation
   DWI::FMLS::SH_coefs coefs (csd.FOD());
-  coefs.vox[0] = pos[0]; coefs.vox[1] = pos[1]; coefs.vox[2] = pos[2];
+  coefs.vox[0] = pos.index(0); coefs.vox[1] = pos.index(1); coefs.vox[2] = pos.index(2);
   DWI::FMLS::FOD_lobes lobes;
   (*fmls) (coefs, lobes);
   if (lobes.empty())
@@ -172,7 +173,8 @@ bool SFSelector::operator() (FODSegResult& out)
   while (it != input.end()) {
     if (it->is_sf (thresholds)) {
       out = *it;
-      Image::Nav::set_value_at_pos (output, it->get_vox(), true);
+      assign_pos_of (it->get_vox()).to (output);
+      output.value() = true;
       ++it;
       return true;
     }
@@ -186,12 +188,12 @@ bool SFSelector::operator() (FODSegResult& out)
 
 bool ResponseEstimator::operator() (const FODSegResult& in)
 {
-  Image::Nav::set_pos (dwi, in.get_vox(), 0, 3);
+  assign_pos_of (in.get_vox(), 0, 3).to (dwi);
 
   // Load the raw DWI data
-  Math::Vector<float> dwi_data (shared.dwis.size());
+  Eigen::Matrix<default_type, Eigen::Dynamic, 1> dwi_data (shared.dwis.size());
   for (size_t n = 0; n < shared.dwis.size(); n++) {
-    dwi[3] = shared.dwis[n];
+    dwi.index(3) = shared.dwis[n];
     dwi_data[n] = dwi.value();
     if (dwi_data[n] < 0.0)
       dwi_data[n] = 0.0;
@@ -199,14 +201,14 @@ bool ResponseEstimator::operator() (const FODSegResult& in)
 
   // Rotate the diffusion gradient orientations into a new reference frame,
   //   where the Z direction is defined by the FOD peak
-  Math::Matrix<float> R = gen_rotation_matrix (in.get_peak_dir());
-  Math::Matrix<float> rotated_grad (shared.grad.rows(), 4);
-  Math::Vector<float> vec (3), rot (3);
-  for (size_t row = 0; row != shared.grad.rows(); ++row) {
+  Eigen::Matrix<default_type, 3, 3> R = gen_rotation_matrix (in.get_peak_dir().cast<default_type>());
+  Eigen::Matrix<default_type, Eigen::Dynamic, 4> rotated_grad (shared.grad.rows(), 4);
+  Eigen::Vector3 vec (3), rot (3);
+  for (size_t row = 0; row != size_t(shared.grad.rows()); ++row) {
     vec[0] = shared.grad (row, 0);
     vec[1] = shared.grad (row, 1);
     vec[2] = shared.grad (row, 2);
-    Math::mult (rot, R, vec);
+    rot = R * vec;
     rotated_grad (row, 0) = rot[0];
     rotated_grad (row, 1) = rot[1];
     rotated_grad (row, 2) = rot[2];
@@ -214,17 +216,17 @@ bool ResponseEstimator::operator() (const FODSegResult& in)
   }
 
   // Convert directions from Euclidean space to azimuth/elevation pairs
-  Math::Matrix<float> dirs = DWI::gen_direction_matrix (rotated_grad, shared.dwis);
+  const Eigen::MatrixXd dirs = DWI::gen_direction_matrix (rotated_grad, shared.dwis);
 
   try {
 
     // Convert the DWI signal to spherical harmonics in the new reference frame
-    Math::SH::Transform<float> transform (dirs, lmax);
-    Math::Vector<float> SH;
+    Math::SH::Transform<default_type> transform (dirs, lmax);
+    Eigen::Matrix<default_type, Eigen::Dynamic, 1> SH;
     transform.A2SH (SH, dwi_data);
 
     // Extract the m=0 components and save
-    Math::Vector<float> response (lmax/2+1);
+    Eigen::Matrix<default_type, Eigen::Dynamic, 1> response (lmax/2+1);
     for (size_t l = 0; l <= lmax; l += 2)
       response[l/2] = SH[Math::SH::index (l, 0)];
 
@@ -240,19 +242,20 @@ bool ResponseEstimator::operator() (const FODSegResult& in)
 
 
 
-Math::Matrix<float> ResponseEstimator::gen_rotation_matrix (const Point<float>& dir) const
+Eigen::Matrix<default_type, 3, 3> ResponseEstimator::gen_rotation_matrix (const Eigen::Vector3& dir) const
 {
   // Generates a matrix that will rotate a unit vector into a new frame of reference,
   //   where the peak direction of the FOD is aligned in Z (3rd dimension)
   // Previously this was done using the tensor eigenvectors
   // Here the other two axes are determined at random (but both are orthogonal to the FOD peak direction)
-  Math::Matrix<float> R (3, 3);
+  Eigen::Matrix<default_type, 3, 3> R;
   R (2, 0) = dir[0]; R (2, 1) = dir[1]; R (2, 2) = dir[2];
-  Point<float> vec2 (rng(), rng(), rng());
+  Eigen::Vector3 vec2 (rng(), rng(), rng());
   vec2 = dir.cross (vec2);
-  vec2.normalise();
+  vec2.normalize();
   R (0, 0) = vec2[0]; R (0, 1) = vec2[1]; R (0, 2) = vec2[2];
-  const Point<float> vec3 ((dir.cross (vec2)).normalise());
+  Eigen::Vector3 vec3 = dir.cross (vec2);
+  vec3.normalize();
   R (1, 0) = vec3[0]; R (1, 1) = vec3[1]; R (1, 2) = vec3[2];
   return R;
 }
