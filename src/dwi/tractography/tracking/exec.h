a0310a0ae18fe29391b7876c9bbc6b9e808eb9c1
Robert Smith <r.smith@brain.org.au>
2015-10-05 12:25:22 +1100

    tckgen: Use fixed batch size of 10
    For non-dynamic-seeding tracking, the default multi-threading batch size of 128 was introducing too much jitter into the progressbar update.

--- src/dwi/tractography/tracking/exec.h
+++ src/dwi/tractography/tracking/exec.h
@@ -44,7 +44,7 @@
 
 #define MAX_NUM_SEED_ATTEMPTS 100000
 
-#define DYNAMIC_SEEDING_BATCH_SIZE 8
+#define TRACKING_BATCH_SIZE 10
 
 
 
@@ -72,7 +72,7 @@ namespace MR
                 typename Method::Shared shared (diff_path, properties);
                 WriteKernel writer (shared, destination, properties);
                 Exec<Method> tracker (shared);
-                Thread::run_queue (Thread::multi (tracker), Thread::batch (GeneratedTrack()), writer);
+                Thread::run_queue (Thread::multi (tracker), Thread::batch (GeneratedTrack(), TRACKING_BATCH_SIZE), writer);
 
               } else {
 
@@ -103,11 +103,11 @@ namespace MR
 
                 Thread::run_queue (
                     Thread::multi (tracker), 
-                    Thread::batch (GeneratedTrack(), DYNAMIC_SEEDING_BATCH_SIZE),
+                    Thread::batch (GeneratedTrack(), TRACKING_BATCH_SIZE),
                     writer, 
-                    Thread::batch (Streamline<value_type>(), DYNAMIC_SEEDING_BATCH_SIZE),
+                    Thread::batch (Streamline<value_type>(), TRACKING_BATCH_SIZE),
                     Thread::multi (mapper), 
-                    Thread::batch (SetDixel(), DYNAMIC_SEEDING_BATCH_SIZE),
+                    Thread::batch (SetDixel(), TRACKING_BATCH_SIZE),
                     *seeder);
 
               }
