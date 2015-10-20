46ad7e1b62e122f9bc980c4215ed4222c65a711d
Dave Raffelt <draffelt@gmail.com>
2015-10-20 12:17:55 +1100

    Added fixelstats, a fixel equivalent of mrstats

--- cmd/mrstats.cpp
+++ cmd/mrstats.cpp
@@ -30,13 +30,12 @@
 #include "memory.h"
 #include "algo/loop.h"
 #include "file/ofstream.h"
-#include "math/median.h"
+#include "stats.h"
 
 
 using namespace MR;
 using namespace App;
 
-const char * field_choices[] = { "mean", "median", "std", "min", "max", "count", NULL };
 
 void usage () {
 DESCRIPTION
@@ -48,33 +47,13 @@ ARGUMENTS
   .type_image_in ();
 
 OPTIONS
-  + Option ("output",
-      "output only the field specified. Multiple such options can be supplied if required. "
-      "Choices are: " + join (field_choices, ", ") + ". Useful for use in scripts.").allow_multiple()
-  +   Argument ("field").type_choice (field_choices)
-
-  + Option ("mask",
-  "only perform computation within the specified binary brain mask image.")
-  + Argument ("image").type_image_in ()
+  + Stats::Options
 
   + Option ("voxel",
   "only perform computation within the specified voxel, supplied as a "
   "comma-separated vector of 3 integer values (multiple voxels can be included).")
   .allow_multiple()
   + Argument ("pos").type_sequence_int ()
-
-  + Option ("histogram",
-  "generate histogram of intensities and store in specified text file. Note "
-  "that the first line of the histogram gives the centre of the bins.")
-  + Argument ("file").type_file_out ()
-
-  + Option ("bins",
-  "the number of bins to use to generate the histogram (default = 100).")
-  + Argument ("num").type_integer (2, 100, std::numeric_limits<int>::max())
-
-  + Option ("dump",
-  "dump the voxel intensities to a text file.")
-  + Argument ("file").type_file_out ()
 
   + Option ("position",
   "dump the position of the voxels in the mask to a text file.")
@@ -83,169 +62,8 @@ OPTIONS
 }
 
 
-typedef float value_type;
-typedef cfloat complex_type;
-
-
-class CalibrateHistogram
-{
-  public:
-    CalibrateHistogram (int nbins) : min (std::numeric_limits<value_type>::infinity()), max (-std::numeric_limits<value_type>::infinity()), width (0.0), bins (nbins) { }
-
-    value_type min, max, width;
-    int bins;
-
-    void operator() (value_type val) {
-      if (std::isfinite (val)) {
-        if (val < min) min = val;
-        if (val > max) max = val;
-      }
-    }
-
-    void init (std::ostream& stream) {
-      width = (max - min) / float (bins+1);
-      for (int i = 0; i < bins; i++)
-        stream << (min + width/2.0) + i* width << " ";
-      stream << "\n";
-    }
-};
-
-
-
-class Stats
-{
-  public:
-    Stats (bool is_complex = false) :
-      mean (0.0, 0.0),
-      std (0.0, 0.0),
-      min (INFINITY, INFINITY),
-      max (-INFINITY, -INFINITY),
-      count (0),
-      dump (NULL),
-      is_complex (is_complex) { }
-
-    void generate_histogram (const CalibrateHistogram& cal) {
-      hmin = cal.min;
-      hwidth = cal.width;
-      hist.resize (cal.bins);
-    }
-
-    void dump_to (std::ostream& stream) {
-      dump = &stream;
-    }
-
-    void write_histogram (std::ostream& stream) {
-      for (size_t i = 0; i < hist.size(); ++i)
-        stream << hist[i] << " ";
-      stream << "\n";
-    }
-
-
-    void operator() (complex_type val) {
-      if (std::isfinite (val.real()) && std::isfinite (val.imag())) {
-        mean += val;
-        std += cdouble (val.real()*val.real(), val.imag()*val.imag());
-        if (min.real() > val.real()) min = complex_type (val.real(), min.imag());
-        if (min.imag() > val.imag()) min = complex_type (min.real(), val.imag());
-        if (max.real() < val.real()) max = complex_type (val.real(), max.imag());
-        if (max.imag() < val.imag()) max = complex_type (max.real(), val.imag());
-        count++;
-
-        if (dump)
-          *dump << str(val) << "\n";
-
-        if (!is_complex)
-          values.push_back(val.real());
-
-
-        if (hist.size()) {
-          int bin = int ( (val.real()-hmin) / hwidth);
-          if (bin < 0)
-            bin = 0;
-          else if (bin >= int (hist.size()))
-            bin = hist.size()-1;
-          hist[bin]++;
-        }
-      }
-    }
-
-    template <class Set> void print (Set& ima, const std::vector<std::string>& fields) {
-
-      if (count) {
-        mean /= double (count);
-        std = complex_type (sqrt (std.real()/double(count) - mean.real()*mean.real()),
-            sqrt (std.imag()/double(count) - mean.imag()*mean.imag()));
-      }
-
-      std::sort (values.begin(), values.end());
-
-      if (fields.size()) {
-        if (!count) 
-          return;
-        for (size_t n = 0; n < fields.size(); ++n) {
-          if (fields[n] == "mean") std::cout << str(mean) << " ";
-          else if (fields[n] == "median") std::cout << Math::median (values) << " ";
-          else if (fields[n] == "std") std::cout << str(std) << " ";
-          else if (fields[n] == "min") std::cout << str(min) << " ";
-          else if (fields[n] == "max") std::cout << str(max) << " ";
-          else if (fields[n] == "count") std::cout << count << " ";
-        }
-        std::cout << "\n";
-
-      }
-      else {
-
-        std::string s = "[ ";
-        if (ima.ndim() > 3) 
-          for (size_t n = 3; n < ima.ndim(); n++)
-            s += str (ima.index(n)) + " ";
-        else 
-          s += "0 ";
-        s += "] ";
-
-        int width = is_complex ? 24 : 12;
-        std::cout << std::setw(15) << std::right << s << " ";
-
-        std::cout << std::setw(width) << std::right << ( count ? str(mean) : "N/A" );
-
-        if (!is_complex) {
-          std::cout << " " << std::setw(width) << std::right << ( count ? str(Math::median (values)) : "N/A" );
-        }
-        std::cout << " " << std::setw(width) << std::right << ( count > 1 ? str(std) : "N/A" )
-          << " " << std::setw(width) << std::right << ( count ? str(min) : "N/A" )
-          << " " << std::setw(width) << std::right << ( count ? str(max) : "N/A" )
-          << " " << std::setw(12) << std::right << count << "\n";
-      }
-
-    }
-
-  private:
-    cdouble mean, std;
-    complex_type min, max;
-    size_t count;
-    value_type hmin, hwidth;
-    std::vector<size_t> hist;
-    std::ostream* dump;
-    bool is_complex;
-    std::vector<float> values;
-};
-
-
-
-void print_header (bool is_complex)
-{
-  int width = is_complex ? 24 : 12;
-  std::cout << std::setw(15) << std::right << "channel"
-    << " " << std::setw(width) << std::right << "mean";
-  if (!is_complex)
-    std::cout << " " << std::setw(width) << std::right << "median";
-  std::cout  << " " << std::setw(width) << std::right << "std. dev."
-    << " " << std::setw(width) << std::right << "min"
-    << " " << std::setw(width) << std::right << "max"
-    << " " << std::setw(12) << std::right << "count\n";
-}
-
-
+typedef Stats::value_type value_type;
+typedef Stats::complex_type complex_type;
 
 class Volume_loop
 {
@@ -286,6 +104,7 @@ class Volume_loop
 
 void run ()
 {
+
   auto data = Image<complex_type>::open (argument[0]);
   const bool is_complex = data.original_header().datatype().is_complex();
   if (data.ndim() > 4)
@@ -306,7 +125,7 @@ void run ()
   opt = get_options ("bins");
   if (opt.size())
     nbins = opt[0][0];
-  CalibrateHistogram calibrate (nbins);
+  Stats::CalibrateHistogram calibrate (nbins);
 
   opt = get_options ("dump");
   if (opt.size())
@@ -347,7 +166,7 @@ void run ()
     }
 
     for (auto i = Volume_loop (data); i; ++i) {
-      Stats stats (is_complex);
+      Stats::Stats stats (is_complex);
 
       if (dumpstream)
         stats.dump_to (*dumpstream);
@@ -367,7 +186,7 @@ void run ()
       }
 
       if (!header_shown)
-        print_header (is_complex);
+        Stats::print_header (is_complex);
       header_shown = true;
 
       stats.print (data, fields);
@@ -396,7 +215,7 @@ void run ()
     }
 
     for (auto l = Volume_loop (data); l; ++l) {
-      Stats stats (is_complex);
+      Stats::Stats stats (is_complex);
 
       if (dumpstream)
         stats.dump_to (*dumpstream);
@@ -414,7 +233,7 @@ void run ()
       }
 
       if (!header_shown)
-        print_header (is_complex);
+        Stats::print_header (is_complex);
       header_shown = true;
 
       stats.print (data, fields);
@@ -459,7 +278,7 @@ void run ()
   }
 
   for (auto i = Volume_loop (data); i; ++i) {
-    Stats stats (is_complex);
+    Stats::Stats stats (is_complex);
 
     if (dumpstream)
       stats.dump_to (*dumpstream);
@@ -480,7 +299,7 @@ void run ()
     }
 
     if (!header_shown)
-      print_header (is_complex);
+      Stats::print_header (is_complex);
     header_shown = true;
 
     stats.print (data, fields);
