ccf360cf401ed4b68f65ce09b215e8e43c74c485
J-Donald Tournier <jdtournier@gmail.com>
2015-11-16 16:34:55 +0000

    MRView: avoid discretisation of scroll values
    as discussed in #397

--- src/gui/mrview/window.cpp
+++ src/gui/mrview/window.cpp
@@ -1484,9 +1484,9 @@ namespace MR
 #if QT_VERSION >= 0x050400
         QPoint delta = event->pixelDelta();
         if (delta.isNull())
-          delta = event->angleDelta() / 120.0;
+          delta = event->angleDelta();
 #else
-        QPoint delta = event->orientation() == Qt::Vertical ? QPoint (0, event->delta() / 120.0) : QPoint (event->delta() / 120.0, 0);
+        QPoint delta = event->orientation() == Qt::Vertical ? QPoint (0, event->delta()) : QPoint (event->delta(), 0);
 #endif
         if (delta.isNull())
           return;
@@ -1500,13 +1500,13 @@ namespace MR
             if (buttons_ == Qt::NoButton) {
 
               if (modifiers_ == Qt::ControlModifier) {
-                set_FOV (FOV() * std::exp (-0.1*delta.y()));
+                set_FOV (FOV() * std::exp (-delta.y()/1200.0));
                 glarea->update();
                 event->accept();
                 return;
               }
 
-              float dx = delta.y();
+              float dx = delta.y()/120.0;
               if (modifiers_ == Qt::ShiftModifier) dx *= 10.0;
               else if (modifiers_ != Qt::NoModifier) 
                 return;
@@ -1522,7 +1522,7 @@ namespace MR
               QAction* action = image_group->checkedAction();
               int N = image_group->actions().size();
               int n = image_group->actions().indexOf (action);
-              image_select_slot (image_group->actions()[(n+N+delta.y())%N]);
+              image_select_slot (image_group->actions()[(n+N+int(std::round(delta.y()/120.0)))%N]);
             }
           }
         }
