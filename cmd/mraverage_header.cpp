24a137f5089ea6239f02fb9b51e4e6bcf4dd35ae
Max Pietsch <mail@maxpietsch.com>
2015-11-26 17:38:31 +0000

    mraverage_header: more output for debuging

--- cmd/mraverage_header.cpp
+++ cmd/mraverage_header.cpp
@@ -40,54 +40,63 @@ void usage ()
 void run ()
 {
 
-	const size_t num_inputs = argument.size()-1;
-	// typedef Eigen::Transform< ComputeType, 3, Eigen::Projective> TransformType;
+    const size_t num_inputs = argument.size()-1;
+    // typedef Eigen::Transform< ComputeType, 3, Eigen::Projective> TransformType;
 
 
 
-	auto opt = get_options ("padding");
-	const ComputeType p = opt.size() ? ComputeType(opt[0][0]) : PADDING_DEFAULT;
-	// VectorType padding = RowVectorType(4);
-	auto padding = Eigen::Matrix<ComputeType, 4, 1>(p, p, p, 1.0);
-	INFO("padding in template voxels: " + str(padding.transpose().head<3>()));
+    auto opt = get_options ("padding");
+    const ComputeType p = opt.size() ? ComputeType(opt[0][0]) : PADDING_DEFAULT;
+    // VectorType padding = RowVectorType(4);
+    auto padding = Eigen::Matrix<ComputeType, 4, 1>(p, p, p, 1.0);
+    INFO("padding in template voxels: " + str(padding.transpose().head<3>()));
 
-	opt = get_options ("template_res");
-	const ComputeType template_res = opt.size() ? ComputeType(opt[0][0]) : TEMPLATE_RESOLUTION;
-	INFO("template voxel subsampling: " + str(template_res));
+    opt = get_options ("template_res");
+    const ComputeType template_res = opt.size() ? ComputeType(opt[0][0]) : TEMPLATE_RESOLUTION;
+    INFO("template voxel subsampling: " + str(template_res));
 
-	std::vector<Header> headers_in;
-	
-	for (size_t i = 0; i != num_inputs; ++i) {
-		headers_in.push_back (Header::open (argument[i]));
-		const Header& temp (headers_in[i]);
-		if (temp.ndim() < 3)
-			throw Exception ("Please provide 3 dimensional images");
-	}
+    std::vector<Header> headers_in;
+
+    for (size_t i = 0; i != num_inputs; ++i) {
+        headers_in.push_back (Header::open (argument[i]));
+        const Header& temp (headers_in[i]);
+        if (temp.ndim() < 3)
+            throw Exception ("Please provide 3 dimensional images");
+    }
 
 
-	for(auto temp: headers_in){
-		std::cerr << temp << std::endl;
-		auto trafo = (Eigen::Transform<ComputeType, 3, Eigen::Projective>) Transform(temp).voxel2scanner;
-		std::cerr << trafo.matrix() << std::endl;
-		
-    	auto bbox = get_bounding_box<ComputeType,decltype(trafo)>(temp, trafo);
-    	std::cerr << bbox << std::endl;
-	}
+    for(auto temp: headers_in){
+        std::cerr << temp << std::endl;
+        auto trafo = (Eigen::Transform<ComputeType, 3, Eigen::Projective>) Transform(temp).voxel2scanner;
+        std::cerr << trafo.matrix() << std::endl;
 
-	auto trafo = headers_in[0].transform();
-	std::vector<decltype(trafo)> transform_header_with;
+        auto bbox = get_bounding_box<ComputeType,decltype(trafo)>(temp, trafo);
+        std::cerr << bbox << std::endl;
+        MAT(bbox);
+    }
 
-	auto H = compute_minimum_average_header<double,decltype(trafo)>(headers_in, template_res, padding, transform_header_with);
+    auto trafo = headers_in[0].transform();
+    std::vector<decltype(trafo)> transform_header_with;
 
-	// std::cerr << H << std::endl;
-	std::cerr << "template header trafo:\n" << H.transform().matrix() << std::endl;
+    auto H = compute_minimum_average_header<double,decltype(trafo)>(headers_in, template_res, padding, transform_header_with);
 
-	auto out = Header::create(argument[argument.size()-1],H);
-	// auto out = Image<float>::create(argument[argument.size()-1],H);
-	// for (size_t i = 0; i<num_inputs; ++i){	
-	// 	auto in = headers_in[i].get_image<float>();
-	// 	for (auto j = Loop() (in, out); j ;++j){
-	// 		out.value() += in.value()/num_inputs;
-	// 	}
-	// }
+    // std::cerr << H << std::endl;
+    std::cerr << "template header trafo:\n" << H.transform().matrix() << std::endl;
+    std::cerr << "template header voxel2scanner trafo:\n" << Transform(H).voxel2scanner.matrix() << std::endl;
+
+    {
+        std::cerr << "template header bounding box:\n";
+        auto trafo = (Eigen::Transform<ComputeType, 3, Eigen::Projective>) Transform(H).voxel2scanner;
+        auto bbox = get_bounding_box<ComputeType,decltype(trafo)>(H, trafo);
+        MAT(bbox);
+    }
+
+    auto out = Header::create(argument[argument.size()-1],H);
+    // auto out = Image<float>::create(argument[argument.size()-1],H);
+    // for (size_t i = 0; i<num_inputs; ++i){
+    //  auto in = headers_in[i].get_image<float>();
+    //  for (auto j = Loop() (in, out); j ;++j){
+    //      out.value() += in.value()/num_inputs;
+    //  }
+    // }
 }
