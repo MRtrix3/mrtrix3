MRtrix3/mrtrix3

commit 33207e75f44531d3154e816b0790c38ef4055c49

tckglobal: support and warn when user does not provide isotropic kernels.

modules/gt/src/externalenergy.cpp
@@ -50,8 +50,11 @@ namespace MR {
           header.size(3) = ncols;
           tod = Image<float>::scratch(header, "TOD image");
           
-          header.size(3) = nf;
-          fiso = Image<float>::scratch(header, "isotropic fractions");
+          if (nf > 0) {
+            WARN("No isotropic response functions provided; using single-tissue white matter model.");
+            header.size(3) = nf;
+            fiso = Image<float>::scratch(header, "isotropic fractions");
+          }
           
           header.set_ndim(3);
           eext = Image<float>::scratch(header, "external energy");
@@ -124,16 +127,15 @@ namespace MR {
           DEBUG("Reset external energy.");
           double e;
           dE = 0.0;
-          for (auto l = Loop(dwi, 0, 3) (dwi, tod, fiso, eext); l; ++l)
+          for (auto l = Loop(dwi, 0, 3) (dwi, tod, eext); l; ++l)
           {
             y = dwi.row(3).cast<double>();
             t = tod.row(3).cast<double>();
             e = calcEnergy();
             eext.value() = e;
             dE += e;
-            fiso.row(3) = fk.tail(nf).cast<float>();
           }
-          stats.incEextTotal(dE - stats.getEextTotal());  // Reset total external energy
+          stats.incEextTotal(dE - stats.getEextTotal());
           dE = 0.0;
         }
         
@@ -142,11 +144,14 @@ namespace MR {
         {
           for (int k = 0; k != changes_vox.size(); ++k) 
           {
-            assign_pos_of(changes_vox[k], 0, 3).to(tod, fiso, eext);
+            assign_pos_of(changes_vox[k], 0, 3).to(tod, eext);
             assert(!is_out_of_bounds(tod));
             tod.row(3) = changes_tod[k].cast<float>();
-            fiso.row(3) = changes_fiso[k].cast<float>();
             eext.value() = changes_eext[k];
+            if (fiso.valid()) {
+              assign_pos_of(changes_vox[k], 0, 3).to(fiso);
+              fiso.row(3) = changes_fiso[k].cast<float>();
+            }
           }
           stats.incEextTotal(dE);
           clearChanges();

modules/gt/cmd/tckglobal.cpp
@@ -340,10 +340,15 @@ void run ()
   
   opt = get_options("fiso");
   if (opt.size()) {
-    INFO("Saving isotropic fractions to file");
-    header.size(3) = properties.resp_ISO.size();
-    auto Fiso = Image<float>::create (opt[0][0], header);
-    threaded_copy(Eext->getFiso(), Fiso);
+    if (properties.resp_ISO.size() > 0) {
+      INFO("Saving isotropic fractions to file");
+      header.size(3) = properties.resp_ISO.size();
+      auto Fiso = Image<float>::create (opt[0][0], header);
+      threaded_copy(Eext->getFiso(), Fiso);
+    }
+    else {
+      WARN("Ignore saving file " + opt[0][0] + ", because no isotropic response functions were provided.");
+    }
   }
   
   opt = get_options("eext");

commit 7002ffa6b1a565f1458ebf535b66006dd77fb870

Fix mistake introduced in 33207e7.

modules/gt/src/externalenergy.cpp
@@ -44,6 +44,7 @@ namespace MR {
             beta(props.beta), mu(props.ppot*M_sqrt4PI), dE(0.0)
         {
           DEBUG("Initialise computation of external energy.");
+          
           // Create images --------------------------------------------------------------
           Header header (dwimage);
           header.datatype() = DataType::Float32;
@@ -51,9 +52,10 @@ namespace MR {
           tod = Image<float>::scratch(header, "TOD image");
           
           if (nf > 0) {
-            WARN("No isotropic response functions provided; using single-tissue white matter model.");
             header.size(3) = nf;
             fiso = Image<float>::scratch(header, "isotropic fractions");
+          } else {
+            WARN("No isotropic response functions provided; using single-tissue white matter model.");
           }
           
           header.set_ndim(3);