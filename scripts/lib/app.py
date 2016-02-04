3bec818ba0679d3bcfd9379a2b9c163bd1588d2a
Robert Smith <r.smith@brain.org.au>
2016-02-04 18:31:51 +1100

    Scripts: Some backend changes
    - Removed function moveFileToDest: This would not work as desired if output image format consisted of more than one file. Direct calls to mrconvert are instead used.
    - Citation database has been changed to have a boolean flag whether or not the reference is external to MRtrix.
    - Made -help option explicit, as it helped with managing subparsers.
    - Added standard option -force, which will enable MRtrix's -force option on the final mrconvert call; presence of output files at script commencement is also checked.

--- scripts/lib/app.py
+++ scripts/lib/app.py
@@ -8,6 +8,7 @@ citationWarning = ''
 cleanup = True
 epilog = ''
 lastFile = ''
+mrtrixForce = ''
 mrtrixQuiet = '-quiet'
 mrtrixNThreads = ''
 numArgs = 0
@@ -38,19 +39,20 @@ def initCitations(cmdlist):
 
   import lib.citations
   global epilog, citationWarning
-  max_level = 0
+  external_refs = False
   for name in cmdlist:
     entry = [item for item in lib.citations.list if item[0] == name][0]
     if entry:
-      max_level = max(max_level, entry[1]);
+      if entry[1]:
+        external_refs = True
       # Construct string containing all relevant citations that will be fed to the argument parser epilog
       if not epilog:
         epilog = 'Relevant citations for tools / algorithms used in this script:\n\n'
       epilog += entry[0] + ':\n' + entry[2] + '\n\n'
 
-  if max_level:
+  if epilog:
     citationWarning += 'Note that this script makes use of commands / algorithms that have relevant articles for citation'
-    if max_level > 1:
+    if external_refs:
       citationWarning += '; INCLUDING FROM EXTERNAL SOFTWARE PACKAGES'
     citationWarning += '. Please consult the help page (-help option) for more information.'
 
@@ -60,9 +62,11 @@ def initCitations(cmdlist):
 def initParser(desc):
   import argparse
   global epilog, parser
-  parser = Parser(description=desc, epilog=epilog, formatter_class=argparse.RawDescriptionHelpFormatter)
+  parser = Parser(description=desc, epilog=epilog, add_help=False, formatter_class=argparse.RawDescriptionHelpFormatter)
   standard_options = parser.add_argument_group('standard options')
   standard_options.add_argument('-continue', nargs=2, dest='cont', metavar=('<TempDir>', '<LastFile>'), help='Continue the script from a previous execution; must provide the temporary directory path, and the name of the last successfully-generated file')
+  standard_options.add_argument('-force', action='store_true', help='Force overwrite of output files if pre-existing')
+  standard_options.add_argument('-help', action='store_true', help='Display help information for the script')
   standard_options.add_argument('-nocleanup', action='store_true', help='Do not delete temporary directory at script completion')
   standard_options.add_argument('-nthreads', metavar='number', help='Use this number of threads in MRtrix multi-threaded applications (0 disables multi-threading)')
   standard_options.add_argument('-tempdir', metavar='/path/to/tmp/', help='Manually specify the path in which to generate the temporary directory')
@@ -80,6 +84,9 @@ def initialise():
   global colourClear, colourConsole, colourError, colourPrint, colourWarn
   workingDir = os.getcwd()
   args = parser.parse_args()
+  if args.help:
+    parser.print_help()
+    sys.exit(0)
 
   use_colour = readMRtrixConfSetting('TerminalColor')
   if use_colour:
@@ -137,6 +144,22 @@ def initialise():
 
 
 
+def checkOutputFile(path):
+  import os
+  from lib.errorMessage import errorMessage
+  from lib.warnMessage  import warnMessage
+  global args, mrtrixForce
+  if not path:
+    return
+  if os.path.exists(path):
+    if args.force:
+      warnMessage('Output file already exists; will be overwritten at script completion')
+      mrtrixForce = ' -force'
+    else:
+      errorMessage('Output file already exists (use -force to override)')
+      sys.exit(1)
+
+
 
 def gotoTempDir():
   import os
@@ -144,16 +167,6 @@ def gotoTempDir():
   if verbosity:
     printMessage('Changing to temporary directory (' + tempDir + ')')
   os.chdir(tempDir)
-
-
-
-def moveFileToDest(local_path, destination):
-  import os, shutil
-  from lib.printMessage import printMessage
-  if destination[0] != '/':
-    destination = os.path.abspath(os.path.join(workingDir, destination))
-  printMessage('Moving output file from temporary directory to user specified location')
-  shutil.move(local_path, destination)
 
 
 
--- scripts/lib/citations.py
+++ scripts/lib/citations.py
@@ -1,12 +1,15 @@
 # First element: Command / publication name
-# Second element: Citation warning level (1 = MRtrix citation, 2 = non-MRtrix citation)
+# Second element: True if citation is external to MRtrix3 collective
 # Third element: Full citation detail
 
-list = [ ( 'ACT',           1,  'Smith, R. E.; Tournier, J.-D.; Calamante, F. & Connelly, A. Anatomically-constrained tractography: Improved diffusion MRI streamlines tractography through effective use of anatomical information. NeuroImage, 2012, 62, 1924-1938'),
-         ( 'bet',           2, 'Smith, S. M. Fast robust automated brain extraction. Human Brain Mapping, 2002, 17, 143-155'),
-         ( 'eddy/topup',    2, 'Andersson, J. L. & Sotiropoulos, S. N. An integrated approach to correction for off-resonance effects and subject movement in diffusion MR imaging. NeuroImage, doi:10.1016/j.neuroimage.2015.10.019'),
-         ( 'fast',          2, 'Zhang, Y.; Brady, M. & Smith, S. Segmentation of brain MR images through a hidden Markov random field model and the expectation-maximization algorithm. IEEE Transactions on Medical Imaging, 2001, 20, 45-57'),
-         ( 'first',         2, 'Patenaude, B.; Smith, S. M.; Kennedy, D. N. & Jenkinson, M. A Bayesian model of shape and appearance for subcortical brain segmentation. NeuroImage, 2011, 56, 907-922'),
-         ( 'FSL',           2, 'Smith, S. M.; Jenkinson, M.; Woolrich, M. W.; Beckmann, C. F.; Behrens, T. E.; Johansen-Berg, H.; Bannister, P. R.; De Luca, M.; Drobnjak, I.; Flitney, D. E.; Niazy, R. K.; Saunders, J.; Vickers, J.; Zhang, Y.; De Stefano, N.; Brady, J. M. & Matthews, P. M. Advances in functional and structural MR image analysis and implementation as FSL. NeuroImage, 2004, 23, S208-S219'),
-         ( 'SIFT_followup', 1, 'Smith, R. E.; Tournier, J.-D.; Calamante, F. & Connelly, A. The effects of SIFT on the reproducibility and biological accuracy of the structural connectome. NeuroImage, 2015, 104, 253-265'),
-         ( 'Skare2010',     2, 'Skare, S. & Bammer, R. Jacobian weighting of distortion corrected EPI data. Proceedings of the International Society for Magnetic Resonance in Medicine, 2010, 5063') ]
+list = [ ( 'ACT',           False, 'Smith, R. E.; Tournier, J.-D.; Calamante, F. & Connelly, A. Anatomically-constrained tractography: Improved diffusion MRI streamlines tractography through effective use of anatomical information. NeuroImage, 2012, 62, 1924-1938'),
+         ( 'bet',           True,  'Smith, S. M. Fast robust automated brain extraction. Human Brain Mapping, 2002, 17, 143-155'),
+         ( 'eddy/topup',    True,  'Andersson, J. L. & Sotiropoulos, S. N. An integrated approach to correction for off-resonance effects and subject movement in diffusion MR imaging. NeuroImage, doi:10.1016/j.neuroimage.2015.10.019'),
+         ( 'fast',          True,  'Zhang, Y.; Brady, M. & Smith, S. Segmentation of brain MR images through a hidden Markov random field model and the expectation-maximization algorithm. IEEE Transactions on Medical Imaging, 2001, 20, 45-57'),
+         ( 'first',         True,  'Patenaude, B.; Smith, S. M.; Kennedy, D. N. & Jenkinson, M. A Bayesian model of shape and appearance for subcortical brain segmentation. NeuroImage, 2011, 56, 907-922'),
+         ( 'FSL',           True,  'Smith, S. M.; Jenkinson, M.; Woolrich, M. W.; Beckmann, C. F.; Behrens, T. E.; Johansen-Berg, H.; Bannister, P. R.; De Luca, M.; Drobnjak, I.; Flitney, D. E.; Niazy, R. K.; Saunders, J.; Vickers, J.; Zhang, Y.; De Stefano, N.; Brady, J. M. & Matthews, P. M. Advances in functional and structural MR image analysis and implementation as FSL. NeuroImage, 2004, 23, S208-S219'),
+         ( 'SIFT_followup', False, 'Smith, R. E.; Tournier, J.-D.; Calamante, F. & Connelly, A. The effects of SIFT on the reproducibility and biological accuracy of the structural connectome. NeuroImage, 2015, 104, 253-265'),
+         ( 'Skare2010',     True,  'Skare, S. & Bammer, R. Jacobian weighting of distortion corrected EPI data. Proceedings of the International Society for Magnetic Resonance in Medicine, 2010, 5063'),
+         ( 'Tax2014',       True,  'Tax, C. M.; Jeurissen, B.; Vos, S. B.; Viergever, M. A. & Leemans, A. Recursive calibration of the fiber response function for spherical deconvolution of diffusion MRI data. NeuroImage, 2014, 86, 67-80'),
+         ( 'Tournier2013',  False, 'Tournier, J.-D.; Calamante, F. & Connelly, A. Determination of the appropriate b value and number of gradient directions for high-angular-resolution diffusion-weighted imaging. NMR Biomedicine, 2013, 26, 1775-1786') ]
+
