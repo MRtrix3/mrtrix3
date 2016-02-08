ebfc694e62a0178afe181d9edc0b5e0187cfd208
rtabbara <rtabbara@gmail.com>
2016-02-09 10:37:16 +1100

    User docs: Adding windows install
    * As suggested, we're exclusively showing installation steps using MSYS2

--- docs/conf.py
+++ docs/conf.py
@@ -284,7 +284,15 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = ["_themes",] #[sphinx_rtd_theme.get_html_theme_path()]
-
+    html_style = 'css/theme.css'
+else:
+    html_context = { 
+    'css_files': [
+        'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+        'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+        '_static/css/theme.css',
+        ],  
+    }   
 # Documents to append as an appendix to all manuals.
 #texinfo_appendices = []
 
