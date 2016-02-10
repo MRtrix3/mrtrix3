61a2febf6c6ea124546982deaf96c834e0b401b2
Max Pietsch <mail@maxpietsch.com>
2016-02-10 14:58:27 +0000

    registration: split affine class into header and source for sake of compilation time

--- lib/registration/transform/affine.h
+++ lib/registration/transform/affine.h
@@ -1,231 +1,73 @@
 /*
-   Copyright 2012 Brain Research Institute, Melbourne, Australia
-
-   Written by David Raffelt, 24/02/2012
-
-   This file is part of MRtrix.
-
-   MRtrix is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   MRtrix is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with MRtrix.  If not, see <http://www.gnu.org/licenses/>.
-
+ * Copyright (c) 2008-2016 the MRtrix3 contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * MRtrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * For more details, see www.mrtrix.org
+ *
  */
 
 #ifndef __registration_transform_affine_h__
 #define __registration_transform_affine_h__
 
 #include "registration/transform/base.h"
-#include "math/gradient_descent.h"
 #include "types.h"
+#include "math/math.h"
 #include "math/median.h"
-#include <algorithm> // std::min_element
-#include <iterator>
 
 using namespace MR::Math;
 
 namespace MR
 {
-  namespace Math
+  namespace Registration
   {
+    namespace Transform
+    {
 
-    class AffineLinearNonSymmetricUpdate {
-      public:
-        template <typename ValueType>
-          inline bool operator() (Eigen::Matrix<ValueType, Eigen::Dynamic, 1>& newx,
-              const Eigen::Matrix<ValueType, Eigen::Dynamic, 1>& x,
-              const Eigen::Matrix<ValueType, Eigen::Dynamic, 1>& g,
-              ValueType step_size) {
+      class AffineLinearNonSymmetricUpdate {
+        public:
+          bool operator() (Eigen::Matrix<default_type, Eigen::Dynamic, 1>& newx,
+              const Eigen::Matrix<default_type, Eigen::Dynamic, 1>& x,
+              const Eigen::Matrix<default_type, Eigen::Dynamic, 1>& g,
+              default_type step_size) {
             assert (newx.size() == x.size());
             assert (g.size() == x.size());
             newx = x - step_size * g;
             return !(newx.isApprox(x));
-          }
-    };
+        }
+      };
 
-    class AffineUpdate {
-      public:
-        template <typename ValueType>
-          inline bool operator() (Eigen::Matrix<ValueType, Eigen::Dynamic, 1>& newx,
-              const Eigen::Matrix<ValueType, Eigen::Dynamic, 1>& x,
-              const Eigen::Matrix<ValueType, Eigen::Dynamic, 1>& g,
-              ValueType step_size) {
-            assert (newx.size() == 12);
-            assert (x.size() == 12);
-            assert (g.size() == 12);
-
-            Eigen::Matrix<ValueType, 12, 1> delta;
-            Eigen::Matrix<ValueType, 4, 4> X, Delta, G, A, Asqrt, B, Bsqrt, Bsqrtinv, Xnew, P, Diff;
-            Registration::Transform::param_vec2mat(g, G);
-            Registration::Transform::param_vec2mat(x, X);
-
-            // enforce updates in the range of small angles
-            if (step_size * G.block(0,0,3,3).array().abs().maxCoeff() > 0.2) {
-              step_size = 0.2 / G.block(0,0,3,3).array().abs().maxCoeff();
-            }
-            // use control points and coherence length as update criterion
-            if (control_points.size()) {
-              P = control_points;
-              const ValueType orig_step_size(step_size);
-              const ValueType step_down_factor(0.5);
-              while (true) {
-                delta = g * step_size;
-                Registration::Transform::param_vec2mat(delta, Delta);
-                if ((X+Delta).determinant() <= 0.0){ step_size *= step_down_factor; continue; }
-                Diff.noalias() = ((X+Delta) * P - X * P).cwiseAbs();
-                if ((Diff.template block<3,1>(0,0) - coherence_distance).maxCoeff() > 0.0) { step_size *= step_down_factor; continue;}
-                if ((Diff.template block<3,1>(0,1) - coherence_distance).maxCoeff() > 0.0) { step_size *= step_down_factor; continue;}
-                if ((Diff.template block<3,1>(0,2) - coherence_distance).maxCoeff() > 0.0) { step_size *= step_down_factor; continue;}
-                if ((Diff.template block<3,1>(0,3) - coherence_distance).maxCoeff() > 0.0) { step_size *= step_down_factor; continue;}
-
-                A = X - Delta;
-                A(3,3) = 1.0;
-                if (A.determinant() <= 0.0){ step_size *= step_down_factor; continue; }
-
-                B = X.inverse() + Delta;
-                B(3,3) = 1.0;
-                if (B.determinant() <= 0.0){ step_size *= step_down_factor; continue; }
-
-                Asqrt = A.sqrt().eval();
-                assert(A.isApprox(Asqrt * Asqrt));
-                Bsqrt = B.sqrt().eval();
-                assert(B.isApprox(Bsqrt * Bsqrt));
-                Bsqrtinv = Bsqrt.inverse().eval();
-
-                Xnew = (Asqrt * Bsqrtinv) - ((Asqrt * Bsqrtinv - Bsqrtinv * Asqrt) * 0.5);
-                Diff.noalias() = ((Xnew) * P - X * P).cwiseAbs();
-                if ((Diff.template block<3,1>(0,0) - coherence_distance).maxCoeff() > 0.0) { step_size *= step_down_factor; continue;}
-                if ((Diff.template block<3,1>(0,1) - coherence_distance).maxCoeff() > 0.0) { step_size *= step_down_factor; continue;}
-                if ((Diff.template block<3,1>(0,2) - coherence_distance).maxCoeff() > 0.0) { step_size *= step_down_factor; continue;}
-                if ((Diff.template block<3,1>(0,3) - coherence_distance).maxCoeff() > 0.0) { step_size *= step_down_factor; continue;}
-
-                break;
-              }
-              if (orig_step_size != step_size) { DEBUG("step size changed from " + str(orig_step_size) + " to " + str(step_size)); }
-            } else {
-              // reduce step size if determinant of matrix becomes negative (happens rarely at first few iterations)
-              size_t cnt = 0;
-              const default_type factor(0.9);
-              while (true) {
-                delta = g * step_size;
-                Registration::Transform::param_vec2mat(delta, Delta);
-                if (Delta.block(0,0,3,3).array().abs().maxCoeff() > 0.1) {
-                  step_size = 0.09 / G.block(0,0,3,3).array().abs().maxCoeff();
-                  INFO(str(step_size) + " " + str(g * step_size));
-                  continue;
-                }
-                if (Delta.block(0,3,3,1).array().abs().maxCoeff() > 10.0){
-                  step_size = 9.0 / G.block(0,3,3,1).array().abs().maxCoeff();
-                  INFO(str(step_size) + " " + str(g * step_size));
-                  continue;
-                }
-                A = X - Delta;
-                A(3,3) = 1.0;
-                if (A.determinant() < 0) {
-                  step_size *= factor;
-                  ++cnt;
-                } else {
-                  break;
-                }
-              }
-              if (cnt > 0) INFO("affine: gradient descent step size was too large. Multiplied by factor "
-               + str(std::pow (factor, cnt), 4) + " (now: "+ str(step_size, 4) + ")");
-
-              B = X.inverse() + Delta;
-              B(3,3) = 1.0;
-              assert(B.determinant() > 0.0);
-              Asqrt = A.sqrt().eval();
-              assert(A.isApprox(Asqrt * Asqrt));
-              Bsqrt = B.sqrt().eval();
-              assert(B.isApprox(Bsqrt * Bsqrt));
-              Bsqrtinv = Bsqrt.inverse().eval();
-
-              // approximation for symmetry reasons as
-              // A and B don't commute
-              Xnew = (Asqrt * Bsqrtinv) - ((Asqrt * Bsqrtinv - Bsqrtinv * Asqrt) * 0.5);
-            }
-
-
-            // MAT(X);
-            // MAT(Xnew);
-            // MAT(X.inverse());
-            // MAT(Delta);
-            // MAT(Asqrt);
-            // MAT(Bsqrtinv);
-            Registration::Transform::param_mat2vec(Xnew, newx);
-
-            // stop criterion based on max shift of control points
-            if (control_points.size()) {
-              Diff.row(0) *= recip_spacing(0);
-              Diff.row(1) *= recip_spacing(1);
-              Diff.row(2) *= recip_spacing(2);
-              Diff.colwise() -= stop_len;
-              // MAT(Diff);
-              if (Diff.template block<3,4>(0,0).maxCoeff() <= 0.0) {
-                DEBUG("max control point movement (" + str(Diff.template block<3,4>(0,0).maxCoeff()) +
-                ") smaller than tolerance" );
-                return false;
-              }
-            }
-// #ifdef REGISTRATION_GRADIENT_DESCENT_DEBUG
-//             if (newx.isApprox(x)){
-//               ValueType debug = 0;
-//               for (ssize_t i=0; i<newx.size(); ++i){
-//                 debug += std::abs(newx[i]-x[i]);
-//               }
-//               INFO("affine update parameter cumulative change: " + str(debug));
-//               VEC(newx);
-//               VEC(g);
-//               VAR(step_size);
-//             }
-// #endif
-            return !(newx.isApprox(x));
-          }
+      class AffineUpdate {
+        public:
+          bool operator() (Eigen::Matrix<default_type, Eigen::Dynamic, 1>& newx,
+              const Eigen::Matrix<default_type, Eigen::Dynamic, 1>& x,
+              const Eigen::Matrix<default_type, Eigen::Dynamic, 1>& g,
+              default_type step_size);
 
           void set_control_points (
             const Eigen::Matrix<default_type, Eigen::Dynamic, Eigen::Dynamic>& points,
             const Eigen::Vector3d& coherence_dist,
             const Eigen::Vector3d& stop_length,
-            const Eigen::Vector3d& voxel_spacing ) {
-            assert(points.rows() == 4);
-            assert(points.cols() == 4);
-            control_points = points;
-            coherence_distance = coherence_dist;
-            stop_len << stop_length, 0.0;
-            recip_spacing << voxel_spacing.cwiseInverse(), 1.0;
-          }
+            const Eigen::Vector3d& voxel_spacing );
 
         private:
           Eigen::Matrix<default_type, Eigen::Dynamic, Eigen::Dynamic> control_points;
           Eigen::Vector3d coherence_distance;
           Eigen::Matrix<default_type, 4, 1> stop_len, recip_spacing;
-    };
-  }
+      };
 
-  namespace Registration
-  {
-    namespace Transform
-    {
       class AffineRobustEstimator {
-      public:
-        template <typename ValueType>
-          inline bool operator() (Eigen::Matrix<ValueType, Eigen::Dynamic, 1>& newx,
-              const Eigen::Matrix<ValueType, Eigen::Dynamic, 1>& x,
-              const Eigen::Matrix<ValueType, Eigen::Dynamic, 1>& g,
-              ValueType step_size) {
-            assert (newx.size() == x.size());
-            assert (g.size() == x.size());
-            newx = x - step_size * g;
-            return !(newx.isApprox(x));
-          }
+        public:
+          inline bool operator() (Eigen::Matrix<default_type, Eigen::Dynamic, 1>& newx,
+              const Eigen::Matrix<default_type, Eigen::Dynamic, 1>& x,
+              const Eigen::Matrix<default_type, Eigen::Dynamic, 1>& g,
+              default_type step_size);
       };
 
 
@@ -245,7 +87,7 @@ namespace MR
 
           typedef typename Base::ParameterType ParameterType;
           // typedef Math::AffineLinearNonSymmetricUpdate UpdateType;
-          typedef Math::AffineUpdate UpdateType;
+          typedef AffineUpdate UpdateType;
           // typedef Math::ParameterUpdateCheck UpdateCheckType;
           typedef AffineRobustEstimator RobustEstimatorType;
           typedef int has_robust_estimator;
@@ -255,32 +97,13 @@ namespace MR
             this->optimiser_weights << weights, weights, weights;
           }
 
-          Eigen::Matrix<default_type, 4, 1> get_jacobian_vector_wrt_params (const Eigen::Vector3& p) const {
-            Eigen::Matrix<default_type, 4, 1> jac;
-            jac.head(3) = p - centre;
-            jac(3) = 1.0;
-            return jac;
-          }
+          Eigen::Matrix<default_type, 4, 1> get_jacobian_vector_wrt_params (const Eigen::Vector3& p) const ;
 
-          Eigen::MatrixXd get_jacobian_wrt_params (const Eigen::Vector3& p) const {
-            Eigen::MatrixXd jacobian (3, 12);
-            jacobian.setZero();
-            const auto v = get_jacobian_vector_wrt_params(p);
-            jacobian.block<1, 4>(0, 0) = v;
-            jacobian.block<1, 4>(1, 4) = v;
-            jacobian.block<1, 4>(2, 8) = v;
-            return jacobian;
-          }
+          Eigen::MatrixXd get_jacobian_wrt_params (const Eigen::Vector3& p) const ;
 
-          void set_parameter_vector (const Eigen::Matrix<ParameterType, Eigen::Dynamic, 1>& param_vector) {
-            this->trafo.matrix() = Eigen::Map<const Eigen::Matrix<ParameterType, 3, 4, Eigen::RowMajor> >(&param_vector(0));
-            this->compute_halfspace_transformations();
-          }
+          void set_parameter_vector (const Eigen::Matrix<ParameterType, Eigen::Dynamic, 1>& param_vector);
 
-          void get_parameter_vector (Eigen::Matrix<ParameterType, Eigen::Dynamic, 1>& param_vector) const {
-            param_vector.resize (12);
-            param_mat2vec (this->trafo.matrix(), param_vector);
-          }
+          void get_parameter_vector (Eigen::Matrix<ParameterType, Eigen::Dynamic, 1>& param_vector) const;
 
           UpdateType* get_gradient_descent_updator () {
             return &gradient_descent_updator;
