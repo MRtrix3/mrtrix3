e1a76daa9b5468f7ec37834def95eb89bd7b3cf6
Max Pietsch <mail@maxpietsch.com>
2016-03-01 18:24:53 +0000

    registration: added option reg_rot_search_scale

--- lib/registration/linear.h
+++ lib/registration/linear.h
@@ -237,9 +237,11 @@ namespace MR
               Transform::Init::initialise_using_FOD (im1_image, im2_image, im1_mask, im2_mask, transform);
             else if (init_type == Transform::Init::set_centre_mass)
               Transform::Init::set_centre_using_image_mass (im1_image, im2_image, im1_mask, im2_mask, transform); // transform is set in mrregister.cpp
-            else if (init_type == Transform::Init::rot_search)
+            else if (init_type == Transform::Init::rot_search) {
+              default_type reg_search_scale = File::Config::get_float ("reg_rot_search_scale", 0.2);
               Transform::Init::initialise_using_rotation_search_around_image_mass (
-                im1_image, im2_image, im1_mask, im2_mask, transform, 0.1, global_search);
+                im1_image, im2_image, im1_mask, im2_mask, transform, reg_search_scale, global_search);
+            }
             // transform.debug();
 
             // if (global_search) {
