8dc4bccdbe3e260ae73d3b87902ee996da953713
Robert Smith <robert.smith@florey.edu.au>
2016-05-23 14:37:55 +1000

    dwi2response msmt_5tt: Propagate settings to recursive call
    The recursive call to dwi2response to obtain a white matter response function will now create its temporary directory within the temporary directory used by the calling script.
    It will also propagate the -nocleanup option to the recursive call if it was used for the msmt_5tt algorithm.

--- scripts/src/dwi2response/msmt_5tt.py
+++ scripts/src/dwi2response/msmt_5tt.py
@@ -82,7 +82,10 @@ def execute():
 
   # Revise WM mask to only include single-fibre voxels
   printMessage('Calling dwi2response recursively to select WM single-fibre voxels using \'' + lib.app.args.wm_algo + '\' algorithm')
-  runCommand('dwi2response -quiet ' + lib.app.args.wm_algo + ' dwi.mif wm_ss_response.txt -mask wm_mask.mif -voxels wm_sf_mask.mif')
+  recursive_cleanup_option=''
+  if not lib.app.args.cleanup:
+    recursive_cleanup_option = ' -nocleanup'
+  runCommand('dwi2response -quiet ' + lib.app.args.wm_algo + ' dwi.mif wm_ss_response.txt -mask wm_mask.mif -voxels wm_sf_mask.mif -tempdir ' + lib.app.tempDir + recursive_cleanup_option)
 
   # Check for empty masks
   gm_voxels  = int(getImageStat('gm_mask.mif',    'count', 'gm_mask.mif'))
