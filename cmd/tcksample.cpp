f4449b7008b18a98bb9651da01982b8f87486c38
Robert Smith <r.smith@brain.org.au>
2016-06-06 17:02:11 +1000

    Update contact details on some commands

--- cmd/tcksample.cpp
+++ cmd/tcksample.cpp
@@ -39,8 +39,6 @@ const char* statistics[] = { "mean", "median", "min", "max", nullptr };
 
 void usage ()
 {
-  AUTHOR = "J-Donald Tournier (jdtournier@gmail.com)";
-
   AUTHOR = "Robert E. Smith (robert.smith@florey.edu.au)";
 
   DESCRIPTION
