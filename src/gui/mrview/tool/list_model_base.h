dc9e406c87cadc0cd698a8ba1ce171b59a0e74ac
rtabbara <rtabbara@gmail.com>
2016-06-22 16:11:03 +1000

    Adding drag and drop support for tools #560
    * Can reorder listview items for Overlay and ROI
    * Can drag and load files for Overlay, ROI, Tractography and Vector plot

--- src/gui/mrview/tool/list_model_base.h
+++ src/gui/mrview/tool/list_model_base.h
@@ -35,12 +35,14 @@ namespace MR
               QAbstractItemModel (parent) { }
 
             QVariant data (const QModelIndex& index, int role) const {
+              // The item may (temporarily) be null during an intermediate step of reordering items
+              // see insertRows / removeRows
               if (!index.isValid()) return QVariant();
               if (role == Qt::CheckStateRole) {
-                return items[index.row()]->show ? Qt::Checked : Qt::Unchecked;
+                return items[index.row()] && items[index.row()]->show ? Qt::Checked : Qt::Unchecked;
               }
               if (role != Qt::DisplayRole) return QVariant();
-              return shorten (items[index.row()]->get_filename(), 35, 0).c_str();
+              return items[index.row()] ? shorten (items[index.row()]->get_filename(), 35, 0).c_str() : "";
             }
 
             bool setData (const QModelIndex& idx, const QVariant& value, int role) {
@@ -63,9 +65,62 @@ namespace MR
               return QAbstractItemModel::setData (idx, value, role);
             }
 
+            Qt::DropActions supportedDropActions () const override
+            {
+              return Qt::CopyAction | Qt::MoveAction;
+            }
+
+            // For some reason, Qt calls insertRows prior to removeRows in the event of a drag-n-drop
+            // item reordering within a given model.
+            // Hence at this point, we simply want to cache where the rows should be moved
+            bool insertRows(int row, int count, const QModelIndex &) override {
+              if (count < 1 || row < 0 || row > rowCount ()) {
+                swapped_rows = { 0, 0 };
+                return false;
+              }
+
+              swapped_rows = { row, count };
+
+              return true;
+            }
+
+            // As alluded above in insertRows, in the case of a drag-n-drop item reordering,
+            // we have to manually perform the swap within our underlying data store
+            bool removeRows (int row, int count, const QModelIndex& parent = QModelIndex()) override {
+              if (count < 1 || row < 0 || row > rowCount () || count != swapped_rows.second)
+                return false;
+
+              std::vector<std::unique_ptr<Displayable>> swapped_items;
+
+              swapped_items.insert ( swapped_items.begin(),
+                std::make_move_iterator (items.begin() + row),
+                std::make_move_iterator (items.begin() + row + count));
+
+              beginRemoveRows (parent, row, row + count - 1);
+              items.erase (items.begin() + row, items.begin() + row + count);
+              endRemoveRows ();
+
+              // Cached row index was prior to removal, so may need to adjust offset
+              if (swapped_rows.first >= row)
+                swapped_rows.first -= count;
+
+              beginInsertRows (parent, swapped_rows.first, swapped_rows.first + swapped_rows.second - 1);
+              items.insert (items.begin() + swapped_rows.first,
+                std::make_move_iterator (swapped_items.begin ()),
+                std::make_move_iterator (swapped_items.end ()));
+              endInsertRows ();
+
+              return true;
+            }
+
             Qt::ItemFlags flags (const QModelIndex& index) const {
-              if (!index.isValid()) return 0;
-              return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsUserCheckable;
+
+              static constexpr auto valid_flags = Qt::ItemIsDragEnabled | Qt::ItemIsEnabled |
+                Qt::ItemIsSelectable | Qt::ItemIsUserCheckable;
+              static constexpr auto invalid_flags = valid_flags | Qt::ItemIsDropEnabled;
+
+              if (!index.isValid()) return invalid_flags;
+              return valid_flags;
             }
 
             QModelIndex index (int row, int column, const QModelIndex& parent = QModelIndex()) const { 
@@ -92,6 +147,8 @@ namespace MR
             }
 
             std::vector<std::unique_ptr<Displayable>> items;
+          private:
+            std::pair<int, int> swapped_rows;
         };
 
 
