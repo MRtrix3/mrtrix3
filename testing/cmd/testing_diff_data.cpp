23682e377f36d89c526ccd87f0b5dd73aa574732
Robert Smith <robert.smith@florey.edu.au>
2016-09-11 00:04:34 +1000

    testing_diff_data: New option -voxel
    Sets threshold for testing of voxel data based on the maximal value within each voxel. Only works for 4D images.

--- testing/cmd/testing_diff_data.cpp
+++ testing/cmd/testing_diff_data.cpp
@@ -26,6 +26,7 @@
 #include "datatype.h"
 
 #include "image.h"
+#include "algo/loop.h"
 #include "algo/threaded_loop.h"
 
 using namespace MR;
@@ -33,7 +34,7 @@ using namespace App;
 
 void usage ()
 {
-  AUTHOR = "J-Donald Tournier (jdtournier@gmail.com) and David Raffelt (david.raffelt@florey.edu.au)";
+  AUTHOR = "J-Donald Tournier (jdtournier@gmail.com) and David Raffelt (david.raffelt@florey.edu.au) and Robert E. Smith (robert.smith@florey.edu.au)";
 
   DESCRIPTION
   + "compare two images for differences, optionally with a specified tolerance.";
@@ -46,6 +47,8 @@ void usage ()
   + Option ("abs", "specify an absolute tolerance") 
     + Argument ("tolerance").type_float (0.0)
   + Option ("frac", "specify a fractional tolerance") 
+    + Argument ("tolerance").type_float (0.0)
+  + Option ("voxel", "specify a fractional tolerance relative to the maximum value in the voxel")
     + Argument ("tolerance").type_float (0.0);
 }
 
@@ -80,20 +83,48 @@ void run ()
         throw Exception ("images \"" + a.name() + "\" and \"" + b.name() + "\" do not match within fractional precision of " + str(tol)
              + " (" + str(cdouble (a.value())) + " vs " + str(cdouble (b.value())) + ")");
         }, in1, in2);
-  
-  } else {
-  
-    double tol = 0.0;
-    opt = get_options ("abs");
-    if (opt.size())
-      tol = opt[0][0];
 
-    ThreadedLoop (in1)
-    .run ([&tol] (const decltype(in1)& a, const decltype(in2)& b) {
-        if (std::abs (a.value() - b.value()) > tol)
-        throw Exception ("images \"" + a.name() + "\" and \"" + b.name() + "\" do not match within absolute precision of " + str(tol)
-             + " (" + str(cdouble (a.value())) + " vs " + str(cdouble (b.value())) + ")");
-        }, in1, in2);
+  } else {
+
+    opt = get_options ("voxel");
+    if (opt.size()) {
+
+      if (in1.ndim() != 4)
+        throw Exception ("Option -voxel only works for 4D images");
+
+      const double tol = opt[0][0];
+
+      auto func = [&tol] (decltype(in1)& a, decltype(in2)& b) {
+        double maxa = 0.0, maxb = 0.0;
+        for (auto l = Loop(3) (a, b); l; ++l) {
+          maxa = std::max (maxa, std::abs (cdouble(a.value())));
+          maxb = std::max (maxb, std::abs (cdouble(b.value())));
+        }
+        const double threshold = tol * 0.5 * (maxa + maxb);
+        for (auto l = Loop(3) (a, b); l; ++l) {
+          if (std::abs (cdouble (a.value()) - cdouble (b.value())) > threshold)
+            throw Exception ("images \"" + a.name() + "\" and \"" + b.name() + "\" do not match within " + str(tol) + " of maximal voxel value"
+                           + " (" + str(cdouble (a.value())) + " vs " + str(cdouble (b.value())) + ")");
+        }
+      };
+
+      ThreadedLoop (in1, 0, 3).run (func, in1, in2);
+  
+    } else {
+  
+      double tol = 0.0;
+      opt = get_options ("abs");
+      if (opt.size())
+        tol = opt[0][0];
+
+      ThreadedLoop (in1)
+      .run ([&tol] (const decltype(in1)& a, const decltype(in2)& b) {
+          if (std::abs (a.value() - b.value()) > tol)
+          throw Exception ("images \"" + a.name() + "\" and \"" + b.name() + "\" do not match within absolute precision of " + str(tol)
+               + " (" + str(cdouble (a.value())) + " vs " + str(cdouble (b.value())) + ")");
+          }, in1, in2);
+
+    }
         
   }
 
