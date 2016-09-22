4379322225560f583a8e3ce6a971a9d80a8f8d29
Dave Raffelt <draffelt@gmail.com>
2016-09-22 17:07:53 +1000

    Ported testing_diff_fixel to new format

--- testing/cmd/testing_diff_data.cpp
+++ testing/cmd/testing_diff_data.cpp
@@ -1,33 +1,25 @@
-
 /*
-    Copyright 2008 Brain Research Institute, Melbourne, Australia
+ * Copyright (c) 2008-2016 the MRtrix3 contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * MRtrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * For more details, see www.mrtrix.org
+ *
+ */
 
-    Written by J-Donald Tournier, 27/06/08.
-
-    This file is part of MRtrix.
-
-    MRtrix is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    MRtrix is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with MRtrix.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
 
 #include "command.h"
-#include "progressbar.h"
 #include "datatype.h"
 
+#include "progressbar.h"
 #include "image.h"
-#include "algo/loop.h"
-#include "algo/threaded_loop.h"
+#include "diff_images.h"
 
 using namespace MR;
 using namespace App;
@@ -44,12 +36,8 @@ void usage ()
   + Argument ("data2", "another image.").type_image_in();
   
   OPTIONS
-  + Option ("abs", "specify an absolute tolerance") 
-    + Argument ("tolerance").type_float (0.0)
-  + Option ("frac", "specify a fractional tolerance") 
-    + Argument ("tolerance").type_float (0.0)
-  + Option ("voxel", "specify a fractional tolerance relative to the maximum value in the voxel")
-    + Argument ("tolerance").type_float (0.0);
+  + Testing::Diff_Image_Options;
+
 }
 
 
@@ -57,76 +45,8 @@ void run ()
 {
   auto in1 = Image<cdouble>::open (argument[0]);
   auto in2 = Image<cdouble>::open (argument[1]);
-  check_dimensions (in1, in2);
-  for (size_t i = 0; i < in1.ndim(); ++i) {
-    if (std::isfinite (in1.size(i)))
-      if (in1.size(i) != in2.size(i))
-        throw Exception ("images \"" + in1.name() + "\" and \"" + in2.name() + "\" do not have matching voxel spacings " +
-                                       str(in1.size(i)) + " vs " + str(in2.size(i)));
-  }
-  for (size_t i  = 0; i < 3; ++i) {
-    for (size_t j  = 0; j < 4; ++j) {
-      if (std::abs (in1.transform().matrix()(i,j) - in2.transform().matrix()(i,j)) > 0.001)
-        throw Exception ("images \"" + in1.name() + "\" and \"" + in2.name() + "\" do not have matching header transforms "
-                           + "\n" + str(in1.transform().matrix()) + "vs \n " + str(in2.transform().matrix()) + ")");
-    }
-  }
 
-  auto opt = get_options ("frac");
-  if (opt.size()) {
-  
-    const double tol = opt[0][0];
-    
-    ThreadedLoop (in1)
-    .run ([&tol] (const decltype(in1)& a, const decltype(in2)& b) {
-        if (std::abs ((a.value() - b.value()) / (0.5 * (a.value() + b.value()))) > tol)
-        throw Exception ("images \"" + a.name() + "\" and \"" + b.name() + "\" do not match within fractional precision of " + str(tol)
-             + " (" + str(cdouble (a.value())) + " vs " + str(cdouble (b.value())) + ")");
-        }, in1, in2);
-
-  } else {
-
-    opt = get_options ("voxel");
-    if (opt.size()) {
-
-      if (in1.ndim() != 4)
-        throw Exception ("Option -voxel only works for 4D images");
-
-      const double tol = opt[0][0];
-
-      auto func = [&tol] (decltype(in1)& a, decltype(in2)& b) {
-        double maxa = 0.0, maxb = 0.0;
-        for (auto l = Loop(3) (a, b); l; ++l) {
-          maxa = std::max (maxa, std::abs (cdouble(a.value())));
-          maxb = std::max (maxb, std::abs (cdouble(b.value())));
-        }
-        const double threshold = tol * 0.5 * (maxa + maxb);
-        for (auto l = Loop(3) (a, b); l; ++l) {
-          if (std::abs (cdouble (a.value()) - cdouble (b.value())) > threshold)
-            throw Exception ("images \"" + a.name() + "\" and \"" + b.name() + "\" do not match within " + str(tol) + " of maximal voxel value"
-                           + " (" + str(cdouble (a.value())) + " vs " + str(cdouble (b.value())) + ")");
-        }
-      };
-
-      ThreadedLoop (in1, 0, 3).run (func, in1, in2);
-  
-    } else {
-  
-      double tol = 0.0;
-      opt = get_options ("abs");
-      if (opt.size())
-        tol = opt[0][0];
-
-      ThreadedLoop (in1)
-      .run ([&tol] (const decltype(in1)& a, const decltype(in2)& b) {
-          if (std::abs (a.value() - b.value()) > tol)
-          throw Exception ("images \"" + a.name() + "\" and \"" + b.name() + "\" do not match within absolute precision of " + str(tol)
-               + " (" + str(cdouble (a.value())) + " vs " + str(cdouble (b.value())) + ")");
-          }, in1, in2);
-
-    }
-        
-  }
+  Testing::diff_images (in1, in2);
 
   CONSOLE ("data checked OK");
 }
