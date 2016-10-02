c78460664eb6838b0d0e21524f3fe5dbbc5dfda8
Robert Smith <robert.smith@florey.edu.au>
2016-10-03 06:22:21 +1100

    maskdump: Option to specify target output file

--- cmd/maskdump.cpp
+++ cmd/maskdump.cpp
@@ -27,10 +27,14 @@ void usage ()
   AUTHOR = "Robert E. Smith (robert.smith@florey.edu.au)";
 
   DESCRIPTION
-  + "Print out the locations of all non-zero voxels in a mask image";
+  + "Print out the locations of all non-zero voxels in a mask image. "
+
+    "If no destination file is specified, the voxel locations will be "
+    "printed to stdout.";
 
   ARGUMENTS
-  + Argument ("input",  "the input image.").type_image_in();
+  + Argument ("input",  "the input image.").type_image_in()
+  + Argument ("output", "The (optional) output text file.").type_file_out().optional();
 }
 
 
@@ -53,5 +57,8 @@ void run ()
   for (size_t row = 0; row != locations.size(); ++row)
     prettyprint.row (row) = std::move (locations[row]);
   INFO ("Printing locations of " + str(prettyprint.rows()) + " non-zero voxels");
-  std::cout << prettyprint;
+  if (argument.size() == 2)
+    save_matrix (prettyprint, argument[1]);
+  else
+    std::cout << prettyprint;
 }
