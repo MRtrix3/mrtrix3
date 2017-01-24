MRtrix3/mrtrix3

commit 05e3e6542d450ed9fe70383d35a7d174ebaaf76e

MacOSX: fix touchpad scrolling

src/gui/mrview/window.cpp
@@ -1524,7 +1524,7 @@ namespace MR
       {
         assert (mode);
 #if QT_VERSION >= 0x050400
-        QPoint delta = event->pixelDelta();
+        QPoint delta = 30 * event->pixelDelta();
         if (delta.isNull())
           delta = event->angleDelta();
 #else

src/gui/mrview/mode/base.cpp
@@ -151,7 +151,7 @@ done_painting:
         void Base::mouse_press_event () { }
         void Base::mouse_release_event () { }
 
-        void Base::slice_move_event (int x) 
+        void Base::slice_move_event (float x) 
         {
           const Projection* proj = get_current_projection();
           if (!proj) return;

src/gui/mrview/mode/base.h
@@ -84,7 +84,7 @@ namespace MR
             virtual void mouse_press_event ();
             virtual void mouse_release_event ();
             virtual void reset_event ();
-            virtual void slice_move_event (int x);
+            virtual void slice_move_event (float x);
             virtual void set_focus_event ();
             virtual void contrast_event ();
             virtual void pan_event ();

src/gui/mrview/mode/ortho.cpp
@@ -140,7 +140,7 @@ namespace MR
 
 
 
-        void Ortho::slice_move_event (int x) 
+        void Ortho::slice_move_event (float x) 
         {
           const Projection* proj = get_current_projection();
           if (!proj) return;

src/gui/mrview/mode/ortho.h
@@ -40,7 +40,7 @@ namespace MR
             virtual void paint (Projection& projection);
 
             virtual void mouse_press_event ();
-            virtual void slice_move_event (int x);
+            virtual void slice_move_event (float x);
             virtual void panthrough_event ();
             virtual const Projection* get_current_projection () const;
 

src/gui/mrview/mode/volume.cpp
@@ -585,7 +585,7 @@ namespace MR
 
 
 
-        void Volume::slice_move_event (int x) 
+        void Volume::slice_move_event (float x) 
         {
          
           std::vector<GL::vec4*> clip = get_clip_planes_to_be_edited();

src/gui/mrview/mode/volume.h
@@ -43,7 +43,7 @@ namespace MR
               }
 
             virtual void paint (Projection& projection);
-            virtual void slice_move_event (int x);
+            virtual void slice_move_event (float x);
             virtual void pan_event ();
             virtual void panthrough_event ();
             virtual void tilt_event ();

commit 20602717fd0b1b7fdb2a61d6522f7dfdf6315c96

mrview: fix mouse wheel event handling
Problem was introduced by 05e3e6542 to handle scrolling with touchpads,
but resulted in ~4x the step when using regular mouse wheel. This
affected scrolling through slices and zooming.

src/gui/mrview/window.cpp
@@ -1584,9 +1584,11 @@ namespace MR
       {
         assert (mode);
 #if QT_VERSION >= 0x050400
-        QPoint delta = 30 * event->pixelDelta();
-        if (delta.isNull())
+        QPoint delta;
+        if (event->source() == Qt::MouseEventNotSynthesized) 
           delta = event->angleDelta();
+        else
+          delta = 30 * event->pixelDelta();
 #else
         QPoint delta = event->orientation() == Qt::Vertical ? QPoint (0, event->delta()) : QPoint (event->delta(), 0);
 #endif