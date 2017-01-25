654b7281c953f1068142d1f8a152f786862d8876
Thijs Dhollander <thijs.dhollander@gmail.com>
2017-01-25 17:57:11 +1100

    header files

--- lib/signals.h
+++ lib/signals.h
@@ -1,4 +1,16 @@
-// Set the handler for a priori known signals only
+/* Copyright (c) 2008-2017 the MRtrix3 contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * MRtrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * For more details, see http://www.mrtrix.org/.
+ */
+
 #ifdef SIGALRM
     __SIGNAL (SIGALRM, "Timer expiration");
 #endif
