632fa5fb05d63cc366cabf1222576c14f8235e0e
Max Pietsch <mail@maxpietsch.com>
2017-02-23 18:34:03 +0000

    __mrtrix_plugin.h: fix NOMEMALIGN

--- core/__mrtrix_plugin.h
+++ core/__mrtrix_plugin.h
@@ -10,7 +10,7 @@
      // typedef long double max_align_t;
      // #else
      // Define 'max_align_t' to match the GCC definition.
-      typedef struct {
+      typedef struct { NOMEMALIGN
        long long __clang_max_align_nonce1
            __attribute__((__aligned__(__alignof__(long long))));
        long double __clang_max_align_nonce2
