5f94e6e679db360efab66394c0fb90cdf41a3a2d
J-Donald Tournier <jdtournier@gmail.com>
2017-03-10 13:33:28 +0000

    mrconvert: change option names for modifying header properties
    As discussed in #934...

--- cmd/mrconvert.cpp
+++ cmd/mrconvert.cpp
@@ -78,21 +78,32 @@ DESCRIPTION
             "effect for floating-point and binary images.")
   + Argument ("values").type_sequence_float()
 
+
   + OptionGroup ("Options for handling JSON (JavaScript Object Notation) files")
+
   + Option ("json_import", "import data from a JSON file into header key-value pairs")
     + Argument ("file").type_file_in()
+
   + Option ("json_export", "export data from an image header key-value pairs into a JSON file")
     + Argument ("file").type_file_out()
 
-  + OptionGroup ("Options for manipulating image header key/value entries")
-  + Option ("header_cat", "concatenate a header key/value entry with the specified text").allow_multiple()
-    + Argument ("key").type_text()
-    + Argument ("value").type_text()
-  + Option ("header_erase", "erase a header key/value entry").allow_multiple()
-    + Argument ("key").type_text()
-  + Option ("header_set", "set a header key/value entry to the specified text (will erase any existing entry)").allow_multiple()
-    + Argument ("key").type_text()
-    + Argument ("value").type_text()
+
+  + OptionGroup ("Options to modify generic header entries")
+
+  + Option ("clear_property", 
+            "remove the specified key from the image header altogether.").allow_multiple()
+  + Argument ("key").type_text()
+
+  + Option ("set_property", 
+            "set the value of the specified key in the image header.").allow_multiple()
+  + Argument ("key").type_text()
+  + Argument ("value").type_text()
+
+  + Option ("append_property", 
+            "append the given value to the specified key in the image header (this adds the value specified as a new line in the header value).").allow_multiple()
+  + Argument ("key").type_text()
+  + Argument ("value").type_text()
+
 
   + Stride::Options
 
@@ -263,28 +274,27 @@ void run ()
   if (opt.size())
     File::JSON::load (header_out, opt[0][0]);
 
-  {
-    opt = get_options ("header_cat");
-    for (size_t i = 0; i != opt.size(); ++i) {
-      auto entry = header_out.keyval().find (opt[i][0]);
-      if (entry == header_out.keyval().end())
-        header_out.keyval()[opt[i][0]] = std::string(opt[i][1]);
-      else
-        add_line (header_out.keyval()[opt[i][0]], std::string(opt[i][1]));
-    }
-    opt = get_options ("header_erase");
-    for (size_t i = 0; i != opt.size(); ++i) {
-      auto entry = header_out.keyval().find (opt[i][0]);
-      if (entry == header_out.keyval().end()) {
-        WARN ("No header key/value entry \"" + opt[i][0] + "\" found; ignored");
-      } else {
-        header_out.keyval().erase (entry);
-      }
-    }
-    opt = get_options ("header_set");
-    for (size_t i = 0; i != opt.size(); ++i)
-      header_out.keyval()[opt[i][0]] = std::string(opt[i][1]);
+
+
+  opt = get_options ("clear_property");
+  for (size_t n = 0; n < opt.size(); ++n) {
+    auto entry = header_out.keyval().find (opt[n][0]);
+    if (entry == header_out.keyval().end()) 
+      WARN ("No header key/value entry \"" + opt[n][0] + "\" found; ignored");
+    else 
+      header_out.keyval().erase (entry);
   }
+
+  opt = get_options ("set_property");
+  for (size_t n = 0; n < opt.size(); ++n) 
+    header_out.keyval()[opt[n][0].as_text()] = opt[n][1].as_text();
+
+  opt = get_options ("append_property");
+  for (size_t n = 0; n < opt.size(); ++n) 
+    add_line (header_out.keyval()[opt[n][0].as_text()], opt[n][1].as_text());
+
+
+
 
   opt = get_options ("coord");
   vector<vector<int>> pos;
