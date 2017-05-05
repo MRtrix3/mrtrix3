66ae662ed8a7cb09e3b70bb8c0a7399dc252bc8c
Max Pietsch <mail@maxpietsch.com>
2017-05-05 12:14:54 +0100

    registration: demons4D weighted. update using eigen vectors. fix negative robustness_parameter

--- src/registration/metric/demons4D.h
+++ src/registration/metric/demons4D.h
@@ -19,6 +19,7 @@
 
 #include "image_helpers.h"
 #include "adapter/gradient3D.h"
+#include "registration/multi_contrast.h"
 
 namespace MR
 {
@@ -27,26 +28,40 @@ namespace MR
     namespace Metric
     {
 
-      template <class Im1ImageType, class Im2ImageType, class Im1MaskType, class Im2MaskType> 
+      template <class Im1ImageType, class Im2ImageType, class Im1MaskType, class Im2MaskType>
       class Demons4D { MEMALIGN(Demons4D<Im1ImageType,Im2ImageType,Im1MaskType,Im2MaskType>)
         public:
           Demons4D (default_type& global_energy, size_t& global_voxel_count,
-                     const Im1ImageType& im1_image, const Im2ImageType& im2_image, const Im1MaskType im1_mask, const Im2MaskType im2_mask) :
+                     const Im1ImageType& im1_image, const Im2ImageType& im2_image, const Im1MaskType im1_mask, const Im2MaskType im2_mask,
+                     const vector<MultiContrastSetting>* contrast_settings = nullptr) :
                        global_cost (global_energy),
                        global_voxel_count (global_voxel_count),
                        thread_cost (0.0),
                        thread_voxel_count (0),
                        mutex (new std::mutex),
                        normaliser (0.0),
-                       robustness_parameter (-1.e12),
+                       robustness_parameter (1.e12), // MP: used to be -1.e12 but then compared to std::abs(speed)
                        intensity_difference_threshold (0.001),
                        denominator_threshold (1e-9),
                        im1_gradient (im1_image, true), im2_gradient (im2_image, true),
-                       im1_mask (im1_mask), im2_mask (im2_mask)
+                       im1_mask (im1_mask), im2_mask (im2_mask),
+                       contrast_settings (contrast_settings),
+                       nvols (im1_image.size(3))
           {
             for (size_t d = 0; d < 3; ++d)
               normaliser += im1_image.spacing(d) * im2_image.spacing(d);
             normaliser /= 3.0;
+
+            weight.resize (nvols);
+            speed.resize (nvols);
+            speed_squared.resize (nvols);
+
+            if (contrast_settings and contrast_settings->size() > 1) {
+              for (const auto & mc : *contrast_settings)
+                weight.segment(mc.start,mc.nvols).fill(mc.weight);
+            } else
+              weight.fill(1.0);
+            DEBUG ("Demons4D weights: " + str(weight.transpose()));
           }
 
           ~Demons4D () {
@@ -68,6 +83,8 @@ namespace MR
                            Im2ImageType& im2_image,
                            Image<default_type>& im1_update,
                            Image<default_type>& im2_update) {
+            assert (im1_image.size(3) == nvols);
+            assert (im2_image.size(3) == nvols);
 
             if (im1_image.index(0) == 0 || im1_image.index(0) == im1_image.size(0) - 1 ||
                 im1_image.index(1) == 0 || im1_image.index(1) == im1_image.size(1) - 1 ||
@@ -102,30 +119,30 @@ namespace MR
 
             assign_pos_of (im1_image, 0, 3).to (im1_gradient, im2_gradient);
 
-            Eigen::Vector3 total_update = Eigen::Vector3::Zero();
-            for (ssize_t vol = 0; vol < im1_image.size(3); ++vol) {
-              im2_image.index(3) = vol;
-              im1_image.index(3) = vol;
-              default_type speed = im2_image.value() - im1_image.value();
-              if (std::abs (speed) < robustness_parameter)
-                speed = 0.0;
+            speed = im2_image.row(3);
+            speed -= im1_image.row(3);
+            // set speed to zero if abs(speed) < robustness_parameter
+            speed = (speed.array().abs() < robustness_parameter).select(0.0, speed);
+            speed_squared = speed.cwiseProduct(speed);
 
-              default_type speed_squared = speed * speed;
-              thread_cost += speed_squared;
-              thread_voxel_count++;
+            thread_cost += weight.dot(speed_squared);
+            thread_voxel_count += nvols;
+
+            total_update.fill(0.0);
+            for (ssize_t vol = 0; vol <= nvols; ++vol) {
+              if (std::abs (speed[vol]) * weight[vol] < intensity_difference_threshold)
+                continue;
               im1_gradient.index(3) = vol;
               im2_gradient.index(3) = vol;
+              grad = (im2_gradient.value() + im1_gradient.value()).array() / 2.0;
 
-              Eigen::Matrix<typename Im1ImageType::value_type, 3, 1> grad = (im2_gradient.value() + im1_gradient.value()).array() / 2.0;
-              default_type denominator = speed_squared / normaliser + grad.squaredNorm();
-              if (!(std::abs (speed) < intensity_difference_threshold || denominator < denominator_threshold)) {
-                auto tmp = (speed * grad) / denominator;
-                total_update[0] += tmp[0];
-                total_update[1] += tmp[1];
-                total_update[2] += tmp[2];
-              }
+              default_type denominator = speed_squared[vol] / normaliser + grad.squaredNorm();
+              if (denominator < denominator_threshold)
+                continue;
+              total_update += (weight[vol] * speed[vol] / denominator) * grad;
             }
-            total_update = total_update / im1_image.size(3);
+            total_update /= nvols;
+
             im1_update.row(3) = total_update;
             im2_update.row(3) = -total_update;
           }
@@ -146,6 +163,14 @@ namespace MR
             Adapter::Gradient3D<Im2ImageType> im2_gradient;
             Im1MaskType im1_mask;
             Im2MaskType im2_mask;
+
+            const vector<MultiContrastSetting>* contrast_settings;
+            const ssize_t nvols;
+            Eigen::VectorXd weight;
+
+            Eigen::Matrix<default_type, Eigen::Dynamic, 1> speed, speed_squared;
+            Eigen::Vector3d total_update;
+            Eigen::Matrix<default_type, 3, 1> grad;
 
       };
     }
