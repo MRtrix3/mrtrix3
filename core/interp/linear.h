fca95ca292cf2d6879290576ab05940ede1782c5
Max Pietsch <mail@maxpietsch.com>
2017-06-13 23:00:58 +0100

    linear interp: assert to not call value on value and gradient interpolator as it calls the image's value

--- core/interp/linear.h
+++ core/interp/linear.h
@@ -126,7 +126,7 @@ namespace MR
 
     template <class ImageType>
     class LinearInterp<ImageType, LinearInterpProcessingType::Value> :
-        public LinearInterpBase<ImageType, LinearInterpProcessingType::Value> 
+        public LinearInterpBase<ImageType, LinearInterpProcessingType::Value>
     { MEMALIGN(LinearInterp<ImageType,LinearInterpProcessingType::Value>)
       public:
         using LinearBase = LinearInterpBase<ImageType, LinearInterpProcessingType::Value>;
@@ -427,6 +427,8 @@ namespace MR
           out_of_bounds_vec.fill(value_when_out_of_bounds);
           out_of_bounds_matrix.fill(value_when_out_of_bounds);
         }
+
+        value_type value () { assert( 0 && "do not call value() on ValueAndDerivative interpolator." ); }
 
         //! Set the current position to <b>voxel space</b> position \a pos
         /*! See file interp/base.h for details. */
