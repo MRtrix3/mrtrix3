03b98934ce40b30321dad2c72ae6c914b0a1250a
Max Pietsch <mail@maxpietsch.com>
2017-06-26 12:21:23 +0100

    math.h: added comments parsing in matrix IO. added support for centre in transformation IO

--- core/math/math.h
+++ core/math/math.h
@@ -127,10 +127,14 @@ namespace MR
 
   //! write the matrix \a M to file
   template <class MatrixType>
-    void save_matrix (const MatrixType& M, const std::string& filename)
+    void save_matrix (const MatrixType& M, const std::string& filename, vector<std::string>* comments = nullptr)
     {
       DEBUG ("saving " + str(M.rows()) + "x" + str(M.cols()) + " matrix to file \"" + filename + "\"...");
       File::OFStream out (filename);
+      if (comments) {
+        for (auto & line : *(comments))
+          out << "#" << line << "\n";
+      }
       Eigen::IOFormat fmt(Eigen::FullPrecision, Eigen::DontAlignCols, " ", "\n", "", "", "", "");
       out << M.format(fmt);
       out << "\n";
@@ -138,14 +142,22 @@ namespace MR
 
   //! read matrix data into a 2D vector \a filename
   template <class ValueType = default_type>
-    vector<vector<ValueType>> load_matrix_2D_vector (const std::string& filename)
+    vector<vector<ValueType>> load_matrix_2D_vector (const std::string& filename, vector<std::string>* comments = nullptr)
     {
       std::ifstream stream (filename, std::ios_base::in | std::ios_base::binary);
       vector<vector<ValueType>> V;
-      std::string sbuf;
+      std::string sbuf, cbuf;
+      size_t hash;
 
       while (getline (stream, sbuf)) {
-        sbuf = strip (sbuf.substr (0, sbuf.find_first_of ('#')));
+        hash = sbuf.find_first_of ('#');
+        if (comments and hash != std::string::npos) {
+          cbuf = strip (sbuf.substr (hash));
+          if (sbuf.size() > 1)
+            comments->push_back (cbuf.substr(1));
+        }
+
+        sbuf = strip (sbuf.substr (0, hash));
         if (sbuf.empty())
           continue;
 
@@ -174,12 +186,12 @@ namespace MR
 
   //! read matrix data into an Eigen::Matrix \a filename
   template <class ValueType = default_type>
-    Eigen::Matrix<ValueType, Eigen::Dynamic, Eigen::Dynamic> load_matrix (const std::string& filename)
+    Eigen::Matrix<ValueType, Eigen::Dynamic, Eigen::Dynamic> load_matrix (const std::string& filename, vector<std::string>* comments = nullptr)
     {
       DEBUG ("loading matrix file \"" + filename + "\"...");
       vector<vector<ValueType>> V;
       try {
-        V = load_matrix_2D_vector<ValueType> (filename);
+        V = load_matrix_2D_vector<ValueType> (filename, comments);
       } catch (Exception& e) {
         throw e;
       }
@@ -195,13 +207,15 @@ namespace MR
     }
 
   //! read matrix data from \a filename into an Eigen::Tranform class
-  inline transform_type load_transform (const std::string& filename)
+  template <class VectorType>
+  inline transform_type load_transform (const std::string& filename, VectorType& centre)
   {
     DEBUG ("loading transform file \"" + filename + "\"...");
 
     vector<vector<default_type>> V;
+    vector<std::string> comments;
     try {
-      V = load_matrix_2D_vector<> (filename);
+      V = load_matrix_2D_vector<> (filename, &comments);
     } catch (Exception& e) {
       throw e;
     }
@@ -216,12 +230,39 @@ namespace MR
       throw Exception ("transform in file " + filename + " is invalid: must contain either 3 or 4 rows.");
 
     transform_type M;
-
     for (ssize_t i = 0; i < 3; i++)
       for (ssize_t j = 0; j < 4; j++)
         M(i,j) = V[i][j];
 
+    if (centre.size() == 3) {
+      std::string key = "centre ";
+      centre[0] = NaN;
+      centre[1] = NaN;
+      centre[2] = NaN;
+      for (auto & line : comments) {
+        if (strncmp(line.c_str(), key.c_str(), key.size()) == 0) {
+          const auto elements = split (strip (line.substr (key.size())), " ,;\t", true);
+          if (elements.size() != 3)
+            throw Exception ("could not parse centre in transformation file " + filename + ": " + strip (line.substr (key.size())));
+          try {
+            centre[0] = to<default_type> (elements[0]);
+            centre[1] = to<default_type> (elements[1]);
+            centre[2] = to<default_type> (elements[2]);
+          } catch (...) {
+            throw Exception ("File \"" + filename + "\" contains non-numerical data in centre" + ": " + strip (line.substr (key.size())));
+          }
+          break;
+        }
+      }
+    }
+
     return M;
+  }
+
+  inline transform_type load_transform (const std::string& filename)
+  {
+    Eigen::VectorXd c;
+    return load_transform (filename, c);
   }
 
   //! write the transform \a M to file
@@ -230,6 +271,20 @@ namespace MR
     DEBUG ("saving transform to file \"" + filename + "\"...");
     File::OFStream out (filename);
     Eigen::IOFormat fmt(Eigen::FullPrecision, Eigen::DontAlignCols, " ", "\n", "", "", "", "");
+    out << M.matrix().format(fmt);
+    out << "\n0 0 0 1\n";
+  }
+
+  template <class Derived>
+  inline void save_transform (const Eigen::MatrixBase<Derived>& centre, const transform_type& M, const std::string& filename)
+  {
+    DEBUG ("saving transform to file \"" + filename + "\"...");
+    if (centre.rows() != 3 or centre.cols() != 1)
+      throw Exception ("save transform requires 3x1 vector as centre");
+    File::OFStream out (filename);
+    Eigen::IOFormat fmt(Eigen::FullPrecision, Eigen::DontAlignCols, " ", "\n", "", "", "", "");
+    Eigen::IOFormat centrefmt(Eigen::FullPrecision, Eigen::DontAlignCols, " ", "\n", "#centre ", "", "", "");
+    out << centre.transpose().format(centrefmt) << "\n";
     out << M.matrix().format(fmt);
     out << "\n0 0 0 1\n";
   }
