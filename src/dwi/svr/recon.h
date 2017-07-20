4c0df5825d3f73d275a5bbb6497e0059181acf00
Daan Christiaens <daan.christiaens@gmail.com>
2017-07-20 16:38:02 +0100

    Make private what can be made private.

--- src/dwi/svr/recon.h
+++ src/dwi/svr/recon.h
@@ -71,12 +71,10 @@ namespace MR
       Eigen::Index rows() const { return nv*nz*nxy; }
       Eigen::Index cols() const { return nxy*nz*nc; }
 
-
       template<typename Rhs>
       Eigen::Product<ReconMatrix,Rhs,Eigen::AliasFreeProduct> operator*(const Eigen::MatrixBase<Rhs>& x) const {
         return Eigen::Product<ReconMatrix,Rhs,Eigen::AliasFreeProduct>(*this, x.derived());
       }
-
 
       typedef Eigen::SparseMatrix<float, Eigen::RowMajor, StorageIndex> SparseMat;
 
@@ -93,15 +91,201 @@ namespace MR
         init_Y(grad);
       }
 
+
       const Eigen::MatrixXf& getY() const { return Y; }
       const Eigen::MatrixXf& getW() const { return W; }
 
       void setW (const Eigen::MatrixXf& weights) { W = weights; }
 
-      inline size_t get_grad_idx(const size_t idx) const { return idx / nxy; }
-      inline size_t get_grad_idx(const size_t v, const size_t z) const { return v*nz + z; }
-
       ReconMatrixAdjoint adjoint() const;
+
+      Eigen::MatrixXf getY0(const Eigen::MatrixXf& grad) const
+      {
+        DEBUG("initialise Y0");
+
+        vector<size_t> idx = get_shellidx(grad);
+        Eigen::MatrixXf Y0 (grad.rows(), nc);
+
+        Eigen::Vector3f vec;
+        Eigen::VectorXf delta;
+
+        for (size_t i = 0; i < grad.rows(); i++) {
+          vec = {grad(i, 0), grad(i, 1), grad(i, 2)};
+
+          // evaluate basis functions
+          Math::SH::delta(delta, vec, lmax);
+          Y0.row(i) = shellbasis[idx[i]]*delta;
+        }
+
+        return Y0;
+      }
+
+      template <typename VectorType1, typename VectorType2>
+      void project_x2y(VectorType1& dst, const VectorType2& rhs) const
+      {
+        DEBUG("Forward projection.");
+        size_t nxyz = nxy*nz;
+        Thread::parallel_for<size_t>(0, nv*nz, [&](size_t idx){
+          size_t v = idx/nz, z = idx%nz;
+          MR::DWI::ReconMatrix::SparseMat M = get_sliceM(v, z);
+          Eigen::VectorXf Yw = W(z, v) * get_sliceY(v, z);
+          for (size_t j = 0; j < nc; j++) {
+            dst.segment(idx*nxy, nxy) += Yw[j] * M * rhs.segment(j*nxyz, nxyz);
+          }
+        });
+      }
+
+      template <typename VectorType1, typename VectorType2>
+      void project_y2x(VectorType1& dst, const VectorType2& rhs) const
+      {
+        DEBUG("Transpose projection.");
+        std::mutex mutex;
+        size_t nxyz = nxy*nz;
+        Thread::parallel_for<size_t>(0, nv*nz, [&](size_t idx){
+          size_t v = idx/nz, z = idx%nz;
+          MR::DWI::ReconMatrix::SparseMat Mt = get_sliceM(v, z).adjoint();
+          Eigen::VectorXf r = Mt * rhs.segment(idx*nxy, nxy);
+          Eigen::VectorXf Yw = W(z, v) * get_sliceY(v, z);
+          std::lock_guard<std::mutex> lock (mutex);
+          for (size_t j = 0; j < nc; j++) {
+            dst.segment(j*nxyz, nxyz) += Yw[j] * r;
+          }
+        });
+      }
+
+
+    private:
+      const int lmax;
+      const size_t nx, ny, nz, nv, nxy, nc;
+      const Transform T0;
+      const vector<Eigen::MatrixXf> shellbasis;
+
+      Eigen::MatrixXf motion;
+      Eigen::MatrixXf Y;
+      Eigen::MatrixXf W;
+
+
+      vector<Eigen::MatrixXf> init_shellbasis(const Eigen::MatrixXf& grad, const vector<Eigen::MatrixXf>& rf) const
+      {
+        Shells shells (grad.template cast<double>());
+        vector<Eigen::MatrixXf> basis;
+
+        for (size_t s = 0; s < shells.count(); s++) {
+          Eigen::MatrixXf B;
+          if (rf.empty()) {
+            B.setIdentity(Math::SH::NforL(lmax), Math::SH::NforL(lmax));
+          }
+          else {
+            B.setZero(nc, Math::SH::NforL(lmax));
+            size_t j = 0;
+            for (auto& r : rf) {
+              for (size_t l = 0; l < r.cols() and 2*l <= lmax; l++)
+                for (size_t i = l*(2*l-1); i < (l+1)*(2*l+1); i++, j++)
+                  B(j,i) = r(s,l);
+            }
+          }
+          basis.push_back(B);
+        }
+
+        return basis;
+      }
+
+      void init_Y(const Eigen::MatrixXf& grad)
+      {
+        DEBUG("initialise Y");
+        assert (grad.rows() == nv);     // one gradient per volume
+
+        vector<size_t> idx = get_shellidx(grad);
+        Y.resize(nv*nz, nc);
+
+        Eigen::Vector3f vec;
+        Eigen::Matrix3f rot;
+        rot.setIdentity();
+        Eigen::VectorXf delta;
+
+        for (size_t i = 0; i < nv; i++) {
+          vec = {grad(i, 0), grad(i, 1), grad(i, 2)};
+          if (motion.rows() == nv)
+            rot = get_rotation(motion(i,3), motion(i,4), motion(i,5));
+
+          for (size_t j = 0; j < nz; j++) {
+            // rotate vector with motion parameters
+            if (motion.rows() == nv*nz)
+              rot = get_rotation(motion(i*nz+j,3), motion(i*nz+j,4), motion(i*nz+j,5));
+
+            // evaluate basis functions
+            Math::SH::delta(delta, rot*vec, lmax);
+            Y.row(i*nz+j) = shellbasis[idx[i]]*delta;
+          }
+
+        }
+
+      }
+
+      inline Eigen::Matrix3f get_rotation(const float a1, const float a2, const float a3) const
+      {
+        Eigen::Matrix3f m;
+        m = Eigen::AngleAxisf(a1, Eigen::Vector3f::UnitX())
+          * Eigen::AngleAxisf(a2, Eigen::Vector3f::UnitY())
+          * Eigen::AngleAxisf(a3, Eigen::Vector3f::UnitZ());
+        return m;
+      }
+
+      inline transform_type get_transform(const Eigen::VectorXf& p) const
+      {
+        transform_type T;
+        T.translation() = Eigen::Vector3d( double(p[0]), double(p[1]), double(p[2]) );
+        T.linear() = get_rotation(p[3], p[4], p[5]).template cast<double>();
+        return T;
+      }
+
+      inline transform_type get_Ts2r(const size_t v, const size_t z) const
+      {
+        transform_type Ts2r;
+        if (motion.rows() == nv) {
+          Ts2r = T0.scanner2voxel * get_transform(motion.row(v)) * T0.voxel2scanner;
+        } else {
+          assert (motion.rows(0) == nv*nz);
+          Ts2r = T0.scanner2voxel * get_transform(motion.row(v*nz+z)) * T0.voxel2scanner;
+        }
+        return Ts2r;
+      }
+
+      inline size_t get_idx(const int x, const int y, const int z) const
+      {
+        return size_t(z*nxy + y*nx + x);
+      }
+
+      inline bool inbounds(const int x, const int y, const int z) const
+      {
+        return (x >= 0) && (x < nx)
+            && (y >= 0) && (y < ny)
+            && (z >= 0) && (z < nz);
+      }
+
+      inline size_t get_ncoefs(const vector<Eigen::MatrixXf>& rf) const
+      {
+        size_t n = 0;
+        if (rf.empty()) {
+          n = Math::SH::NforL(lmax);
+        } else {
+          for (auto& r : rf)
+            n += Math::SH::NforL(std::min(2*(int(r.cols())-1), lmax));
+        }
+        return n;
+      }
+
+      vector<size_t> get_shellidx(const Eigen::MatrixXf& grad) const
+      {
+        Shells shells (grad.template cast<double>());
+        vector<size_t> idx (shells.volumecount());
+
+        for (size_t s = 0; s < shells.count(); s++) {
+          for (auto v : shells[s].get_volumes())
+            idx[v] = s;
+        }
+        return idx;
+      }
 
       SparseMat get_sliceM(const size_t v, const size_t z) const
       {
@@ -159,201 +343,8 @@ namespace MR
         return Y.row(v*nz+z);
       }
 
-      Eigen::MatrixXf getY0(const Eigen::MatrixXf& grad) const
-      {
-        DEBUG("initialise Y0");
-
-        vector<size_t> idx = get_shellidx(grad);
-        Eigen::MatrixXf Y0 (grad.rows(), nc);
-
-        Eigen::Vector3f vec;
-        Eigen::VectorXf delta;
-
-        for (size_t i = 0; i < grad.rows(); i++) {
-          vec = {grad(i, 0), grad(i, 1), grad(i, 2)};
-
-          // evaluate basis functions
-          Math::SH::delta(delta, vec, lmax);
-          Y0.row(i) = shellbasis[idx[i]]*delta;
-        }
-
-        return Y0;
-      }
-
-      template <typename VectorType1, typename VectorType2>
-      void project_x2y(VectorType1& dst, const VectorType2& rhs) const
-      {
-        DEBUG("Forward projection.");
-        size_t nxyz = nxy*nz;
-        Thread::parallel_for<size_t>(0, nv*nz, [&](size_t idx){
-          size_t v = idx/nz, z = idx%nz;
-          MR::DWI::ReconMatrix::SparseMat M = get_sliceM(v, z);
-          Eigen::VectorXf Yw = W(z, v) * get_sliceY(v, z);
-          for (size_t j = 0; j < nc; j++) {
-            dst.segment(idx*nxy, nxy) += Yw[j] * M * rhs.segment(j*nxyz, nxyz);
-          }
-        });
-      }
-
-      template <typename VectorType1, typename VectorType2>
-      void project_y2x(VectorType1& dst, const VectorType2& rhs) const
-      {
-        DEBUG("Transpose projection.");
-        std::mutex mutex;
-        size_t nxyz = nxy*nz;
-        Thread::parallel_for<size_t>(0, nv*nz, [&](size_t idx){
-          size_t v = idx/nz, z = idx%nz;
-          MR::DWI::ReconMatrix::SparseMat Mt = get_sliceM(v, z).adjoint();
-          Eigen::VectorXf r = Mt * rhs.segment(idx*nxy, nxy);
-          Eigen::VectorXf Yw = W(z, v) * get_sliceY(v, z);
-          std::lock_guard<std::mutex> lock (mutex);
-          for (size_t j = 0; j < nc; j++) {
-            dst.segment(j*nxyz, nxyz) += Yw[j] * r;
-          }
-        });
-      }
-
-
-      const int lmax;
-      const size_t nx, ny, nz, nv, nxy, nc;
-      const Transform T0;
-      const vector<Eigen::MatrixXf> shellbasis;
-
-    private:
-      Eigen::MatrixXf motion;
-      Eigen::MatrixXf Y;
-      Eigen::MatrixXf W;
-
-
-      vector<Eigen::MatrixXf> init_shellbasis(const Eigen::MatrixXf& grad, const vector<Eigen::MatrixXf>& rf) const
-      {
-        Shells shells (grad.template cast<double>());
-        vector<Eigen::MatrixXf> basis;
-
-        for (size_t s = 0; s < shells.count(); s++) {
-          Eigen::MatrixXf B;
-          if (rf.empty()) {
-            B.setIdentity(Math::SH::NforL(lmax), Math::SH::NforL(lmax));
-          }
-          else {
-            B.setZero(nc, Math::SH::NforL(lmax));
-            size_t j = 0;
-            for (auto& r : rf) {
-              for (size_t l = 0; l < r.cols() and 2*l <= lmax; l++)
-                for (size_t i = l*(2*l-1); i < (l+1)*(2*l+1); i++, j++)
-                  B(j,i) = r(s,l);
-            }
-          }
-          basis.push_back(B);
-        }
-
-        return basis;
-      }
-
-
-      void init_Y(const Eigen::MatrixXf& grad)
-      {
-        DEBUG("initialise Y");
-        assert (grad.rows() == nv);     // one gradient per volume
-
-        vector<size_t> idx = get_shellidx(grad);
-        Y.resize(nv*nz, nc);
-
-        Eigen::Vector3f vec;
-        Eigen::Matrix3f rot;
-        rot.setIdentity();
-        Eigen::VectorXf delta;
-
-        for (size_t i = 0; i < nv; i++) {
-          vec = {grad(i, 0), grad(i, 1), grad(i, 2)};
-          if (motion.rows() == nv)
-            rot = get_rotation(motion(i,3), motion(i,4), motion(i,5));
-
-          for (size_t j = 0; j < nz; j++) {
-            // rotate vector with motion parameters
-            if (motion.rows() == nv*nz)
-              rot = get_rotation(motion(i*nz+j,3), motion(i*nz+j,4), motion(i*nz+j,5));
-
-            // evaluate basis functions
-            Math::SH::delta(delta, rot*vec, lmax);
-            Y.row(i*nz+j) = shellbasis[idx[i]]*delta;
-          }
-
-        }
-
-      }
-
-
-      inline Eigen::Matrix3f get_rotation(const float a1, const float a2, const float a3) const
-      {
-        Eigen::Matrix3f m;
-        m = Eigen::AngleAxisf(a1, Eigen::Vector3f::UnitX())
-          * Eigen::AngleAxisf(a2, Eigen::Vector3f::UnitY())
-          * Eigen::AngleAxisf(a3, Eigen::Vector3f::UnitZ());
-        return m;
-      }
-
-
-      inline transform_type get_transform(const Eigen::VectorXf& p) const
-      {
-        transform_type T;
-        T.translation() = Eigen::Vector3d( double(p[0]), double(p[1]), double(p[2]) );
-        T.linear() = get_rotation(p[3], p[4], p[5]).template cast<double>();
-        return T;
-      }
-
-
-      inline transform_type get_Ts2r(const size_t v, const size_t z) const
-      {
-        transform_type Ts2r;
-        if (motion.rows() == nv) {
-          Ts2r = T0.scanner2voxel * get_transform(motion.row(v)) * T0.voxel2scanner;
-        } else {
-          assert (motion.rows(0) == nv*nz);
-          Ts2r = T0.scanner2voxel * get_transform(motion.row(v*nz+z)) * T0.voxel2scanner;
-        }
-        return Ts2r;
-      }
-
-
-      inline size_t get_idx(const int x, const int y, const int z) const
-      {
-        return size_t(z*nxy + y*nx + x);
-      }
-
-
-      inline bool inbounds(const int x, const int y, const int z) const
-      {
-        return (x >= 0) && (x < nx)
-            && (y >= 0) && (y < ny)
-            && (z >= 0) && (z < nz);
-      }
-
-
-      inline size_t get_ncoefs(const vector<Eigen::MatrixXf>& rf) const
-      {
-        size_t n = 0;
-        if (rf.empty()) {
-          n = Math::SH::NforL(lmax);
-        } else {
-          for (auto& r : rf)
-            n += Math::SH::NforL(std::min(2*(int(r.cols())-1), lmax));
-        }
-        return n;
-      }
-
-
-      vector<size_t> get_shellidx(const Eigen::MatrixXf& grad) const
-      {
-        Shells shells (grad.template cast<double>());
-        vector<size_t> idx (shells.volumecount());
-
-        for (size_t s = 0; s < shells.count(); s++) {
-          for (auto v : shells[s].get_volumes())
-            idx[v] = s;
-        }
-        return idx;
-      }
+      inline size_t get_grad_idx(const size_t idx) const { return idx / nxy; }
+      inline size_t get_grad_idx(const size_t v, const size_t z) const { return v*nz + z; }
 
 
     };
