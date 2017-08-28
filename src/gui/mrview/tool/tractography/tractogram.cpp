1af00afbcea3c72ce1566fa9c90ddf148c127ff0
Rami Tabbara <rtabbara@gmail.com>
2017-08-28 10:45:02 +1000

    mrview: tract_geom: Fixing up surface normal calc for pseudosphere

--- src/gui/mrview/tool/tractography/tractogram.cpp
+++ src/gui/mrview/tool/tractography/tractogram.cpp
@@ -298,7 +298,7 @@ namespace MR
                 "  surface_normal.z -= s * sqrt(tangent.x*tangent.x + tangent.y*tangent.y);\n";
              } else if (using_points) {
                source +=
-               "vec3 surface_normal = normalize(vec3(pos, sin(d_pos - 0.25)));\n";
+               "vec3 surface_normal = normalize(vec3(pos, sin((d_pos - 0.25) *" + str(Math::pi_2) + ")));\n";
              }
 
              source +=
