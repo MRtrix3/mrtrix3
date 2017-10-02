67aa476d9627d2056b2e1e7d4644c7d3483ff2f7
J-Donald Tournier <jdtournier@gmail.com>
2017-10-02 16:49:53 +0100

    dirstat: add support for reporting from image
    and add options for more fine-grained queries

--- cmd/dirstat.cpp
+++ cmd/dirstat.cpp
@@ -14,8 +14,10 @@
 
 #include "command.h"
 #include "progressbar.h"
+#include "header.h"
 #include "dwi/directions/file.h"
 #include "dwi/gradient.h"
+#include "dwi/shells.h"
 
 
 
@@ -29,121 +31,278 @@ void usage ()
   SYNOPSIS = "Report statistics on a direction set";
 
   ARGUMENTS
-    + Argument ("dirs", "the text file containing the directions.").type_file_in();
+    + Argument ("dirs", "the text file or image containing the directions.").type_file_in();
+
+  OPTIONS
+    + OptionGroup ("Output options")
+    + Option ("bipolar", "output statistics for bipolar electrostatic repulsion model")
+    + Option ("unipolar", "output statistics for unipolar electrostatic repulsion model")
+    + Option ("shfit", "output statistics for spherical harmonics fit")
+    + Option ("symmetry", "output measure of symmetry of spherical coverage (as given by the norm of mean direction vector). This is important to ensure minimal bias due to eddy-currents.")
+    + Option ("nearest_neighour", "output nearest-neighbour angle statistics")
+    + Option ("energy", "output energy statistics")
+    + Option ("total", "output total of statistic (affects -energy only)")
+    + Option ("mean", "output mean of statistic (affects -nearest_neighbour or -energy only)")
+    + Option ("range", "output range of statistic (affects -nearest_neighbour or -energy only)")
+    + DWI::GradImportOptions()
+    + DWI::ShellOption;
 }
 
 
 
 int precision = 6;
 
+void report (const std::string& title, Eigen::MatrixXd& directions);
 
-
-
-void report (const std::string& title, const Eigen::MatrixXd& directions) 
-{
-  vector<double> NN_bipolar (directions.rows(), -1.0);
-  vector<double> NN_unipolar (directions.rows(), -1.0);
-
-  vector<double> E_bipolar (directions.rows(), 0.0);
-  vector<double> E_unipolar (directions.rows(), 0.0);
-
-  for (ssize_t i = 0; i < directions.rows()-1; ++i) {
-    for (ssize_t j = i+1; j < directions.rows(); ++j) {
-      double cos_angle = directions.row(i).head(3).normalized().dot (directions.row(j).head(3).normalized());
-      NN_unipolar[i] = std::max (NN_unipolar[i], cos_angle);
-      NN_unipolar[j] = std::max (NN_unipolar[j], cos_angle);
-      cos_angle = std::abs (cos_angle);
-      NN_bipolar[i] = std::max (NN_bipolar[i], cos_angle);
-      NN_bipolar[j] = std::max (NN_bipolar[j], cos_angle);
-
-      double E = 1.0 / (directions.row(i).head(3) - directions.row(j).head(3)).norm();
-
-      E_unipolar[i] += E;
-      E_unipolar[j] += E;
-
-      E += 1.0 / (directions.row(i).head(3) + directions.row(j).head(3)).norm();
-
-      E_bipolar[i] += E;
-      E_bipolar[j] += E;
-
-    }
-  }
-
-
-
-
-  auto report_NN = [](const vector<double>& NN) {
-    double min = std::numeric_limits<double>::max();
-    double mean = 0.0;
-    double max = 0.0;
-    for (auto a : NN) {
-      a = (180.0/Math::pi) * std::acos (a);
-      mean += a;
-      min = std::min (min, a);
-      max = std::max (max, a);
-    }
-    mean /= NN.size();
-
-    print ("    nearest-neighbour angles: mean = " + str(mean, precision) + ", range [ " + str(min, precision) + " - " + str(max, precision) + " ]\n");
-  };
-
-
-
-  auto report_E = [](const vector<double>& E) {
-    double min = std::numeric_limits<double>::max();
-    double total = 0.0;
-    double max = 0.0;
-    for (auto e : E) {
-      total += e;
-      min = std::min (min, e);
-      max = std::max (max, e);
-    }
-    print ("    energy: total = " + str(0.5*total, precision) + ", mean = " + str(total/E.size(), precision) + ", range [ " + str(min, precision) + " - " + str(max, precision) + " ]\n");
-  };
-
-
-
-
-  print (title + " [ " + str(directions.rows(), precision) + " directions ]\n\n");
-
-  print ("  Bipolar electrostatic repulsion model:\n");
-  report_NN (NN_bipolar);
-  report_E (E_bipolar);
-
-  print ("\n  Unipolar electrostatic repulsion model:\n");
-  report_NN (NN_unipolar);
-  report_E (E_unipolar);
-
-  std::string lmax_results;
-  for (size_t lmax = 2; lmax <= Math::SH::LforN (directions.rows()); lmax += 2) 
-    lmax_results += " " + str(DWI::condition_number_for_lmax (directions, lmax), precision);
-  print ("\n  Spherical Harmonic fit:\n    condition numbers for lmax = " + str(2) + " -> " 
-      + str(Math::SH::LforN (directions.rows()), precision) + ":" + lmax_results + "\n\n");
-}
+bool bipolar = false;
+bool unipolar = false;
+bool shfit = false;
+bool nearest_neighour = false;
+bool energy = false;
+bool symmetry = false;
+bool total = false;
+bool mean = false;
+bool range = false;
 
 
 
 void run () 
 {
+  bipolar = get_options ("bipolar").size();
+  unipolar = get_options ("unipolar").size();
+  shfit = get_options ("shfit").size();
+  symmetry = get_options ("symmetry").size();
+  nearest_neighour = get_options ("nearest_neighour").size();
+  energy = get_options ("energy").size();
+  total = get_options ("total").size();
+  mean = get_options ("mean").size();
+  range = get_options ("range").size();
+
+
+  if (!(bipolar | unipolar | shfit | symmetry))
+    bipolar = unipolar = shfit = symmetry = true;
+
+  if (!(nearest_neighour | energy))
+    nearest_neighour = energy = true;
+
+  if (!(total | mean | range))
+    total = mean = range = true;
+
+  Eigen::MatrixXd directions;
+
   try {
-    auto directions = DWI::Directions::load_cartesian (argument[0]);
-    report (argument[0], directions);
+    directions = DWI::Directions::load_cartesian (argument[0]);
   }
   catch (Exception& E) {
-    auto directions = load_matrix<double> (argument[0]);
-    DWI::normalise_grad (directions);
-    if (directions.cols() < 3) 
-      throw Exception ("unexpected matrix size for DW scheme \"" + str(argument[0]) + "\"");
-
-    print (str(argument[0]) + " [ " + str(directions.rows()) + " volumes ]\n");
-    DWI::Shells shells (directions);
-
-    for (size_t n = 0; n < shells.count(); ++n) {
-      Eigen::MatrixXd subset (shells[n].count(), 3);
-      for (ssize_t i = 0; i < subset.rows(); ++i)
-        subset.row(i) = directions.row(shells[n].get_volumes()[i]).head(3);
-      report ("\nb = " + str(shells[n].get_mean(), precision), subset);
+    try {
+      directions = load_matrix<double> (argument[0]);
+    }
+    catch (Exception& E) {
+      auto header = Header::open (argument[0]);
+      directions = DWI::get_valid_DW_scheme (header);
     }
   }
+
+  if (directions.cols() >= 4) {
+    auto shells = DWI::Shells (directions).select_shells (false, false, false);
+    Eigen::MatrixXd dirs;
+
+    for (size_t n = 0; n < shells.count(); ++n) {
+      dirs.resize (shells[n].count(), 3);
+      for (size_t idx = 0; idx < shells[n].count(); ++idx) 
+        dirs.row (idx) = directions.row (shells[n].get_volumes()[idx]).head (3);
+
+      report (std::string (argument[0]) + " (b=" + str(shells[n].get_mean()) + ")", dirs);
+    }
+
+  }
+  else 
+    report (argument[0], directions);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+void report (const std::string& title, Eigen::MatrixXd& directions)
+{
+  if (directions.cols() < 3) 
+    throw Exception ("unexpected matrix size for DW scheme \"" + str(argument[0]) + "\"");
+  DWI::normalise_grad (directions);
+
+  if (log_level)
+    print (title + " [ " + str(directions.rows(), precision) + " directions ]\n");
+
+
+  if ((bipolar | unipolar) && (energy | nearest_neighour)) {
+    vector<double> NN_bipolar (directions.rows(), -1.0);
+    vector<double> NN_unipolar (directions.rows(), -1.0);
+
+    vector<double> E_bipolar (directions.rows(), 0.0);
+    vector<double> E_unipolar (directions.rows(), 0.0);
+
+    for (ssize_t i = 0; i < directions.rows()-1; ++i) {
+      for (ssize_t j = i+1; j < directions.rows(); ++j) {
+        double cos_angle = directions.row(i).head(3).normalized().dot (directions.row(j).head(3).normalized());
+        NN_unipolar[i] = std::max (NN_unipolar[i], cos_angle);
+        NN_unipolar[j] = std::max (NN_unipolar[j], cos_angle);
+        cos_angle = std::abs (cos_angle);
+        NN_bipolar[i] = std::max (NN_bipolar[i], cos_angle);
+        NN_bipolar[j] = std::max (NN_bipolar[j], cos_angle);
+
+        double E = 1.0 / (directions.row(i).head(3) - directions.row(j).head(3)).norm();
+
+        E_unipolar[i] += E;
+        E_unipolar[j] += E;
+
+        E += 1.0 / (directions.row(i).head(3) + directions.row(j).head(3)).norm();
+
+        E_bipolar[i] += E;
+        E_bipolar[j] += E;
+
+      }
+    }
+
+
+
+
+    auto report_NN = [](const vector<double>& NN) {
+      double NN_min = std::numeric_limits<double>::max();
+      double NN_mean = 0.0;
+      double NN_max = 0.0;
+      for (auto a : NN) {
+        a = (180.0/Math::pi) * std::acos (a);
+        NN_mean += a;
+        NN_min = std::min (NN_min, a);
+        NN_max = std::max (NN_max, a);
+      }
+      NN_mean /= NN.size();
+
+      std::string output;
+
+      if (log_level) 
+        output = "    nearest-neighbour angles: ";
+
+      if (mean) {
+        if (log_level) 
+          output += "mean = ";
+        output += str(NN_mean, precision);
+      }
+
+      if (range) {
+        if (mean)
+          output += log_level ? ", " : " ";
+        if (log_level) 
+          output += "range [ " + str(NN_min, precision) + " - " + str(NN_max, precision) + " ]";
+        else 
+          output += str(NN_min, precision) + " " + str(NN_max, precision);
+      }
+
+      if (output.size())
+        print (output + "\n");
+    };
+
+
+
+    auto report_E = [](const vector<double>& E) {
+      double E_min = std::numeric_limits<double>::max();
+      double E_total = 0.0;
+      double E_max = 0.0;
+      for (auto e : E) {
+        E_total += e;
+        E_min = std::min (E_min, e);
+        E_max = std::max (E_max, e);
+      }
+
+
+      std::string output;
+
+      if (log_level) 
+        output = "    energy: ";
+
+      if (total) {
+        if (log_level) 
+          output += "total = ";
+        output += str(0.5*E_total, precision);
+      }
+
+      if (mean) {
+        if (total)
+          output += log_level ? ", " : " ";
+        if (log_level) 
+          output += "mean = ";
+        output += str(E_total/E.size(), precision);
+      }
+
+      if (range) {
+        if (mean | total)
+          output += log_level ? ", " : " ";
+        if (log_level) 
+          output += "range [ " + str(E_min, precision) + " - " + str(E_max, precision) + " ]";
+        else 
+          output += str(E_min, precision) + " " + str(E_max, precision);
+      }
+
+      if (output.size())
+        print (output + "\n");
+    };
+
+
+
+
+
+
+    if (bipolar) {
+      if (log_level)
+        print ("\n  Bipolar electrostatic repulsion model:\n");
+      if (nearest_neighour)
+        report_NN (NN_bipolar);
+      if (energy) 
+        report_E (E_bipolar);
+    }
+
+    if (unipolar) {
+      if (log_level)
+        print ("\n  Unipolar electrostatic repulsion model:\n");
+      if (nearest_neighour)
+        report_NN (NN_unipolar);
+      if (energy)
+        report_E (E_unipolar);
+    }
+
+  }
+
+
+  if (shfit) {
+    std::string lmax_results;
+    for (size_t lmax = 2; lmax <= Math::SH::LforN (directions.rows()); lmax += 2) 
+      lmax_results += str(DWI::condition_number_for_lmax (directions, lmax), precision) + " ";
+    if (log_level) 
+      print ("\n  Spherical Harmonic fit:\n    condition numbers for lmax = " + str(2) + " -> " 
+        + str(Math::SH::LforN (directions.rows()), precision) + ": ");
+    print (lmax_results + "\n");
+  }
+
+  if (symmetry) {
+    if (log_level)
+      print ("\n  Symmetry of sampling:\n    norm of mean direction vector = ");
+    auto symmetry_index = directions.leftCols(3).colwise().mean().norm();
+    print (str (symmetry_index, precision) + "\n");
+    if (log_level && symmetry_index >= 0.1)
+        print (std::string ("    WARNING: sampling is ") + ( symmetry_index >= 0.4 ? "strongly" : "moderately" ) 
+            + " asymmetric - this may affect resiliance to eddy-current distortions");
+  }
+
+  if (log_level) 
+    print ("\n");
+}
+
 
