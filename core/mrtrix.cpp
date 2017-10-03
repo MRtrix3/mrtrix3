dc7db273d1cbf208425f2c75553dea4cd3d6d089
Robert Smith <robert.smith@florey.edu.au>
2017-10-04 10:54:22 +1100

    split(): Ignore leading delimiters
    When the "ignore_empty_fields" parameter is set to true, one would expect to not have any empty strings present in the output vector. However, if the input string commenced with a delimiter character, an empty string would be written as the first element in the vector. This change ensures that when "ignore_empty_fields" is set to true, any leading delimiters are ignored.

--- core/mrtrix.cpp
+++ core/mrtrix.cpp
@@ -128,6 +128,8 @@ namespace MR
     vector<std::string> V;
     std::string::size_type start = 0, end;
     try {
+      if (ignore_empty_fields)
+        start = string.find_first_not_of (delimiters);
       do {
         end = string.find_first_of (delimiters, start);
         V.push_back (string.substr (start, end-start));
