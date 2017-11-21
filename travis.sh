e136e7f1885597c83a47605b20b1e8f7b59d9ef2
J-Donald Tournier <jdtournier@gmail.com>
2017-11-21 15:06:00 +0000

    TravsCI: use case statements in bash scripts

--- travis.sh
+++ travis.sh
@@ -4,19 +4,26 @@ set -ev
 
 # Script expects environment variables ${py} and ${test} to be set
 
-if [ ${test} = "sphinx" ]; then
-    $py -m sphinx -n -N -W -w sphinx.log -c docs/ docs/ tmp/;
-elif [ ${test} = "memalign" ]; then
-    ./check_memalign;
-elif [ ${test} = "pylint" ]; then
+case  $test in
+  "sphinx")
+    $py -m sphinx -n -N -W -w sphinx.log -c docs/ docs/ tmp/
+    ;;
+  "memalign")
+    ./check_memalign
+    ;;
+  "pylint")
     # Normally rely on build script to create this file
-    echo "__version__ = pylint_testing" > ./lib/mrtrix3/_version.py;
-    PYTHON=$py ./run_pylint;
-elif [ ${test} = "build" ]; then
-    $py ./configure -nooptim && $py ./build -nowarnings;
-elif [ ${test} = "run" ]; then
-    $py ./configure -assert && $py ./build -nowarnings && ./run_tests && ./docs/generate_user_docs.sh && git diff --exit-code docs/ > gitdiff.log;
-else
+    echo "__version__ = pylint_testing" > ./lib/mrtrix3/_version.py
+    PYTHON=$py ./run_pylint
+    ;;
+  "build")
+    $py ./configure -nooptim && $py ./build -nowarnings
+    ;;
+  "run")
+    $py ./configure -assert && $py ./build -nowarnings && ./run_tests && ./docs/generate_user_docs.sh && git diff --exit-code docs/ > gitdiff.log
+    ;;
+  *)
     echo "Envvar \"test\" not defined";
     exit 1
-fi
+    ;;
+esac
