e5975700987225843677a9014f23062d1d97ac2c
Thijs Dhollander <thijs.dhollander@gmail.com>
2017-12-07 10:58:12 +1100

    fod2dec -outputmap --> -contrast Further changes to the interface, functionality and default behaviour will happen at some point in the future.

--- cmd/fod2dec.cpp
+++ cmd/fod2dec.cpp
@@ -44,7 +44,7 @@ void usage ()
   SYNOPSIS = "Generate FOD-based DEC maps, with optional panchromatic sharpening and/or luminance/perception correction";
 
   DESCRIPTION
-    + "By default, the FOD-based DEC is weighted by the integral of the FOD. To weight by another scalar map, use the outputmap option. This option can also be used for panchromatic sharpening, e.g., by supplying a T1 (or other sensible) anatomical volume with a higher spatial resolution.";
+    + "By default, the FOD-based DEC is weighted by the integral of the FOD. To weight by another scalar map, use the -contrast option. This option can also be used for panchromatic sharpening, e.g., by supplying a T1 (or other sensible) anatomical volume with a higher spatial resolution.";
 
   REFERENCES
     + "Dhollander T, Smith RE, Tournier JD, Jeurissen B, Connelly A. " // Internal
@@ -62,13 +62,10 @@ void usage ()
     + Option ("mask","Only perform DEC computation within the specified mask image.")
     + Argument ("image").type_image_in()
 
-    + Option ("threshold","FOD amplitudes below the threshold value are considered zero.")
-    + Argument ("value").type_float()
-
-    + Option ("outputmap","Weight the computed DEC map by a provided outputmap. If the outputmap has a different grid, the DEC map is first resliced and renormalised. To achieve panchromatic sharpening, provide an image with a higher spatial resolution than the input FOD image; e.g., a T1 anatomical volume. Only the DEC is subject to the mask, so as to allow for partial colouring of the outputmap. \nDefault when this option is *not* provided: integral of input FOD, subject to the same mask/threshold as used for DEC computation.")
+    + Option ("contrast","Weight the computed DEC map by the provided image contrast. If the contrast has a different image grid, the DEC map is first resliced and renormalised. To achieve panchromatic sharpening, provide an image with a higher spatial resolution than the input FOD image; e.g., a T1 anatomical volume. Only the DEC is subject to the mask, so as to allow for partial colouring of the contrast image. \nDefault when this option is *not* provided: integral of input FOD, subject to the same mask/threshold as used for DEC computation.")
     + Argument ("image").type_image_in()
 
-    + Option ("no_weight","Do not weight the DEC map (reslicing and renormalising still possible by explicitly providing the outputmap option as a template).")
+    + Option ("no_weight","Do not weight the DEC map (reslicing and renormalising of colours will still happen when providing the -contrast option as a template).")
 
     + Option ("lum","Correct for luminance/perception, using default values Cr,Cg,Cb = " + str(DEFAULT_LUM_CR, 2) + "," + str(DEFAULT_LUM_CG, 2) + "," + str(DEFAULT_LUM_CB, 2) + " and gamma = " + str(DEFAULT_LUM_GAMMA, 2) + " (*not* correcting is the theoretical equivalent of Cr,Cg,Cb = 1,1,1 and gamma = 2).")
 
@@ -76,7 +73,11 @@ void usage ()
     + Argument ("values").type_sequence_float()
 
     + Option ("lum_gamma","The gamma value to correct for luminance/perception. \nNote: this implicitly switches on luminance/perception correction, using a default Cr,Cg,Cb = " + str(DEFAULT_LUM_CR, 2) + "," + str(DEFAULT_LUM_CG, 2) + "," + str(DEFAULT_LUM_CB, 2) + " unless specified otherwise.")
+    + Argument ("value").type_float()
+
+    + Option ("threshold","FOD amplitudes below the threshold value are considered zero.")
     + Argument ("value").type_float();
+
 }
 
 
@@ -241,7 +242,7 @@ void run () {
 
   bool needtoslice = false;
   auto map_hdr = Header();
-  auto opto = get_options ("outputmap");
+  auto opto = get_options ("contrast");
   if (opto.size()) {
     map_hdr = Header::open(opto[0][0]);
     if (!dimensions_match(map_hdr, fod_hdr, 0, 3) ||
