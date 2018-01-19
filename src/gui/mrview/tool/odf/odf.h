0ff5d75f36d058eb31805c98151f9a6c25d50078
J-Donald Tournier <jdtournier@gmail.com>
2018-01-19 17:03:52 +0000

    MRView ODF tool: add option to colour relative to projection

--- src/gui/mrview/tool/odf/odf.h
+++ src/gui/mrview/tool/odf/odf.h
@@ -94,7 +94,7 @@ namespace MR
              QLabel *dirs_label, *shell_label;
              QComboBox *dirs_selector, *shell_selector;
              QCheckBox *use_lighting_box, *hide_negative_values_box, *lock_to_grid_box, *main_grid_box;
-             QCheckBox *colour_by_direction_box, *interpolation_box;
+             QCheckBox *colour_by_direction_box, *interpolation_box, *colour_relative_to_projection_box;
              QColorButton *colour_button;
 
              AdjustButton *scale;
@@ -103,7 +103,7 @@ namespace MR
              GL::Lighting* lighting;
 
              int lmax;
-             
+
              void add_images (vector<std::string>& list, const odf_type_t mode);
 
              virtual void closeEvent (QCloseEvent* event) override;
