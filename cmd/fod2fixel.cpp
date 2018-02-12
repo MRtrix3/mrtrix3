MRtrix3/mrtrix3

commit ed17ef148b636885a6cd913426d5c9a9bb25eed3

FOD FMLS: Apply peak amp threshold after peak revision
This means that if the maximal amplitude value sampled for a lobe from the dixel set is just below the threshold, but Newton revision on the sphere yields an amplitude that is just above the thresold, the fixel survives the threshold test.
Also removed multiplie redundant calls to FOD_lobe::finalise().
Identified from discussion in #1228.

src/dwi/fmls.cpp
@@ -257,7 +257,7 @@ namespace MR {
 
         for (auto i = out.begin(); i != out.end();) { // Empty increment
 
-          if (i->is_negative() || i->get_max_peak_value() < peak_value_threshold || i->get_integral() < integral_threshold) {
+          if (i->is_negative() || i->get_integral() < integral_threshold) {
             i = out.erase (i);
           } else {
 
@@ -267,12 +267,16 @@ namespace MR {
               const default_type new_peak_value = Math::SH::get_peak (in, lmax, newton_peak, &(*precomputer));
               if (std::isfinite (new_peak_value) && newton_peak.allFinite())
                 i->revise_peak (peak_index, newton_peak, new_peak_value);
-              i->finalise();
             }
+            if (i->get_max_peak_value() < peak_value_threshold) {
+              i = out.erase (i);
+            } else {
+              i->finalise();
 #ifdef FMLS_OPTIMISE_MEAN_DIR
-            optimise_mean_dir (*i);
+              optimise_mean_dir (*i);
 #endif
-            ++i;
+              ++i;
+            }
           }
         }
 
@@ -386,8 +390,8 @@ namespace MR {
 
               // Transform unit direction onto tangent plane defined by the current mean direction estimate
               Point<float> p (dir[0]*Tx[0] + dir[1]*Tx[1] + dir[2]*Tx[2],
-                  dir[0]*Ty[0] + dir[1]*Ty[1] + dir[2]*Ty[2],
-                  dir[0]*Tz[0] + dir[1]*Tz[1] + dir[2]*Tz[2]);
+                              dir[0]*Ty[0] + dir[1]*Ty[1] + dir[2]*Ty[2],
+                              dir[0]*Tz[0] + dir[1]*Tz[1] + dir[2]*Tz[2]);
 
               if (p[2] < 0.0)
                 p = -p;
@@ -412,8 +416,8 @@ namespace MR {
 
           // Transform the offset from the tangent plane origin to euclidean space
           u.set (u[0]*Tx[0] + u[1]*Ty[0] + u[2]*Tz[0],
-              u[0]*Tx[1] + u[1]*Ty[1] + u[2]*Tz[1],
-              u[0]*Tx[2] + u[1]*Ty[2] + u[2]*Tz[2]);
+                 u[0]*Tx[1] + u[1]*Ty[1] + u[2]*Tz[1],
+                 u[0]*Tx[2] + u[1]*Ty[2] + u[2]*Tz[2]);
 
           mean_dir += u;
           mean_dir.normalise();

commit 49cfc7bea85631a638563b67146711c1c8a34dff

FOD FMLS segmenter: Fix peak revision
When the initial FOD peak identified via the FMLS process is then revised via Newton optimisation, apply additional constraints to decide whether or not the outcome of Newton optimisation should be trusted:
- The dixel to which the new peak direction is closest must be a part of the same FOD lobe.
- The new peak direction must be closer to the initial peak direction (that was used as the seed point for the Newton optimisation) than to any other FOD peaks within the same lobe.
Addresses issue introduced in commit ed17ef1 as reported in #1228.

cmd/fod2fixel.cpp
@@ -326,7 +326,7 @@ void run ()
 
   FMLS::FODQueueWriter writer (fod_data, mask);
 
-  const DWI::Directions::Set dirs (1281);
+  const DWI::Directions::FastLookupSet dirs (1281);
   Segmenter fmls (dirs, Math::SH::LforN (H.size(3)));
   load_fmls_thresholds (fmls);
 

src/dwi/fmls.cpp
@@ -133,7 +133,7 @@ namespace MR {
 
 
 
-      Segmenter::Segmenter (const DWI::Directions::Set& directions, const size_t l) :
+      Segmenter::Segmenter (const DWI::Directions::FastLookupSet& directions, const size_t l) :
           dirs                         (directions),
           lmax                         (l),
           precomputer                  (new Math::SH::PrecomputedAL<default_type> (lmax, 2 * dirs.size())),
@@ -264,9 +264,31 @@ namespace MR {
             // Revise multiple peaks if present
             for (size_t peak_index = 0; peak_index != i->num_peaks(); ++peak_index) {
               Eigen::Vector3 newton_peak = i->get_peak_dir (peak_index);
-              const default_type new_peak_value = Math::SH::get_peak (in, lmax, newton_peak, &(*precomputer));
-              if (std::isfinite (new_peak_value) && newton_peak.allFinite())
-                i->revise_peak (peak_index, newton_peak, new_peak_value);
+              const default_type newton_peak_value = Math::SH::get_peak (in, lmax, newton_peak, &(*precomputer));
+              if (std::isfinite (newton_peak_value) && newton_peak.allFinite()) {
+
+                // Ensure that the new peak direction found via Newton optimisation
+                //   is still approximately the same peak as that found via FMLS:
+
+                // - Needs to be closer to this peak than any other peaks within the lobe
+                default_type max_dp = 0.0;
+                size_t nearest_original_peak = i->num_peaks();
+                for (size_t j = 0; j != i->num_peaks(); ++j) {
+                  const default_type this_dp = std::abs (newton_peak.dot (i->get_peak_dir (j)));
+                  if (this_dp > max_dp) {
+                    max_dp = this_dp;
+                    nearest_original_peak = j;
+                  }
+                }
+                if (nearest_original_peak == peak_index) {
+
+                  // - Needs to still lie within the lobe: Determined via mask
+                  const index_type newton_peak_closest_dir_index = dirs.select_direction (newton_peak);
+                  if (i->get_mask()[newton_peak_closest_dir_index])
+                    i->revise_peak (peak_index, newton_peak, newton_peak_value);
+
+                }
+              }
               i->finalise();
             }
 #ifdef FMLS_OPTIMISE_MEAN_DIR

src/dwi/fmls.h
@@ -177,7 +177,7 @@ namespace MR
           Eigen::Array3i vox;
       };
 
-      class FODQueueWriter 
+      class FODQueueWriter
       { MEMALIGN (FODQueueWriter)
 
           using FODImageType = Image<float>;
@@ -219,7 +219,7 @@ namespace MR
       // Store a vector of weights to be applied when computing integrals, to account for non-uniformities in direction distribution
       // These weights are applied to the amplitude along each direction as the integral for each lobe is summed,
       //   in order to take into account the relative spacing between adjacent directions
-      class IntegrationWeights 
+      class IntegrationWeights
       { MEMALIGN (IntegrationWeights)
         public:
           IntegrationWeights (const DWI::Directions::Set& dirs);
@@ -234,7 +234,7 @@ namespace MR
       class Segmenter { MEMALIGN(Segmenter)
 
         public:
-          Segmenter (const DWI::Directions::Set&, const size_t);
+          Segmenter (const DWI::Directions::FastLookupSet&, const size_t);
 
           bool operator() (const SH_coefs&, FOD_lobes&) const;
 
@@ -255,7 +255,10 @@ namespace MR
 
         private:
 
-          const DWI::Directions::Set& dirs;
+          // FastLookupSet is required for ensuring that when a peak direction is
+          //   revised using Newton optimisation, that direction is still reflective
+          //   of the original peak; i.e. it hasn't 'leaped' across to a different peak
+          const DWI::Directions::FastLookupSet& dirs;
 
           const size_t lmax;
 