4a91079a20b0e62910080ba68c400aa69af356b8
Thijs Dhollander <thijs.dhollander@gmail.com>
2018-03-07 16:24:16 +1100

    other pylint fixes (gif.py)
    Co-authored-by: Matteo Mancini <ingmatteomancini@gmail.com>

--- lib/mrtrix3/_5ttgen/gif.py
+++ lib/mrtrix3/_5ttgen/gif.py
@@ -1,34 +1,32 @@
-def initialise(base_parser, subparsers):
-  import argparse
-  from mrtrix3 import app
+def initialise(base_parser, subparsers): #pylint: disable=unused-variable
   parser = subparsers.add_parser('gif', author='Matteo Mancini (m.mancini@ucl.ac.uk)', synopsis='Generate the 5TT image based on a GIF segmentation image', parents=[base_parser])
   parser.add_argument('input',  help='The input GIF segmentation image')
   parser.add_argument('output', help='The output 5TT image')
 
 
-def checkOutputPaths():
+def checkOutputPaths(): #pylint: disable=unused-variable
   pass
 
 
 def checkGIFinput(image_path):
   from mrtrix3 import app, image
-  dim = [ int(i) for i in image.headerField(image_path, 'size').strip().split() ]
+  dim = [ int(i) for i in image.mrinfo(image_path, 'size').strip().split() ]
   if len(dim) < 4:
     app.error('Image \'' + image_path + '\' does not look like GIF segmentation (less than 4 spatial dimensions)')
   if min(dim[:4]) == 1:
     app.error('Image \'' + image_path + '\' does not look like GIF segmentation (axis with size 1)')
 
 
-def getInputs():
-  import os, shutil
+def getInputs(): #pylint: disable=unused-variable
+  import os, shutil #pylint: disable=unused-variable
   from mrtrix3 import app, path, run
   checkGIFinput(path.fromUser(app.args.input, True))
   run.command('mrconvert ' + path.fromUser(app.args.input, True) + ' ' + path.toTemp('input.mif', True))
 
 
-def execute():
-  import os, sys
-  from mrtrix3 import app, path, run
+def execute(): #pylint: disable=unused-variable
+  import os, sys #pylint: disable=unused-variable
+  from mrtrix3 import app, path, run #pylint: disable=unused-variable
 
   # Generate the images related to each tissue
   run.command('mrconvert input.mif -coord 3 1 CSF.mif')
@@ -49,4 +47,3 @@ def execute():
     run.command('mrconvert 5tt.mif result.mif')
   else:
     run.command('mrmath 5tt.mif sum - -axis 3 | mrthreshold - - -abs 0.5 | mrcrop 5tt.mif result.mif -mask -')
-
