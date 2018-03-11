b3e59b0acff52b4b2f356f0793c88724f466ca48
Robert Smith <robert.smith@florey.edu.au>
2018-03-11 18:55:25 +1100

    Surface::Algos::mesh2image(): Better region filling
    Previously, after identifying voxels lying on the surface, the image volume was filled inwards from the eight external corners of the FoV to select those voxels outside the surface, and all remaining voxels were labelled as being inside the surface. This however failed to account for more complex surface shapes, that could result in separation of some voxels strictly outside the mesh from the FoV corners.
    This new approach uses the vertex normals to label some voxels just inside / outside the surface, and then exploits them for majority voting labelling of all voxel connected components.

--- src/surface/types.h
+++ src/surface/types.h
@@ -39,6 +39,7 @@ namespace MR
     { MEMALIGN (Vox)
       public:
         using Eigen::Array3i::Array3i;
+        Vox (const Eigen::Vector3& p) : Eigen::Array3i (int(std::round (p[0])), int(std::round (p[1])), int(std::round (p[2]))) { }
         bool operator< (const Vox& i) const
         {
           return ((*this)[2] == i[2] ? (((*this)[1] == i[1]) ? ((*this)[0] < i[0]) : ((*this)[1] < i[1])) : ((*this)[2] < i[2]));
