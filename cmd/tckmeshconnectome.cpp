c55dfe3e1b498a14d58a06704dc699f58f651c5b
Chun-Hung Jimmy Yeh <jimmy.chyeh@gmail.com>
2018-03-19 18:06:05 +1100

    tckmeshconnectome: -assign_in option
    allow using track-to-mesh-vertex assignment file to generate connectomes

--- cmd/tckmeshconnectome.cpp
+++ cmd/tckmeshconnectome.cpp
@@ -22,7 +22,6 @@
 #include "dwi/tractography/weights.h"
 #include "dwi/tractography/MACT/scenemodeller.h"
 #include "surface/freesurfer.h"
-#include "surface/meshfactory.h"
 
 
 using namespace MR;
@@ -40,7 +39,7 @@ class NodePair
 
   public:
 
-    NodePair() : _weight( 1.0 ) {};
+    NodePair() : _tck_weight( 1.0 ) {};
 
     ~NodePair() {};
 
@@ -50,13 +49,17 @@ class NodePair
     uint32_t getFirst() const { return _nodePair.first; };
     uint32_t getSecond() const { return _nodePair.second; };
 
-    void setWeight( const float& weight ) { _weight = weight; };
-    float getWeight() const { return _weight; };
+    void set_tck_index( const size_t& index ) { _tck_index = index; };
+    float get_tck_index() const { return _tck_index; };
+
+    void set_tck_weight( const float& weight ) { _tck_weight = weight; };
+    float get_tck_weight() const { return _tck_weight; };
 
   protected:
 
     std::pair< uint32_t, uint32_t > _nodePair;
-    float _weight;
+    size_t _tck_index;
+    float _tck_weight;
 
 };
 
@@ -83,7 +86,8 @@ class Assigner
       _sceneModeller->nearestTissue( last, ix2 );
       nodePair.setSecond( ix2.nearestVertex() );
 
-      nodePair.setWeight( tck.weight );
+      nodePair.set_tck_index( tck.index );
+      nodePair.set_tck_weight( tck.weight );
 
       return true;
     }
@@ -128,23 +132,6 @@ class Graph
       return true;
     }
 
-    void update( const NodePair& nodePair )
-    {
-      _assignment_pairs.push_back( nodePair );
-    }
- 
-    void write_assign( const std::string& path ) const
-    {    
-      File::OFStream out( path, std::ios_base::out | std::ios_base::trunc );
-      auto a = _assignment_pairs.begin(), ae = _assignment_pairs.end();
-      while ( a != ae )
-      {
-        out << a->getFirst() << " " << a->getSecond() << " -> "
-            << _labels( a->getFirst() ) << " " << _labels( a->getSecond() ) << "\n";
-        ++ a;    
-      }
-    }
-
     void write_matrix( const std::string& path )
     {      
       // map assignment to matrix
@@ -153,14 +140,8 @@ class Graph
       {
         uint32_t node1 = _labels( a->getFirst() ) - 1;
         uint32_t node2 = _labels( a->getSecond() ) - 1;
-        if ( node1 <= node2 )
-        {
-          _M( node1, node2 ) += a->getWeight();
-        }
-        else
-        {
-          _M( node2, node1 ) += a->getWeight();
-        }
+        ( node1 <= node2 ) ? _M( node1, node2 ) += a->get_tck_weight() :
+                             _M( node2, node1 ) += a->get_tck_weight();
         ++ a;
       }
       // write to file
@@ -170,6 +151,66 @@ class Graph
                            " ", "\n", "", "", "", "");
       out << _M.format(fmt);
       out << "\n";
+    }
+
+    void write_assign_vertex( const std::string& path ) const
+    {
+      std::vector< NodePair > out_assign;
+      out_assign.resize( _assignment_pairs.size() );
+      auto a = _assignment_pairs.begin(), ae = _assignment_pairs.end();
+      while ( a != ae )
+      {
+        out_assign[ a->get_tck_index() ] = *a;
+        ++ a;    
+      }
+      File::OFStream out_vertices( path, std::ios_base::out | std::ios_base::trunc );
+      auto o = out_assign.begin(), oe = out_assign.end();
+      while ( o != oe )
+      {
+        out_vertices << o->getFirst() << " " << o->getSecond() << "\n";
+        ++ o;    
+      }
+    }
+
+    void write_assign_label( const std::string& path ) const
+    {
+      std::vector< NodePair > out_assign;
+      out_assign.resize( _assignment_pairs.size() );
+      auto a = _assignment_pairs.begin(), ae = _assignment_pairs.end();
+      while ( a != ae )
+      {
+        out_assign[ a->get_tck_index() ] = *a;
+        ++ a;    
+      }
+      File::OFStream out_labels( path, std::ios_base::out | std::ios_base::trunc );
+      auto o = out_assign.begin(), oe = out_assign.end();
+      while ( o != oe )
+      {
+        out_labels << _labels( o->getFirst() ) << " " << _labels( o->getSecond() ) << "\n";
+        ++ o;
+      }
+    }
+    
+    void read_assign( const std::string& path )
+    {
+      uint32_t v1 = 0, v2 = 0, l = 0;
+      NodePair nodePair;
+      std::ifstream in( path, std::ios_base::in );
+      std::string line;
+      while ( std::getline( in, line ) )
+      {
+        sscanf( line.c_str(), "%u %u", &v1, &v2 );
+        nodePair.setFirst( v1 );
+        nodePair.setSecond( v2 );
+        nodePair.set_tck_index( l );
+        _assignment_pairs.push_back( nodePair );
+        ++ l;
+      }
+    }
+
+    void set_weight( const Streamline< float >& tck )
+    {
+      _assignment_pairs[ tck.index ].set_tck_weight( tck.weight );
     }
 
   protected:
@@ -203,17 +244,28 @@ void usage ()
   OPTIONS
   + Tractography::TrackWeightsInOption
 
+  + Option( "assign_in", "generate connectome based on the track-to-mesh assignment file "
+                         "(only valid when the assignment file is obtained from the same tractogram and mesh files)" )
+    + Argument( "file" ).type_file_in()
+
+  + Option( "out_assign_vertex", "output pairs of vertices from streamline assignments" )
+    + Argument( "path" ).type_file_out()
+
+  + Option( "out_assign_label", "output pairs of labels from streamline assignments" )
+    + Argument( "path" ).type_file_out();
+
   + Option( "lut", "edge length in mm for spatial lookup table (default=0.2mm)" )
     + Argument( "value" ).type_float( 0.0, 25.0 )
-
-  + Option( "out_assign", "output the node assignments of each streamline to a file" )
-    + Argument( "path" ).type_file_out();
 
 };
 
 
 void run ()
 {
+
+  // read input track file
+  Tractography::Properties properties;
+  Tractography::Reader< float > reader( argument[ 0 ], properties );
 
   // read surface
   std::map< std::string, Mesh > meshes;
@@ -224,64 +276,83 @@ void run ()
   label_vector_type labels;
   FreeSurfer::read_annot( argument[ 2 ], labels, ctable );
 
-  // determine the mesh bounding box
-  Eigen::Vector3d lower_p = meshes.begin()->second.vert( 0 );
-  Eigen::Vector3d upper_p = meshes.begin()->second.vert( 0 );
-  for ( auto m = meshes.begin(); m != meshes.end(); ++ m )
-  {
-    for ( size_t v = 0; v < m->second.num_vertices(); v++ )
-    {
-      Vertex thisVertex = m->second.vert( v );
-      for ( size_t axis = 0; axis < 3; axis++ )
-      {
-        lower_p[ axis ] = std::min( lower_p[ axis ], thisVertex[ axis ] );
-        upper_p[ axis ] = std::max( upper_p[ axis ], thisVertex[ axis ] );
-      }
-    }
-  }
-
-  // build bounding box and scene modeller
-  auto opt = get_options( "lut" );
-  double edge_length = opt.size() ? ( opt[ 0 ][ 0 ] ) : 0.2;
-
-  BoundingBox< double > boundingBox( lower_p[ 0 ], upper_p[ 0 ],
-                                     lower_p[ 1 ], upper_p[ 1 ],
-                                     lower_p[ 2 ], upper_p[ 2 ] );
-  Eigen::Vector3i lutSize( ( upper_p[ 0 ] - lower_p[ 0 ] ) / edge_length,
-                           ( upper_p[ 1 ] - lower_p[ 1 ] ) / edge_length,
-                           ( upper_p[ 2 ] - lower_p[ 2 ] ) / edge_length );
-
-  std::shared_ptr< SceneModeller > sceneModeller( new SceneModeller( boundingBox, lutSize ) );
-
-  // build tissues in scene modeller
-  std::set< Tissue_ptr > tissues;
-  Tissue_ptr tissue;
-  tissue.reset( new Tissue( OTHER, "ROI", meshes[ "ROI" ], sceneModeller ) );
-  tissues.insert( tissue );
-  sceneModeller->addTissues( tissues );
-
-  // preparing methods
-  Assigner assigner( sceneModeller );
+  // preparing method for output
   Graph graph( labels, ctable );
 
-  // preparing input track file
-  Tractography::Properties properties;
-  Tractography::Reader< float > reader( argument[ 0 ], properties );
-
-  // Supporting multi-threading application
-  Mapping::TrackLoader loader( reader,
-                               properties[ "count" ].empty() ? 0 : to< size_t >( properties[ "count" ] ),
-                               "Streamline assignment... " );
-  Thread::run_queue( loader, 
-                     Thread::batch( Tractography::Streamline< float >() ),
-                     Thread::multi( assigner ),
-                     Thread::batch( NodePair() ),
-                     graph );
-
-  opt = get_options( "out_assign" );
+  auto opt = get_options( "assign_in" );
   if ( opt.size() )
   {
-    graph.write_assign( opt[ 0 ][ 0 ] );
+    graph.read_assign( opt[ 0 ][ 0 ] );
+    Tractography::Streamline<> tck;
+    ProgressBar progress( "processing assignment... ",
+                          properties[ "count" ].empty() ? 0 : to< unsigned int >( properties[ "count" ] ) );
+    while ( reader( tck ) )
+    {
+      graph.set_weight( tck );
+      ++ progress;
+    }
+  }
+  else
+  {
+    // determine the mesh bounding box
+    Eigen::Vector3d lower_p = meshes.begin()->second.vert( 0 );
+    Eigen::Vector3d upper_p = meshes.begin()->second.vert( 0 );
+    for ( auto m = meshes.begin(); m != meshes.end(); ++ m )
+    {
+      for ( size_t v = 0; v < m->second.num_vertices(); v++ )
+      {
+        Vertex thisVertex = m->second.vert( v );
+        for ( size_t axis = 0; axis < 3; axis++ )
+        {
+          lower_p[ axis ] = std::min( lower_p[ axis ], thisVertex[ axis ] );
+          upper_p[ axis ] = std::max( upper_p[ axis ], thisVertex[ axis ] );
+        }
+      }
+    }
+  
+    // build bounding box and scene modeller
+    opt = get_options( "lut" );
+    double edge_length = opt.size() ? ( opt[ 0 ][ 0 ] ) : 0.2;
+  
+    BoundingBox< double > boundingBox( lower_p[ 0 ], upper_p[ 0 ],
+                                       lower_p[ 1 ], upper_p[ 1 ],
+                                       lower_p[ 2 ], upper_p[ 2 ] );
+    Eigen::Vector3i lutSize( ( upper_p[ 0 ] - lower_p[ 0 ] ) / edge_length,
+                             ( upper_p[ 1 ] - lower_p[ 1 ] ) / edge_length,
+                             ( upper_p[ 2 ] - lower_p[ 2 ] ) / edge_length );
+  
+    std::shared_ptr< SceneModeller > sceneModeller( new SceneModeller( boundingBox, lutSize ) );
+  
+    // build tissues in scene modeller
+    std::set< Tissue_ptr > tissues;
+    Tissue_ptr tissue;
+    tissue.reset( new Tissue( OTHER, "ROI", meshes[ "ROI" ], sceneModeller ) );
+    tissues.insert( tissue );
+    sceneModeller->addTissues( tissues );
+  
+    // preparing method
+    Assigner assigner( sceneModeller );
+  
+    // Supporting multi-threading application
+    Mapping::TrackLoader loader( reader,
+                                 properties[ "count" ].empty() ? 0 : to< size_t >( properties[ "count" ] ),
+                                 "processing assignment... " );
+    Thread::run_queue( loader, 
+                       Thread::batch( Tractography::Streamline< float >() ),
+                       Thread::multi( assigner ),
+                       Thread::batch( NodePair() ),
+                       graph );
+  
+    opt = get_options( "out_assign_vertex" );
+    if ( opt.size() )
+    {
+      graph.write_assign_vertex( opt[ 0 ][ 0 ] );
+    }
+  }
+  opt = get_options( "out_assign_label" );
+  if ( opt.size() )
+  {
+    graph.write_assign_label( opt[ 0 ][ 0 ] );
   }
   graph.write_matrix( argument[ 3 ] );
 
