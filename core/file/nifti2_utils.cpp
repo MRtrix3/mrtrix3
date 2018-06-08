f924b73e15a0c824b938746fdddf63ac83cc4527
J-Donald Tournier <jdtournier@gmail.com>
2018-06-08 12:11:44 +0100

    NIfTI: relax transform consistency check
    As discussed in #1318

--- core/file/nifti2_utils.cpp
+++ core/file/nifti2_utils.cpp
@@ -215,7 +215,7 @@ namespace MR
           if (sform_code) {
             Header header2 (H);
             header2.transform() = M_qform;
-            if (!voxel_grids_match_in_scanner_space (H, header2)) {
+            if (!voxel_grids_match_in_scanner_space (H, header2, 0.1)) {
               const bool use_sform = File::Config::get_bool ("NIfTIUseSform", false);
               WARN ("qform and sform are inconsistent in NIfTI image \"" + H.name() + "\" - using " + (use_sform ? "sform" : "qform"));
               if (!use_sform)
