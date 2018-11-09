a36064c8e18ac4f428a329d730ce9c4a977566c6
Max Pietsch <mail@maxpietsch.com>
2018-11-09 18:27:50 +0000

    mrcalc and math.h: added euler constant.

--- cmd/mrcalc.cpp
+++ cmd/mrcalc.cpp
@@ -75,7 +75,7 @@ DESCRIPTION
 ARGUMENTS
   + Argument ("operand", "an input image, intensity value, or the special keywords "
       "'rand' (random number between 0 and 1) or 'randn' (random number from unit "
-      "std.dev. normal distribution) or 'pi' the mathematical constant.").type_various().allow_multiple();
+      "std.dev. normal distribution) or the mathematical constants 'e' and 'pi'.").type_various().allow_multiple();
 
 OPTIONS
   + OptionGroup ("Unary operators")
@@ -246,6 +246,7 @@ class StackEntry { NOMEMALIGN
           try {
             std::string a = lowercase (arg);
             if      (a == "pi")    { value = Math::pi; }
+            else if (a == "e")     { value = Math::e; }
             else if (a ==  "nan")  { value =  std::numeric_limits<real_type>::quiet_NaN(); }
             else if (a == "-nan")  { value = -std::numeric_limits<real_type>::quiet_NaN(); }
             else if (a ==  "inf")  { value =  std::numeric_limits<real_type>::infinity(); }
