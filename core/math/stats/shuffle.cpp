7a5537e0f29eb1ea0dac0f60afc17e6942076422
Robert Smith <robert.smith@florey.edu.au>
2019-04-16 13:29:21 +1000

    Math::Stats::Shuffler: Re-arrange some code
    Principal purpose of re-arrangement is to provide a class constructor that does not read command-line options; this will enable unit testing of shuffling capabilities.

--- core/math/stats/shuffle.cpp
+++ core/math/stats/shuffle.cpp
@@ -102,12 +102,12 @@ namespace MR
       {
         using namespace App;
         auto opt = get_options ("errors");
-        bool ee = true, ise = false;
+        error_t error_types = error_t::EE;
         if (opt.size()) {
           switch (int(opt[0][0])) {
-            case 0: ee = true;  ise = false; break;
-            case 1: ee = false; ise = true;  break;
-            case 2: ee = true;  ise = true;  break;
+            case 0: error_types = error_t::EE; break;
+            case 1: error_types = error_t::ISE; break;
+            case 2: error_types = error_t::BOTH; break;
           }
         }
 
@@ -118,11 +118,94 @@ namespace MR
           nshuffles_explicit = true;
         }
 
+        opt = get_options (is_nonstationarity ? "permutations_nonstationarity" : "permutations");
+        if (opt.size()) {
+          if (error_types == error_t::EE || error_types == error_t::BOTH) {
+            load_permutations (opt[0][0]);
+            if (permutations[0].size() != rows)
+              throw Exception ("Number of entries per shuffle in file \"" + std::string (opt[0][0]) + "\" does not match number of rows in design matrix (" + str(rows) + ")");
+            if (nshuffles_explicit && nshuffles != permutations.size())
+              throw Exception ("Number of shuffles explicitly requested (" + str(nshuffles) + ") does not match number of shuffles in file \"" + std::string (opt[0][0]) + "\" (" + str(permutations.size()) + ")");
+            nshuffles = permutations.size();
+          } else {
+            throw Exception ("Cannot manually provide permutations if errors are not exchangeable");
+          }
+        }
+
+        initialise (error_types, nshuffles_explicit, is_nonstationarity);
+
+        if (msg.size())
+          progress.reset (new ProgressBar (msg, nshuffles));
+      }
+
+
+
+
+      Shuffler::Shuffler (const size_t num_rows,
+                          const size_t num_shuffles,
+                          const error_t error_types,
+                          const bool is_nonstationarity,
+                          const std::string msg) :
+          rows (num_rows),
+          nshuffles (num_shuffles)
+      {
+        initialise (error_types, false, is_nonstationarity);
+        if (msg.size())
+          progress.reset (new ProgressBar (msg, nshuffles));
+      }
+
+
+
+
+
+      bool Shuffler::operator() (Shuffle& output)
+      {
+        output.index = counter;
+        if (counter >= nshuffles) {
+          if (progress)
+            progress.reset (nullptr);
+          output.data.resize (0, 0);
+          return false;
+        }
+        if (permutations.size()) {
+          output.data = matrix_type::Zero (rows, rows);
+          for (size_t i = 0; i != rows; ++i)
+            output.data (i, permutations[counter][i]) = 1.0;
+        } else {
+          output.data = matrix_type::Identity (rows, rows);
+        }
+        if (signflips.size()) {
+          for (size_t r = 0; r != rows; ++r) {
+            if (signflips[counter][r]) {
+              for (size_t c = 0; c != rows; ++c) {
+                if (output.data (r, c))
+                  output.data (r, c) *= -1.0;
+              }
+            }
+          }
+        }
+        ++counter;
+        if (progress)
+          ++(*progress);
+        return true;
+      }
+
+
+
+
+
+
+      void Shuffler::initialise (const error_t error_types,
+                                 const bool nshuffles_explicit,
+                                 const bool is_nonstationarity)
+      {
+        const bool ee = (error_types == error_t::EE || error_types == error_t::BOTH);
+        const bool ise = (error_types == error_t::ISE || error_types == error_t::BOTH);
+
         const size_t max_num_permutations = factorial (rows);
         const size_t max_num_signflips = (nshuffles >= 8*sizeof(size_t)) ?
                                          (std::numeric_limits<size_t>::max()) :
-                                         ((size_t(1) << num_rows));
-
+                                         ((size_t(1) << rows));
         size_t max_shuffles;
         if (ee) {
           if (ise) {
@@ -161,19 +244,7 @@ namespace MR
         //     while disabling detection of duplicates
         //   - Repeat the three steps above for signflips
 
-        opt = get_options (is_nonstationarity ? "permutations_nonstationarity" : "permutations");
-        if (opt.size()) {
-          if (ee) {
-            load_permutations (opt[0][0]);
-            if (permutations[0].size() != rows)
-              throw Exception ("Number of entries per shuffle in file \"" + std::string (opt[0][0]) + "\" does not match number of rows in design matrix (" + str(rows) + ")");
-            if (nshuffles_explicit && nshuffles != permutations.size())
-              throw Exception ("Number of shuffles explicitly requested (" + str(nshuffles) + ") does not match number of shuffles in file \"" + std::string (opt[0][0]) + "\" (" + str(permutations.size()) + ")");
-            nshuffles = permutations.size();
-          } else {
-            throw Exception ("Cannot manually provide permutations if errors are not exchangeable");
-          }
-        } else if (ee) {
+        if (ee && !permutations.size()) {
           if (ise) {
             if (nshuffles == max_shuffles) {
               generate_all_permutations (rows);
@@ -227,190 +298,154 @@ namespace MR
         }
 
         nshuffles = std::min (nshuffles, max_shuffles);
-
-        if (msg.size())
-          progress.reset (new ProgressBar (msg, nshuffles));
       }
 
 
 
 
-      bool Shuffler::operator() (Shuffle& output)
+
+      bool Shuffler::is_duplicate (const PermuteLabels& v1, const PermuteLabels& v2) const
       {
-        output.index = counter;
-        if (counter >= nshuffles) {
-          if (progress)
-            progress.reset (nullptr);
-          output.data.resize (0, 0);
-          return false;
+        assert (v1.size() == v2.size());
+        for (size_t i = 0; i < v1.size(); i++) {
+          if (v1[i] != v2[i])
+            return false;
         }
-        if (permutations.size()) {
-          output.data = matrix_type::Zero (rows, rows);
-          for (size_t i = 0; i != rows; ++i)
-            output.data (i, permutations[counter][i]) = 1.0;
-        } else {
-          output.data = matrix_type::Identity (rows, rows);
-        }
-        if (signflips.size()) {
-          for (size_t r = 0; r != rows; ++r) {
-            if (signflips[counter][r]) {
-              for (size_t c = 0; c != rows; ++c) {
-                if (output.data (r, c))
-                  output.data (r, c) *= -1.0;
-              }
-            }
-          }
-        }
-        ++counter;
-        if (progress)
-          ++(*progress);
         return true;
       }
 
 
 
-        bool Shuffler::is_duplicate (const PermuteLabels& v1, const PermuteLabels& v2) const
-        {
-          assert (v1.size() == v2.size());
-          for (size_t i = 0; i < v1.size(); i++) {
-            if (v1[i] != v2[i])
-              return false;
-          }
-          return true;
+      bool Shuffler::is_duplicate (const PermuteLabels& perm) const
+      {
+        for (const auto& p : permutations) {
+          if (is_duplicate (perm, p))
+            return true;
         }
+        return false;
+      }
 
 
 
-        bool Shuffler::is_duplicate (const PermuteLabels& perm) const
-        {
-          for (const auto p : permutations) {
-            if (is_duplicate (perm, p))
-              return true;
-          }
-          return false;
+      void Shuffler::generate_random_permutations (const size_t num_perms,
+                                                   const size_t num_rows,
+                                                   const bool include_default,
+                                                   const bool permit_duplicates)
+      {
+        permutations.clear();
+        permutations.reserve (num_perms);
+
+        PermuteLabels default_labelling (num_rows);
+        for (size_t i = 0; i < num_rows; ++i)
+          default_labelling[i] = i;
+
+        size_t p = 0;
+        if (include_default) {
+          permutations.push_back (default_labelling);
+          ++p;
         }
-
-
-
-        void Shuffler::generate_random_permutations (const size_t num_perms,
-                                                     const size_t num_rows,
-                                                     const bool include_default,
-                                                     const bool permit_duplicates)
-        {
-          permutations.clear();
-          permutations.reserve (num_perms);
-
-          PermuteLabels default_labelling (num_rows);
-          for (size_t i = 0; i < num_rows; ++i)
-            default_labelling[i] = i;
-
-          size_t p = 0;
-          if (include_default) {
-            permutations.push_back (default_labelling);
-            ++p;
-          }
-          for (; p != num_perms; ++p) {
-            PermuteLabels permuted_labelling (default_labelling);
-            do {
-              std::random_shuffle (permuted_labelling.begin(), permuted_labelling.end());
-            } while (!permit_duplicates && is_duplicate (permuted_labelling));
-            permutations.push_back (permuted_labelling);
-          }
+        for (; p != num_perms; ++p) {
+          PermuteLabels permuted_labelling (default_labelling);
+          do {
+            std::random_shuffle (permuted_labelling.begin(), permuted_labelling.end());
+          } while (!permit_duplicates && is_duplicate (permuted_labelling));
+          permutations.push_back (permuted_labelling);
         }
+      }
 
 
 
-        void Shuffler::generate_all_permutations (const size_t num_rows)
-        {
-          permutations.clear();
-          permutations.reserve (factorial (num_rows));
-          PermuteLabels temp (num_rows);
-          for (size_t i = 0; i < num_rows; ++i)
-            temp[i] = i;
+      void Shuffler::generate_all_permutations (const size_t num_rows)
+      {
+        permutations.clear();
+        permutations.reserve (factorial (num_rows));
+        PermuteLabels temp (num_rows);
+        for (size_t i = 0; i < num_rows; ++i)
+          temp[i] = i;
+        permutations.push_back (temp);
+        while (std::next_permutation (temp.begin(), temp.end()))
           permutations.push_back (temp);
-          while (std::next_permutation (temp.begin(), temp.end()))
-            permutations.push_back (temp);
+      }
+
+
+
+      void Shuffler::load_permutations (const std::string& filename)
+      {
+        vector<vector<size_t> > temp = load_matrix_2D_vector<size_t> (filename);
+        if (!temp.size())
+          throw Exception ("no data found in permutations file: " + str(filename));
+
+        const size_t min_value = *std::min_element (std::begin (temp[0]), std::end (temp[0]));
+        if (min_value > 1)
+          throw Exception ("indices for relabelling in permutations file must start from either 0 or 1");
+
+        // TODO Support transposed permutations
+        permutations.assign (temp[0].size(), PermuteLabels (temp.size()));
+        for (size_t i = 0; i != temp[0].size(); i++) {
+          for (size_t j = 0; j != temp.size(); j++)
+            permutations[i][j] = temp[j][i] - min_value;
         }
+      }
 
 
 
-        void Shuffler::load_permutations (const std::string& filename)
-        {
-          vector<vector<size_t> > temp = load_matrix_2D_vector<size_t> (filename);
-          if (!temp.size())
-            throw Exception ("no data found in permutations file: " + str(filename));
 
-          const size_t min_value = *std::min_element (std::begin (temp[0]), std::end (temp[0]));
-          if (min_value > 1)
-            throw Exception ("indices for relabelling in permutations file must start from either 0 or 1");
-
-          // TODO Support transposed permutations
-          permutations.assign (temp[0].size(), PermuteLabels (temp.size()));
-          for (size_t i = 0; i != temp[0].size(); i++) {
-            for (size_t j = 0; j != temp.size(); j++)
-              permutations[i][j] = temp[j][i] - min_value;
-          }
+      bool Shuffler::is_duplicate (const BitSet& sign) const
+      {
+        for (const auto s : signflips) {
+          if (sign == s)
+            return true;
         }
+        return false;
+      }
 
 
 
-
-        bool Shuffler::is_duplicate (const BitSet& sign) const
-        {
-          for (const auto s : signflips) {
-            if (sign == s)
-              return true;
-          }
-          return false;
+      void Shuffler::generate_random_signflips (const size_t num_signflips,
+                                                const size_t num_rows,
+                                                const bool include_default,
+                                                const bool permit_duplicates)
+      {
+        signflips.clear();
+        signflips.reserve (num_signflips);
+        size_t s = 0;
+        if (include_default) {
+          BitSet default_labelling (num_rows, false);
+          signflips.push_back (default_labelling);
+          ++s;
         }
-
-
-
-        void Shuffler::generate_random_signflips (const size_t num_signflips,
-                                                  const size_t num_rows,
-                                                  const bool include_default,
-                                                  const bool permit_duplicates)
-        {
-          signflips.clear();
-          signflips.reserve (num_signflips);
-          size_t s = 0;
-          if (include_default) {
-            BitSet default_labelling (num_rows, false);
-            signflips.push_back (default_labelling);
-            ++s;
-          }
-          std::random_device rd;
-          std::mt19937 generator (rd());
-          std::uniform_int_distribution<> distribution (0, 1);
-          for (; s != num_signflips; ++s) {
-            BitSet rows_to_flip (num_rows);
-            do {
-              // TODO Should be a faster mechanism for generating / storing random bits
-              for (size_t index = 0; index != num_rows; ++index)
-                rows_to_flip[index] = distribution (generator);
-            } while (!permit_duplicates && is_duplicate (rows_to_flip));
-            signflips.push_back (rows_to_flip);
-          }
+        std::random_device rd;
+        std::mt19937 generator (rd());
+        std::uniform_int_distribution<> distribution (0, 1);
+        for (; s != num_signflips; ++s) {
+          BitSet rows_to_flip (num_rows);
+          do {
+            // TODO Should be a faster mechanism for generating / storing random bits
+            for (size_t index = 0; index != num_rows; ++index)
+              rows_to_flip[index] = distribution (generator);
+          } while (!permit_duplicates && is_duplicate (rows_to_flip));
+          signflips.push_back (rows_to_flip);
         }
+      }
 
 
 
-        void Shuffler::generate_all_signflips (const size_t num_rows)
-        {
-          signflips.clear();
-          assert (nshuffles >= 8*sizeof(size_t));
-          signflips.reserve (size_t(1) << num_rows);
-          BitSet temp (num_rows, false);
+      void Shuffler::generate_all_signflips (const size_t num_rows)
+      {
+        signflips.clear();
+        assert (nshuffles >= 8*sizeof(size_t));
+        signflips.reserve (size_t(1) << num_rows);
+        BitSet temp (num_rows, false);
+        signflips.push_back (temp);
+        while (!temp.full()) {
+          size_t last_zero_index;
+          for (last_zero_index = num_rows - 1; temp[last_zero_index]; --last_zero_index);
+          temp[last_zero_index] = true;
+          for (size_t indices_zeroed = last_zero_index + 1; indices_zeroed != num_rows; ++indices_zeroed)
+            temp[indices_zeroed] = false;
           signflips.push_back (temp);
-          while (!temp.full()) {
-            size_t last_zero_index;
-            for (last_zero_index = num_rows - 1; temp[last_zero_index]; --last_zero_index);
-            temp[last_zero_index] = true;
-            for (size_t indices_zeroed = last_zero_index + 1; indices_zeroed != num_rows; ++indices_zeroed)
-              temp[indices_zeroed] = false;
-            signflips.push_back (temp);
-          }
         }
+      }
 
 
 
