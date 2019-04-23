10cd3c1502fb65475b93d14f483cd055d8227323
Robert Smith <robert.smith@florey.edu.au>
2019-04-23 18:38:00 +1000

    app.Parser.add_citation(): Change syntax
    Use named keyword arguments to function to disambiguate / remove empty arguments in usage.

--- lib/mrtrix3/dwibiascorrect/ants.py
+++ lib/mrtrix3/dwibiascorrect/ants.py
@@ -7,7 +7,7 @@ def usage(base_parser, subparsers): #pylint: disable=unused-variable
   parser = subparsers.add_parser('ants', parents=[base_parser])
   parser.set_author('Robert E. Smith (robert.smith@florey.edu.au)')
   parser.set_synopsis('Perform DWI bias field correction using the N4 algorithm as provided in ANTs')
-  parser.add_citation('', 'Tustison, N.; Avants, B.; Cook, P.; Zheng, Y.; Egan, A.; Yushkevich, P. & Gee, J. N4ITK: Improved N3 Bias Correction. IEEE Transactions on Medical Imaging, 2010, 29, 1310-1320', True)
+  parser.add_citation('Tustison, N.; Avants, B.; Cook, P.; Zheng, Y.; Egan, A.; Yushkevich, P. & Gee, J. N4ITK: Improved N3 Bias Correction. IEEE Transactions on Medical Imaging, 2010, 29, 1310-1320', is_external=True)
   ants_options = parser.add_argument_group('Options for ANTs N4BiasFieldCorrection command')
   for key in sorted(OPT_N4_BIAS_FIELD_CORRECTION):
     ants_options.add_argument('-ants.'+key, metavar=OPT_N4_BIAS_FIELD_CORRECTION[key][0], help='N4BiasFieldCorrection option -%s. %s' % (key,OPT_N4_BIAS_FIELD_CORRECTION[key][1]))
