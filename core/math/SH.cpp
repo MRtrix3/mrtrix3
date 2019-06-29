860f5acc113478b73b59c9e3fd1e9f0a7c1b2191
Robert Smith <robert.smith@florey.edu.au>
2019-06-29 13:56:38 +1000

    Revise SH convention description & include in more commands

--- core/math/SH.cpp
+++ core/math/SH.cpp
@@ -36,11 +36,13 @@ namespace MR
         "Each volume in the output image corresponds to a different spherical harmonic "
         "component. Each volume will correspond to the following: \n"
         "volume 0: l = 0, m = 0 ; \n"
-        "volume 1: l = 2, m = -2 (imaginary part of m=2 SH) ; \n"
-        "volume 2: l = 2, m = -1 (imaginary part of m=1 SH) ; \n"
+        "volume 1: l = 2, m = 2 (imaginary part) ; \n"
+        "volume 2: l = 2, m = 1 (imaginary part) ; \n"
         "volume 3: l = 2, m = 0 ; \n"
-        "volume 4: l = 2, m = 1 (real part of m=1 SH) ; \n"
-        "volume 5: l = 2, m = 2 (real part of m=2 SH) ; \n"
+        "volume 4: l = 2, m = 1 (real part) ; \n"
+        "volume 5: l = 2, m = 2 (real part) ; \n"
+        "volume 6: l = 4, m = 4 (imaginary part) ; \n"
+        "volume 7: l = 4, m = 3 (imaginary part) ; \n"
         "etc...\n";
 
 
