60e4d97ddc2506ecd715f2cbc3eac917ede0005f
Robert Smith <robert.smith@florey.edu.au>
2019-08-04 17:30:03 +1000

    mrthreshold: Multiple changes
    - New option -allvolumes that processes an entire 4D image as a single batch of data for which to determine a single threshold, rather than individual 3D volumes to be thresholded individually.
    - Modify MinMax functor and interface to permit use of a mask image (#738); this should make -mask option in mrthreshold behave as expected.

--- cmd/mrthreshold.cpp
+++ cmd/mrthreshold.cpp
@@ -19,6 +19,8 @@
 #include "image.h"
 #include "image_helpers.h"
 
+#include "adapter/replicate.h"
+#include "adapter/subset.h"
 #include "algo/loop.h"
 #include "filter/optimal_threshold.h"
 
@@ -28,7 +30,7 @@ using namespace App;
 
 void usage ()
 {
-  AUTHOR = "J-Donald Tournier (jdtournier@gmail.com) and Robert E. Smith (robert.smith@florey.edu.au)";
+  AUTHOR = "Robert E. Smith (robert.smith@florey.edu.au) and J-Donald Tournier (jdtournier@gmail.com)";
 
   SYNOPSIS = "Create bitwise image by thresholding image intensity";
 
@@ -79,7 +81,9 @@ void usage ()
 
   + OptionGroup ("Options that influence determination of the threshold based on the input image")
 
-  + Option ("ignorezero", "ignore zero-valued input values")
+  + Option ("allvolumes", "compute and apply a single threshold for all image volumes, rather than an individual threshold per volume")
+
+  + Option ("ignore_zero", "ignore zero-valued input values")
 
   + Option ("mask", "compute the threshold based only on values within an input mask image")
     + Argument ("image").type_image_in ()
@@ -97,43 +101,57 @@ void usage ()
 using value_type = float;
 
 
+
+bool issue_degeneracy_warning = false;
+
+
+
 Image<bool> get_mask (const Image<value_type>& in)
 {
   Image<bool> mask;
   auto opt = get_options ("mask");
   if (opt.size()) {
     mask = Image<bool>::open (opt[0][0]);
-    check_dimensions (in, mask);
+    if (mask.ndim() > in.ndim())
+      throw Exception ("Cannot use mask image with more axes than input image");
+    check_dimensions (in, mask, 0, 3);
+    for (size_t axis = 3; axis != mask.ndim(); ++axis) {
+      if (mask.size (axis) > 1 && mask.size (axis) != in.size (axis))
+        throw Exception ("Dimensions of mask image do not match those of main image");
+    }
   }
   return mask;
 }
 
 
-vector<value_type> get_data (Image<value_type> in)
+vector<value_type> get_data (Image<value_type>& in,
+                             Image<bool>& mask,
+                             const size_t max_axis,
+                             const bool ignore_zero)
 {
-  const bool ignorezero = get_options("ignorezero").size();
   vector<value_type> data;
-  Image<bool> mask = get_mask (in);
+  data.reserve (voxel_count (in, 0, max_axis));
   if (mask.valid()) {
-    if (ignorezero) {
-      for (auto l = Loop(in) (in, mask); l; ++l) {
-        if (mask.value() && in.value())
+    Adapter::Replicate<Image<bool>> mask_replicate (mask, in);
+    if (ignore_zero) {
+      for (auto l = Loop(in, 0, max_axis) (in, mask_replicate); l; ++l) {
+        if (mask_replicate.value() && in.value())
           data.push_back (in.value());
       }
     } else {
-      for (auto l = Loop(in) (in, mask); l; ++l) {
-        if (mask.value() && std::isfinite (in.value()))
+      for (auto l = Loop(in, 0, max_axis) (in, mask_replicate); l; ++l) {
+        if (mask_replicate.value() && std::isfinite (in.value()))
           data.push_back (in.value());
       }
     }
   } else {
-    if (ignorezero) {
-      for (auto l = Loop(in) (in); l; ++l) {
+    if (ignore_zero) {
+      for (auto l = Loop(in, 0, max_axis) (in); l; ++l) {
         if (in.value())
           data.push_back (in.value());
       }
     } else {
-      for (auto l = Loop(in) (in); l; ++l) {
+      for (auto l = Loop(in, 0, max_axis) (in); l; ++l) {
           if (std::isfinite (in.value()))
         data.push_back (in.value());
       }
@@ -177,46 +195,27 @@ void save (Image<float> in,
 
 
 
-void run ()
+default_type calculate (Image<value_type>& in,
+                        Image<bool>& mask,
+                        const size_t max_axis,
+                        const default_type abs,
+                        const default_type percentile,
+                        const ssize_t bottom,
+                        const ssize_t top,
+                        const bool ignore_zero,
+                        const bool to_cout)
 {
-  const default_type abs = get_option_value ("abs", NaN);
-  const default_type percentile = get_option_value ("percentile", NaN);
-  const ssize_t bottom = get_option_value ("bottom", -1);
-  const ssize_t top = get_option_value ("top", -1);
-  if ((std::isfinite (abs) ? 1 : 0) +
-      (std::isfinite (percentile) ? 1 : 0) +
-      (bottom >= 0 ? 1 : 0) +
-      (top >= 0 ? 1 : 0) > 1)
-    throw Exception ("Cannot specify more than one mechanism for threshold selection");
-
-  auto header = Header::open (argument[0]);
-  if (header.datatype().is_complex())
-    throw Exception ("Cannot perform thresholding on complex images");
-  auto in = header.get_image<value_type>();
-
-  const bool to_cout = argument.size() == 1;
-
-  bool equal_as_above = true;
-  default_type threshold;
   if (std::isfinite (abs)) {
 
-    auto ignore_input_option = [&] (const std::string& opt) {
-      if (get_options (opt).size()) {
-        WARN ("Option -" + opt + " ignored: has no influence due to use of -abs option");
-      }
-    };
-    ignore_input_option ("ignorezero");
-    ignore_input_option ("mask");
+    return abs;
 
-    threshold = abs;
+  } else if (std::isfinite (percentile)) {
 
-  } else if (get_options ("percentile").size()) {
-
-    auto data = get_data (in);
+    auto data = get_data (in, mask, max_axis, ignore_zero);
     if (percentile == 100.0) {
-      threshold = *std::max_element (data.begin(), data.end());
+      return *std::max_element (data.begin(), data.end());
     } else if (!percentile) {
-      threshold = *std::min_element (data.begin(), data.end());
+      return *std::min_element (data.begin(), data.end());
     } else {
       const default_type interp_index = 0.01 * percentile * (data.size()-1);
       const size_t lower_index = std::floor (interp_index);
@@ -225,78 +224,239 @@ void run ()
       const value_type lower_value = data[lower_index];
       std::nth_element (data.begin(), data.begin() + lower_index + 1, data.end());
       const value_type upper_value = data[lower_index + 1];
-      threshold = (1.0-mu)*lower_value + mu*upper_value;
+      return (1.0-mu)*lower_value + mu*upper_value;
     }
 
   } else if (std::max (bottom, top) >= 0) {
 
-    auto data = get_data (in);
-    const size_t index (bottom >= 0 ?
-                        size_t(bottom) - 1 :
-                        (data.size() - top));
-    if (index > data.size())
+    auto data = get_data (in, mask, max_axis, ignore_zero);
+    const ssize_t index (bottom >= 0 ?
+                         size_t(bottom) - 1 :
+                         (ssize_t(data.size()) - ssize_t(top)));
+    if (index < 0 || index >= data.size())
       throw Exception ("Number of valid input image values (" + str(data.size()) + ") less than number of voxels requested via -" + (bottom >= 0 ? "bottom" : "top") + " option (" + str(index) + ")");
     std::nth_element (data.begin(), data.begin() + index, data.end());
     const value_type threshold_float = data[index];
-    bool degeneracy_warning = false;
     if (index) {
       std::nth_element (data.begin(), data.begin() + index - 1, data.end());
       if (data[index-1] == threshold_float)
-        degeneracy_warning = true;
+        issue_degeneracy_warning = true;
     }
     if (index < data.size() - 1) {
       std::nth_element (data.begin(), data.begin() + index + 1, data.end());
       if (data[index+1] == threshold_float)
-        degeneracy_warning = true;
+        issue_degeneracy_warning = true;
     }
     std::sort (data.begin(), data.end());
-    if (degeneracy_warning) {
-      WARN ("Duplicate image values surrounding threshold; "
-            "exact number of voxels influenced by numerical threshold may not match requested number");
-    }
-    threshold = default_type(threshold_float);
-    // If threhsolding to remove some lower number of voxels, we want to
-    //   _not_ accept any voxels for which the value is precisely equal to the threshold
-    if (bottom >= 0)
-      equal_as_above = false;
+    return default_type(threshold_float);
 
   } else { // No explicit mechanism option: do automatic thresholding
 
-    if (get_options ("ignorezero").size()) {
-      WARN ("Option -ignorezero ignored by automatic threshold calculation");
-    }
-    try {
-      check_3D_nonunity (in);
-    } catch (Exception& e) {
-      throw Exception (e, "Automatic thresholding can only be performed for voxel data");
-    }
-    Image<bool> mask = get_mask (in);
-    if (to_cout) {
-      LogLevelLatch latch (App::log_level - 1);
-      threshold = Filter::estimate_optimal_threshold (in, mask);
+    std::unique_ptr<LogLevelLatch> latch;
+    if (to_cout)
+      latch.reset (new LogLevelLatch (App::log_level - 1));
+    if (max_axis < in.ndim()) {
+
+      // Need to extract just the current 3D volume
+      vector<size_t> in_from (in.ndim()), in_size (in.ndim());
+      size_t axis;
+      for (axis = 0; axis != 3; ++axis) {
+        in_from[axis] = 0;
+        in_size[axis] = in.size (axis);
+      }
+      for (; axis != in.ndim(); ++axis) {
+        in_from[axis] = in.index (axis);
+        in_size[axis] = 1;
+      }
+      Adapter::Subset<Image<value_type>> in_subset (in, in_from, in_size);
+      if (mask.valid()) {
+        vector<size_t> mask_from (mask.ndim()), mask_size (mask.ndim());
+        for (axis = 0; axis != 3; ++axis) {
+          mask_from[axis] = 0;
+          mask_size[axis] = mask.size (axis);
+        }
+        for (; axis != mask.ndim(); ++axis) {
+          mask_from[axis] = mask.index (axis);
+          mask_size[axis] = 1;
+        }
+        Adapter::Subset<Image<bool>> mask_subset (mask, mask_from, mask_size);
+        Adapter::Replicate<decltype(mask_subset)> mask_replicate (mask_subset, in_subset);
+        return Filter::estimate_optimal_threshold (in_subset, mask_replicate);
+      } else {
+        return Filter::estimate_optimal_threshold (in_subset);
+      }
+
     } else {
-      threshold = Filter::estimate_optimal_threshold (in, mask);
+      return Filter::estimate_optimal_threshold (in, mask);
     }
 
   }
+}
 
+
+
+template <typename T>
+void apply (Image<value_type>& in,
+            Image<T>& out,
+            const size_t max_axis,
+            const default_type threshold,
+            const bool to_cout,
+            const bool equal_as_above,
+            const bool invert)
+{
   if (to_cout) {
-    auto ignore_output_option = [&] (const std::string& opt) {
-      if (get_options (opt).size()) {
-        WARN ("Option -" + opt + " ignored: has no influence when no output image is provided");
-      }
-    };
-    ignore_output_option ("invert");
-    ignore_output_option ("nan");
     std::cout << threshold;
     return;
   }
 
+  T above = std::is_floating_point<T>::value ? 1.0 : true;
+  T below = std::is_floating_point<T>::value ? NaN : false;
+  const T nonfinite = below;
+  if (invert)
+    std::swap (above, below);
+
+  if (equal_as_above) {
+    const value_type threshold_float (threshold);
+    for (auto l = Loop(in, 0, max_axis) (in, out); l; ++l)
+      out.value() = std::isfinite (in.value()) ?
+                    ((in.value() >= threshold_float) ? above : below) :
+                    nonfinite;
+  } else {
+    for (auto l = Loop(in, 0, max_axis) (in, out); l; ++l)
+      out.value() = std::isfinite (in.value()) ?
+                    ((default_type (in.value()) > threshold) ? above : below) :
+                    nonfinite;
+  }
+}
+
+
+
+
+template <typename T>
+void execute (Image<value_type>& in,
+              Image<bool>& mask,
+              const std::string& out_path,
+              const default_type abs,
+              const default_type percentile,
+              const ssize_t bottom,
+              const ssize_t top,
+              const bool ignore_zero,
+              const bool all_volumes,
+              const bool invert)
+{
+  const bool to_cout = out_path.empty();
+  Image<T> out;
+  if (!to_cout) {
+    Header header_out (in);
+    header_out.datatype() = DataType::from<T>();
+    header_out.datatype().set_byte_order_native();
+    out = Image<T>::create (out_path, header_out);
+  }
+
+  // If threhsolding to remove some lower number of voxels, we want to
+  //   _not_ accept any voxels for which the value is precisely equal to the threshold
+  const bool equal_as_above = bottom < 0;
+
+  // Branch based on whether or not we need to process each image volume individually
+  if (in.ndim() > 3 && !all_volumes) {
+
+    // Do one volume at a time
+    // If writing to cout, also add a newline between each volume
+    bool is_first_loop = true;
+    for (auto l = Loop("Determining and applying per-volume thresholds", 3, in.ndim()) (in); l; ++l) {
+      if (to_cout) {
+        if (is_first_loop)
+          is_first_loop = false;
+        else
+          std::cout << "\n";
+      }
+      LogLevelLatch latch (App::log_level - 1);
+      const default_type threshold = calculate (in, mask, 3, abs, percentile, bottom, top, ignore_zero, to_cout);
+      if (out.valid())
+        assign_pos_of (in, 3).to (out);
+      apply (in, out, 3, threshold, to_cout, equal_as_above, invert);
+    }
+
+    return;
+
+  } else if (in.ndim() <= 3 && all_volumes) {
+    WARN ("Option -allvolumes ignored; input image is less than 4D");
+  }
+
+  // Process whole input image as a single block
+  const default_type threshold = calculate (in, mask, in.ndim(), abs, percentile, bottom, top, ignore_zero, to_cout);
+  apply (in, out, in.ndim(), threshold, to_cout, equal_as_above, invert);
+
+}
+
+
+
+
+
+void run ()
+{
+  const default_type abs = get_option_value ("abs", NaN);
+  const default_type percentile = get_option_value ("percentile", NaN);
+  const ssize_t bottom = get_option_value ("bottom", -1);
+  const ssize_t top = get_option_value ("top", -1);
+  const size_t num_explicit_mechanisms = (std::isfinite (abs) ? 1 : 0) +
+                                         (std::isfinite (percentile) ? 1 : 0) +
+                                         (bottom >= 0 ? 1 : 0) +
+                                         (top >= 0 ? 1 : 0);
+  if (num_explicit_mechanisms > 1)
+    throw Exception ("Cannot specify more than one mechanism for threshold selection");
+
+  auto header_in = Header::open (argument[0]);
+  if (header_in.datatype().is_complex())
+    throw Exception ("Cannot perform thresholding directly on complex image data");
+  auto in = header_in.get_image<value_type>();
+
+  const bool to_cout = argument.size() == 1;
+  const std::string output_path = to_cout ? std::string("") : argument[1];
+
+  const bool all_volumes = get_options("allvolumes").size();
+  const bool ignore_zero = get_options("ignorezero").size();
   const bool use_nan = get_options ("nan").size();
   const bool invert = get_options ("invert").size();
-  if (use_nan)
-    save<value_type> (in, header, threshold, invert, equal_as_above, argument[1]);
-  else
-    save<bool> (in, header, threshold, invert, equal_as_above, argument[1]);
 
+  Image<bool> mask;
+  if (std::isfinite (abs)) {
+    if (ignore_zero) {
+      WARN ("-ignorezero option has no effect if combined with -abs option");
+    }
+    if (get_options ("mask").size()) {
+      WARN ("-mask option has no effect if combined with -abs option");
+    }
+  } else  {
+    mask = get_mask (in);
+    if (!num_explicit_mechanisms) {
+      if (ignore_zero) {
+        WARN ("Option -ignorezero ignored by automatic threshold calculation");
+      }
+      try {
+        check_3D_nonunity (in);
+      } catch (Exception& e) {
+        throw Exception (e, "Automatic thresholding can only be performed for voxel data");
+      }
+    }
+  }
+
+  if (to_cout) {
+    if (invert) {
+      WARN ("Option -invert ignored: has no influence when no output image is specified");
+    }
+    if (use_nan) {
+      WARN ("Option -nan ignored: has no influence when no output image is specified");
+    }
+  }
+
+  if (use_nan)
+    execute<value_type> (in, mask, output_path, abs, percentile, bottom, top, ignore_zero, all_volumes, invert);
+  else
+    execute<bool>       (in, mask, output_path, abs, percentile, bottom, top, ignore_zero, all_volumes, invert);
+
+  if (issue_degeneracy_warning) {
+    WARN ("Duplicate image values surrounding threshold; "
+          "exact number of voxels influenced by numerical threshold may not match requested number");
+  }
 }
