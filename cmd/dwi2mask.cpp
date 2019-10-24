1937c7a47360fad06c733155c1be2bf28b8455ba
Robert Smith <robert.smith@florey.edu.au>
2019-10-24 17:31:58 +1100

    dwi2mask: Fix clean filter on cropped data
    Because the mask cleaning filter operates only on an input mask, and ignores whatever data were used to generate that mask, it is possible for the output of that filter to include voxels that should never be a part of the mask. This includes cases where the image data are not spatially smooth, but instead possess a sharp transition between valid and invalid data; for instance following motion correction where part of the brain is zero-filled due to moving outside of the field of view in at least one volume. This change ensures that DWI voxels that should never be included in the output mask (e.g. filled with zeroes or NaNs) do not get erroneously included in the mask due to use of the mask cleaning filter.

--- cmd/dwi2mask.cpp
+++ cmd/dwi2mask.cpp
@@ -87,7 +87,21 @@ void run () {
     try {
       Filter::MaskClean clean_filter (temp_mask, std::string("applying mask cleaning filter"));
       clean_filter.set_scale (scale);
-      clean_filter (temp_mask, output);
+      clean_filter (temp_mask, temp_mask);
+      for (auto l = Loop (0,3) (input, temp_mask, output); l; ++l) {
+        if (temp_mask.value()) {
+          bool all_zero = true;
+          for (auto vl = Loop (3) (input); vl; ++vl) {
+            if (std::isfinite (input.value()) && input.value()) {
+              all_zero = false;
+              break;
+            }
+          }
+          output.value() = !all_zero;
+        } else {
+          output.value() = false;
+        }
+      }
     } catch (...) {
       WARN("Unable to run mask cleaning filter (image is not truly 3D); skipping");
       for (auto l = Loop (0,3) (temp_mask, output); l; ++l)
