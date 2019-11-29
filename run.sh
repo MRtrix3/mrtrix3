d2b8d8ffb6baf478502578d5b2a2407499ec293f
J-Donald Tournier <jdtournier@gmail.com>
2019-11-29 20:17:51 +0000

    add debugging output

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
+echo $#
+echo $@
+
 if [[ $# -ne 2 ]]; then
   echo usage: run.sh tag user
 else
 
-  set -e
-  sed -i "s|TAGNAME|$1|g" PKGBUILD
-  sed -i "s|GIT_USER|$2|g" PKGBUILD
+  set -ex
+  sed -i'.orig' -e "s|TAGNAME|$1|g" -e "s|GIT_USER|$2|g" PKGBUILD
   makepkg
 
 fi
