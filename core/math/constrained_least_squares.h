5723915ea41ab6a19c55634b43d6537056032efd
J-Donald Tournier <jdtournier@gmail.com>
2019-12-20 14:16:37 +0000

    constrained least squares: allow separate inequality and equality constraint matrices

--- core/math/constrained_least_squares.h
+++ core/math/constrained_least_squares.h
@@ -40,6 +40,34 @@ namespace MR
 
 
 
+    //! functionality for solving constrained least-squares problems
+    /*! the problem is to solve norm(\e Hx - \e b) subject to \e Ax >=
+     * t and optionally \e Bx = s. This sets up a class to be re-used and
+     * shared across threads, assuming the matrices \e H, \e A & \e B and
+     * vectors \e t & \e s don't change, but the vector \e b does.
+     *
+     * The arguments passed to the Problem constructor correspond to:
+     * - \e H - \a problem_matrix
+     * - \e A - \a inequality_constraint_matrix
+     * - \e B - \a equality_constraint_matrix
+     * - \e b - \a problem_vector
+     * - \e t - \a inequality_constraint_vector
+     * - \e s - \a equality_constraint_vector
+     *
+     * It is also possible to apply an additional minimum norm constraint that
+     * will be added to the problem cost function, to stablise ill-posed
+     * problems, and/or a separate minimum norm constraint on the Lagrangian
+     * multipliers to handle cases where they become degenerate (otherwise
+     * leads to errors in the Cholesky decomposition). Both default to zero,
+     * set one or both of them to a small value such as 1e-10 to help with
+     * these kinds of problem.
+
+     * It is also possible to pass the problem already in standard form,
+     * whereby the matrix provided as \a problem_matrix is assumed to contain
+     * \e H<sup>T</sup>H (only its lower triangular part is taken into
+     * account), while the vectors provided to the solver will be assumed to
+     * contain \e H<sup>T</sup>b.
+     */
     namespace ICLS {
 
       template <typename ValueType>
@@ -52,38 +80,17 @@ namespace MR
 
             Problem () { }
             //! set up constrained least-squares problem
-            /*! the problem is to solve norm(\e Hx - \e b) subject to \e Ax >=
-             * t. This sets up a class to be re-used and shared across threads,
-             * assuming the matrices \e H & \e A don't change, but the vectors
-             * \e b & \e t do.
+            /*! With this constructor, equality constraints (if present) are
+             * assumed to have been included as the last \e N rows of the
+             * constraint matrix, and the number of equality constraints is
+             * passed as the \a num_equalities argument.
              *
-             * It is also possible to use equality constraints by telling the
-             * solver how many of the constraints should be treated as equality
-             * constraints. If non-zero, the corresponding last rows of the
-             * constraint matrix (and vector) will be treated as equalities.
-             *
-             *  - \a problem_matrix: \e H
-             *  - \a constraint_matrix: \e A
-             *  - \a solution_min_norm_regularisation: an additional minimum
-             *  norm constraint that will be added to the problem cost
-             *  function, to stablise ill-posed problems.
-             *  - \a solution_min_norm_regularisation: used to
-             *  stabilise the estimation of the Lagrangian multipliers to
-             *  handle cases where they become degenerate (otherwise leads to
-             *  errors in the Cholesky decomposition). Default is zero, set it
-             *  to a small value such as 1e-10 to help with these kinds of
-             *  problem.
-             *  - \a max_iterations: the maximum number of iterations to run.
-             *  If zero (default), this number is set to 10x the size of \e x.
-             *  - \a problem_in_standard_form: if true, the matrix provided as
-             *  \a problem_matrix is assumed to contain \e H<sup>T</sup>H (only
-             *  its lower triangular part is taken into account), while the
-             *  vectors provided to the solver will be assumed to contain
-             *  \e H<sup>T</sup>b.
+             * If omitted, the \a inequality_constraint_vector defaults to a
+             * vector of zeros.
              */
             Problem (const matrix_type& problem_matrix,
-                const matrix_type& constraint_matrix,
-                const vector_type& thresholds = vector_type(),
+                const matrix_type& inequality_constraint_matrix,
+                const vector_type& inequality_constraint_vector = vector_type(),
                 size_t num_equalities = 0,
                 value_type solution_min_norm_regularisation = 0.0,
                 value_type constraint_min_norm_regularisation = 0.0,
@@ -92,13 +99,13 @@ namespace MR
                 bool problem_in_standard_form = false) :
               H (problem_matrix),
               chol_HtH (H.cols(), H.cols()),
-              t (thresholds),
+              t (inequality_constraint_vector),
               lambda_min_norm (constraint_min_norm_regularisation),
               tol (tolerance),
               max_niter (max_iterations ? max_iterations : 10*problem_matrix.cols()),
               num_eq (num_equalities) {
 
-                if (H.cols() != constraint_matrix.cols())
+                if (H.cols() != inequality_constraint_matrix.cols())
                   throw Exception ("FIXME: dimensions of problem and constraint matrices do not match (ICLS)");
 
                 if (solution_min_norm_regularisation < 0.0)
@@ -109,6 +116,9 @@ namespace MR
 
                 if (tolerance < 0.0)
                   throw Exception ("FIXME: tolerance is negative (ICLS)");
+
+                if (t.size() && t.size() != inequality_constraint_matrix.rows())
+                  throw Exception ("FIXME: dimensions of constraint matrix and vector do not match (ICLS)");
 
                 if (problem_in_standard_form) {
                   chol_HtH = H;
@@ -131,7 +141,7 @@ namespace MR
                   b2d.noalias() = chol_HtH.template triangularView<Eigen::Lower>().transpose().template solve<Eigen::OnTheRight> (H);
 
                 // project constraint onto preconditioned quadratic domain,
-                B.noalias() = chol_HtH.template triangularView<Eigen::Lower>().transpose().template solve<Eigen::OnTheRight> (constraint_matrix);
+                B.noalias() = chol_HtH.template triangularView<Eigen::Lower>().transpose().template solve<Eigen::OnTheRight> (inequality_constraint_matrix);
                 for (ssize_t n = 0; n < B.rows(); ++n) {
                   double norm = B.row(n).norm();
                   B.row(n) /= norm;
@@ -140,6 +150,41 @@ namespace MR
                 }
 
               }
+
+            //! set up constrained least-squares problem
+            /*! With this constructor, equality constraints are passed as a
+             * distinct \a equality_constraint_matrix along with the
+             * corresponding \a equality_constraint_vector.
+             *
+             * Note that if either \a inequality_constraint_vector or \a
+             * equality_constraint_vector are specified, \e both must be
+             * specified.
+             */
+            Problem (const matrix_type& problem_matrix,
+                const matrix_type& inequality_constraint_matrix,
+                const matrix_type& equality_constraint_matrix,
+                const vector_type& inequality_constraint_vector = vector_type(),
+                const vector_type& equality_constraint_vector = vector_type(),
+                value_type solution_min_norm_regularisation = 0.0,
+                value_type constraint_min_norm_regularisation = 0.0,
+                size_t max_iterations = 0,
+                value_type tolerance = 0.0,
+                bool problem_in_standard_form = false) :
+              Problem (problem_matrix,
+                  concat (inequality_constraint_matrix, equality_constraint_matrix),
+                  concat (inequality_constraint_vector, equality_constraint_vector),
+                  equality_constraint_matrix.rows(),
+                  solution_min_norm_regularisation,
+                  constraint_min_norm_regularisation,
+                  max_iterations,
+                  tolerance,
+                  problem_in_standard_form) {
+                if (equality_constraint_vector.size() || inequality_constraint_vector.size()) {
+                  if (num_eq != equality_constraint_vector.size())
+                    throw Exception ("FIXME: dimensions of equality constraint matrix and vector do not match (ICLS)");
+                }
+              }
+
 
             size_t num_parameters () const { return H.cols(); }
             size_t num_measurements () const { return H.rows(); }
@@ -150,6 +195,20 @@ namespace MR
             vector_type t;
             value_type lambda_min_norm, tol;
             size_t max_niter, num_eq;
+
+            static inline matrix_type concat (const matrix_type& a, const matrix_type& b) {
+              matrix_type c (a.rows()+b.rows(), a.cols());
+              c.topRows (a.rows()) = a;
+              c.bottomRows (b.rows()) = b;
+              return c;
+            }
+
+            static inline vector_type concat (const vector_type& a, const vector_type& b) {
+              vector_type c (a.size()+b.size());
+              c.head (a.size()) = a;
+              c.tail (b.size()) = b;
+              return c;
+            }
         };
 
 
@@ -261,7 +320,7 @@ namespace MR
                     break;
                   }
 #ifdef MRTRIX_ICLS_DEBUG
-                l_stream << lambda << "\n";
+                  l_stream << lambda << "\n";
 #endif
 
                   // remove worst offending lambda from active set,
