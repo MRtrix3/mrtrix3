MRtrix3/mrtrix3

commit 67ce65b4733373a5e93285b274e6d249525afaf8

Track files: Support multi-line key-values
Issue arose due to potential for multi-line "command-history" field following #1603.
Closes #1925.

src/dwi/tractography/file_base.h
@@ -91,8 +91,10 @@ namespace MR
               out << "mrtrix " + type + "\nEND\n";
 
               for (const auto& i : properties) {
-                if ((i.first != "count") && (i.first != "total_count"))
-                  out << i.first << ": " << i.second << "\n";
+                if ((i.first != "count") && (i.first != "total_count")) {
+                  for (const auto line : split_lines (i.second))
+                    out << i.first << ": " << i.second << "\n";
+                }
               }
 
               for (const auto& i : properties.comments)

src/dwi/tractography/file_base.cpp
@@ -45,7 +45,7 @@ namespace MR {
           else if (key == "comment") properties.comments.push_back (kv.value());
           else if (key == "file") data_file = kv.value();
           else if (key == "datatype") dtype = DataType::parse (kv.value());
-          else properties[kv.key()] = kv.value();
+          else add_line (properties[kv.key()], kv.value());
         }
 
         if (dtype == DataType::Undefined)

commit 2634da7c9fa3d9dadc11631692ed9b93d67d94ee

Track files: Fix export of multi-line properties
Bug introduced in 67ce65b4.

src/dwi/tractography/file_base.h
@@ -93,7 +93,7 @@ namespace MR
               for (const auto& i : properties) {
                 if ((i.first != "count") && (i.first != "total_count")) {
                   for (const auto line : split_lines (i.second))
-                    out << i.first << ": " << i.second << "\n";
+                    out << i.first << ": " << line << "\n";
                 }
               }
 
@@ -158,4 +158,3 @@ namespace MR
 
 
 #endif
-