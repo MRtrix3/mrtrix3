MRtrix3/mrtrix3

commit 6dcd6edb2651e6e8f6ad7e091f5cf3fe6d052332

picked registration developments part of the mrreg branch without the splitting of mrregister into mrreg_* commands

core/math/average_space.h
@@ -24,7 +24,6 @@
 #include "image.h"
 #include "debug.h"
 
-
 namespace MR
 {
    namespace Math
@@ -39,26 +38,21 @@ namespace MR
   Eigen::Matrix<default_type, 8, 4> get_bounding_box (const Header& header, const Eigen::Transform<default_type, 3, Eigen::Projective>& voxel2scanner);
 
   Header compute_minimum_average_header (const vector<Header>& input_headers,
-                                         int voxel_subsampling,
-                                         const Eigen::Matrix<default_type, 4, 1>& padding,
-                                         const vector<Eigen::Transform<default_type, 3, Eigen::Projective>>& transform_header_with);
+                                         const vector<Eigen::Transform<default_type, 3, Eigen::Projective>>& transform_header_with,
+                                         int voxel_subsampling = 1,
+                                         Eigen::Matrix<default_type, 4, 1> padding = Eigen::Matrix<default_type, 4, 1>(1.0, 1.0, 1.0, 1.0));
 
-  template<class ImageType1, class ImageType2, class TransformationType>
+  template<class ImageType1, class ImageType2>
     Header compute_minimum_average_header (
         const ImageType1& im1,
         const ImageType2& im2,
-        const TransformationType& transformation,
-        const default_type& voxel_subsampling,
-        const Eigen::Matrix<default_type, 4, 1>& padding) {
-      vector<Eigen::Transform<default_type, 3, Eigen::Projective>> init_transforms;
-      Eigen::Transform<default_type, 3, Eigen::Projective> trafo_1 = transformation.get_transform_half_inverse();
-      Eigen::Transform<default_type, 3, Eigen::Projective> trafo_2 = transformation.get_transform_half();
-      init_transforms.push_back (trafo_1);
-      init_transforms.push_back (trafo_2);
-      vector<Header> headers;
-      headers.push_back(Header(im1));
-      headers.push_back(Header(im2));
-      return compute_minimum_average_header(headers, voxel_subsampling, padding, init_transforms);
+        Eigen::Transform<default_type, 3, Eigen::Projective> transform_1 = Eigen::Transform<default_type, 3, Eigen::Projective>(),
+        Eigen::Transform<default_type, 3, Eigen::Projective> transform_2 = Eigen::Transform<default_type, 3, Eigen::Projective>(),
+        Eigen::Matrix<default_type, 4, 1> padding = Eigen::Matrix<default_type, 4, 1>(1.0, 1.0, 1.0, 1.0),
+        int voxel_subsampling = 1) {
+      vector<Eigen::Transform<default_type, 3, Eigen::Projective>> init_transforms {transform_1, transform_2};
+      vector<Header> headers {im1,im2};
+      return compute_minimum_average_header (headers, init_transforms, voxel_subsampling, padding);
     }
  }
 #endif

cmd/mraverageheader.cpp
@@ -86,7 +86,7 @@ void run ()
     }
 
     vector<Eigen::Transform<default_type, 3, Eigen::Projective>> transform_header_with;
-    auto H = compute_minimum_average_header (headers_in, resolution, padding, transform_header_with);
+    auto H = compute_minimum_average_header (headers_in, transform_header_with, resolution, padding);
     H.datatype() = DataType::Bit;
     if (fill) {
       H.ndim() = dim;

cmd/mrfilter.cpp
@@ -24,13 +24,14 @@
 #include "filter/normalise.h"
 #include "filter/median.h"
 #include "filter/smooth.h"
+#include "filter/zclean.h"
 
 
 using namespace MR;
 using namespace App;
 
 
-const char* filters[] = { "fft", "gradient", "median", "smooth", "normalise", nullptr };
+const char* filters[] = { "fft", "gradient", "median", "smooth", "normalise", "zclean", NULL };
 
 
 const OptionGroup FFTOption = OptionGroup ("Options for FFT filter")
@@ -101,6 +102,18 @@ const OptionGroup SmoothOption = OptionGroup ("Options for smooth filter")
             "The default extent is 2 * ceil(2.5 * stdev / voxel_size) - 1.")
   + Argument ("voxels").type_sequence_int();
 
+const OptionGroup ZcleanOption = OptionGroup ("Options for zclean filter")
++ Option ("zupper", "define high intensity outliers: default: 2.5")
+  + Argument ("num").type_float(0.1, std::numeric_limits<float>::infinity())
++ Option ("zlower", "define low intensity outliers: default: 2.5")
+  + Argument ("num").type_float(0.1, std::numeric_limits<float>::infinity())
++ Option ("bridge", "number of voxels to gap to fill holes in mask: default: 4")
+  + Argument ("num").type_integer(0)
++ Option ("maskin", "initial mask that defines the maximum spatial extent and the region from "
+          "which to smaple the intensity range.")
+  + Argument ("image").type_image_in()
++ Option ("maskout", "Output a refined mask based on a spatially coherent region with normal intensity range.")
+  + Argument ("image").type_image_out();
 
 
 void usage ()
@@ -110,7 +123,7 @@ void usage ()
   SYNOPSIS = "Perform filtering operations on 3D / 4D MR images";
 
   DESCRIPTION
-  + "The available filters are: fft, gradient, median, smooth, normalise."
+  + "The available filters are: fft, gradient, median, smooth, normalise, zclean."
   + "Each filter has its own unique set of optional parameters."
   + "For 4D images, each 3D volume is processed independently.";
 
@@ -125,6 +138,7 @@ void usage ()
   + MedianOption
   + NormaliseOption
   + SmoothOption
+  + ZcleanOption
   + Stride::Options;
 }
 
@@ -257,6 +271,37 @@ void run () {
       filter (input, output);
       break;
      }
+
+    // Zclean
+    case 5:
+    {
+      auto input = Image<float>::open (argument[0]);
+      Filter::ZClean filter (input);
+
+      auto opt = get_options ("maskin");
+      if (!opt.size())
+        throw Exception (std::string(argument[1]) + " filter requires initial mask");
+      Image<float> maskin = Image<float>::open (opt[0][0]);
+      check_dimensions (maskin, input, 0, 3);
+
+      filter.set_message (std::string("applying ") + std::string(argument[1]) + " filter to image " + std::string(argument[0]) + "...");
+      Stride::set_from_command_line (filter);
+
+      filter.set_voxels_to_bridge (get_option_value ("bridge", 4));
+      float zlower = get_option_value ("zlower", 2.5);
+      float zupper = get_option_value ("zupper", 2.5);
+      filter.set_zlim (zlower, zupper);
+
+      auto output = Image<float>::create (argument[2], filter);
+      filter (input, maskin, output);
+
+      opt = get_options ("maskout");
+      if (opt.size()) {
+        auto maskout = Image<bool>::create (opt[0][0], filter.mask);
+        threaded_copy (filter.mask, maskout);
+      }
+      break;
+    }
 
     default:
       assert (0);

cmd/mrmetric.cpp
@@ -334,14 +334,8 @@ void run ()
       using ImageTypeM = Header;
 
       n_voxels = 0;
-      vector<Header> headers;
       Registration::Transform::Rigid transform;
-      vector<Eigen::Transform<default_type, 3, Eigen::Projective>> init_transforms;
-      Eigen::Matrix<default_type, 4, 1> padding (0.0, 0.0, 0.0, 0.0);
-      headers.push_back (Header (input1));
-      headers.push_back (Header (input2));
-
-      Header midway_image_header = compute_minimum_average_header (headers, 1, padding, init_transforms);
+      Header midway_image_header = compute_minimum_average_header (input1, input2);
 
       using LinearInterpolatorType1 = Interp::LinearInterp<Image<value_type>, Interp::LinearInterpProcessingType::Value>;
       using LinearInterpolatorType2 = Interp::LinearInterp<Image<value_type>, Interp::LinearInterpProcessingType::Value>;

cmd/mrtransform.cpp
@@ -557,13 +557,11 @@ void run ()
 
     if (get_options ("midway_space").size()) {
       INFO("regridding to midway space");
-      vector<Header> headers;
-      headers.push_back(input_header);
-      headers.push_back(template_header);
-      vector<Eigen::Transform<default_type, 3, Eigen::Projective>> void_trafo;
-      auto padding = Eigen::Matrix<double, 4, 1>(1.0, 1.0, 1.0, 1.0);
-      int subsampling = 1;
-      auto midway_header = compute_minimum_average_header (headers, subsampling, padding, void_trafo);
+      if (!half)
+        WARN ("regridding to midway_space assumes the linear transformation to be a transformation from input to midway space. Use -half if the input transformation is a full transformation.");
+      transform_type linear_transform_inverse;
+      linear_transform_inverse.matrix() = linear_transform.inverse().matrix();
+      auto midway_header = compute_minimum_average_header (input_header, template_header, linear_transform_inverse, linear_transform);
       for (size_t i = 0; i < 3; ++i) {
         output_header.size(i) = midway_header.size(i);
         output_header.spacing(i) = midway_header.spacing(i);

core/app.cpp
@@ -172,13 +172,18 @@ namespace MR
       }
 
 
-      std::string underline (const std::string& text)
+      std::string underline (const std::string& text, bool ignore_whitespace = false)
       {
+        size_t ws (0), m (0);
+        if (ignore_whitespace) ws = std::count(text.begin(), text.end(), ' ');
         std::string retval (3*text.size(), '\0');
         for (size_t n = 0; n < text.size(); ++n) {
-          retval[3*n] = '_';
-          retval[3*n+1] = 0x08U;
-          retval[3*n+2] = text[n];
+          if (ignore_whitespace and text[n]==' ')
+            retval[m++] = ' ';
+          else
+            retval[m++] = '_';
+          retval[m++] = 0x08U;
+          retval[m++] = text[n];
         }
         return retval;
       }
@@ -296,12 +301,12 @@ namespace MR
         s = bold (s) + "\n\n     ";
       else
         s += ": ";
-      s += ( format ? underline (NAME) : NAME ) + " [ options ]";
+      s += ( format ? underline (NAME, true) : NAME ) + " [ options ]";
 
       for (size_t i = 0; i < ARGUMENTS.size(); ++i) {
 
         if (ARGUMENTS[i].flags & Optional)
-          s += "[";
+          s += " [";
         s += std::string(" ") + ARGUMENTS[i].id;
 
         if (ARGUMENTS[i].flags & AllowMultiple) {
@@ -374,7 +379,7 @@ namespace MR
 
     std::string Argument::syntax (int format) const
     {
-      std::string retval = paragraph (( format ? underline (id) : id ), desc, HELP_ARG_INDENT);
+      std::string retval = paragraph (( format ? underline (id, true) : id ), desc, HELP_ARG_INDENT);
       if (format)
         retval += "\n";
       return retval;

commit 8d0e18d4782ce67b152dd8fe977ede5c953228f1

average_space: set default trafo to identity trafo.
fixes mrmetric bug introduced in 6dcd6edb265

core/math/average_space.h
@@ -46,8 +46,8 @@ namespace MR
     Header compute_minimum_average_header (
         const ImageType1& im1,
         const ImageType2& im2,
-        Eigen::Transform<default_type, 3, Eigen::Projective> transform_1 = Eigen::Transform<default_type, 3, Eigen::Projective>(),
-        Eigen::Transform<default_type, 3, Eigen::Projective> transform_2 = Eigen::Transform<default_type, 3, Eigen::Projective>(),
+        Eigen::Transform<default_type, 3, Eigen::Projective> transform_1 = Eigen::Transform<default_type, 3, Eigen::Projective>::Identity(),
+        Eigen::Transform<default_type, 3, Eigen::Projective> transform_2 = Eigen::Transform<default_type, 3, Eigen::Projective>::Identity(),
         Eigen::Matrix<default_type, 4, 1> padding = Eigen::Matrix<default_type, 4, 1>(1.0, 1.0, 1.0, 1.0),
         int voxel_subsampling = 1) {
       vector<Eigen::Transform<default_type, 3, Eigen::Projective>> init_transforms {transform_1, transform_2};