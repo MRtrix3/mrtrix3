90fa50fcbece30fe82d874784b0101f562772059
Warda Syeda <wtsyeda@unimelb.edu.au>
2020-06-18 13:24:30 +1000

    added exvivo algorithm for brain masking

---
+++ lib/mrtrix3/dwi2mask/exvivo.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2008-2019 the MRtrix3 contributors.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Covered Software is provided under this License on an "as is"
+# basis, without warranty of any kind, either expressed, implied, or
+# statutory, including, without limitation, warranties that the
+# Covered Software is free of defects, merchantable, fit for a
+# particular purpose or non-infringing.
+# See the Mozilla Public License v. 2.0 for more details.
+#
+# For more details, see http://www.mrtrix.org/.
+
+#import os
+#from mrtrix3 import MRtrixError
+from mrtrix3 import app, image, path, run
+
+
+
+def usage(base_parser, subparsers): #pylint: disable=unused-variable
+  parser = subparsers.add_parser('exvivo', parents=[base_parser])
+  parser.set_author('Warda Syeda (wtsyeda@unimelb.edu.au) and Robert E. Smith (robert.smith@florey.edu.au)')
+  parser.set_synopsis('A method to generate a brain mask for exvivo data')
+  parser.add_argument('input',  help='The input DWI series')
+  parser.add_argument('output', help='The output mask image')
+  options = parser.add_argument_group('Options specific to the \'exvivo\' algorithm')
+  options.add_argument('-avg_all', action='store_true', help='Average all shells')
+  options.add_argument('-shells', help='Comma separated list of shells used for masking')
+
+
+def execute(): #pylint: disable=unused-variable
+
+  # Generating Mask
+
+  # Averaging shells
+  if app.ARGS.avg_all:
+    run.command('mrmath input.mif mean - -axis 3 | '
+                'mrthreshold - - | '
+                'mrconvert - mask.mif -strides +1,+2,+3')
+
+  else:
+    if app.ARGS.shells:
+      run.command('dwiextract input.mif input_shells.mif -shells ' + app.ARGS.shells)
+      run.command('dwishellmath input_shells.mif mean mean_shells.mif')
+    else:
+      run.command('dwishellmath input.mif mean mean_shells.mif')
+    run.command('mrconvert mean_shells.mif -coord 3 0 meanb0.mif')
+
+    # run per-shell histogram matching
+    files = []
+    for index in range(int((image.mrinfo('mean_shells.mif', 'size').split()).pop())):
+      filename = 'shell-{:02d}.mif'.format(index)
+      print(filename)
+      run.command('mrconvert mean_shells.mif -coord 3 ' + str(index) + ' - | '
+                  'mrhistmatch scale - meanb0.mif ' + filename)
+      files.append(filename)
+
+    # concatenate matched shells
+    run.command('mrcat -axis 3 ' + ' '.join(files) + ' cat.mif')
+    run.command('mrmath cat.mif mean - -axis 3 | mrthreshold - mask.mif')
+  run.command('mrconvert mask.mif ' + path.from_user(app.ARGS.output), mrconvert_keyval=path.from_user(app.ARGS.input, False), force=app.FORCE_OVERWRITE)
