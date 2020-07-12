252c2b55e18c57ddf6414f8397ecfb2a2433bf56
Thea Bautista <thea.bautista00@gmail.com>
2020-07-12 18:35:07 +0100

    1D shift: Unsure about code

--- cmd/deGibbs3D.cpp
+++ cmd/deGibbs3D.cpp
@@ -8,6 +8,7 @@
 #include "image.h"
 #include "header.h"
 
+
 using namespace std;
 using namespace MR;
 using namespace App;
@@ -31,7 +32,7 @@ void usage() {
 void run() {
 
 
-  // insert code here to use fft
+  // reading input and assigning output
   auto input = Image<cdouble>::open(argument[0]);
   Header header (input);
   header.datatype() = DataType::CFloat32;
@@ -43,6 +44,23 @@ void run() {
   Math::FFT (output, 1, FFTW_FORWARD);
   Math::FFT (output, 2, FFTW_FORWARD);
 
+  // creating temporary image to store intermediate image shifts
+  //auto tmp = Image<cdouble>::scratch(header)
+
+  // performing a shift in the image domain is equivalent to multiplying the image in the fourier domain with an exponential
+  double shift = 100.0;
+  const double pi = 3.14159265358979323846;
+  const complex<double> j(0.0,-1.0);
+
+  // performing shift in 1D
+  for (int x = 0; x < output.size(0); x++){
+  	double ind = output.get_index(0);
+  	cdouble val = output.get_value() * exp(j * 2.0 * ind * pi * shift);
+  	output.set_value(val);
+  	output.move_index(0, shift);
+  };
+
+
   // FFT back to check we get back the original image:
   Math::FFT (output, 0, FFTW_BACKWARD);
   Math::FFT (output, 1, FFTW_BACKWARD);
@@ -52,13 +70,12 @@ void run() {
   // This rescales the output because the FFT is unnormalised
   // http://www.fftw.org/fftw3_doc/The-1d-Discrete-Fourier-Transform-_0028DFT_0029.html#The-1d-Discrete-Fourier-Transform-_0028DFT_0029
 
-
   // This is done using multi-threading - it's about the simplest example of
   // multi-threading you'll come across...
 
   // This class will perform the per-voxel operation (divide by N):
   struct RescaleFn {
-    RescaleFn (double N) : N (N) { }
+    RescaleFn (double N) : N (N) { } // intialiser list 
     void operator() (decltype(output)& out) { out.value() /= N; }
     const double N;
   };
