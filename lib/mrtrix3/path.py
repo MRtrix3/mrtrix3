7fa7899c7582259a8ae5e6886922f8e86edf0de6
J-Donald Tournier <jdtournier@gmail.com>
2021-07-09 16:06:35 +0100

    changes to pass pylint tests

--- lib/mrtrix3/path.py
+++ lib/mrtrix3/path.py
@@ -89,7 +89,8 @@ def make_temporary(suffix): #pylint: disable=unused-variable
       if is_directory:
         os.makedirs(temp_path)
       else:
-        open(temp_path, 'a').close()
+        with open(temp_path, 'a'):
+          pass
       app.debug(temp_path)
       return temp_path
     except OSError as exception:
