2c1a0a21a6254fb46c6f08a33378c364be871e81
Ben Jeurissen <ben.jeurissen@gmail.com>
2021-09-20 10:43:37 +0200

    dwi2response: define a function in algorithms that specifies whether it supports a mask

--- lib/mrtrix3/dwi2response/fa.py
+++ lib/mrtrix3/dwi2response/fa.py
@@ -49,6 +49,11 @@ def needs_single_shell(): #pylint: disable=unused-variable
 
 
 
+def supports_mask(): #pylint: disable=unused-variable
+  return True 
+
+
+
 def execute(): #pylint: disable=unused-variable
   bvalues = [ int(round(float(x))) for x in image.mrinfo('dwi.mif', 'shell_bvalues').split() ]
   if len(bvalues) < 2:
