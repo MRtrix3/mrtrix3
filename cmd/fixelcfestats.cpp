MRtrix3/mrtrix3

commit 6ba0133dfbb8bd171b931ad403a67709d3a04242

Testing updates for modified F-test interface
Includes pulling updated binary test data for vectorstats.
Includes some minor fixes to said interface change.

cmd/fixelcfestats.cpp
@@ -271,6 +271,7 @@ void run()
   } else {
     posthoc = Image<bool>::scratch (fixel_mask_header, "scratch fixel post-hoc mask");
     copy (mask, posthoc);
+    mask.reset(); posthoc.reset();
     posthoc_fixels = num_fixels;
   }
 
@@ -478,7 +479,7 @@ void run()
   }
 
   // Perform permutation testing
-  if (get_options ("notest").size()) {
+  if (!get_options ("notest").size()) {
 
     if (get_options("posthoc").size()) {
       WARN ("-posthoc option has no effect if -notest is also specified");

cmd/mrclusterstats.cpp
@@ -254,7 +254,9 @@ void run() {
   } else {
     posthoc_mask = Stats::PermTest::mask_type::Ones (num_voxels);
     posthoc_voxels = num_voxels;
+    posthoc_image = Image<bool>::scratch (mask_header, "scratch posthoc mask image");
     copy (mask_image, posthoc_image);
+    mask_image.reset(); posthoc_image.reset();
   }
 
   // Read file names and check files exist

src/stats/permtest.cpp
@@ -274,7 +274,7 @@ namespace MR
                              matrix_type& uncorrected_pvalues)
       {
         assert (stats_calculator);
-        assert (stats_calculator->num_elements() == mask.size());
+        assert (stats_calculator->num_elements() == size_t(mask.size()));
         Math::Stats::Shuffler shuffler (stats_calculator->num_inputs(), false, "Running permutations");
         null_dist.resize (shuffler.size(), fwe_strong ? 1 : stats_calculator->num_hypotheses());
         null_dist_contributions = count_matrix_type::Zero (stats_calculator->num_elements(), stats_calculator->num_hypotheses());

testing/binaries/tests/fixelcfestats
@@ -1,4 +1,4 @@
-fixelcfestats fixelfilter/smooth/out/ fixelcfestats/subjects.txt fixelcfestats/design.txt fixelcfestats/contrast.txt SIFT_phantom/matrix/ tmp/ -cfe_legacy -force && testing_diff_image tmp/abs_effect.mif fixelcfestats/legacy/abs_effect.mif -frac 1e-5 && testing_diff_image tmp/beta0.mif fixelcfestats/legacy/beta0.mif -frac 1e-5 && testing_diff_image tmp/beta1.mif fixelcfestats/legacy/beta1.mif -frac 1e-5 && testing_diff_image tmp/std_dev.mif fixelcfestats/legacy/std_dev.mif -frac 1e-5 && testing_diff_image tmp/std_effect.mif fixelcfestats/legacy/std_effect.mif -frac 1e-5 && testing_diff_image tmp/tvalue.mif fixelcfestats/legacy/tvalue.mif -frac 1e-5 && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/fixels/upper.mif
-fixelcfestats fixelfilter/smooth/out/ fixelcfestats/subjects.txt fixelcfestats/design.txt fixelcfestats/contrast.txt SIFT_phantom/matrix/ tmp/ -force && testing_diff_image tmp/abs_effect.mif fixelcfestats/default/abs_effect.mif && testing_diff_image tmp/beta0.mif fixelcfestats/default/beta0.mif && testing_diff_image tmp/beta1.mif fixelcfestats/default/beta1.mif && testing_diff_image tmp/cfe.mif fixelcfestats/default/cfe.mif && testing_diff_image tmp/std_dev.mif fixelcfestats/default/std_dev.mif && testing_diff_image tmp/std_effect.mif fixelcfestats/default/std_effect.mif && testing_diff_image tmp/tvalue.mif fixelcfestats/default/tvalue.mif && testing_diff_image tmp/Zstat.mif fixelcfestats/default/Zstat.mif && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/fixels/upper.mif
-fixelcfestats fixelfilter/smooth/out/ fixelcfestats/subjects.txt fixelcfestats/design.txt fixelcfestats/contrast.txt SIFT_phantom/matrix/ tmp/ -mask SIFT_phantom/fixels/upper.mif -force && testing_diff_image tmp/abs_effect.mif fixelcfestats/masked/abs_effect.mif && testing_diff_image tmp/beta0.mif fixelcfestats/masked/beta0.mif && testing_diff_image tmp/beta1.mif fixelcfestats/masked/beta1.mif && testing_diff_image tmp/cfe.mif fixelcfestats/masked/cfe.mif && testing_diff_image tmp/std_dev.mif fixelcfestats/masked/std_dev.mif && testing_diff_image tmp/std_effect.mif fixelcfestats/masked/std_effect.mif && testing_diff_image tmp/tvalue.mif fixelcfestats/masked/tvalue.mif && testing_diff_image tmp/Zstat.mif fixelcfestats/masked/Zstat.mif && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/fixels/upper.mif
+fixelcfestats fixelfilter/smooth/out/ fixelcfestats/subjects.txt fixelcfestats/design.txt SIFT_phantom/matrix/ tmp/ -ttests fixelcfestats/contrast.txt -cfe_legacy -force && testing_diff_image tmp/abs_effect.mif fixelcfestats/legacy/abs_effect.mif -frac 1e-5 && testing_diff_image tmp/beta0.mif fixelcfestats/legacy/beta0.mif -frac 1e-5 && testing_diff_image tmp/beta1.mif fixelcfestats/legacy/beta1.mif -frac 1e-5 && testing_diff_image tmp/std_dev.mif fixelcfestats/legacy/std_dev.mif -frac 1e-5 && testing_diff_image tmp/std_effect.mif fixelcfestats/legacy/std_effect.mif -frac 1e-5 && testing_diff_image tmp/tvalue.mif fixelcfestats/legacy/tvalue.mif -frac 1e-5 && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/fixels/upper.mif
+fixelcfestats fixelfilter/smooth/out/ fixelcfestats/subjects.txt fixelcfestats/design.txt SIFT_phantom/matrix/ tmp/ -ttests fixelcfestats/contrast.txt -force && testing_diff_image tmp/abs_effect.mif fixelcfestats/default/abs_effect.mif && testing_diff_image tmp/beta0.mif fixelcfestats/default/beta0.mif && testing_diff_image tmp/beta1.mif fixelcfestats/default/beta1.mif && testing_diff_image tmp/cfe.mif fixelcfestats/default/cfe.mif && testing_diff_image tmp/std_dev.mif fixelcfestats/default/std_dev.mif && testing_diff_image tmp/std_effect.mif fixelcfestats/default/std_effect.mif && testing_diff_image tmp/tvalue.mif fixelcfestats/default/tvalue.mif && testing_diff_image tmp/Zstat.mif fixelcfestats/default/Zstat.mif && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/fixels/upper.mif
+fixelcfestats fixelfilter/smooth/out/ fixelcfestats/subjects.txt fixelcfestats/design.txt SIFT_phantom/matrix/ tmp/ -ttests fixelcfestats/contrast.txt -mask SIFT_phantom/fixels/upper.mif -force && testing_diff_image tmp/abs_effect.mif fixelcfestats/masked/abs_effect.mif && testing_diff_image tmp/beta0.mif fixelcfestats/masked/beta0.mif && testing_diff_image tmp/beta1.mif fixelcfestats/masked/beta1.mif && testing_diff_image tmp/cfe.mif fixelcfestats/masked/cfe.mif && testing_diff_image tmp/std_dev.mif fixelcfestats/masked/std_dev.mif && testing_diff_image tmp/std_effect.mif fixelcfestats/masked/std_effect.mif && testing_diff_image tmp/tvalue.mif fixelcfestats/masked/tvalue.mif && testing_diff_image tmp/Zstat.mif fixelcfestats/masked/Zstat.mif && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/fixels/upper.mif
 

testing/binaries/tests/mrclusterstats
@@ -1,5 +1,4 @@
-rm -rf tmp/ && mkdir tmp/ && mrclusterstats mrclusterstats/subjects.txt mrclusterstats/design.txt mrclusterstats/contrast.txt SIFT_phantom/mask.mif tmp/ && testing_diff_image tmp/abs_effect.mif mrclusterstats/legacy/abs_effect.mif && testing_diff_image tmp/beta0.mif mrclusterstats/legacy/beta0.mif && testing_diff_image tmp/beta1.mif mrclusterstats/legacy/beta1.mif && testing_diff_image tmp/std_dev.mif mrclusterstats/legacy/std_dev.mif && testing_diff_image tmp/std_effect.mif mrclusterstats/legacy/std_effect.mif && testing_diff_image tmp/tvalue.mif mrclusterstats/legacy/tvalue.mif && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/upper.mif
-rm -rf tmp/ && mkdir tmp/ && mrclusterstats mrclusterstats/subjects.txt mrclusterstats/design.txt mrclusterstats/contrast.txt SIFT_phantom/mask.mif tmp/ && testing_diff_image tmp/abs_effect.mif mrclusterstats/default/abs_effect.mif && testing_diff_image tmp/beta0.mif mrclusterstats/default/beta0.mif && testing_diff_image tmp/beta1.mif mrclusterstats/default/beta1.mif && testing_diff_image tmp/std_dev.mif mrclusterstats/default/std_dev.mif && testing_diff_image tmp/std_effect.mif mrclusterstats/default/std_effect.mif && testing_diff_image tmp/tfce.mif mrclusterstats/default/tfce.mif && testing_diff_image tmp/tvalue.mif mrclusterstats/default/tvalue.mif && testing_diff_image tmp/Zstat.mif mrclusterstats/default/Zstat.mif && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/upper.mif
-rm -rf tmp/ && mkdir tmp/ && mrclusterstats mrclusterstats/subjects.txt mrclusterstats/design.txt mrclusterstats/contrast.txt SIFT_phantom/upper.mif tmp/ && testing_diff_image tmp/abs_effect.mif mrclusterstats/masked/abs_effect.mif && testing_diff_image tmp/beta0.mif mrclusterstats/masked/beta0.mif && testing_diff_image tmp/beta1.mif mrclusterstats/masked/beta1.mif && testing_diff_image tmp/std_dev.mif mrclusterstats/masked/std_dev.mif && testing_diff_image tmp/std_effect.mif mrclusterstats/masked/std_effect.mif && testing_diff_image tmp/tvalue.mif mrclusterstats/masked/tvalue.mif && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/upper.mif
-rm -rf tmp/ && mkdir tmp/ && mrclusterstats mrclusterstats/subjects.txt mrclusterstats/design.txt mrclusterstats/contrast.txt SIFT_phantom/mask.mif tmp/ -threshold 3.5 && testing_diff_image tmp/clustersize.mif mrclusterstats/threshold/cluster_sizes.mif && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/upper.mif
-
+rm -rf tmp/ && mkdir tmp/ && mrclusterstats mrclusterstats/subjects.txt mrclusterstats/design.txt SIFT_phantom/mask.mif tmp/ -ttests mrclusterstats/contrast.txt && testing_diff_image tmp/abs_effect.mif mrclusterstats/legacy/abs_effect.mif && testing_diff_image tmp/beta0.mif mrclusterstats/legacy/beta0.mif && testing_diff_image tmp/beta1.mif mrclusterstats/legacy/beta1.mif && testing_diff_image tmp/std_dev.mif mrclusterstats/legacy/std_dev.mif && testing_diff_image tmp/std_effect.mif mrclusterstats/legacy/std_effect.mif && testing_diff_image tmp/tvalue.mif mrclusterstats/legacy/tvalue.mif && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/upper.mif
+rm -rf tmp/ && mkdir tmp/ && mrclusterstats mrclusterstats/subjects.txt mrclusterstats/design.txt SIFT_phantom/mask.mif tmp/ -ttests mrclusterstats/contrast.txt && testing_diff_image tmp/abs_effect.mif mrclusterstats/default/abs_effect.mif && testing_diff_image tmp/beta0.mif mrclusterstats/default/beta0.mif && testing_diff_image tmp/beta1.mif mrclusterstats/default/beta1.mif && testing_diff_image tmp/std_dev.mif mrclusterstats/default/std_dev.mif && testing_diff_image tmp/std_effect.mif mrclusterstats/default/std_effect.mif && testing_diff_image tmp/tfce.mif mrclusterstats/default/tfce.mif && testing_diff_image tmp/tvalue.mif mrclusterstats/default/tvalue.mif && testing_diff_image tmp/Zstat.mif mrclusterstats/default/Zstat.mif && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/upper.mif
+rm -rf tmp/ && mkdir tmp/ && mrclusterstats mrclusterstats/subjects.txt mrclusterstats/design.txt SIFT_phantom/upper.mif tmp/ -ttests mrclusterstats/contrast.txt && testing_diff_image tmp/abs_effect.mif mrclusterstats/masked/abs_effect.mif && testing_diff_image tmp/beta0.mif mrclusterstats/masked/beta0.mif && testing_diff_image tmp/beta1.mif mrclusterstats/masked/beta1.mif && testing_diff_image tmp/std_dev.mif mrclusterstats/masked/std_dev.mif && testing_diff_image tmp/std_effect.mif mrclusterstats/masked/std_effect.mif && testing_diff_image tmp/tvalue.mif mrclusterstats/masked/tvalue.mif && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/upper.mif
+rm -rf tmp/ && mkdir tmp/ && mrclusterstats mrclusterstats/subjects.txt mrclusterstats/design.txt SIFT_phantom/mask.mif tmp/ -ttests mrclusterstats/contrast.txt -threshold 3.5 && testing_diff_image tmp/clustersize.mif mrclusterstats/threshold/cluster_sizes.mif && mrcalc tmp/fwe_1mpvalue.mif 0.95 -gt - | testing_diff_image - SIFT_phantom/upper.mif

testing/binaries/tests/vectorstats
@@ -1,10 +1,10 @@
-#N=16 SNR=5 vectorstats/gen0.py && vectorstats tmpsubjects.txt tmpdesign.csv tmpcontrast.csv tmpout -ftests tmpftests.csv -force && vectorstats/test0.py
-vectorstats vectorstats/0/subjects.txt vectorstats/0/design.csv vectorstats/0/contrast.csv tmpout -ftests vectorstats/0/ftests.csv -force && testing_diff_matrix tmpoutFvalue_F1.csv vectorstats/0/outFvalue_F1.csv -frac 1e-6 && testing_diff_matrix tmpoutFvalue_F2.csv vectorstats/0/outFvalue_F2.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_F1.csv vectorstats/0/outZstat_F1.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_F2.csv vectorstats/0/outZstat_F2.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t1.csv vectorstats/0/outZstat_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t2.csv vectorstats/0/outZstat_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t3.csv vectorstats/0/outZstat_t3.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t1.csv vectorstats/0/outabs_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t2.csv vectorstats/0/outabs_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t3.csv vectorstats/0/outabs_effect_t3.csv -frac 1e-6 && testing_diff_matrix tmpoutbetas.csv vectorstats/0/outbetas.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_dev.csv vectorstats/0/outstd_dev.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t1.csv vectorstats/0/outstd_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t2.csv vectorstats/0/outstd_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t3.csv vectorstats/0/outstd_effect_t3.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t1.csv vectorstats/0/outtvalue_t1.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t2.csv vectorstats/0/outtvalue_t2.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t3.csv vectorstats/0/outtvalue_t3.csv -frac 1e-6 && vectorstats/test0.py
-#N=16 SNR=5 vectorstats/gen1.py && vectorstats tmpsubjects.txt tmpdesign.csv tmpcontrast.csv tmpout -errors ise -force && && vectorstats/test1.py
-vectorstats vectorstats/1/subjects.txt vectorstats/1/design.csv vectorstats/1/contrast.csv tmpout -errors ise -force && testing_diff_matrix tmpoutZstat_t1.csv vectorstats/1/outZstat_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t2.csv vectorstats/1/outZstat_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t1.csv vectorstats/1/outabs_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t2.csv vectorstats/1/outabs_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutbetas.csv vectorstats/1/outbetas.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_dev.csv vectorstats/1/outstd_dev.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t1.csv vectorstats/1/outstd_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t2.csv vectorstats/1/outstd_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t1.csv vectorstats/1/outtvalue_t1.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t2.csv vectorstats/1/outtvalue_t2.csv && vectorstats/test1.py
-#N=16 SNR=5 vectorstats/gen2.py && vectorstats tmpsubjects.txt tmpdesign.csv tmpcontrast.csv tmpout -column tmpcolumn.txt -ftests tmpftests.csv -force && vectorstats/test2.py
-vectorstats vectorstats/2/subjects.txt vectorstats/2/design.csv vectorstats/2/contrast.csv tmpout -column vectorstats/2/column.txt -ftests vectorstats/2/ftests.csv -force && testing_diff_matrix tmpoutFvalue_F1.csv vectorstats/2/outFvalue_F1.csv -frac 1e-6 && testing_diff_matrix tmpoutFvalue_F2.csv vectorstats/2/outFvalue_F2.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_F1.csv vectorstats/2/outZstat_F1.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_F2.csv vectorstats/2/outZstat_F2.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t1.csv vectorstats/2/outZstat_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t2.csv vectorstats/2/outZstat_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t3.csv vectorstats/2/outZstat_t3.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t4.csv vectorstats/2/outZstat_t4.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t1.csv vectorstats/2/outabs_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t2.csv vectorstats/2/outabs_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t3.csv vectorstats/2/outabs_effect_t3.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t4.csv vectorstats/2/outabs_effect_t4.csv -frac 1e-6 && testing_diff_matrix tmpoutbetas.csv vectorstats/2/outbetas.csv -frac 1e-6 && testing_diff_matrix tmpoutcond.csv vectorstats/2/outcond.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_dev.csv vectorstats/2/outstd_dev.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t1.csv vectorstats/2/outstd_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t2.csv vectorstats/2/outstd_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t3.csv vectorstats/2/outstd_effect_t3.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t4.csv vectorstats/2/outstd_effect_t4.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t1.csv vectorstats/2/outtvalue_t1.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t2.csv vectorstats/2/outtvalue_t2.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t3.csv vectorstats/2/outtvalue_t3.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t4.csv vectorstats/2/outtvalue_t4.csv -frac 1e-6 && vectorstats/test2.py
-#N=16 SNR=5 vectorstats/gen3.py && vectorstats tmpsubjects.txt tmpdesign.csv tmpcontrast.csv tmpout -errors ise -force && vectorstats/test3.py
-vectorstats vectorstats/3/subjects.txt vectorstats/3/design.csv vectorstats/3/contrast.csv tmpout -errors ise -force && testing_diff_matrix tmpoutZstat_t1.csv vectorstats/3/outZstat_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t2.csv vectorstats/3/outZstat_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t1.csv vectorstats/3/outabs_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t2.csv vectorstats/3/outabs_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutbetas.csv vectorstats/3/outbetas.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_dev.csv vectorstats/3/outstd_dev.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t1.csv vectorstats/3/outstd_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t2.csv vectorstats/3/outstd_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t1.csv vectorstats/3/outtvalue_t1.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t2.csv vectorstats/3/outtvalue_t2.csv -frac 1e-6 && vectorstats/test3.py
-#N=16 SNR=5 vectorstats/gen4.py && vectorstats tmpsubjects.txt tmpdesign.csv tmpcontrast.csv tmpout -errors ise -force && vectorstats/test4.py
-vectorstats vectorstats/4/subjects.txt vectorstats/4/design.csv vectorstats/4/contrast.csv tmpout -errors ise -force && testing_diff_matrix tmpoutZstat.csv vectorstats/4/outZstat.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect.csv vectorstats/4/outabs_effect.csv -frac 1e-6 && testing_diff_matrix tmpoutbetas.csv vectorstats/4/outbetas.csv -frac 1e-6 && testing_diff_matrix tmpoutcond.csv vectorstats/4/outcond.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_dev.csv vectorstats/4/outstd_dev.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect.csv vectorstats/4/outstd_effect.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue.csv vectorstats/4/outtvalue.csv -frac 1e-6 && vectorstats/test4.py
+#N=16 SNR=5 vectorstats/gen0.py && vectorstats tmpsubjects.txt tmpdesign.csv tmpout -ttests tmpcontrast.csv -ftest tmpftest0.csv -ftest tmpftest1.csv -force && vectorstats/test0.py
+vectorstats vectorstats/0/subjects.txt vectorstats/0/design.csv tmpout -ttests vectorstats/0/contrast.csv -ftest vectorstats/0/ftest0.csv -ftest vectorstats/0/ftest1.csv -force && testing_diff_matrix tmpoutFvalue_F1.csv vectorstats/0/outFvalue_F1.csv -frac 1e-6 && testing_diff_matrix tmpoutFvalue_F2.csv vectorstats/0/outFvalue_F2.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_F1.csv vectorstats/0/outZstat_F1.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_F2.csv vectorstats/0/outZstat_F2.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t1.csv vectorstats/0/outZstat_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t2.csv vectorstats/0/outZstat_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t3.csv vectorstats/0/outZstat_t3.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t1.csv vectorstats/0/outabs_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t2.csv vectorstats/0/outabs_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t3.csv vectorstats/0/outabs_effect_t3.csv -frac 1e-6 && testing_diff_matrix tmpoutbetas.csv vectorstats/0/outbetas.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_dev.csv vectorstats/0/outstd_dev.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t1.csv vectorstats/0/outstd_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t2.csv vectorstats/0/outstd_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t3.csv vectorstats/0/outstd_effect_t3.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t1.csv vectorstats/0/outtvalue_t1.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t2.csv vectorstats/0/outtvalue_t2.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t3.csv vectorstats/0/outtvalue_t3.csv -frac 1e-6 && vectorstats/test0.py
+#N=16 SNR=5 vectorstats/gen1.py && vectorstats tmpsubjects.txt tmpdesign.csv tmpout -ttests tmpcontrast.csv -errors ise -force && && vectorstats/test1.py
+vectorstats vectorstats/1/subjects.txt vectorstats/1/design.csv tmpout -ttests vectorstats/1/contrast.csv -errors ise -force && testing_diff_matrix tmpoutZstat_t1.csv vectorstats/1/outZstat_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t2.csv vectorstats/1/outZstat_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t1.csv vectorstats/1/outabs_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t2.csv vectorstats/1/outabs_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutbetas.csv vectorstats/1/outbetas.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_dev.csv vectorstats/1/outstd_dev.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t1.csv vectorstats/1/outstd_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t2.csv vectorstats/1/outstd_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t1.csv vectorstats/1/outtvalue_t1.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t2.csv vectorstats/1/outtvalue_t2.csv && vectorstats/test1.py
+#N=16 SNR=5 vectorstats/gen2.py && vectorstats tmpsubjects.txt tmpdesign.csv tmpout -ttests tmpcontrast.csv -column tmpcolumn.txt -ftests tmpftests.csv -force && vectorstats/test2.py
+vectorstats vectorstats/2/subjects.txt vectorstats/2/design.csv tmpout -ttests vectorstats/2/contrast.csv -column vectorstats/2/column.txt -ftest vectorstats/2/ftest0.csv -ftest vectorstats/2/ftest1.csv -force && testing_diff_matrix tmpoutFvalue_F1.csv vectorstats/2/outFvalue_F1.csv -frac 1e-6 && testing_diff_matrix tmpoutFvalue_F2.csv vectorstats/2/outFvalue_F2.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_F1.csv vectorstats/2/outZstat_F1.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_F2.csv vectorstats/2/outZstat_F2.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t1.csv vectorstats/2/outZstat_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t2.csv vectorstats/2/outZstat_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t3.csv vectorstats/2/outZstat_t3.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t4.csv vectorstats/2/outZstat_t4.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t1.csv vectorstats/2/outabs_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t2.csv vectorstats/2/outabs_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t3.csv vectorstats/2/outabs_effect_t3.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t4.csv vectorstats/2/outabs_effect_t4.csv -frac 1e-6 && testing_diff_matrix tmpoutbetas.csv vectorstats/2/outbetas.csv -frac 1e-6 && testing_diff_matrix tmpoutcond.csv vectorstats/2/outcond.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_dev.csv vectorstats/2/outstd_dev.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t1.csv vectorstats/2/outstd_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t2.csv vectorstats/2/outstd_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t3.csv vectorstats/2/outstd_effect_t3.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t4.csv vectorstats/2/outstd_effect_t4.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t1.csv vectorstats/2/outtvalue_t1.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t2.csv vectorstats/2/outtvalue_t2.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t3.csv vectorstats/2/outtvalue_t3.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t4.csv vectorstats/2/outtvalue_t4.csv -frac 1e-6 && vectorstats/test2.py
+#N=16 SNR=5 vectorstats/gen3.py && vectorstats tmpsubjects.txt tmpdesign.csv tmpout -ttests tmpcontrast.csv -errors ise -force && vectorstats/test3.py
+vectorstats vectorstats/3/subjects.txt vectorstats/3/design.csv tmpout -ttests vectorstats/3/contrast.csv -errors ise -force && testing_diff_matrix tmpoutZstat_t1.csv vectorstats/3/outZstat_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutZstat_t2.csv vectorstats/3/outZstat_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t1.csv vectorstats/3/outabs_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect_t2.csv vectorstats/3/outabs_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpoutbetas.csv vectorstats/3/outbetas.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_dev.csv vectorstats/3/outstd_dev.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t1.csv vectorstats/3/outstd_effect_t1.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect_t2.csv vectorstats/3/outstd_effect_t2.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t1.csv vectorstats/3/outtvalue_t1.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue_t2.csv vectorstats/3/outtvalue_t2.csv -frac 1e-6 && vectorstats/test3.py
+#N=16 SNR=5 vectorstats/gen4.py && vectorstats tmpsubjects.txt tmpdesign.csv tmpout -ttests tmpcontrast.csv -errors ise -force && vectorstats/test4.py
+vectorstats vectorstats/4/subjects.txt vectorstats/4/design.csv tmpout -ttests vectorstats/4/contrast.csv -errors ise -force && testing_diff_matrix tmpoutZstat.csv vectorstats/4/outZstat.csv -frac 1e-6 && testing_diff_matrix tmpoutabs_effect.csv vectorstats/4/outabs_effect.csv -frac 1e-6 && testing_diff_matrix tmpoutbetas.csv vectorstats/4/outbetas.csv -frac 1e-6 && testing_diff_matrix tmpoutcond.csv vectorstats/4/outcond.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_dev.csv vectorstats/4/outstd_dev.csv -frac 1e-6 && testing_diff_matrix tmpoutstd_effect.csv vectorstats/4/outstd_effect.csv -frac 1e-6 && testing_diff_matrix tmpouttvalue.csv vectorstats/4/outtvalue.csv -frac 1e-6 && vectorstats/test4.py

commit 4a4aac34db78e7777bcade599f616ddf0b38b65a

fixelcfestats: Fix -notest option
Error introduced in 6ba0133.

cmd/fixelcfestats.cpp
@@ -479,7 +479,7 @@ void run()
   }
 
   // Perform permutation testing
-  if (!get_options ("notest").size()) {
+  if (get_options ("notest").size()) {
 
     if (get_options("posthoc").size()) {
       WARN ("-posthoc option has no effect if -notest is also specified");