0f4214c247dce1e00f8ba338ee01938e1dc4eba5
J-Donald Tournier <jdtournier@gmail.com>
2021-11-09 16:00:55 +0000

    mrfilter: update to use FFTW classes

--- core/math/fft.h
+++ core/math/fft.h
@@ -47,47 +47,16 @@ namespace MR {
 
     namespace {
       inline std::string direction_str (int direction) { return ( direction == FFTW_FORWARD ? "forward" : "backward" ); }
-    }
 
-
-
-
-    //! perform FFT in-place along specified axis
-     /* The \a direction parameter should be either FFTW_FORWARD or
-      * FFTW_BACKWARD */
-    template <class ImageType>
-      inline void FFT (ImageType& image, size_t axis, int direction)
-      {
-        class FFTFunctor { MEMALIGN (FFTFunctor)
-          public:
-            FFTFunctor (const ImageType& image, size_t axis, int direction) :
-              image (image),
-              fft (image.size(axis), direction),
-              axis (axis) { }
-
-            void operator() (const Iterator& pos)
-            {
-              assign_pos_of (pos).to (image);
-              for (auto l = Loop (axis, axis+1) (image); l; ++l)
-                fft[image.index(axis)] = cdouble (image.value());
-              fft.run();
-              for (auto l = Loop (axis, axis+1) (image); l; ++l)
-                image.value() = fft[image.index(axis)];
-            }
-
-          protected:
-            ImageType image;
-            FFT1D fft;
-            size_t axis;
-        };
-
-        auto outer_axes = Stride::order (image);
-        outer_axes.erase (std::find (outer_axes.begin(), outer_axes.end(), axis));
-        outer_axes.insert (outer_axes.begin(), axis);
-
-        ThreadedLoop ("performing " + direction_str (direction) + " FFT along slice " + str(axis), image, outer_axes, 1)
-          .run_outer (FFTFunctor (image, axis, direction));
+      inline size_t shift (size_t pos, const size_t size, const bool centre_FFT, const bool inverse) {
+        if (centre_FFT) {
+          pos += (inverse ? size+1 : size) /2;
+          if (pos >= size)
+            pos -= size;
+        }
+        return pos;
       }
+    }
 
 
 
@@ -96,26 +65,31 @@ namespace MR {
 
     //! perform FFT of input image \a in along specified axis, writing results into output image \a out
      /* The \a direction parameter should be either FFTW_FORWARD or
-      * FFTW_BACKWARD */
+      * FFTW_BACKWARD.
+      *
+      * The output image \a out can be the same as the input \a in, in which
+      * case the FFT will be performed in-place.  */
     template <class ImageTypeIn, class ImageTypeOut>
-      inline void FFT (ImageTypeIn& in, ImageTypeOut& out, size_t axis, int direction)
+      inline void FFT (ImageTypeIn& in, ImageTypeOut& out, size_t axis, int direction, bool centre_FFT = false)
       {
         class FFTFunctor { MEMALIGN (FFTFunctor)
           public:
-            FFTFunctor (const ImageTypeIn& in, const ImageTypeOut& out, size_t axis, int direction) :
+            FFTFunctor (const ImageTypeIn& in, const ImageTypeOut& out, size_t axis, int direction, bool centre_FFT) :
               in (in),
               out (out),
               fft (in.size(axis), direction),
-              axis (axis) { }
+              axis (axis),
+              direction (direction),
+              centre_FFT (centre_FFT) { }
 
             void operator() (const Iterator& pos)
             {
               assign_pos_of (pos).to (in, out);
               for (auto l = Loop (axis, axis+1) (in); l; ++l)
-                fft[in.index(axis)] = cdouble (in.value());
+                fft[shift (in.index(axis),in.size(axis),centre_FFT,direction==FFTW_FORWARD)] = cdouble (in.value());
               fft.run();
               for (auto l = Loop (axis, axis+1) (out); l; ++l)
-                out.value() = fft[out.index(axis)];
+                out.value() = fft[shift (out.index(axis),out.size(axis),centre_FFT,direction==FFTW_BACKWARD)];
             }
 
           protected:
@@ -123,14 +97,16 @@ namespace MR {
             ImageTypeOut out;
             FFT1D fft;
             size_t axis;
+            const int direction;
+            const bool centre_FFT;
         };
 
         auto outer_axes = Stride::order (in);
         outer_axes.erase (std::find (outer_axes.begin(), outer_axes.end(), axis));
         outer_axes.insert (outer_axes.begin(), axis);
 
-        ThreadedLoop ("performing " + direction_str (direction) + " FFT along slice " + str(axis), in, outer_axes, 1)
-          .run_outer (FFTFunctor (in, out, axis, direction));
+        ThreadedLoop ("performing " + direction_str (direction) + " FFT along axis " + str(axis), in, outer_axes, 1)
+          .run_outer (FFTFunctor (in, out, axis, direction, centre_FFT));
       }
 
 
