54e5ba6c8d727bf376fef068d62515a0e7eea688
Robert Smith <robert.smith@florey.edu.au>
2021-11-22 12:58:55 +1100

    5ttgen freesurfer: Use output of ScLimbic module

--- lib/mrtrix3/_5ttgen/freesurfer.py
+++ lib/mrtrix3/_5ttgen/freesurfer.py
@@ -26,7 +26,7 @@ def usage(base_parser, subparsers): #pylint: disable=unused-variable
   parser.add_argument('input',  help='The input FreeSurfer parcellation image (any image containing \'aseg\' in its name)')
   parser.add_argument('output', help='The output 5TT image')
   options = parser.add_argument_group('Options specific to the \'freesurfer\' algorithm')
-  options.add_argument('-lut', help='Manually provide path to the lookup table on which the input parcellation image is based (e.g. FreeSurferColorLUT.txt)')
+  options.add_argument('-sclimbic', metavar=('image'), help='Incorporate results of subcortical limbic segmentation')
 
 
 
@@ -36,21 +36,23 @@ def check_output_paths(): #pylint: disable=unused-variable
 
 
 def get_inputs(): #pylint: disable=unused-variable
-  run.command('mrconvert ' + path.from_user(app.ARGS.input) + ' ' + path.to_scratch('input.mif'))
-  if app.ARGS.lut:
-    run.function(shutil.copyfile, path.from_user(app.ARGS.lut, False), path.to_scratch('LUT.txt', False))
+  # Don't bother converting into scratch directory, as it's only used once
+  #run.command('mrconvert ' + path.from_user(app.ARGS.input) + ' ' + path.to_scratch('input.mif'))
+  pass
 
 
 
 def execute(): #pylint: disable=unused-variable
-  lut_input_path = 'LUT.txt'
-  if not os.path.exists('LUT.txt'):
-    freesurfer_home = os.environ.get('FREESURFER_HOME', '')
-    if not freesurfer_home:
-      raise MRtrixError('Environment variable FREESURFER_HOME is not set; please run appropriate FreeSurfer configuration script, set this variable manually, or provide script with path to file FreeSurferColorLUT.txt using -lut option')
-    lut_input_path = os.path.join(freesurfer_home, 'FreeSurferColorLUT.txt')
-    if not os.path.isfile(lut_input_path):
-      raise MRtrixError('Could not find FreeSurfer lookup table file (expected location: ' + lut_input_path + '), and none provided using -lut')
+  freesurfer_home = os.environ.get('FREESURFER_HOME', '')
+  if not freesurfer_home:
+    raise MRtrixError('Environment variable FREESURFER_HOME is not set; please run appropriate FreeSurfer configuration script, set this variable manually, or provide script with path to file FreeSurferColorLUT.txt using -lut option')
+  lut_input_path = os.path.join(freesurfer_home, 'FreeSurferColorLUT.txt')
+  if not os.path.isfile(lut_input_path):
+    raise MRtrixError('Could not find FreeSurfer lookup table file (expected location: ' + lut_input_path + ')')
+  if app.ARGS.sclimbic:
+    sclimbic_lut_input_path = os.path.join(freesurfer_home, 'models', 'sclimbic.ctab')
+    if not os.path.isfile(sclimbic_lut_input_path):
+      raise MRtrixError('Could not find FreeSurfer ScLimbic module lookup table file (expected location: ' + sclimbic_lut_input_path + ')')
 
   if app.ARGS.sgm_amyg_hipp:
     lut_output_file_name = 'FreeSurfer2ACT_sgm_amyg_hipp.txt'
@@ -59,24 +61,43 @@ def execute(): #pylint: disable=unused-variable
   lut_output_path = os.path.join(path.shared_data_path(), path.script_subdir_name(), lut_output_file_name)
   if not os.path.isfile(lut_output_path):
     raise MRtrixError('Could not find lookup table file for converting FreeSurfer parcellation output to tissues (expected location: ' + lut_output_path + ')')
+  if app.ARGS.sclimbic:
+    sclimbic_lut_output_path = os.path.join(path.shared_data_path(), path.script_subdir_name(), 'ScLimbic2ACT.txt')
+    if not os.path.isfile(sclimbic_lut_output_path):
+      raise MRtrixError('Could not find lookup table file for converting ScLimbic module output to tissues (expected location: ' + sclimbic_lut_output_path + ')')
 
   # Initial conversion from FreeSurfer parcellation to five principal tissue types
-  run.command('labelconvert input.mif ' + lut_input_path + ' ' + lut_output_path + ' indices.mif')
+  index_image = 'indices.mif'
+  run.command(['labelconvert', path.from_user(app.ARGS.input, False), lut_input_path, lut_output_path, index_image])
+  if app.ARGS.sclimbic:
+    sclimbic_image = 'sclimbic.mif'
+    run.command(['labelconvert', path.from_user(app.ARGS.sclimbic, False), sclimbic_lut_input_path, sclimbic_lut_output_path, sclimbic_image])
 
   # Crop to reduce file size
-  if app.ARGS.nocrop:
-    image = 'indices.mif'
-  else:
-    image = 'indices_cropped.mif'
-    run.command('mrthreshold indices.mif - -abs 0.5 | mrgrid indices.mif crop ' + image + ' -mask -')
+  if not app.ARGS.nocrop:
+    cropped_index_image = 'indices_cropped.mif'
+    if app.ARGS.sclimbic:
+      crop_mask = 'crop_mask.mif'
+      run.command('mrthreshold ' + index_image + ' -abs 0.5 ' + crop_mask)
+      run.command('mrgrid ' + index_image + ' crop ' + cropped_index_image + ' -mask ' + crop_mask)
+      cropped_sclimbic_image = 'sclimbic_cropped.mif'
+      run.command('mrgrid ' + sclimbic_image + ' crop ' + cropped_sclimbic_image + ' -mask ' + crop_mask)
+      sclimbic_image = cropped_sclimbic_image
+    else:
+      run.command('mrthreshold ' + index_image + ' - -abs 0.5 | mrgrid indices.mif crop ' + cropped_index_image + ' -mask -')
+    index_image = cropped_index_image
 
   # Convert into the 5TT format for ACT
-  run.command('mrcalc ' + image + ' 1 -eq cgm.mif')
-  run.command('mrcalc ' + image + ' 2 -eq sgm.mif')
-  run.command('mrcalc ' + image + ' 3 -eq  wm.mif')
-  run.command('mrcalc ' + image + ' 4 -eq csf.mif')
-  run.command('mrcalc ' + image + ' 5 -eq path.mif')
+  if app.ARGS.sclimbic:
+    for index in range(1, 6):
+      run.command('mrcalc ' + sclimbic_image + ' 0 ' + index_image + ' -if ' + str(index) + ' -eq ' + sclimbic_image + ' ' + str(index) + ' -eq -add tissue' + str(index) + '.mif')
+  else:
+    for index in range(1, 6):
+      run.command('mrcalc ' + index_image + ' ' + str(index) + ' -eq tissue' + str(index) + '.mif')
 
-  run.command('mrcat cgm.mif sgm.mif wm.mif csf.mif path.mif - -axis 3 | mrconvert - result.mif -datatype float32')
+  run.command(['mrcat', ['tissue' + str(index) + '.mif' for index in range(1, 6)], '-', '-axis', '3', '|',
+               'mrconvert', '-', 'result.mif', '-datatype', 'float32'])
 
-  run.command('mrconvert result.mif ' + path.from_user(app.ARGS.output), mrconvert_keyval=path.from_user(app.ARGS.input, False), force=app.FORCE_OVERWRITE)
+  run.command('mrconvert result.mif ' + path.from_user(app.ARGS.output),
+              mrconvert_keyval=path.from_user(app.ARGS.input, False),
+              force=app.FORCE_OVERWRITE)
