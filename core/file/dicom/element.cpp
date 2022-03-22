1250788036eb7fe6f4488bb79b4da3d0502b9e5f
J-Donald Tournier <jdtournier@gmail.com>
2022-03-22 17:42:40 +0000

    DICOM: allow parsing of Philips PrivatePerFrameSq

--- core/file/dicom/element.cpp
+++ core/file/dicom/element.cpp
@@ -286,6 +286,9 @@ namespace MR {
           // ignore anything within IconImageSequence:
           if (seq.group ==  0x0088U && seq.element == 0x0200U)
             return true;
+          // allow Philips PrivatePerFrameSq:
+          if (seq.group == 0x2005U && seq.element == 0x140FU)
+            continue;
           // ignore anything within sequences with unknown (private) group:
           if (seq.group & 1U)
             return true;
