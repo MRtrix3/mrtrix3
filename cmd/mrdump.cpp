58e94de2b21c8057c89b1a6bda0432a259234fe2
J-Donald Tournier <jdtournier@gmail.com>
2023-01-18 12:13:37 +0000

    mrdump: various enhancements
    - loop through in predictable x,y,z order
    - loop through all volumes for each voxel location, so that all values
      for each voxel are dumped before moving the next
    - add options to also write out voxel indices and scanner-space
      coordinates

--- cmd/mrdump.cpp
+++ cmd/mrdump.cpp
@@ -17,6 +17,7 @@
 #include "command.h"
 #include "datatype.h"
 #include "image.h"
+#include "transform.h"
 #include "adapter/replicate.h"
 #include "algo/loop.h"
 #include "file/ofstream.h"
@@ -41,57 +42,71 @@ void usage ()
 
   OPTIONS
   + Option ("mask", "only write the image values within voxels specified by a mask image")
-    + Argument ("image").type_image_in();
+    + Argument ("image").type_image_in()
+
+  + Option ("indices", "output voxel indices as first 3 columns of output.")
+
+  + Option ("positions", "output voxel real-space position as first 3 columns of output "
+      "(after voxel indices if also requested)");
 }
 
 
 
 template <typename T, class StreamType>
-void write (Image<T>& image, StreamType& out)
+void write (Image<T>&& image, Image<bool>& mask, StreamType& out, bool write_idx, bool write_pos)
 {
-  for (auto l = Loop(image) (image); l; ++l)
-    out << image.value() << "\n";
-}
+  auto M = Transform(image).voxel2scanner;
 
-template <typename T, class StreamType>
-void write (Image<T> image, Image<bool>& mask, StreamType& out)
-{
-  if (!mask.valid()) {
-    write (image, out);
-    return;
-  }
-  Adapter::Replicate<Image<bool>> replicate (mask, image);
-  for (auto l = Loop(image) (image, replicate); l; ++l) {
-    if (replicate.value())
+  for (auto l = Loop(0,3) (image); l; ++l) {
+    if (mask.valid()) {
+      assign_pos_of (image, 0, 3).to (mask);
+      if (!mask.value())
+        continue;
+    }
+
+    if (write_idx)
+      out << image.index(0) << " " << image.index(1) << " " << image.index(2) << " ";
+
+    if (write_pos) {
+      Eigen::Vector3d p = M * Eigen::Vector3d ({ double(image.index(0)), double(image.index(1)), double(image.index(2)) });
+      out << p[0] << " " << p[1] << " " << p[2] << " ";
+    }
+
+
+    if (image.ndim() > 3) {
+      for (auto il = Loop (3,image.ndim()) (image); il; ++il)
+        out << image.value() << " ";
+      out << "\n";
+    }
+    else
       out << image.value() << "\n";
   }
 }
 
 
-
 template <class StreamType>
-void write (Header& header, Image<bool>& mask, StreamType& out)
+void write (Header& header, Image<bool>& mask, StreamType& out, bool write_idx, bool write_pos)
 {
   switch (uint8_t(DataType(header.datatype())()) & ~(DataType::BigEndian | DataType::LittleEndian | DataType::Complex)) {
     case DataType::Bit: case DataType::UInt8: case DataType::UInt16: case DataType::UInt32:
-      write (header.get_image<uint32_t>(), mask, out);
+      write (header.get_image<uint32_t>(), mask, out, write_idx, write_pos);
       break;
     case DataType::Int8: case DataType::Int16: case DataType::Int32:
-      write (header.get_image<int32_t>(), mask, out);
+      write (header.get_image<int32_t>(), mask, out, write_idx, write_pos);
       break;
-    case DataType::UInt64: write (header.get_image<uint64_t>(), mask, out); break;
-    case DataType::Int64:  write (header.get_image<int64_t>(), mask, out); break;
+    case DataType::UInt64: write (header.get_image<uint64_t>(), mask, out, write_idx, write_pos); break;
+    case DataType::Int64:  write (header.get_image<int64_t>(), mask, out, write_idx, write_pos); break;
     case DataType::Float32:
       if (header.datatype().is_complex())
-        write (header.get_image<cfloat>(), mask, out);
+        write (header.get_image<cfloat>(), mask, out, write_idx, write_pos);
       else
-        write (header.get_image<float>(), mask, out);
+        write (header.get_image<float>(), mask, out, write_idx, write_pos);
       break;
     case DataType::Float64:
       if (header.datatype().is_complex())
-        write (header.get_image<cdouble>(), mask, out);
+        write (header.get_image<cdouble>(), mask, out, write_idx, write_pos);
       else
-        write (header.get_image<double>(), mask, out);
+        write (header.get_image<double>(), mask, out, write_idx, write_pos);
       break;
     default:
       throw Exception ("Unknown data type: " + std::string(header.datatype().description()) + " (" + str(uint32_t(uint8_t(DataType(header.datatype())()))) + ")");
@@ -109,10 +124,13 @@ void run ()
   if (opt.size())
     mask = Image<bool>::open (opt[0][0]);
 
+  bool write_idx = get_options("indices").size();
+  bool write_pos = get_options("positions").size();
+
   if (argument.size() == 2) {
     File::OFStream out (argument[1]);
-    write (H, mask, out);
+    write (H, mask, out, write_idx, write_pos);
   } else {
-    write (H, mask, std::cout);
+    write (H, mask, std::cout, write_idx, write_pos);
   }
 }
