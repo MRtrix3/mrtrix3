366ba7cecdb9d4ee436cffc1169fe24b27c13968
Robert Smith <robert.smith@florey.edu.au>
2023-02-25 23:41:32 +1100

    Initial revisions of "mtnorm" Python algorithms

--- lib/mrtrix3/dwinormalise/mtnorm.py
+++ lib/mrtrix3/dwinormalise/mtnorm.py
@@ -15,66 +15,91 @@
 
 import math
 from mrtrix3 import CONFIG, MRtrixError
-from mrtrix3 import app, run, image, path, matrix,  _version
+from mrtrix3 import app, image, matrix, path, run
 
-DEFAULT_TARGET_INTENSITY=1000
 
-LMAXES_MULTI = '4,0,0'
-LMAXES_SINGLE = '4,0'
+REFERENCE_INTENSITY = 1000
+
+LMAXES_MULTI = [4, 0, 0]
+LMAXES_SINGLE = [4, 0]
 
 
 def usage(base_parser, subparsers): #pylint: disable=unused-variable
   parser = subparsers.add_parser('mtnorm', parents=[base_parser])
   parser.set_author('Robert E. Smith (robert.smith@florey.edu.au) and Arshiya Sangchooli (asangchooli@student.unimelb.edu.au)')
-  parser.set_synopsis('Performs a global DWI intensity normalisation, normalising mean b=0 CSF signal intensity')
-  parser.add_description('This script first performs response function estimation and multi-tissue CSD within a provided brain mask'
-                         '(with a lower lmax than the dwi2fod default, for higher speed) before using the global scaling factor derived '
-                         'by mtnormalise to scale the intensity of the DWI series such that the mean b=0 CSF signal intensity is '
-                         'approximately close to some reference value (by default ' + str(DEFAULT_TARGET_INTENSITY) + ').'
-                         'Note that if the -mask command-line option is not specified, the MRtrix3 command dwi2mask will automatically be '
-                         'called to derive a mask that will be passed to the relevant bias field estimation command. '
-                         'More information on mask derivation from DWI data can be found at the following link: \n'
-                         'https://mrtrix.readthedocs.io/en/' + _version.__tag__ + '/dwi_preprocessing/masking.html')
-  parser.add_argument('input_dwi',  help='The input DWI series')
+  parser.set_synopsis('Normalise a DWI series to the estimated b=0 CSF intensity')
+  parser.add_description('This algorithm determines an appropriate global scaling factor to apply to a DWI series '
+                         'such that after the scaling is applied, the b=0 CSF intensity corresponds to some '
+                         'reference value (' + str(REFERENCE_INTENSITY) + ' by default).')
+  parser.add_description('The operation of this script is a subset of that performed by the script "dwibiasnormmask". '
+                         'Many users may find that comprehensive solution preferable; this dwinormalise algorithm is '
+                         'nevertheless provided to demonstrate specifically the global intensituy normalisation portion of that command.')
+  parser.add_description('The ODFs estimated within this optimisation procedure are by default of lower maximal spherical harmonic '
+                         'degree than what would be advised for analysis. This is done for computational efficiency. This '
+                         'behaviour can be modified through the -lmax command-line option.')
+  parser.add_argument('input', help='The input DWI series')
   parser.add_argument('output', help='The normalised DWI series')
   options = parser.add_argument_group('Options specific to the "mtnorm" algorithm')
-  options.add_argument('-target', type=float, default=DEFAULT_TARGET_INTENSITY,
-                       help='the threshold on the balanced tissue sum image used to derive the brain mask. the default is 0.5')
-  options.add_argument('-lmax', type=str,
-                       help='the maximum spherical harmonic order for the output FODs. The value is passed to '
-                            'the dwi2fod command and should be provided in the format which it expects '
-                            '(Default is "' + str(LMAXES_MULTI) + '" for multi-shell and "' + str(LMAXES_SINGLE) + '" for single-shell data)')
-  options.add_argument('-mask', metavar='image', help='Manually provide a mask image for normalisation')
+  options.add_argument('-lmax',
+                       metavar='values',
+                       help='The maximum spherical harmonic degree for the estimated FODs (see Description); '
+                            'defaults are "' + ','.join(str(item) for item in LMAXES_MULTI) + '" for multi-shell and "' + ','.join(str(item) for item in LMAXES_SINGLE) + '" for single-shell data)')
+  options.add_argument('-mask',
+                       metavar='image',
+                       help='Provide a mask image for relevant calculations '
+                            '(if not provided, the default dwi2mask algorithm will be used)')
+  options.add_argument('-reference',
+                       type=float,
+                       metavar='value',
+                       default=REFERENCE_INTENSITY,
+                       help='Set the target CSF b=0 intensity in the output DWI series '
+                            '(default: ' + str(REFERENCE_INTENSITY) + ')')
+  options.add_argument('-scale',
+                       metavar='file',
+                       help='Write the scaling factor applied to the DWI series to a text file')
   app.add_dwgrad_import_options(parser)
+
+
 
 def check_output_paths(): #pylint: disable=unused-variable
   app.check_output_path(app.ARGS.output)
 
 
+
 def execute(): #pylint: disable=unused-variable
+
+  # Verify user inputs
+  lmax = None
+  if app.ARGS.lmax:
+    try:
+      lmax = [int(i) for i in app.ARGS.lmax.split(',')]
+    except ValueError as exc:
+      raise MRtrixError('Values provided to -lmax option must be a comma-separated list of integers') from exc
+    if any(value < 0 or value % 2 for value in lmax):
+      raise MRtrixError('lmax values must be non-negative even integers')
+    if len(lmax) not in [2, 3]:
+      raise MRtrixError('Length of lmax vector expected to be either 2 or 3')
+  if app.ARGS.reference <= 0.0:
+    raise MRtrixError('Reference intensity must be positive')
+
+  grad_option = app.read_dwgrad_import_options()
+
   # Get input data into the scratch directory
-  grad_option = ''
-  if app.ARGS.grad:
-    grad_option = ' -grad ' + path.from_user(app.ARGS.grad)
-  elif app.ARGS.fslgrad:
-    grad_option = ' -fslgrad ' + path.from_user(app.ARGS.fslgrad[0]) + ' ' + path.from_user(app.ARGS.fslgrad[1])
-
   app.make_scratch_dir()
-  app.goto_scratch_dir()
-
   run.command('mrconvert '
-              + path.from_user(app.ARGS.input_dwi)
+              + path.from_user(app.ARGS.input)
               + ' '
               + path.to_scratch('input.mif')
               + grad_option)
-
-  # Get mask into scratch directory
   if app.ARGS.mask:
-    run.command('mrconvert ' + path.from_user(app.ARGS.mask) + ' ' + path.to_scratch('mask.mif'))
+    run.command('mrconvert ' + path.from_user(app.ARGS.mask) + ' ' + path.to_scratch('mask.mif') + ' -datatype bit')
+  app.goto_scratch_dir()
+
+  # Make sure we have a valid mask available
+  if app.ARGS.mask:
     if not image.match('input.mif', 'mask.mif', up_to_dim=3):
       raise MRtrixError('Provided mask image does not match input DWI')
   else:
-    app.debug('Deriving brain mask...')
     run.command('dwi2mask ' + CONFIG.get('Dwi2maskAlgorithm', 'legacy') + ' input.mif mask.mif')
 
   # Determine whether we are working with single-shell or multi-shell data
@@ -83,6 +108,10 @@ def execute(): #pylint: disable=unused-variable
       for value in image.mrinfo('input.mif', 'shell_bvalues') \
                                .strip().split()]
   multishell = (len(bvalues) > 2)
+  if lmax is None:
+    lmax = LMAXES_MULTI if multishell else LMAXES_SINGLE
+  elif len(lmax) == 3 and not multishell:
+    raise MRtrixError('User specified 3 lmax values for three-tissue decomposition, but input DWI is not multi-shell')
 
   # RF estimation and multi-tissue CSD
   class Tissue(object): #pylint: disable=useless-object-inheritance
@@ -94,52 +123,41 @@ def execute(): #pylint: disable=unused-variable
 
   tissues = [Tissue('WM'), Tissue('GM'), Tissue('CSF')]
 
-  app.debug('Estimating response function using initial brain mask...')
-  run.command('dwi2response dhollander '
-              + 'input.mif '
-              + '-mask mask.mif '
+  run.command('dwi2response dhollander input.mif -mask mask.mif '
               + ' '.join(tissue.tissue_rf for tissue in tissues))
 
-  # Remove GM if we can't deal with it
-  if app.ARGS.lmax:
-    lmaxes = app.ARGS.lmax
-  else:
-    lmaxes = LMAXES_MULTI
-    if not multishell:
-      app.cleanup(tissues[1].tissue_rf)
-      tissues = tissues[::2]
-      lmaxes = LMAXES_SINGLE
+  # Immediately remove GM if we can't deal with it
+  if not multishell:
+    app.cleanup(tissues[1].tissue_rf)
+    tissues = tissues[::2]
 
-  app.debug('FOD estimation with lmaxes ' + lmaxes + '...')
-  run.command('dwi2fod msmt_csd '
-              + 'input.mif'
-              + ' -lmax ' + lmaxes
+  run.command('dwi2fod msmt_csd input.mif'
+              + ' -lmax ' + ','.join(str(item) for item in lmax)
               + ' '
               + ' '.join(tissue.tissue_rf + ' ' + tissue.fod
-                          for tissue in tissues))
+                         for tissue in tissues))
 
   # Normalisation in brain mask
-  run.command('maskfilter'
-              + ' mask.mif'
-              + ' erode - |'
+  run.command('maskfilter mask.mif erode - |'
               + ' mtnormalise -mask - -balanced'
               + ' -check_factors factors.txt '
               + ' '.join(tissue.fod + ' ' + tissue.fod_norm
-                          for tissue in tissues))
+                         for tissue in tissues))
   app.cleanup([tissue.fod for tissue in tissues])
   app.cleanup([tissue.fod_norm for tissue in tissues])
 
-  app.debug('applying appropiate scaling factor to DWI series...')
   csf_rf = matrix.load_matrix(tissues[-1].tissue_rf)
-  csf_rf_bzero_lzero = csf_rf[0][0]
   app.cleanup([tissue.tissue_rf for tissue in tissues])
+  csf_rf_bzero_lzero = csf_rf[0][0]
   balance_factors = matrix.load_vector('factors.txt')
-  csf_balance_factor = balance_factors[-1]
   app.cleanup('factors.txt')
+  csf_balance_factor = balance_factors[-1]
   scale_multiplier = (1000.0 * math.sqrt(4.0*math.pi)) / (csf_rf_bzero_lzero / csf_balance_factor)
-  run.command('mrcalc '
-              + 'input.mif '
-              + str(scale_multiplier)
-              + ' -mult '
-              + path.from_user(app.ARGS.output),
+
+  run.command('mrcalc input.mif ' + str(scale_multiplier) + ' -mult - | '
+              + 'mrconvert - ' + path.from_user(app.ARGS.output),
+              mrconvert_keyval=path.from_user(app.ARGS.input, False),
               force=app.FORCE_OVERWRITE)
+
+  if app.ARGS.scale:
+    matrix.save_vector(path.from_user(app.ARGS.scale, False), [scale_multiplier])
