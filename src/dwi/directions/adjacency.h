MRtrix3/mrtrix3

commit 934ca19d625726d17e4ce553ff2ce6ea16568765

Remove class DWI::Directions::Mask
For now, class BitSet is used in its place.
Mask dilation and erosion operations are currently not used due to prior changes to the FMLS class lookup table generation; if these are re-introduced, they belong in the Adjacency class rather than a Mask class.

src/dwi/fmls.cpp
@@ -177,7 +177,7 @@ namespace MR {
           for (uint32_t l = 0; l != out.size(); ++l) {
             if ((((i.first <= 0.0) &&  out[l].is_negative())
                   || ((i.first >  0.0) && !out[l].is_negative()))
-                && (out[l].get_mask().is_adjacent (i.second))) {
+                && (dirs.adjacency (l, i.second))) {
 
               adj_lobes.push_back (l);
 
@@ -190,7 +190,7 @@ namespace MR {
 
           } else if (adj_lobes.size() == 1) {
 
-            out[adj_lobes.front()].add (i.second, i.first, (*weights)[i.second]);
+            out[adj_lobes.front()].add (dirs, i.second, i.first, (*weights)[i.second]);
 
           } else {
 
@@ -202,7 +202,7 @@ namespace MR {
               std::sort (adj_lobes.begin(), adj_lobes.end());
               for (size_t j = 1; j != adj_lobes.size(); ++j)
                 out[adj_lobes[0]].merge (out[adj_lobes[j]]);
-              out[adj_lobes[0]].add (i.second, i.first, (*weights)[i.second]);
+              out[adj_lobes[0]].add (dirs, i.second, i.first, (*weights)[i.second]);
               for (auto j = retrospective_assignments.begin(); j != retrospective_assignments.end(); ++j) {
                 bool modified = false;
                 for (size_t k = 1; k != adj_lobes.size(); ++k) {
@@ -238,7 +238,7 @@ namespace MR {
         }
 
         for (const auto& i : retrospective_assignments)
-          out[i.second].add (i.first, values[i.first], (*weights)[i.first]);
+          out[i.second].add (dirs, i.first, values[i.first], (*weights)[i.first]);
 
         for (auto i = out.begin(); i != out.end();) { // Empty increment
 
@@ -301,7 +301,7 @@ namespace MR {
           out.lut = lookup_type::Constant (dirs.size(), out.size());
           size_t index = 0;
           for (auto i = out.begin(); i != out.end(); ++i, ++index) {
-            const DWI::Directions::Mask& this_mask (i->get_mask());
+            const BitSet& this_mask (i->get_mask());
             for (size_t d = 0; d != dirs.size(); ++d) {
               if (this_mask[d])
                 out.lut[d] = index;

src/dwi/directions/mask.cpp
@@ -1,81 +1,0 @@
-/* Copyright (c) 2008-2023 the MRtrix3 contributors.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * Covered Software is provided under this License on an "as is"
- * basis, without warranty of any kind, either expressed, implied, or
- * statutory, including, without limitation, warranties that the
- * Covered Software is free of defects, merchantable, fit for a
- * particular purpose or non-infringing.
- * See the Mozilla Public License v. 2.0 for more details.
- *
- * For more details, see http://www.mrtrix.org/.
- */
-
-#include "dwi/directions/mask.h"
-
-
-
-namespace MR {
-  namespace DWI {
-    namespace Directions {
-
-
-
-      void Mask::erode (const size_t iterations)
-      {
-        for (size_t iter = 0; iter != iterations; ++iter) {
-          Mask temp (*this);
-          for (size_t d = 0; d != size(); ++d) {
-            if (!temp[d]) {
-              for (vector<index_type>::const_iterator i = dirs->adjacency[d].begin(); i != dirs->adjacency[d].end(); ++i)
-                reset (*i);
-            }
-          }
-        }
-      }
-
-
-
-      void Mask::dilate (const size_t iterations)
-      {
-        for (size_t iter = 0; iter != iterations; ++iter) {
-          Mask temp (*this);
-          for (size_t d = 0; d != size(); ++d) {
-            if (temp[d]) {
-              for (vector<index_type>::const_iterator i = dirs->adjacency[d].begin(); i != dirs->adjacency[d].end(); ++i)
-                set (*i);
-            }
-          }
-        }
-      }
-
-
-
-      size_t Mask::get_min_linkage (const Mask& that)
-      {
-        size_t iterations = 0;
-        for (Mask temp (that); !(temp & *this).empty(); ++iterations)
-          temp.dilate();
-        return iterations;
-      }
-
-
-
-      bool Mask::is_adjacent (const size_t d) const
-      {
-        for (vector<index_type>::const_iterator i = dirs->adjacency[d].begin(); i != dirs->adjacency[d].end(); ++i) {
-          if (test (*i))
-            return true;
-        }
-        return false;
-      }
-
-
-
-
-    }
-  }
-}

src/dwi/directions/mask.h
@@ -1,66 +1,0 @@
-/* Copyright (c) 2008-2023 the MRtrix3 contributors.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * Covered Software is provided under this License on an "as is"
- * basis, without warranty of any kind, either expressed, implied, or
- * statutory, including, without limitation, warranties that the
- * Covered Software is free of defects, merchantable, fit for a
- * particular purpose or non-infringing.
- * See the Mozilla Public License v. 2.0 for more details.
- *
- * For more details, see http://www.mrtrix.org/.
- */
-
-#ifndef __dwi_directions_mask_h__
-#define __dwi_directions_mask_h__
-
-
-#include <fstream>
-
-#include "misc/bitset.h"
-
-#include "dwi/directions/adjacency.h"
-
-
-
-namespace MR {
-  namespace DWI {
-    namespace Directions {
-
-
-
-      // TODO This class might go entirely; replace with vector<bool> and move erosion / dilation elsewhere
-      class Mask : public BitSet {
-
-        public:
-          Mask (const CartesianWithAdjacency& master, const bool allocator = false) :
-              BitSet (master.size(), allocator),
-              dirs (&master) { }
-
-          Mask (const Mask& that) :
-              BitSet (that),
-              dirs (that.dirs) { }
-
-          const CartesianWithAdjacency& get_dirs() const { return *dirs; }
-
-          void erode  (const size_t iterations = 1);
-          void dilate (const size_t iterations = 1);
-
-          bool is_adjacent (const size_t) const;
-          size_t get_min_linkage (const Mask&);
-
-        private:
-          const CartesianWithAdjacency* dirs;
-
-      };
-
-
-
-    }
-  }
-}
-
-#endif

src/dwi/fixel_map.h
@@ -22,7 +22,6 @@
 #include "image.h"
 #include "algo/loop.h"
 #include "dwi/fmls.h"
-#include "dwi/directions/mask.h"
 
 
 namespace MR
@@ -33,7 +32,7 @@ namespace MR
 
 
     template <class Fixel>
-    class Fixel_map { 
+    class Fixel_map {
 
       public:
         Fixel_map (const Header& H) :
@@ -82,7 +81,7 @@ namespace MR
         vector<Fixel> fixels;
 
       private:
-        const class HeaderHelper : public ::MR::Header { 
+        const class HeaderHelper : public ::MR::Header {
           public:
             HeaderHelper (const ::MR::Header& H) :
                 ::MR::Header (H)
@@ -101,7 +100,7 @@ namespace MR
 
     template <class Fixel>
     class Fixel_map<Fixel>::MapVoxel
-    { 
+    {
       public:
         MapVoxel (const FMLS::FOD_lobes& in, const size_t first) :
             first_fixel_index (first),
@@ -146,7 +145,7 @@ namespace MR
 
 
     template <class Fixel>
-    class Fixel_map<Fixel>::Iterator { 
+    class Fixel_map<Fixel>::Iterator {
         friend class Fixel_map<Fixel>::ConstIterator;
       public:
         Iterator (const MapVoxel* const voxel, Fixel_map<Fixel>& parent) :
@@ -164,7 +163,7 @@ namespace MR
     };
 
     template <class Fixel>
-    class Fixel_map<Fixel>::ConstIterator { 
+    class Fixel_map<Fixel>::ConstIterator {
       public:
         ConstIterator (const MapVoxel* const voxel, const Fixel_map& parent) :
             index     (voxel ? voxel->first_index() : 0),

src/dwi/fmls.h
@@ -19,12 +19,12 @@
 
 #include <map> // Used for sorting FOD samples
 
-#include "memory.h"
-#include "math/SH.h"
-#include "dwi/directions/mask.h"
-#include "dwi/directions/set.h"
 #include "image.h"
+#include "memory.h"
 #include "algo/loop.h"
+#include "math/SH.h"
+#include "misc/bitset.h"
+#include "dwi/directions/set.h"
 
 
 
@@ -43,7 +43,6 @@ namespace MR
     {
 
 
-      using DWI::Directions::Mask;
       using DWI::Directions::index_type;
       using lookup_type = Eigen::Array<uint8_t, Eigen::Dynamic, 1>;
 
@@ -59,7 +58,7 @@ namespace MR
 
         public:
           FOD_lobe (const DWI::Directions::CartesianWithAdjacency& dirs, const index_type seed, const default_type value, const default_type weight) :
-              mask (dirs),
+              mask (dirs.size()),
               values (Eigen::Array<default_type, Eigen::Dynamic, 1>::Zero (dirs.size())),
               max_peak_value (abs (value)),
               peak_dirs (1, dirs[seed]),
@@ -74,7 +73,7 @@ namespace MR
 
           // This is used for creating a `null lobe' i.e. an FOD lobe with zero size, containing all directions not
           //   assigned to any other lobe in the voxel
-          FOD_lobe (const DWI::Directions::Mask& i) :
+          FOD_lobe (const BitSet& i) :
               mask (i),
               values (Eigen::Array<default_type, Eigen::Dynamic, 1>::Zero (i.size())),
               max_peak_value (0.0),
@@ -82,12 +81,12 @@ namespace MR
               neg (false) { }
 
 
-          void add (const index_type bin, const default_type value, const default_type weight)
+          void add (const DWI::Directions::CartesianWithAdjacency& dirs, const index_type bin, const default_type value, const default_type weight)
           {
             assert ((value <= 0.0 && neg) || (value >= 0.0 && !neg));
             mask[bin] = true;
             values[bin] = value;
-            const Eigen::Vector3d& dir = mask.get_dirs()[bin];
+            const Eigen::Vector3d& dir = dirs[bin];
             const default_type multiplier = (mean_dir.dot (dir)) > 0.0 ? 1.0 : -1.0;
             mean_dir += dir * multiplier * abs(value) * weight;
             integral += abs (value * weight);
@@ -127,7 +126,7 @@ namespace MR
 
           void set_lsq_dir (const Eigen::Vector3d& d) { lsq_dir = d; }
 
-          const DWI::Directions::Mask& get_mask() const { return mask; }
+          const BitSet& get_mask() const { return mask; }
           const Eigen::Array<default_type, Eigen::Dynamic, 1>& get_values() const { return values; }
           default_type get_max_peak_value() const { return max_peak_value; }
           size_t num_peaks() const { return peak_dirs.size(); }
@@ -139,7 +138,7 @@ namespace MR
 
 
         private:
-          DWI::Directions::Mask mask;
+          BitSet mask;
           Eigen::Array<default_type, Eigen::Dynamic, 1> values;
           default_type max_peak_value;
           vector<Eigen::Vector3d> peak_dirs;

commit 8a055252986c1be67bb2972961a413305680854c

fod2fixel: Fix FMLS refactoring bug
Bug introduced in 934ca19 due to test of adjacency between dixel and FOD lobe erroneously operating on lobe index rather than the dixel mask for that lobe.

src/dwi/directions/adjacency.h
@@ -20,6 +20,7 @@
 
 #include "types.h"
 #include "math/sphere.h"
+#include "misc/bitset.h"
 
 #include "dwi/directions/directions.h"
 
@@ -41,10 +42,22 @@ namespace MR {
           }
 
           // Are the directions corresponding to these two indices adjacent to one another?
-          bool operator() (const index_type one, const index_type two) const {
+          bool operator() (const index_type one, const index_type two) const
+          {
             assert (one < size());
             assert (two < size());
             return (std::find ((*this)[one].begin(), (*this)[one].end(), two) != (*this)[one].end());
+          }
+          // Is this direction adjacent to a mask?
+          bool operator() (const BitSet& mask, const index_type index) const
+          {
+            assert (mask.size() == size());
+            assert (index < size());
+            for (const auto i : (*this)[index]) {
+              if (mask[i])
+                return true;
+            }
+            return false;
           }
 
           index_type distance (const index_type one, const index_type two) const;

src/dwi/fmls.cpp
@@ -177,7 +177,7 @@ namespace MR {
           for (uint32_t l = 0; l != out.size(); ++l) {
             if ((((i.first <= 0.0) &&  out[l].is_negative())
                   || ((i.first >  0.0) && !out[l].is_negative()))
-                && (dirs.adjacency (l, i.second))) {
+                  && (dirs.adjacency (out[l].get_mask(), i.second))) {
 
               adj_lobes.push_back (l);
 