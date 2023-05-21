MRtrix3/mrtrix3

commit b435e582369b16253414b853cb86b700a8e866f8

fod2fixel and FMLS: Revise "peak" nomenclature

src/dwi/fmls.cpp
@@ -34,20 +34,20 @@ namespace MR {
             "Default: " + str(FMLS_INTEGRAL_THRESHOLD_DEFAULT, 2) + ".")
         + App::Argument ("value").type_float (0.0)
 
-        + App::Option ("fmls_max_value",
-            "threshold the raw maximal amplitude of positive FOD lobes. "
-            "Any lobe for which the maximal amplitude is smaller than this threshold will be discarded. "
-            "Default: " + str(FMLS_MAX_VALUE_THRESHOLD_DEFAULT, 2) + ".")
+        + App::Option ("fmls_peak_value",
+            "threshold peak amplitude of positive FOD lobes. "
+            "Any lobe for which the maximal peak amplitude is smaller than this threshold will be discarded. "
+            "Default: " + str(FMLS_PEAK_VALUE_THRESHOLD_DEFAULT, 2) + ".")
         + App::Argument ("value").type_float (0.0)
 
         + App::Option ("fmls_no_thresholds",
-            "disable all FOD lobe thresholding; every lobe with a positive FOD amplitude will be retained.")
+            "disable all FOD lobe thresholding; every lobe where the FOD is positive will be retained.")
 
-        + App::Option ("fmls_max_ratio_to_merge",
-            "Specify the ratio between a given FOD amplitude sample between two lobes, and the smallest maximal amplitude of the adjacent lobes, above which those lobes will be merged. "
-            "This is the relative amplitude between the smallest of two adjoining lobes, and the 'bridge' between the two lobes. "
+        + App::Option ("fmls_lobe_merge_ratio",
+            "Specify the ratio between a given FOD amplitude sample between two lobes, and the smallest peak amplitude of the adjacent lobes, above which those lobes will be merged. "
+            "This is the amplitude of the FOD at the 'bridge' point between the two lobes, divided by the peak amplitude of the smaller of the two adjoining lobes. "
             "A value of 1.0 will never merge two lobes into one; a value of 0.0 will always merge lobes unless they are bisected by a zero-valued crossing. "
-            "Default: " + str(FMLS_RATIO_OF_MAX_VALUES_TO_MERGE_DEFAULT, 2) + ".")
+            "Default: " + str(FMLS_MERGE_RATIO_BRIDGE_TO_PEAK_DEFAULT, 2) + ".")
         + App::Argument ("value").type_float (0.0, 1.0);
 
 
@@ -61,7 +61,7 @@ namespace MR {
         const bool no_thresholds = opt.size();
         if (no_thresholds) {
           segmenter.set_integral_threshold (0.0);
-          segmenter.set_max_value_threshold (0.0);
+          segmenter.set_peak_value_threshold (0.0);
         }
 
         opt = get_options ("fmls_integral");
@@ -73,18 +73,18 @@ namespace MR {
           }
         }
 
-        opt = get_options ("fmls_max_value");
+        opt = get_options ("fmls_peak_value");
         if (opt.size()) {
           if (no_thresholds) {
-            WARN ("Option -fmls_max_value ignored: -fmls_no_thresholds overrides this");
+            WARN ("Option -fmls_peak_value ignored: -fmls_no_thresholds overrides this");
           } else {
-            segmenter.set_max_value_threshold (default_type(opt[0][0]));
+            segmenter.set_peak_value_threshold (default_type(opt[0][0]));
           }
         }
 
-        opt = get_options ("fmls_max_ratio_to_merge");
+        opt = get_options ("fmls_merge_ratio");
         if (opt.size())
-          segmenter.set_ratio_of_max_values_to_merge (default_type(opt[0][0]));
+          segmenter.set_lobe_merge_ratio (default_type(opt[0][0]));
 
       }
 
@@ -135,15 +135,15 @@ namespace MR {
 
 
       Segmenter::Segmenter (const DWI::Directions::FastLookupSet& directions, const size_t l) :
-          dirs                         (directions),
-          lmax                         (l),
-          precomputer                  (new Math::SH::PrecomputedAL<default_type> (lmax, 2 * dirs.size())),
-          integral_threshold           (FMLS_INTEGRAL_THRESHOLD_DEFAULT),
-          max_value_threshold          (FMLS_MAX_VALUE_THRESHOLD_DEFAULT),
-          ratio_of_max_values_to_merge (FMLS_RATIO_OF_MAX_VALUES_TO_MERGE_DEFAULT),
-          create_null_lobe             (false),
-          create_lookup_table          (true),
-          dilate_lookup_table          (false)
+          dirs                 (directions),
+          lmax                 (l),
+          precomputer          (new Math::SH::PrecomputedAL<default_type> (lmax, 2 * dirs.size())),
+          integral_threshold   (FMLS_INTEGRAL_THRESHOLD_DEFAULT),
+          peak_value_threshold (FMLS_PEAK_VALUE_THRESHOLD_DEFAULT),
+          lobe_merge_ratio     (FMLS_MERGE_RATIO_BRIDGE_TO_PEAK_DEFAULT),
+          create_null_lobe     (false),
+          create_lookup_table  (true),
+          dilate_lookup_table  (false)
       {
         Eigen::Matrix<default_type, Eigen::Dynamic, 2> az_el_pairs (dirs.size(), 2);
         for (size_t row = 0; row != dirs.size(); ++row) {
@@ -213,7 +213,7 @@ namespace MR {
             // Changed handling of lobe merges
             // Merge lobes as they appear to be merged, but update the
             //   contents of retrospective_assignments accordingly
-            if (abs (i.first) / out[adj_lobes.back()].get_max_value() > ratio_of_max_values_to_merge) {
+            if (abs (i.first) / out[adj_lobes.back()].get_max_peak_value() > lobe_merge_ratio) {
 
               std::sort (adj_lobes.begin(), adj_lobes.end());
               for (size_t j = 1; j != adj_lobes.size(); ++j)
@@ -265,8 +265,8 @@ namespace MR {
             // Revise multiple peaks if present
             for (size_t peak_index = 0; peak_index != i->num_peaks(); ++peak_index) {
               Eigen::Vector3 newton_peak_dir = i->get_peak_dir (peak_index); // to be updated by subsequent Math::SH::get_peak() call
-              const default_type newton_peak_amplitude = Math::SH::get_peak (in, lmax, newton_peak_dir, &(*precomputer));
-              if (std::isfinite (newton_peak_amplitude) && newton_peak_dir.allFinite()) {
+              const default_type newton_peak_value = Math::SH::get_peak (in, lmax, newton_peak_dir, &(*precomputer));
+              if (std::isfinite (newton_peak_value) && newton_peak_dir.allFinite()) {
 
                 // Ensure that the new peak direction found via Newton optimisation
                 //   is still approximately the same direction as that found via FMLS:
@@ -285,12 +285,12 @@ namespace MR {
                   // Needs to still lie within the lobe: Determined via mask
                   const index_type newton_peak_closest_dir_index = dirs.select_direction (newton_peak_dir);
                   if (i->get_mask()[newton_peak_closest_dir_index])
-                    i->revise_peak (peak_index, newton_peak_dir, newton_peak_amplitude);
+                    i->revise_peak (peak_index, newton_peak_dir, newton_peak_value);
 
                 }
               }
             }
-            if (i->get_max_value() < max_value_threshold) {
+            if (i->get_max_peak_value() < peak_value_threshold) {
               i = out.erase (i);
             } else {
               i->finalise();

cmd/fod2fixel.cpp
@@ -49,12 +49,12 @@ const OptionGroup OutputOptions = OptionGroup ("Metric values for fixel-based sp
             "output the total Apparent Fibre Density per fixel (integral of FOD lobe)")
     + Argument ("image").type_image_out()
 
-  + Option ("maximum",
-            "output the maximum FOD amplitude per fixel")
+  + Option ("peak_amp",
+            "output the amplitude of the maximal FOD peak per fixel")
     + Argument ("image").type_image_out()
 
   + Option ("disp",
-            "output a measure of dispersion per fixel as the ratio between FOD lobe integral and maximum amplitude")
+            "output a measure of dispersion per fixel as the ratio between FOD lobe integral and maximal peak amplitude")
     + Argument ("image").type_image_out();
 
 
@@ -117,7 +117,7 @@ class Segmented_FOD_receiver { MEMALIGN(Segmented_FOD_receiver)
     void set_index_output (const std::string& path) { index_path = path; }
     void set_directions_output (const std::string& path) { dir_path = path; }
     void set_afd_output (const std::string& path) { afd_path = path; }
-    void set_max_output (const std::string& path) { max_path = path; }
+    void set_peak_amp_output (const std::string& path) { peak_amp_path = path; }
     void set_disp_output (const std::string& path) { disp_path = path; }
 
     bool operator() (const FOD_lobes&);
@@ -128,9 +128,9 @@ class Segmented_FOD_receiver { MEMALIGN(Segmented_FOD_receiver)
     struct Primitive_FOD_lobe { MEMALIGN (Primitive_FOD_lobe)
       Eigen::Vector3f dir;
       float integral;
-      float max_value;
-      Primitive_FOD_lobe (Eigen::Vector3f dir, float integral, float max_value) :
-          dir (dir), integral (integral), max_value (max_value) {}
+      float peak_value;
+      Primitive_FOD_lobe (Eigen::Vector3f dir, float integral, float peak_value) :
+          dir (dir), integral (integral), peak_value (peak_value) {}
     };
 
 
@@ -143,16 +143,16 @@ class Segmented_FOD_receiver { MEMALIGN(Segmented_FOD_receiver)
           for (index_type i = 0; i != N; ++i) {
             const FOD_lobe& lobe (in[i]);
             if (dir_from_peak)
-              this->emplace_back (lobe.get_peak_dir(0).cast<float>(), lobe.get_integral(), lobe.get_max_value());
+              this->emplace_back (lobe.get_peak_dir(0).cast<float>(), lobe.get_integral(), lobe.get_max_peak_value());
             else
-              this->emplace_back (lobe.get_mean_dir().cast<float>(), lobe.get_integral(), lobe.get_max_value());
+              this->emplace_back (lobe.get_mean_dir().cast<float>(), lobe.get_integral(), lobe.get_max_peak_value());
           }
         }
         Eigen::Array3i vox;
     };
 
     Header H;
-    std::string fixel_directory_path, index_path, dir_path, afd_path, max_path, disp_path;
+    std::string fixel_directory_path, index_path, dir_path, afd_path, peak_amp_path, disp_path;
     vector<Primitive_FOD_lobes> lobes;
     index_type fixel_count;
     index_type max_per_voxel;
@@ -186,7 +186,7 @@ void Segmented_FOD_receiver::commit ()
   std::unique_ptr<IndexImage> index_image;
   std::unique_ptr<DataImage> dir_image;
   std::unique_ptr<DataImage> afd_image;
-  std::unique_ptr<DataImage> max_image;
+  std::unique_ptr<DataImage> peak_amp_image;
   std::unique_ptr<DataImage> disp_image;
 
   auto index_header (H);
@@ -220,12 +220,12 @@ void Segmented_FOD_receiver::commit ()
     Fixel::check_fixel_size (*index_image, *afd_image);
   }
 
-  if (max_path.size()) {
-    auto max_header(fixel_data_header);
-    max_header.size(1) = 1;
-    max_image = make_unique<DataImage> (DataImage::create (Path::join(fixel_directory_path, max_path), max_header));
-    max_image->index(1) = 0;
-    Fixel::check_fixel_size (*index_image, *max_image);
+  if (peak_amp_path.size()) {
+    auto peak_amp_header (fixel_data_header);
+    peak_amp_header.size(1) = 1;
+    peak_amp_image = make_unique<DataImage> (DataImage::create (Path::join(fixel_directory_path, peak_amp_path), peak_amp_header));
+    peak_amp_image->index(1) = 0;
+    Fixel::check_fixel_size (*index_image, *peak_amp_image);
   }
 
   if (disp_path.size()) {
@@ -264,17 +264,17 @@ void Segmented_FOD_receiver::commit ()
       }
     }
 
-    if (max_image) {
+    if (peak_amp_image) {
       for (size_t i = 0; i < n_vox_fixels; ++i) {
-        max_image->index(0) = offset + i;
-        max_image->value() = vox_fixels[i].max_value;
+        peak_amp_image->index(0) = offset + i;
+        peak_amp_image->value() = vox_fixels[i].peak_value;
       }
     }
 
     if (disp_image) {
       for (size_t i = 0; i < n_vox_fixels; ++i) {
         disp_image->index(0) = offset + i;
-        disp_image->value() = vox_fixels[i].integral / vox_fixels[i].max_value;
+        disp_image->value() = vox_fixels[i].integral / vox_fixels[i].peak_value;
       }
     }
 
@@ -312,9 +312,9 @@ void run ()
   receiver.set_directions_output (default_directions_filename);
 
   auto
-  opt = get_options ("afd");     if (opt.size()) receiver.set_afd_output  (opt[0][0]);
-  opt = get_options ("maximum"); if (opt.size()) receiver.set_max_output  (opt[0][0]);
-  opt = get_options ("disp");    if (opt.size()) receiver.set_disp_output (opt[0][0]);
+  opt = get_options ("afd");      if (opt.size()) receiver.set_afd_output      (opt[0][0]);
+  opt = get_options ("peak_amp"); if (opt.size()) receiver.set_peak_amp_output (opt[0][0]);
+  opt = get_options ("disp");     if (opt.size()) receiver.set_disp_output     (opt[0][0]);
 
   opt = get_options ("mask");
   Image<float> mask;

src/dwi/fmls.h
@@ -29,8 +29,8 @@
 
 
 #define FMLS_INTEGRAL_THRESHOLD_DEFAULT 0.0 // By default, don't threshold by integral (tough to get a good number)
-#define FMLS_MAX_VALUE_THRESHOLD_DEFAULT 0.1
-#define FMLS_RATIO_OF_MAX_VALUES_TO_MERGE_DEFAULT 1.0 // By default, never perform merging of lobes generated from discrete peaks such that a single lobe contains multiple peaks
+#define FMLS_PEAK_VALUE_THRESHOLD_DEFAULT 0.1
+#define FMLS_MERGE_RATIO_BRIDGE_TO_PEAK_DEFAULT 1.0 // By default, never perform merging of lobes generated from discrete peaks such that a single lobe contains multiple peaks
 
 
 // By default, the mean direction of each FOD lobe is calculated by taking a weighted average of the
@@ -67,7 +67,7 @@ namespace MR
           FOD_lobe (const DWI::Directions::Set& dirs, const index_type seed, const default_type value, const default_type weight) :
               mask (dirs),
               values (Eigen::Array<default_type, Eigen::Dynamic, 1>::Zero (dirs.size())),
-              max_value (abs (value)),
+              max_peak_value (abs (value)),
               peak_dirs (1, dirs.get_dir (seed)),
               mean_dir (peak_dirs.front() * abs(value) * weight),
               integral (abs (value * weight)),
@@ -82,7 +82,7 @@ namespace MR
           FOD_lobe (const DWI::Directions::Mask& i) :
               mask (i),
               values (Eigen::Array<default_type, Eigen::Dynamic, 1>::Zero (i.size())),
-              max_value (0.0),
+              max_peak_value (0.0),
               integral (0.0),
               neg (false) { }
 
@@ -98,13 +98,13 @@ namespace MR
             integral += abs (value * weight);
           }
 
-          void revise_peak (const size_t index, const Eigen::Vector3& revised_peak, const default_type revised_max)
+          void revise_peak (const size_t index, const Eigen::Vector3& revised_peak_dir, const default_type revised_peak_value)
           {
             assert (!neg);
             assert (index < num_peaks());
-            peak_dirs[index] = revised_peak;
+            peak_dirs[index] = revised_peak_dir;
             if (!index)
-              max_value = revised_max;
+              max_peak_value = revised_peak_value;
           }
 
 #ifdef FMLS_OPTIMISE_MEAN_DIR
@@ -127,8 +127,8 @@ namespace MR
             mask |= that.mask;
             for (size_t i = 0; i != mask.size(); ++i)
               values[i] += that.values[i];
-            if (that.max_value > max_value) {
-              max_value = that.max_value;
+            if (that.max_peak_value > max_peak_value) {
+              max_peak_value = that.max_peak_value;
               peak_dirs.insert (peak_dirs.begin(), that.peak_dirs.begin(), that.peak_dirs.end());
             } else {
               peak_dirs.insert (peak_dirs.end(), that.peak_dirs.begin(), that.peak_dirs.end());
@@ -140,7 +140,7 @@ namespace MR
 
           const DWI::Directions::Mask& get_mask() const { return mask; }
           const Eigen::Array<default_type, Eigen::Dynamic, 1>& get_values() const { return values; }
-          default_type get_max_value() const { return max_value; }
+          default_type get_max_peak_value() const { return max_peak_value; }
           size_t num_peaks() const { return peak_dirs.size(); }
           const Eigen::Vector3& get_peak_dir (const size_t i) const { assert (i < num_peaks()); return peak_dirs[i]; }
           const Eigen::Vector3& get_mean_dir() const { return mean_dir; }
@@ -151,7 +151,7 @@ namespace MR
         private:
           DWI::Directions::Mask mask;
           Eigen::Array<default_type, Eigen::Dynamic, 1> values;
-          default_type max_value;
+          default_type max_peak_value;
           vector<Eigen::Vector3> peak_dirs;
           Eigen::Vector3 mean_dir;
           default_type integral;
@@ -240,18 +240,18 @@ namespace MR
           bool operator() (const SH_coefs&, FOD_lobes&) const;
 
 
-          default_type get_integral_threshold           ()               const { return integral_threshold; }
-          void         set_integral_threshold           (const default_type i) { integral_threshold = i; }
-          default_type get_max_value_threshold          ()               const { return max_value_threshold; }
-          void         set_max_value_threshold          (const default_type i) { max_value_threshold = i; }
-          default_type get_ratio_of_max_values_to_merge ()               const { return ratio_of_max_values_to_merge; }
-          void         set_ratio_of_max_values_to_merge (const default_type i) { ratio_of_max_values_to_merge = i; }
-          bool         get_create_null_lobe             ()               const { return create_null_lobe; }
-          void         set_create_null_lobe             (const bool i)         { create_null_lobe = i; verify_settings(); }
-          bool         get_create_lookup_table          ()               const { return create_lookup_table; }
-          void         set_create_lookup_table          (const bool i)         { create_lookup_table = i; verify_settings(); }
-          bool         get_dilate_lookup_table          ()               const { return dilate_lookup_table; }
-          void         set_dilate_lookup_table          (const bool i)         { dilate_lookup_table = i; verify_settings(); }
+          default_type get_integral_threshold   ()               const { return integral_threshold; }
+          void         set_integral_threshold   (const default_type i) { integral_threshold = i; }
+          default_type get_peak_value_threshold ()               const { return peak_value_threshold; }
+          void         set_peak_value_threshold (const default_type i) { peak_value_threshold = i; }
+          default_type get_lobe_merge_ratio     ()               const { return lobe_merge_ratio; }
+          void         set_lobe_merge_ratio     (const default_type i) { lobe_merge_ratio = i; }
+          bool         get_create_null_lobe     ()               const { return create_null_lobe; }
+          void         set_create_null_lobe     (const bool i)         { create_null_lobe = i; verify_settings(); }
+          bool         get_create_lookup_table  ()               const { return create_lookup_table; }
+          void         set_create_lookup_table  (const bool i)         { create_lookup_table = i; verify_settings(); }
+          bool         get_dilate_lookup_table  ()               const { return dilate_lookup_table; }
+          void         set_dilate_lookup_table  (const bool i)         { dilate_lookup_table = i; verify_settings(); }
 
 
         private:
@@ -267,12 +267,12 @@ namespace MR
           std::shared_ptr<Math::SH::PrecomputedAL<default_type>> precomputer;
           std::shared_ptr<IntegrationWeights> weights;
 
-          default_type integral_threshold; // Integral of positive lobe must be at least this value
-          default_type max_value_threshold; // Absolute threshold for the maximal amplitude of the lobe
-          default_type ratio_of_max_values_to_merge; // Determines whether two lobes get agglomerated into one, depending on the FOD amplitude at the current point and how it compares to the maximal amplitudes of the lobes to which it could be assigned
-          bool         create_null_lobe; // If this is set, an additional lobe will be created after segmentation with zero size, containing all directions not assigned to any other lobe
-          bool         create_lookup_table; // If this is set, an additional lobe will be created after segmentation with zero size, containing all directions not assigned to any other lobe
-          bool         dilate_lookup_table; // If this is set, the lookup table created for each voxel will be dilated so that all directions correspond to the nearest positive non-zero FOD lobe
+          default_type integral_threshold;   // Integral of positive lobe must be at least this value
+          default_type peak_value_threshold; // Absolute threshold for the peak amplitude of the lobe
+          default_type lobe_merge_ratio;     // Determines whether two lobes get agglomerated into one, depending on the FOD amplitude at the current point and how it compares to the maximal amplitudes of the lobes to which it could be assigned
+          bool         create_null_lobe;     // If this is set, an additional lobe will be created after segmentation with zero size, containing all directions not assigned to any other lobe
+          bool         create_lookup_table;  // If this is set, an additional lobe will be created after segmentation with zero size, containing all directions not assigned to any other lobe
+          bool         dilate_lookup_table;  // If this is set, the lookup table created for each voxel will be dilated so that all directions correspond to the nearest positive non-zero FOD lobe
 
 
           void verify_settings() const

commit 039277fc5c8646c9fee9b2d72f2779437d51f6d8

fod2fixel: Fix -fmls_lobe_merge_ratio
Mismatch between option string as loaded in usage() and the get_options() call results in user-specified option being ignored.
Error introduced in b435e58 as part of #1932.

src/dwi/fmls.cpp
@@ -82,7 +82,7 @@ namespace MR {
           }
         }
 
-        opt = get_options ("fmls_merge_ratio");
+        opt = get_options ("fmls_lobe_merge_ratio");
         if (opt.size())
           segmenter.set_lobe_merge_ratio (default_type(opt[0][0]));
 