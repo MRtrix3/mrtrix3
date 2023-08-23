7d0cab07f66c164f873ee1e8ae0887a47800c656
Tom Close <tom.g.close@gmail.com>
2023-08-23 14:49:27 +1000

    split converters so they aren't wrapped twice

--- core/app.cpp
+++ core/app.cpp
@@ -962,10 +962,10 @@ std::string restructured_text_usage() {
             || opt[0].type == ArgDirectoryOut
           )
         ) {
+          is_output_file = true;
           bool is_multi = type_string.length() > 19 && type_string.substr(0, 19) == "specs.MultiInputObj";
-          if (!is_multi) {
+          if (!is_multi)
             type_string = "ty.Union[" + type_string + ", bool]";
-          }
         }
         // Print type
         f += indent + type_string + ",\n";
--- pydra/fileformats-mrtrix3-extras/fileformats/extras/mrtrix3/converters.py
+++ pydra/fileformats-mrtrix3-extras/fileformats/extras/mrtrix3/converters.py
@@ -9,23 +9,42 @@ try:
     from pydra.tasks.mrtrix3.utils import MRConvert
 except ImportError:
     from pydra.tasks.mrtrix3.latest import mrconvert as MRConvert
+    in_out_file_kwargs = {"in_file": "input", "out_file": "output"}
+else:
+    in_out_file_kwargs = {}
 
 
 @mark.converter(
     source_format=MedicalImage,
     target_format=MrtrixImage,
     out_ext=MrtrixImage.ext,
-    in_file="input",
-    out_file="output",
+    **in_out_file_kwargs
 )
+def mrconvert(name, out_ext: str, **kwargs):
+    """Initiate an MRConvert task with the output file extension set
+
+    Parameters
+    ----------
+    name : str
+        name of the converter task
+    out_ext : str
+        extension of the output file, used by MRConvert to determine the desired format
+
+    Returns
+    -------
+    pydra.ShellCommandTask
+        the converter task
+    """
+    return MRConvert(name=name, out_file="out" + out_ext, **kwargs)
+
+
 @mark.converter(
     source_format=MedicalImage,
     target_format=MrtrixImageHeader,
     out_ext=MrtrixImageHeader.ext,
-    in_file="input",
-    out_file="output",
+    **in_out_file_kwargs
 )
-def mrconvert(name, out_ext: str, **kwargs):
+def mrconvert2(name, out_ext: str, **kwargs):
     """Initiate an MRConvert task with the output file extension set
 
     Parameters
