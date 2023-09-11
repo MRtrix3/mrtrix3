ed37736541ad0bbd14fca592c0e1970c57e7cdfd
Robert Smith <robert.smith@florey.edu.au>
2023-09-11 15:47:20 +0200

    PNG: Fix check for too few axes
    Fixes erroneous check put in place in #2535 (170405f).

--- core/formats/png.cpp
+++ core/formats/png.cpp
@@ -132,14 +132,14 @@ namespace MR
       if (H.ndim() == 4 && H.size(3) > 4)
         throw Exception ("A 4D image written to PNG must have between one and four volumes (requested: " + str(H.size(3)) + ")");
 
-      // TODO After looping over axes via square-bracket notation,
+      // After looping over axes via square-bracket notation,
       //   there needs to be at least two axes with size greater than one
-      size_t unity_axes = 0;
+      size_t nonunity_axes = 0;
       for (size_t axis = 0; axis != H.ndim(); ++axis) {
-        if (H.size (axis) == 1)
-          ++unity_axes;
+        if (H.size (axis) > 1)
+          ++nonunity_axes;
       }
-      if (unity_axes - (H.ndim() - num_axes) < 2)
+      if (nonunity_axes - (H.ndim() - num_axes) < 2)
         throw Exception ("Too few (non-unity) image axes to support PNG export");
 
       // For 4D images, can support:
