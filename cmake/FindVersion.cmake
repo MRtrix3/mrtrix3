f49f93647bbd3c4cec2ff918fd12380b72723a79
Daljit <daljit7991@gmail.com>
2023-09-29 12:57:52 +0100

    Add more robust support for Python commands
    - Moved all Python scripts in a separate directory
    - Generate version file for Python modules
    - C++/Python commands are built in "bin" inside the build folder.
    - Copy Python scripts in installation directory

--- cmake/FindVersion.cmake
+++ cmake/FindVersion.cmake
@@ -4,26 +4,26 @@ if(GIT_EXECUTABLE)
     execute_process(
         COMMAND ${GIT_EXECUTABLE} describe --abbrev=0
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        OUTPUT_VARIABLE GIT_TAG
-        RESULT_VARIABLE GIT_TAG_ERROR
+        OUTPUT_VARIABLE MRTRIX_GIT_TAG
+        RESULT_VARIABLE MRTRIX_GIT_TAG_ERROR
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
     # Get commit
     execute_process(
         COMMAND ${GIT_EXECUTABLE} describe --abbrev=8 --dirty --always
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        OUTPUT_VARIABLE GIT_COMMIT
-        RESULT_VARIABLE GIT_COMMIT_ERROR
+        OUTPUT_VARIABLE MRTRIX_GIT_COMMIT
+        RESULT_VARIABLE MRTRIX_GIT_COMMIT_ERROR
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 
-    if(NOT "${GIT_TAG}" STREQUAL ${MRTRIX_BASE_VERSION})
+    if(NOT "${MRTRIX_GIT_TAG}" STREQUAL ${MRTRIX_BASE_VERSION})
         message(FATAL_ERROR "MRtrix3 base version does not match the git tag!")
     endif()
 
-    if(NOT GIT_TAG_ERROR AND NOT GIT_COMMIT_ERROR)
-        message(VERBOSE "Git tag: ${GIT_TAG}; Git commit: ${GIT_COMMIT}")
-        set(MRTRIX_VERSION ${GIT_COMMIT})
+    if(NOT MRTRIX_GIT_TAG_ERROR AND NOT MRTRIX_GIT_COMMIT_ERROR)
+        message(VERBOSE "Git tag: ${MRTRIX_GIT_TAG}; Git commit: ${MRTRIX_GIT_COMMIT}")
+        set(MRTRIX_VERSION ${MRTRIX_GIT_COMMIT})
     else()
         message(VERBOSE "Git tag: not found.")
     endif()
