5b43adef6cb5b74537e44525eeddbdae2f686a7c
J-Donald Tournier <jdtournier@gmail.com>
2023-11-29 11:29:59 +0000

    Update fft.h following suggestions from clang-tidy

--- core/math/fft.h
+++ core/math/fft.h
@@ -32,21 +32,21 @@ namespace Math {
 class FFT1D {
 public:
   FFT1D(size_t N, int direction) : _data(N), direction(direction) {
-    fftw_complex *p = reinterpret_cast<fftw_complex *>(&(_data[0]));
-    _plan = fftw_plan_dft_1d(N, p, p, direction, FFTW_MEASURE);
+    fftw_complex *p = reinterpret_cast<fftw_complex *>(_data.data());
+    _plan = fftw_plan_dft_1d(static_cast<int>(N), p, p, direction, FFTW_MEASURE);
   }
 
   FFT1D(const FFT1D &other) : _data(other._data.size()), direction(other.direction) {
-    fftw_complex *p = reinterpret_cast<fftw_complex *>(&(_data[0]));
-    _plan = fftw_plan_dft_1d(other._data.size(), p, p, direction, FFTW_MEASURE);
+    fftw_complex *p = reinterpret_cast<fftw_complex *>(_data.data());
+    _plan = fftw_plan_dft_1d(static_cast<int>(_data.size()), p, p, direction, FFTW_MEASURE);
   }
 
   FFT1D &operator=(const FFT1D &other) {
     fftw_destroy_plan(_plan);
     _data.resize(other._data.size());
     direction = other.direction;
-    fftw_complex *p = reinterpret_cast<fftw_complex *>(&(_data[0]));
-    _plan = fftw_plan_dft_1d(other._data.size(), p, p, direction, FFTW_MEASURE);
+    fftw_complex *p = reinterpret_cast<fftw_complex *>(_data.data());
+    _plan = fftw_plan_dft_1d(static_cast<int>(_data.size()), p, p, direction, FFTW_MEASURE);
     return *this;
   }
 
