7283d7b68f55911964c10d68eccdedfea13d2b58
adsouza <arkievdsouza@sydney.edu.au>
2024-02-20 09:53:50 +1100

    imported gzipped image formats into base fileformats package

--- pydra/fileformats-medimage-mrtrix3/fileformats/medimage_mrtrix3/__init__.py
+++ pydra/fileformats-medimage-mrtrix3/fileformats/medimage_mrtrix3/__init__.py
@@ -1,6 +1,6 @@
 from ._version import __version__
 
-from .image import ImageFormat, ImageHeader
+from .image import ImageFormat, ImageFormatGz, ImageHeader, ImageDataFile
 from .in_out import ImageIn, ImageOut
 from .dwi import (
     BFile, NiftiB, NiftiGzB, NiftiGzXB, NiftiXB, ImageFormatB, ImageFormatGzB, ImageHeaderB
