81086a0a2c0967fd303f2b6a769e1cd223957460
Daljit Singh <daljit7991@gmail.com>
2024-02-27 14:27:30 +0000

    Update msys2 packaging scripts for CMake build

--- packaging/mingw/run.sh
+++ packaging/mingw/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ $# -ne 2 ]]; then
-  echo usage: run.sh tag user
+  echo usage: run.sh commit user
   exit 1
 else
 
   set -e
-  sed -i'.orig' -e "s|TAGNAME|$1|g" -e "s|GIT_USER|$2|g" PKGBUILD
+  sed -i'.orig' -e "s|GIT_COMMIT|$1|g" -e "s|GIT_USER|$2|g" PKGBUILD
   makepkg -s
 
 fi
