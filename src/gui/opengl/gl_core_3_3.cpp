52838a9119486dae26f75322bd0462eb27a1b5e0
Robert Smith <robert.smith@florey.edu.au>
2024-05-23 10:46:50 +1000

    OpenGL: Minimise what is imported by windows.h

--- src/gui/opengl/gl_core_3_3.cpp
+++ src/gui/opengl/gl_core_3_3.cpp
@@ -15,6 +15,13 @@
  */
 
 #if defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#define NOSERVICE
+#define NOMCX
+#define NOIME
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 #include <windows.h>
 #endif
 
