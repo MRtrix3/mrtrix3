e73cf778430ae3999ad9fe6a4c395ca5468b3ced
Daljit Singh <daljit7991@gmail.com>
2024-06-04 17:35:07 +0100

    Create wrapper scripts for mrview and shview

--- cmake/MacOSBundle.cmake
+++ cmake/MacOSBundle.cmake
@@ -13,3 +13,17 @@ function(set_bundle_properties executable_name)
         INSTALL_RPATH "@executable_path/../../../../lib"
     )
 endfunction()
+
+function(install_bundle_wrapper_scripts executable_name)
+    set(wrapper_script ${CMAKE_CURRENT_SOURCE_DIR}/../packaging/macos/bundle/wrapper_launcher.sh.in)
+    configure_file(${wrapper_script} ${PROJECT_BINARY_DIR}/bin/${executable_name}
+        FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
+        @ONLY
+    )
+
+    install(FILES ${PROJECT_BINARY_DIR}/bin/${executable_name}
+            DESTINATION ${CMAKE_INSTALL_BINDIR}
+            RENAME "${executable_name}"
+            PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+
+endfunction()
