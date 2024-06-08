e411e329d20ab34dc6c0dc3ad5d4bb92dc7242e7
Robert Smith <robert.smith@florey.edu.au>
2024-06-08 12:12:36 +1000

    transformconvert flirt_import: Use on-disk transform

--- cmd/transformconvert.cpp
+++ cmd/transformconvert.cpp
@@ -65,15 +65,14 @@ void usage() {
 // clang-format on
 
 transform_type get_flirt_transform(const Header &header) {
-  Axes::permutations_type axes;
-  transform_type nifti_transform = File::NIfTI::adjust_transform(header, axes);
-  if (nifti_transform.matrix().topLeftCorner<3, 3>().determinant() < 0.0)
-    return nifti_transform;
+  const transform_type ondisk_transform = header.realignment().orig_transform();
+  if (ondisk_transform.matrix().topLeftCorner<3, 3>().determinant() < 0.0)
+    return ondisk_transform;
   transform_type coord_switch;
   coord_switch.setIdentity();
   coord_switch(0, 0) = -1.0f;
-  coord_switch(0, 3) = (header.size(axes[0]) - 1) * header.spacing(axes[0]);
-  return nifti_transform * coord_switch;
+  coord_switch(0, 3) = (header.size(header.realignment().permutation(0)) - 1) * header.spacing(header.realignment().permutation(0));
+  return ondisk_transform * coord_switch;
 }
 
 // transform_type parse_surfer_transform (const Header& header) {
