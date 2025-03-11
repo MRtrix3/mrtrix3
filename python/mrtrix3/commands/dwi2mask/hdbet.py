cb604695ed214d9267524efe6a174bc8fcdb3f27
Robert Smith <robert.smith@florey.edu.au>
2025-03-11 12:26:45 +1100

    dwi2mask hdbet: Fix exception handling

--- python/mrtrix3/commands/dwi2mask/hdbet.py
+++ python/mrtrix3/commands/dwi2mask/hdbet.py
@@ -56,8 +56,9 @@ def execute(): #pylint: disable=unused-variable
     try:
       run.command('hd-bet -i bzero.nii')
       return OUTPUT_IMAGE_PATH
-    except run.MRtrixCmdError as e_gpu: #pylint: disable=unused-variable
+    except run.MRtrixCmdError as exc: #pylint: disable=unused-variable
       app.warn('HD-BET failed when running on GPU; attempting on CPU')
+      e_gpu = exc
   try:
     run.command('hd-bet -i bzero.nii -device cpu -mode fast -tta 0')
     return OUTPUT_IMAGE_PATH
