c374e293b7512635793bea5e6d07181bf4a8cac4
Thomas G. Close <tom.g.close@gmail.com>
2025-08-22 09:44:54 +1000

    debugged pydra code generation

--- python/mrtrix3/app.py
+++ python/mrtrix3/app.py
@@ -156,6 +156,9 @@ def _execute(usage_function, execute_function): #pylint: disable=unused-variable
   elif sys.argv[-1] == '__print_usage_rst__':
     CMDLINE.print_usage_rst()
     sys.exit(0)
+  elif sys.argv[-1] == '__print_usage_pydra__':
+    CMDLINE.print_usage_pydra()
+    sys.exit(0)
 
   # Do the main command-line input parsing
   ARGS = CMDLINE.parse_args()
