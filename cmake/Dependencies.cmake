69855b0d94864631f2d66cff13ac8b4eeb5ca45c
Daljit Singh <daljit7991@gmail.com>
2025-11-26 12:44:01 +0000

    Add Slang + Dawn dependencies

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -102,3 +102,101 @@ if(MRTRIX_BUILD_TESTS)
         FetchContent_MakeAvailable(googletest)
     endif()
 endif()
+
+
+# Dawn
+
+find_package(Dawn QUIET)
+
+if(NOT Dawn_FOUND)
+    message(STATUS "Dawn not found, downloading prebuilt binaries...")
+    include(FetchContent)
+    set(FETCHCONTENT_QUIET OFF)
+
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        set(DAWN_PLATFORM "linux")
+    elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+        set(DAWN_PLATFORM "macos")
+    elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+        set(DAWN_PLATFORM "windows-msys2")
+    else()
+        message(FATAL_ERROR "Unsupported platform: ${CMAKE_SYSTEM_NAME}")
+    endif()
+
+    set(DAWN_VERSION 7495)
+
+
+    set(DAWN_BINARIES_URL_PREFIX
+        "https://github.com/mrtrix3/webgpu-dawn-binaries/releases/download/chromium")
+    set(DAWN_BINARIES_URL
+        ${DAWN_BINARIES_URL_PREFIX}-${DAWN_VERSION}/webgpu-dawn-chromium-${DAWN_VERSION}-${DAWN_PLATFORM}.zip
+    )
+
+    FetchContent_Declare(
+        dawn
+        URL ${DAWN_BINARIES_URL}
+        DOWNLOAD_EXTRACT_TIMESTAMP   1
+        DOWNLOAD_NO_PROGRESS         1
+    )
+    FetchContent_MakeAvailable(dawn)
+
+    if(LINUX)
+        set(DAWN_LIB_DIR_NAME "lib64")
+    else()
+        set(DAWN_LIB_DIR_NAME "lib")
+    endif()
+    set(
+      Dawn_DIR
+      "${dawn_SOURCE_DIR}/${DAWN_LIB_DIR_NAME}/cmake/Dawn"
+      CACHE PATH "Folder containing DawnConfig.cmake"
+      FORCE
+    )
+    set(FETCHCONTENT_QUIET ON)
+endif()
+
+
+# Slang
+
+find_package(slang QUIET)
+
+if(NOT Slang_FOUND)
+    message(STATUS "Slang not found, downloading prebuilt binaries...")
+    set(SLANG_VERSION "2025.22.1" CACHE STRING "Slang version to download from GitHub releases")
+
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        set(SLANG_OS "linux")
+    elseif(APPLE)
+        set(SLANG_OS "macos")
+    else()
+        set(SLANG_OS "windows")
+    endif()
+
+    if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64" OR CMAKE_SYSTEM_PROCESSOR MATCHES "arm64")
+        set(SLANG_ARCH "aarch64")
+    else()
+        set(SLANG_ARCH "x86_64")
+    endif()
+
+    set(SLANG_SUBSTRING "-${SLANG_OS}-${SLANG_ARCH}")
+
+    set(SLANG_DOWNLOAD_LINK
+      "https://github.com/shader-slang/slang/releases/download/v${SLANG_VERSION}/slang-${SLANG_VERSION}${SLANG_SUBSTRING}.zip"
+    )
+
+    message(STATUS "Downloading Slang ${SLANG_VERSION} (${SLANG_OS}/${SLANG_ARCH})...")
+
+    FetchContent_Declare(
+      slang
+        URL                          ${SLANG_DOWNLOAD_LINK}
+        DOWNLOAD_EXTRACT_TIMESTAMP   1
+        DOWNLOAD_NO_PROGRESS         1
+    )
+    FetchContent_MakeAvailable(slang)
+
+    set(
+      slang_DIR
+      "${slang_SOURCE_DIR}/lib/cmake/slang"
+      CACHE PATH "Folder containing SlangConfig.cmake"
+      FORCE
+    )
+endif()
