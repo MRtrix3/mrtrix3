530ca0288bf2de752b6731eecb06fc376cb18df7
Robert Smith <robert.smith@florey.edu.au>
2026-04-28 13:50:52 +1000

    Updates for 5ttgen freesurfer
    - Finish implementation of ScLimbic support.
    - Modify code syntax to conform to latest dev (upcoming 3.1.0).
    - Fix splicing of aseg and FSL FIRST segmentations.
    - Move cerebellar cortex from cortical GM to subcortical GM.
    - Move brain stem and ventral diencephalon to 5th volume, and add missing substancia nigra.
    - Add test data and tests for new 5ttgen freesurfer features.

--- python/mrtrix3/commands/5ttgen/freesurfer.py
+++ python/mrtrix3/commands/5ttgen/freesurfer.py
@@ -13,7 +13,7 @@
 #
 # For more details, see http://www.mrtrix.org/.
 
-import glob, os.path, shutil
+import os, pathlib, shutil
 from mrtrix3 import MRtrixError
 from mrtrix3 import app, image, path, run, utils
 
@@ -41,107 +41,121 @@ def usage(base_parser, subparsers): #pylint: disable=unused-variable
                          ' If image "*_all_*_firstseg.nii[.gz]" is provided,'
                          ' then the hard segmentations from that image will be imported directly.'
                          ' If instead the FIRST output directory is provided,'
-                         ' then the surfaces will be used to produce partial volume estimates.')
+                         ' then the surfaces will be used to produce partial volume estimates'
+                         ' (without explicit smoothing for computational efficiency).'
+                         ' The former can slightly over-estimate the sizes of these structures,'
+                         ' and produces a binary tissue segmentation;'
+                         ' the latter is more precise and provides partial volume fractions'
+                         ' but is more computationall expensive.')
   options = parser.add_argument_group('Options specific to the "freesurfer" algorithm')
   options.add_argument('-lut',
                        type=app.Parser.FileIn(),
                        help='Manually provide path to the lookup table on which the input parcellation image is based '
                             '(e.g. FreeSurferColorLUT.txt)')
   options.add_argument('-sclimbic',
-                       metavar=('image'),
-                       help='Incorporate results of subcortical limbic segmentation')
+                       type=app.Parser.ImageIn(),
+                       help='Incorporate results of subcortical limbic segmentation;'
+                            ' path should be default label image output from FreeSurfer mri_sclimbic_seg')
   options.add_argument('-first',
+                       # TODO Make a custom type that can be either a directory or an image
                        metavar=('path'),
                        help='Use sub-cortical segmentations from FSL FIRST (see Description)')
 
 def execute(): #pylint: disable=unused-variable
 
   freesurfer_home = os.environ.get('FREESURFER_HOME', '')
-  if not freesurfer_home:
+  lut_input_path = app.ARGS.lut # May be None
+  if not freesurfer_home and (lut_input_path is None or app.ARGS.sclimbic is not None):
     raise MRtrixError('Environment variable FREESURFER_HOME is not set;'
-                      ' please run appropriate FreeSurfer configuration script,'
-                      ' set this variable manually,'
-                      ' or provide script with path to file FreeSurferColorLUT.txt using -lut option')
-  lut_input_path = os.path.join(freesurfer_home, 'FreeSurferColorLUT.txt')
-  if not os.path.isfile(lut_input_path):
-    raise MRtrixError('Could not find FreeSurfer lookup table file'
-                      ' (expected location: ' + lut_input_path + ')')
+                      ' please run appropriate FreeSurfer configuration script'
+                      + (', set this variable manually,'
+                         ' or provide script with path to file FreeSurferColorLUT.txt using -lut option'
+                         if app.ARGS.sclimbic is None
+                         else ' or set this variable manually'))
+  if lut_input_path is None:
+    lut_input_path = pathlib.Path(freesurfer_home, 'FreeSurferColorLUT.txt')
+    if not lut_input_path.is_file():
+      raise MRtrixError('Could not find FreeSurfer lookup table file'
+                        f' (expected location: {lut_input_path})')
   if app.ARGS.sclimbic:
-    sclimbic_lut_input_path = os.path.join(freesurfer_home, 'models', 'sclimbic.ctab')
-    if not os.path.isfile(sclimbic_lut_input_path):
+    sclimbic_lut_input_path = pathlib.Path(freesurfer_home, 'models', 'sclimbic.ctab')
+    if not sclimbic_lut_input_path.is_file():
       raise MRtrixError('Could not find FreeSurfer ScLimbic module lookup table file'
-                        ' (expected location: ' + sclimbic_lut_input_path + ')')
+                        f' (expected location: {sclimbic_lut_input_path})')
 
   run.command(['mrconvert', app.ARGS.input, 'input.mif'],
               preserve_pipes=True)
   if app.VERBOSITY >= 3:
     run.command('labelvalidate input.mif')
 
-  if app.ARGS.first:
-    first_path = path.from_user(app.ARGS.first, False)
-    if os.path.isfile(first_path):
-      if not image.match(path.from_user(app.ARGS.input, False), first_path):
+  if app.ARGS.first is not None:
+    # TODO Conversion should not be required if option type is changed
+    first_path = pathlib.Path(app.WORKING_DIR, app.ARGS.first)
+    if first_path.is_file():
+      if not image.match('input.mif', first_path):
         raise MRtrixError('Voxel grids of input index image and FIRST segmentation image do not match')
-      run.command(['mrconvert', first_path, path.to_scratch('first_seg.mif', False)])
-    elif os.path.isdir(first_path):
+      run.command(['mrconvert', first_path, 'first_seg.mif'],
+                  preserve_pipes=True)
+    elif first_path.is_dir():
+
       # Verify whether all requisite VTK files can be found, and have the same prefix
       sgm_list = list(FIRST_SGM_LIST.keys()) + (list(FIRST_AMYG_HIPP.keys()) if app.ARGS.sgm_amyg_hipp else [])
       vtk_filelist = []
       for first_sgm in sgm_list:
-        candidate_files = glob.glob(os.path.join(first_path, '*-' + first_sgm + '_first.vtk'))
+        candidate_files = list(first_path.glob(f'*-{first_sgm}_first.vtk'))
         if not candidate_files:
-          raise MRtrixError('Unable to find VTK file for structure "' + first_sgm + '" in directory "' + first_path + '"')
+          raise MRtrixError(f'Unable to find VTK file for structure "{first_sgm}" in directory "{first_path}"')
         if len(candidate_files) > 1:
-          raise MRtrixError('Multiple candidate VTK files for structure "' + first_sgm + '" in directory "' + first_path + '"')
+          raise MRtrixError(f'Multiple candidate VTK files for structure "{first_sgm}" in directory "{first_path}"')
         vtk_filelist.append(candidate_files[0])
-      if not all(os.path.basename(vtk_file).split('-')[0] == os.path.basename(vtk_filelist[0]).split('-')[0] for vtk_file in vtk_filelist[1:]):
-        raise MRtrixError('VTK files in FIRST directory "' + first_path + '" do not all possess same prefix')
+      if not all(vtk_file.stem.split('-')[:-1] == vtk_filelist[0].stem.split('-')[:-1] for vtk_file in vtk_filelist[1:]):
+        raise MRtrixError(f'VTK files in FIRST directory "{first_path}" do not all possess same prefix')
       # Also need a template image in order to convert the VTKs
-      firstseg_image = glob.glob(os.path.join(first_path, '*_all_*_firstseg.nii*'))
+      firstseg_image = list(first_path.glob('*_all_*_firstseg.nii*'))
       if not firstseg_image:
         raise MRtrixError('Unable to find FIRST "firstseg" image (required as template for VTK conversion)')
       if len(firstseg_image) > 1:
         raise MRtrixError('Multiple candidate FIRST "firstseg" images (required as template for VTK conversion)')
       first_copy = utils.RunList('Copying FIRST .vtk files to scratch directory', len(sgm_list))
       for name, filepath in zip(sgm_list, vtk_filelist):
-        first_copy.function(shutil.copyfile, filepath, path.to_scratch('first-' + name + '.vtk', False))
-      run.command(['mrconvert', firstseg_image[0], path.to_scratch('first_template.mif', False)])
+        first_copy.function(shutil.copyfile, filepath, f'first-{name}.vtk')
+      run.command(['mrconvert', firstseg_image[0], 'first_template.mif'])
 
     else:
-      raise MRtrixError('Unable to interpret input to -first option "' + app.ARGS.first + '" as filesystem path')
+      raise MRtrixError(f'Unable to interpret input to -first option "{app.ARGS.first}" as filesystem path')
 
   if app.ARGS.sgm_amyg_hipp:
     lut_output_file_name = 'FreeSurfer2ACT_sgm_amyg_hipp.txt'
   else:
     lut_output_file_name = 'FreeSurfer2ACT.txt'
-  lut_output_path = os.path.join(path.shared_data_path(), '5ttgen', lut_output_file_name)
-  if not os.path.isfile(lut_output_path):
-    raise MRtrixError(f'Could not find lookup table file for converting FreeSurfer parcellation output to tissues '
+  lut_output_path = pathlib.Path(path.shared_data_path(), '5ttgen', lut_output_file_name)
+  if not lut_output_path.is_file():
+    raise MRtrixError('Could not find lookup table file for converting FreeSurfer parcellation output to tissues '
                       f'(expected location: {lut_output_path})')
   if app.ARGS.sclimbic:
-    sclimbic_lut_output_path = os.path.join(path.shared_data_path(), path.script_subdir_name(), 'ScLimbic2ACT.txt')
-    if not os.path.isfile(sclimbic_lut_output_path):
+    sclimbic_lut_output_path = pathlib.Path(path.shared_data_path(), '5ttgen', 'ScLimbic2ACT.txt')
+    if not sclimbic_lut_output_path.is_file():
       raise MRtrixError('Could not find lookup table file for converting ScLimbic module output to tissues'
-                        ' (expected location: ' + sclimbic_lut_output_path + ')')
+                        f' (expected location: {sclimbic_lut_output_path})')
 
   # Initial conversion from FreeSurfer parcellation to five principal tissue types
   index_image = 'indices.mif'
   run.command(f'labelconvert input.mif {lut_input_path} {lut_output_path} {index_image}')
   if app.ARGS.sclimbic:
     sclimbic_image = 'sclimbic.mif'
-    run.command(['labelconvert', path.from_user(app.ARGS.sclimbic, False), sclimbic_lut_input_path, sclimbic_lut_output_path, sclimbic_image])
+    run.command(['labelconvert', app.ARGS.sclimbic, sclimbic_lut_input_path, sclimbic_lut_output_path, sclimbic_image])
 
   # Generate separate images per tissue
-  tissue_images = ['tissue' + str(index) + '.mif' for index in range(1, 6)]
+  tissue_images = [f'tissue{index}.mif' for index in range(1, 6)]
 
   split_vols = utils.RunList('Splitting tissue index image into per-tissue images', 5)
   for index, filename in zip(range(1, 6), tissue_images):
     if app.ARGS.sclimbic:
-      split_vols.command('mrcalc ' + sclimbic_image + ' 0 ' + index_image + ' -if ' + str(index) + ' -eq ' + sclimbic_image + ' ' + str(index) + ' -eq -add ' + filename)
+      split_vols.command(f'mrcalc {sclimbic_image} 0 {index_image} -if {index} -eq {sclimbic_image} {index} -eq -add {filename}')
     else:
-      split_vols.command('mrcalc ' + index_image + ' ' + str(index) + ' -eq ' + filename)
+      split_vols.command(f'mrcalc {index_image} {index} -eq {filename}')
 
-  # TODO Integrating data from FIRST requires that the tissues have been separated,
+  # Integrating data from FIRST requires that the tissues have been separated,
   #   since it's possible to have soft segmentations that necessitate partial volume fractions
 
   # Integrate data from FIRST
@@ -153,23 +167,13 @@ def execute(): #pylint: disable=unused-variable
     # Pointless; this code now appears after conversion to 1 image per tissue
     #run.command('mrcalc ' + index_image + ' 1 -eq 2 ' + index_image + ' -if indices_nosgm.mif')
     new_sgm_image = 'sgm.mif'
-    new_tissue_images = ['tissue' + str(index) + '_first.mif' for index in range(1, 6)]
-    if os.path.isfile('first_seg.mif'):
+    new_tissue_images = [f'tissue{index}_first.mif' for index in range(1, 6)]
+    if pathlib.Path('first_seg.mif').is_file():
       # Hard segmentation
-      run.command('mrcalc '
-                  + ' '.join('first_seg.mif ' + str(index) + ' -eq' for index in sgm_list.values())
-                  + ' '
-                  + ' '.join(['-add'] * (len(sgm_list)-1))
-                  + ' 1.0 -min '
-                  + new_sgm_image)
-      # TODO Update WM and SGM tissue images
-      # If SGM:
-      #   Set to SGM; erase from all others
-      # Else if old SGM (and not new SGM):
-      #   Remove from SGM, set to WM
-      # Else:
-      #   Use old tissue
-      # TODO Redo logic on a per-tissue basis
+      mrcalc_extractions = ' '.join(f'first_seg.mif {index} -eq' for index in sgm_list.values())
+      mrcalc_additions = ' '.join(['-add'] * (len(sgm_list)-1))
+      run.command(f'mrcalc {mrcalc_extractions} {mrcalc_additions} 1.0 -min {new_sgm_image}')
+      # Update WM and SGM tissue images
       # For SGM:
       #   Simply replace with new image
       # For WM:
@@ -182,29 +186,34 @@ def execute(): #pylint: disable=unused-variable
         if index == 2:
           first_replace.function(shutil.copyfile, new_sgm_image, output_image)
         else:
-          first_replace.command('mrcalc ' + input_image + ' ' + (tissue_images[1] + ' -add ' if index == 3 else '') + new_sgm_image + ' -sub 0 -max ' + output_image)
+          insert_old_sgm = f'{tissue_images[1]} -add ' if index == 3 else ''
+          first_replace.command(f'mrcalc {input_image} {insert_old_sgm} {new_sgm_image} -sub 0 -max {output_image}')
     else:
       # Soft segmentation
       first_pve = utils.RunList('Mapping FIRST segmentations to partial volumes', 2 * len(sgm_list))
-      for struct in sgm_list.keys():
-        filename = 'first-' + struct + '.vtk'
-        intername = 'real-' + struct + '.vtk'
+      for struct in sgm_list:
+        filename = f'first-{struct}.vtk'
+        intername = f'real-{struct}.vtk'
         # TODO Modify first2real to read from new header transform realignment parameters and modify transform accordingly
-        first_pve.command(['meshconvert', filename, intername, '-transform', 'first2real', 'first_template.mif', '-config', 'RealignTransform', 'false'])
-        first_pve.command(['mesh2voxel', intername, index_image, 'first-' + struct + '.mif'])
-      run.command(['mrmath', ['first-' + struct + '.mif' for struct in sgm_list.keys()], 'sum', '-', '|',
-                   'mrcalc', '-', '1.0', '-min', index_image, '0', '-gt', '-mult', new_sgm_image])
+        first_pve.command(['meshconvert', filename, intername,
+                           '-transform', 'first2real', 'first_template.mif',
+                           '-config', 'RealignTransform', 'false'])
+        first_pve.command(['mesh2voxel', intername, index_image, f'first-{struct}.mif'])
+      sgm_image_list = [f'first-{struct}.mif' for struct in sgm_list]
+      run.command(['mrmath', sgm_image_list, 'sum', '-', '|',
+                   'mrcalc', '-', '1.0', '-min', new_sgm_image])
       # Insert into index image
-      # TODO More complex than the hard segmentation approach:
-      # Rather than a direct if replacement, need to retain some fraction of the existing tissue segmentation
-      # TODO Fix: Erroneous voxels at SGM edges
       run.command('mrcalc 1 sgm.mif -sub fs_multiplier.mif')
       first_modulate = utils.RunList('Incorporating FIRST segmentations into tissue images', 5)
       for index, input_image, output_image in zip(range(1, 6), tissue_images, new_tissue_images):
         if index == 2:
-          first_modulate.function(shutil.copy, new_sgm_image, output_image)
+          # If tissue density in index image is zero,
+          #   but SGM volume fraction is non-zero,
+          #   need to boost the SGM volume fraction to 1.0
+          first_modulate.command(f'mrcalc {index_image} 0 -eq {new_sgm_image} 0 -gt -mult 1 {new_sgm_image} -if {output_image}')
         else:
-          first_modulate.command('mrcalc ' + input_image + ' ' + (tissue_images[1] + ' -add ' if index == 3 else '') + 'fs_multiplier.mif -mult ' + output_image)
+          insert_old_sgm = f'{tissue_images[1]} -add ' if index == 3 else ''
+          first_modulate.command(f'mrcalc {input_image} {insert_old_sgm} fs_multiplier.mif -mult {output_image}')
     tissue_images = new_tissue_images
 
   final_command = ['mrcat', tissue_images, '-', '-axis', '3', '|']
@@ -213,6 +222,9 @@ def execute(): #pylint: disable=unused-variable
   final_command.extend(['mrconvert', '-', 'result.mif', '-datatype', 'float32'])
   run.command(final_command)
 
-  run.command('mrconvert result.mif ' + path.from_user(app.ARGS.output),
-              mrconvert_keyval=path.from_user(app.ARGS.input, False),
-              force=app.FORCE_OVERWRITE)
+  run.command(f'mrconvert result.mif {app.ARGS.output}',
+              mrconvert_keyval=app.ARGS.input,
+              force=app.FORCE_OVERWRITE,
+              preserve_pipes=True)
+
+  return 'result.mif'
