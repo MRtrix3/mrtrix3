MRtrix3/mrtrix3

commit 7b7b59231befe641a0f38fb1f831b66bb6942582

More rectifications for clang-tidy enforcement #3349

cpp/core/file/ofstream.cpp
@@ -25,7 +25,7 @@
 namespace MR::File {
 
 void OFStream::open(const std::filesystem::path &path, const std::ios_base::openmode mode) {
-  if (!(mode & std::ios_base::app) && !(mode & std::ios_base::ate) && !(mode & std::ios_base::in)) {
+  if ((mode & std::ios_base::app == 0) && (mode & std::ios_base::ate == 0) && (mode & std::ios_base::in == 0)) {
     if (!File::is_tempfile(path)) {
       if (std::filesystem::exists(path)) {
         App::check_overwrite(path);

cpp/core/signal_handler.cpp
@@ -80,7 +80,7 @@ void handler(int i) noexcept {
     char str[256]; // check_syntax off
     str[255] = '\0';
     snprintf(str, 255, "\n%s: [SYSTEM FATAL CODE: %s (%d)] %s\n", App::NAME.c_str(), sig, i, msg);
-    const auto _ = write(STDERR_FILENO, str, strnlen(str, 256));
+    (void)write(STDERR_FILENO, str, strnlen(str, 256));
     std::_Exit(i);
   }
 }

cpp/CMakeLists.txt
@@ -30,6 +30,9 @@ if(MINGW AND CMAKE_BUILD_TYPE MATCHES "Debug")
     target_compile_options(mrtrix-common INTERFACE -Wa,-mbig-obj)
 endif()
 
+# Add "#pragma once" to precompiled headers to stop clang-tidy confusion
+set(CMAKE_PCH_PROLOGUE "#pragma once")
+
 add_subdirectory(cmd)
 add_subdirectory(core)
 

cpp/cmd/dwidenoise.cpp
@@ -160,7 +160,7 @@ public:
                    bool exp1)
       : extent{{extent[0] / 2, extent[1] / 2, extent[2] / 2}},
         m(ndwi),
-        n(extent[0] * extent[1] * extent[2]),
+        n(static_cast<ssize_t>(extent[0]) * static_cast<ssize_t>(extent[1]) * static_cast<ssize_t>(extent[2])),
         r(std::min(m, n)),
         q(std::max(m, n)),
         exp1(exp1),
@@ -335,12 +335,12 @@ void run() {
         throw Exception("-extent must not exceed the image dimensions");
     }
   } else {
-    uint32_t e = 1;
+    ssize_t e = 1;
     while (e * e * e < dwi.size(3))
       e += 2;
-    extent = {std::min(e, static_cast<uint32_t>(dwi.size(0))),
-              std::min(e, static_cast<uint32_t>(dwi.size(1))),
-              std::min(e, static_cast<uint32_t>(dwi.size(2)))};
+    extent = {std::min(static_cast<uint32_t>(e), static_cast<uint32_t>(dwi.size(0))),
+              std::min(static_cast<uint32_t>(e), static_cast<uint32_t>(dwi.size(1))),
+              std::min(static_cast<uint32_t>(e), static_cast<uint32_t>(dwi.size(2)))};
   }
   INFO("selected patch size: " + str(extent[0]) + " x " + str(extent[1]) + " x " + str(extent[2]) + ".");
 

cpp/core/dwi/directions/set.cpp
@@ -270,6 +270,9 @@ void Set::initialise_adjacency() {
           case 2:
             edge = std::make_pair(p->indices[2], p->indices[0]);
             break;
+          default:
+            assert(false);
+            break;
           }
           bool found = false;
           for (auto h = horizon.begin(); h != horizon.end(); ++h) {
@@ -331,6 +334,9 @@ void Set::initialise_adjacency() {
       case 5:
         from = vertices[current.indices[2]].index;
         to = vertices[current.indices[0]].index;
+        break;
+      default:
+        assert(false);
         break;
       }
       bool found = false;

cpp/core/dwi/tractography/editing/worker.cpp
@@ -24,47 +24,47 @@ bool Worker::operator()(Streamline<> &in, Streamline<> &out) const {
   out.set_index(in.get_index());
   out.weight = in.weight;
 
-  // Need to track exclusion separately, since we may still need to apply
-  //   mask (or, more accurately, their inverse) afterwards if -inverse is specified
-  bool exclude = false;
+  // Need to track exclusion separately, since we may still need to apply mask
+  //   (or, more accurately, their inverse) afterwards if -inverse is specified
+  bool exclude = !thresholds(in);
 
-  if (!thresholds(in)) {
-    exclude = true;
-  } else if (include_visitation.size() || properties.exclude.size()) {
-
-    // Assign to ROIs
-    include_visitation.reset();
-
-    if (ends_only) {
-      for (size_t i = 0; i != 2; ++i) {
-        const Eigen::Vector3f &p(i ? in.back() : in.front());
-        include_visitation(p);
-        if (properties.exclude.contains(p)) {
-          exclude = true;
-          break;
-        }
-      }
-    } else {
-      for (const auto &p : in) {
-        include_visitation(p);
-        if (properties.exclude.contains(p)) {
-          exclude = true;
-          break;
-        }
-      }
-    }
-
-    // Make sure all of the include regions were visited
-    if (!include_visitation)
-      exclude = true;
-
-  } else if (inverse) {
-
+  if (!exclude) {
     // If no thresholds are specified, and no include / exclude ROIs are defined, then
     //   it's still possible that one or more masks have been provided;
     //   if this is the case, then we want to continue processing this streamline,
     //   regardless of whether or not -inverse has been specified
-    exclude = true;
+    if (include_visitation.empty() && properties.exclude.empty() && inverse) {
+
+      exclude = true;
+
+    } else if (include_visitation.size() || properties.exclude.size()) {
+
+      // Assign to ROIs
+      include_visitation.reset();
+
+      if (ends_only) {
+        for (size_t i = 0; i != 2; ++i) {
+          const Eigen::Vector3f &p(i ? in.back() : in.front());
+          include_visitation(p);
+          if (properties.exclude.contains(p)) {
+            exclude = true;
+            break;
+          }
+        }
+      } else {
+        for (const auto &p : in) {
+          include_visitation(p);
+          if (properties.exclude.contains(p)) {
+            exclude = true;
+            break;
+          }
+        }
+      }
+
+      // Make sure all of the include regions were visited
+      if (!include_visitation)
+        exclude = true;
+    }
   }
 
   // In default usage, pass the empty track down the queue if track is excluded

cpp/core/dwi/tractography/roi.h
@@ -205,6 +205,7 @@ public:
   IncludeROIVisitation(const IncludeROIVisitation &) = default;
   IncludeROIVisitation &operator=(const IncludeROIVisitation &) = delete;
 
+  bool empty() const { return unordered.empty() && ordered.empty(); }
   void reset() {
     visited.setZero();
     state.reset();

cpp/core/file/dicom/image.cpp
@@ -251,6 +251,7 @@ void Image::parse_item(Element &item) {
       bvalue = item.get_float(0, bvalue);
       return;
     case 0x1004:
+      // NOLINTNEXTLINE(bugprone-string-literal-with-embedded-nul)
       philips_orientation = item.get_string(0, "\0")[0];
       return;
     default:

commit 4fc5e16f2fd576386ba0cbece8a1b9353431ee83

More fixes trying to get clang-tidy enforcement working
File::OFStream: Fix operator precedence (issue introduced in 7b7b59231befe641a0f38fb1f831b66bb6942582).
- Signal handler: prevent GCC error for ignoring result of write() despite prepending with (void).

cpp/core/file/ofstream.cpp
@@ -25,7 +25,7 @@
 namespace MR::File {
 
 void OFStream::open(const std::filesystem::path &path, const std::ios_base::openmode mode) {
-  if ((mode & std::ios_base::app == 0) && (mode & std::ios_base::ate == 0) && (mode & std::ios_base::in == 0)) {
+  if ((mode & std::ios_base::app) == 0 && (mode & std::ios_base::ate) == 0 && (mode & std::ios_base::in) == 0) {
     if (!File::is_tempfile(path)) {
       if (std::filesystem::exists(path)) {
         App::check_overwrite(path);

cpp/core/signal_handler.cpp
@@ -21,6 +21,7 @@
 #include <filesystem>
 #include <iostream>
 #include <signal.h>
+#include <tuple>
 #include <unistd.h>
 #include <vector>
 
@@ -80,7 +81,7 @@ void handler(int i) noexcept {
     char str[256]; // check_syntax off
     str[255] = '\0';
     snprintf(str, 255, "\n%s: [SYSTEM FATAL CODE: %s (%d)] %s\n", App::NAME.c_str(), sig, i, msg);
-    (void)write(STDERR_FILENO, str, strnlen(str, 256));
+    std::ignore = write(STDERR_FILENO, str, strnlen(str, 256));
     std::_Exit(i);
   }
 }